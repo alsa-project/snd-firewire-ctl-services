2d5f36559e9993f685b0042d56279d37035f9eb8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-07 18:06:08 +0900

    tascam: isoc_console_unit/isoc_rack_unit: add common control implementation
    This commit adds common control implementation for clock configuration and
    digital I/O routing. Some control element sets are added and visible for
    ALSA control applications. ALSA control events from the application are
    dispatched and units are controlled by vendor-specific protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/tascam/fw1082_model.rs
+++ src/tascam/fw1082_model.rs
@@ -4,39 +4,68 @@ use glib::Error;
 
 use crate::card_cntr;
 
-pub struct Fw1082Model {}
+use super::protocol::ClkSrc;
+use super::common_ctl::CommonCtl;
 
-impl Fw1082Model {
+pub struct Fw1082Model<'a> {
+    req: hinawa::FwReq,
+    common: CommonCtl<'a>,
+}
+
+impl<'a> Fw1082Model<'a> {
+    const CLK_SRCS: &'a [ClkSrc] = &[
+        ClkSrc::Internal,
+        ClkSrc::Spdif,
+    ];
+
+    const CLK_SRC_LABELS: &'a [&'a str] = &[
+        "Internal",
+        "S/PDIF",
+    ];
+
     pub fn new() -> Self {
-        Fw1082Model{}
+        Fw1082Model{
+            req: hinawa::FwReq::new(),
+            common: CommonCtl::new(Self::CLK_SRCS,
+                                   Self::CLK_SRC_LABELS),
+        }
     }
 }
 
-impl card_cntr::CtlModel<hinawa::SndTscm> for Fw1082Model {
+impl<'a> card_cntr::CtlModel<hinawa::SndTscm> for Fw1082Model<'a> {
     fn load(
         &mut self,
-        _: &hinawa::SndTscm,
-        _: &mut card_cntr::CardCntr,
+        unit: &hinawa::SndTscm,
+        card_cntr: &mut card_cntr::CardCntr,
     ) -> Result<(), Error> {
+        self.common.load(unit, &self.req, card_cntr)?;
         Ok(())
     }
 
     fn read(
         &mut self,
-        _: &hinawa::SndTscm,
-        _: &alsactl::ElemId,
-        _: &mut alsactl::ElemValue,
+        unit: &hinawa::SndTscm,
+        elem_id: &alsactl::ElemId,
+        elem_value: &mut alsactl::ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.common.read(unit, &self.req, elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(
         &mut self,
-        _: &hinawa::SndTscm,
-        _: &alsactl::ElemId,
-        _: &alsactl::ElemValue,
-        _: &alsactl::ElemValue,
+        unit: &hinawa::SndTscm,
+        elem_id: &alsactl::ElemId,
+        old: &alsactl::ElemValue,
+        new: &alsactl::ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.common.write(unit, &self.req, elem_id, old, new)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
