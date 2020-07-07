419db511287e49478b2e71693282a89ef64c34fe
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-07 18:06:08 +0900

    tascam: isoc_console_unit/isoc_rack_unit: add console control implementation
    FW-1884 and FW-1082 are models of console type and supports some
    features apart from rack model.
    
    This commit adds control implementation for console specific features.
    When enabled, master fader is available to adjust playback volume from
    streaming. The mode of monitoring is visible by notification of
    control event.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/tascam/fw1082_model.rs
+++ src/tascam/fw1082_model.rs
@@ -9,11 +9,13 @@ use crate::card_cntr;
 use super::protocol::ClkSrc;
 use super::common_ctl::CommonCtl;
 use super::meter_ctl::MeterCtl;
+use super::console_ctl::ConsoleCtl;
 
 pub struct Fw1082Model<'a> {
     req: hinawa::FwReq,
     common: CommonCtl<'a>,
     meter: MeterCtl<'a>,
+    console: ConsoleCtl,
 }
 
 impl<'a> Fw1082Model<'a> {
@@ -33,6 +35,7 @@ impl<'a> Fw1082Model<'a> {
             common: CommonCtl::new(Self::CLK_SRCS,
                                    Self::CLK_SRC_LABELS),
             meter: MeterCtl::new(Self::CLK_SRC_LABELS, 2, false, false),
+            console: ConsoleCtl::new(),
         }
     }
 }
@@ -40,22 +43,26 @@ impl<'a> Fw1082Model<'a> {
 impl<'a> card_cntr::MonitorModel<hinawa::SndTscm> for Fw1082Model<'a> {
     fn get_monitored_elems(&mut self, elem_id_list: &mut Vec<alsactl::ElemId>) {
         elem_id_list.extend_from_slice(&self.meter.get_monitored_elems());
+        elem_id_list.extend_from_slice(&self.console.get_monitored_elems());
     }
 
     fn monitor_unit(&mut self, unit: &hinawa::SndTscm) -> Result<(), Error> {
         let states = unit.get_state()?;
         self.meter.parse_states(states);
+        self.console.parse_states(states);
         Ok(())
     }
 
     fn monitor_elems(
         &mut self,
-        _: &hinawa::SndTscm,
+        unit: &hinawa::SndTscm,
         elem_id: &alsactl::ElemId,
         _: &alsactl::ElemValue,
         new: &mut alsactl::ElemValue,
     ) -> Result<bool, Error> {
         if self.meter.read(elem_id, new)? {
+            Ok(true)
+        } else if self.console.read(unit, &self.req, elem_id, new)? {
             Ok(true)
         } else {
             Ok(false)
@@ -71,6 +78,8 @@ impl<'a> card_cntr::CtlModel<hinawa::SndTscm> for Fw1082Model<'a> {
     ) -> Result<(), Error> {
         self.common.load(unit, &self.req, card_cntr)?;
         self.meter.load(card_cntr)?;
+        self.console.load(unit, &self.req, card_cntr)?;
+
         Ok(())
     }
 
@@ -81,6 +90,8 @@ impl<'a> card_cntr::CtlModel<hinawa::SndTscm> for Fw1082Model<'a> {
         elem_value: &mut alsactl::ElemValue,
     ) -> Result<bool, Error> {
         if self.common.read(unit, &self.req, elem_id, elem_value)? {
+            Ok(true)
+        } else if self.console.read(unit, &self.req, elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -95,6 +106,8 @@ impl<'a> card_cntr::CtlModel<hinawa::SndTscm> for Fw1082Model<'a> {
         new: &alsactl::ElemValue,
     ) -> Result<bool, Error> {
         if self.common.write(unit, &self.req, elem_id, old, new)? {
+            Ok(true)
+        } else if self.console.write(unit, &self.req, elem_id, old, new)? {
             Ok(true)
         } else {
             Ok(false)
