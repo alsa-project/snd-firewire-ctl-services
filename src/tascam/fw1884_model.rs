48d736ce18fc0ca754e674cc1e98052f22f830ac
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-07 18:06:08 +0900

    tascam: isoc_console_unit: add model structures
    This commit adds two structures to represent two console models in TASCAM
    FireWire series. At present, they are just stubs and extended by future
    commits.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/tascam/fw1884_model.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+pub struct Fw1884Model {}
+
+impl Fw1884Model {
+    pub fn new() -> Self {
+        Fw1884Model{}
+    }
+}
