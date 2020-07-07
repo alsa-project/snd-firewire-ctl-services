1af68b19a2d5be270df5c3d4b20b99f40c34aea1
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-07 18:06:08 +0900

    tascam: isoc_rack_unit: add unit structure for rack model
    This commit adds a structure to represent unit for rack model in TASCAM
    FireWire series. Additional two threads run with main thread for node event
    and system event. The model is distinguished according to model name in
    Configuration ROM which has vendor-specific layout.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/tascam/isoc_rack_unit.rs
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+use glib::source;
+
+use nix::sys::signal;
+use std::sync::mpsc;
+
+use hinawa::{FwNodeExt, SndUnitExt};
+
+use crate::dispatcher;
+
+enum RackUnitEvent {
+    Shutdown,
+    Disconnected,
+    BusReset(u32),
+}
+
+pub struct IsocRackUnit {
+    unit: hinawa::SndTscm,
+    rx: mpsc::Receiver<RackUnitEvent>,
+    tx: mpsc::SyncSender<RackUnitEvent>,
+    dispatchers: Vec<dispatcher::Dispatcher>,
+}
+
+impl<'a> Drop for IsocRackUnit {
+    fn drop(&mut self) {
+        self.dispatchers.clear();
+    }
+}
+
+impl<'a> IsocRackUnit {
+    const NODE_DISPATCHER_NAME: &'a str = "node event dispatcher";
+    const SYSTEM_DISPATCHER_NAME: &'a str = "system event dispatcher";
+
+    pub fn new(unit: hinawa::SndTscm, _: String, _: u32) -> Result<Self, Error> {
+        // Use uni-directional channel for communication to child threads.
+        let (tx, rx) = mpsc::sync_channel(32);
+
+        let dispatchers = Vec::new();
+
+        Ok(IsocRackUnit {
+            unit,
+            tx,
+            rx,
+            dispatchers,
+        })
+    }
+
+    fn launch_node_event_dispatcher(&mut self) -> Result<(), Error> {
+        let name = Self::NODE_DISPATCHER_NAME.to_string();
+        let mut dispatcher = dispatcher::Dispatcher::run(name)?;
+
+        let tx = self.tx.clone();
+        dispatcher.attach_snd_unit(&self.unit, move |_| {
+            let _ = tx.send(RackUnitEvent::Disconnected);
+        })?;
+
+        let tx = self.tx.clone();
+        dispatcher.attach_fw_node(&self.unit.get_node(), move |_| {
+            let _ = tx.send(RackUnitEvent::Disconnected);
+        })?;
+
+        let tx = self.tx.clone();
+        self.unit.get_node().connect_bus_update(move |node| {
+            let generation = node.get_property_generation();
+            let _ = tx.send(RackUnitEvent::BusReset(generation));
+        });
+
+        self.dispatchers.push(dispatcher);
+
+        Ok(())
+    }
+
+    fn launch_system_event_dispatcher(&mut self) -> Result<(), Error> {
+        let name = Self::SYSTEM_DISPATCHER_NAME.to_string();
+        let mut dispatcher = dispatcher::Dispatcher::run(name)?;
+
+        let tx = self.tx.clone();
+        dispatcher.attach_signal_handler(signal::Signal::SIGINT, move || {
+            let _ = tx.send(RackUnitEvent::Shutdown);
+            source::Continue(false)
+        });
+
+        self.dispatchers.push(dispatcher);
+
+        Ok(())
+    }
+
+    pub fn listen(&mut self) -> Result<(), Error> {
+        self.launch_node_event_dispatcher()?;
+        self.launch_system_event_dispatcher()?;
+
+        Ok(())
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let ev = match self.rx.recv() {
+                Ok(ev) => ev,
+                Err(_) => continue,
+            };
+
+            match ev {
+                RackUnitEvent::Shutdown => break,
+                RackUnitEvent::Disconnected => break,
+                RackUnitEvent::BusReset(generation) => {
+                    println!("IEEE 1394 bus is updated: {}", generation);
+                }
+            }
+        }
+    }
+}
