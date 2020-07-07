93c8f1d292eac608cdd9500df5d3b19ca6a99592
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-07 18:06:08 +0900

    dg00x: monitor_ctl: handle control event for monitor features
    This commit adds monitor controls for activation and input gains. I note
    that actual functionality depends on the state of packet streaming. When
    packet streaming runs, everything works well. Without packet streaming,
    monitor configuration is not updated according to quadlet write transaction
    even if the transaction actually changes the value of registers. For this
    quirk, the registers are updated according to cached state whenever packet
    streaming is detected.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/dg00x/model.rs
+++ src/dg00x/model.rs
@@ -8,10 +8,12 @@ use crate::ta1394;
 use crate::card_cntr;
 
 use super::common_ctl::CommonCtl;
+use super::monitor_ctl::MonitorCtl;
 
 pub struct Dg00xModel {
     req: hinawa::FwReq,
     common: CommonCtl,
+    monitor: MonitorCtl,
 }
 
 impl Dg00xModel {
@@ -33,9 +35,30 @@ impl Dg00xModel {
         let model = Dg00xModel{
             req: hinawa::FwReq::new(),
             common: CommonCtl::new(has_word_bnc),
+            monitor: MonitorCtl::new(),
         };
 
         Ok(model)
+    }
+}
+
+impl card_cntr::MonitorModel<hinawa::SndDg00x> for Dg00xModel {
+    fn get_monitored_elems(&mut self, elem_id_list: &mut Vec<alsactl::ElemId>) {
+        elem_id_list.extend_from_slice(&self.monitor.monitored_elems);
+    }
+
+    fn monitor_unit(&mut self, _: &hinawa::SndDg00x) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn monitor_elems(
+        &mut self,
+        unit: &hinawa::SndDg00x,
+        elem_id: &alsactl::ElemId,
+        old: &alsactl::ElemValue,
+        _: &mut alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        self.monitor.monitor_elems(unit, &self.req, elem_id, old)
     }
 }
 
@@ -46,6 +69,7 @@ impl card_cntr::CtlModel<hinawa::SndDg00x> for Dg00xModel {
         card_cntr: &mut card_cntr::CardCntr,
     ) -> Result<(), Error> {
         self.common.load(&unit, &self.req, card_cntr)?;
+        self.monitor.load(&unit, &self.req, card_cntr)?;
         Ok(())
     }
 
@@ -56,6 +80,8 @@ impl card_cntr::CtlModel<hinawa::SndDg00x> for Dg00xModel {
         elem_value: &mut alsactl::ElemValue,
     ) -> Result<bool, Error> {
         if self.common.read(unit, &self.req, elem_id, elem_value)? {
+            Ok(true)
+        } else if self.monitor.read(unit, &self.req, elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -70,6 +96,8 @@ impl card_cntr::CtlModel<hinawa::SndDg00x> for Dg00xModel {
         new: &alsactl::ElemValue,
     ) -> Result<bool, Error> {
         if self.common.write(unit, &self.req, elem_id, old, new)? {
+            Ok(true)
+        } else if self.monitor.write(unit, &self.req, elem_id, old, new)? {
             Ok(true)
         } else {
             Ok(false)
---
+++ src/dg00x/monitor_ctl.rs
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use glib::{Error, FileError};
+
+use hinawa::SndUnitExt;
+
+use alsactl::{ElemValueExt, ElemValueExtManual};
+
+use crate::card_cntr;
+
+use super::protocol::{CommonProtocol, MonitorProtocol};
+
+pub struct MonitorCtl {
+    pub monitored_elems: Vec<alsactl::ElemId>,
+}
+
+impl<'a> MonitorCtl {
+    const ENABLE_NAME: &'a str = "monitor-enable";
+    const SRC_GAIN_NAME: &'a str = "monitor-source-gain";
+
+    const MIXER_LABELS: &'a [&'a str] = &["monitor-1", "monitor-2"];
+    const IN_LABELS: &'a [&'a str] = &[
+        "Analog-1", "Analog-2", "Analog-3", "Analog-4", "Analog-5", "Analog-6", "Analog-7",
+        "Analog-8", "S/PDIF-1", "S/PDIF-2", "ADAT-1", "ADAT-2", "ADAT-3", "ADAT-4", "ADAT-5",
+        "ADAT-6", "ADAT-7", "ADAT-8",
+    ];
+
+    const GAIN_MIN: i32 = 0;
+    const GAIN_MAX: i32 = 0x80;
+    const GAIN_STEP: i32 = 1;
+    const GAIN_TLV: &'a [i32; 4] = &[5, 8, -4800, 0];
+
+    const ENABLE_OFFSET: u64 = 0x0124;
+
+    pub fn new() -> Self {
+        MonitorCtl {
+            monitored_elems: Vec::new(),
+        }
+    }
+
+    pub fn load(
+        &mut self,
+        _: &hinawa::SndDg00x,
+        _: &hinawa::FwReq,
+        card_cntr: &mut card_cntr::CardCntr,
+    ) -> Result<(), Error> {
+        let elem_id =
+            alsactl::ElemId::new_by_name(alsactl::ElemIfaceType::Mixer, 0, 0, Self::ENABLE_NAME, 0);
+        let mut elem_id_list = card_cntr.add_bool_elems(&elem_id, 1, 1, true)?;
+        self.monitored_elems.push(elem_id_list.remove(0));
+
+        let elem_id = alsactl::ElemId::new_by_name(
+            alsactl::ElemIfaceType::Mixer,
+            0,
+            0,
+            Self::SRC_GAIN_NAME,
+            0,
+        );
+        let mut elem_id_list = card_cntr.add_int_elems(
+            &elem_id,
+            Self::MIXER_LABELS.len(),
+            Self::GAIN_MIN,
+            Self::GAIN_MAX,
+            Self::GAIN_STEP,
+            Self::IN_LABELS.len(),
+            Some(Self::GAIN_TLV),
+            true,
+        )?;
+        self.monitored_elems.append(&mut elem_id_list);
+
+        Ok(())
+    }
+
+    pub fn monitor_elems(&mut self, unit: &hinawa::SndDg00x, req: &hinawa::FwReq,
+                         elem_id: &alsactl::ElemId, old: &alsactl::ElemValue)
+        -> Result<bool, Error>
+    {
+        // Any write transaction to register has effective to configure internal multiplexer just
+        // during packet streaming. Without packet streaming, the transaction can change register
+        // to keep its value, however has no effects to the internal multiplexer. Here, attempt to
+        // update the registers with cached value at the beginning of packet streaming.
+        if unit.get_property_streaming() {
+            let dummy = alsactl::ElemValue::new();
+            self.write(unit, req, elem_id, &dummy, old)
+        } else {
+            Ok(false)
+        }
+    }
+
+    pub fn read(
+        &mut self,
+        unit: &hinawa::SndDg00x,
+        req: &hinawa::FwReq,
+        elem_id: &alsactl::ElemId,
+        elem_value: &mut alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        let node = unit.get_node();
+
+        match elem_id.get_name().as_str() {
+            Self::ENABLE_NAME => {
+                let mut vals = [false];
+                vals[0] = req.read_quadlet(&node, Self::ENABLE_OFFSET)? > 0;
+                elem_value.set_bool(&vals);
+                Ok(true)
+            }
+            Self::SRC_GAIN_NAME => {
+                let monitor = elem_id.get_index() as usize;
+                let mut vals = [0; 18];
+                Self::IN_LABELS.iter().enumerate().try_for_each(|(i, _)| {
+                    let val = req.read_gain(&node, monitor, i)?;
+                    vals[i] = (val >> 24) as i32;
+                    Ok(())
+                })?;
+                elem_value.set_int(&vals);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    pub fn write(
+        &mut self,
+        unit: &hinawa::SndDg00x,
+        req: &hinawa::FwReq,
+        elem_id: &alsactl::ElemId,
+        old: &alsactl::ElemValue,
+        new: &alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        if !unit.get_property_streaming() {
+            let label = "elements are not available without packet streaming";
+            Err(Error::new(FileError::Again, &label))
+        } else {
+            let node = unit.get_node();
+
+            match elem_id.get_name().as_str() {
+                Self::ENABLE_NAME => {
+                    let mut vals = [false];
+                    new.get_bool(&mut vals);
+                    let val = vals[0];
+                    req.write_quadlet(&node, Self::ENABLE_OFFSET, val as u32)?;
+                    Ok(true)
+                }
+                Self::SRC_GAIN_NAME => {
+                    let monitor = elem_id.get_index() as usize;
+                    let len = Self::IN_LABELS.len();
+                    let mut vals = vec![0; len * 2];
+                    new.get_int(&mut vals[0..len]);
+                    old.get_int(&mut vals[len..]);
+
+                    Self::IN_LABELS.iter().enumerate().try_for_each(|(i, _)| {
+                        if vals[i] != vals[i + len] {
+                            let val = (vals[i] << 24) as u32;
+                            req.write_gain(&node, monitor, i, val)
+                        } else {
+                            Ok(())
+                        }
+                    })?;
+                    Ok(true)
+                }
+                _ => Ok(false),
+            }
+        }
+    }
+}
