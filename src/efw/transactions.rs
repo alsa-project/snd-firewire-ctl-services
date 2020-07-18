76e49c9237c2979e562969a8a37afbca1cd164c8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-18 10:53:16 +0900

    efw: add support for commands in guitar category
    Fireworks board module is used for some products which works as audio
    interface for guitar. Some command/response transaction is implemented
    for guitar-specific functionality.
    
    This commit adds support for commands in guitar category.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/efw/transactions.rs
+++ src/efw/transactions.rs
@@ -12,6 +12,7 @@ enum Category {
     Playback,
     Monitor,
     PortConf,
+    Guitar,
 }
 
 impl From<Category> for u32 {
@@ -24,6 +25,7 @@ impl From<Category> for u32 {
             Category::Playback => 0x06,
             Category::Monitor => 0x08,
             Category::PortConf => 0x09,
+            Category::Guitar=> 0x0a,
         }
     }
 }
@@ -901,5 +903,51 @@ impl EfwPortConf {
             .map(|pos| (params[38 + pos] / 2) as usize)
             .collect();
         Ok((rx_entries, tx_entries))
+    }
+}
+
+#[derive(Debug)]
+pub struct GuitarChargeState {
+    pub manual_charge: bool,
+    pub auto_charge: bool,
+    pub suspend_to_charge: u32,
+}
+
+pub struct EfwGuitar {}
+
+impl EfwGuitar {
+    const CMD_SET_CHARGE_STATE: u32 = 7;
+    const CMD_GET_CHARGE_STATE: u32 = 8;
+
+    pub fn get_charge_state(unit: &hinawa::SndEfw) -> Result<GuitarChargeState, Error> {
+        let mut params = [0;3];
+        let _ = unit.transaction(
+            u32::from(Category::Guitar),
+            Self::CMD_GET_CHARGE_STATE,
+            &[],
+            &mut params,
+        )?;
+        let state = GuitarChargeState{
+            manual_charge: params[0] > 0,
+            auto_charge: params[1] > 0,
+            suspend_to_charge: params[2],
+        };
+        Ok(state)
+    }
+
+    pub fn set_charge_state(unit: &hinawa::SndEfw, state: &GuitarChargeState) -> Result<(), Error> {
+        let args = [
+            state.manual_charge as u32,
+            state.auto_charge as u32,
+            state.suspend_to_charge,
+        ];
+        let mut params = [0;3];
+        let _ = unit.transaction(
+            u32::from(Category::Guitar),
+            Self::CMD_SET_CHARGE_STATE,
+            &args,
+            &mut params,
+        )?;
+        Ok(())
     }
 }
