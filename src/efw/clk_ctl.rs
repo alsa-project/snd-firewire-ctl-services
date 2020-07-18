alsa-project/snd-firewire-ctl-services

commit 4b43427b9ebd697158573cbbf74e9854c11a093c

efw: add clock control elements
This commit adds some control elements for clock configuration.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/efw/clk_ctl.rs
@@ -1,0 +1,143 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+
+use crate::card_cntr;
+
+use hinawa::SndUnitExt;
+
+use alsactl::{ElemValueExt, ElemValueExtManual};
+
+use super::transactions::{ClkSrc, HwInfo, EfwHwCtl};
+
+pub struct ClkCtl {
+    srcs: Vec<ClkSrc>,
+    rates: Vec<u32>,
+}
+
+impl<'a> ClkCtl {
+    const SRC_NAME: &'a str = "clock-source";
+    const RATE_NAME: &'a str = "clock-rate";
+
+    pub fn new() -> Self {
+        ClkCtl {
+            srcs: Vec::new(),
+            rates: Vec::new(),
+        }
+    }
+
+    pub fn load(
+        &mut self,
+        hwinfo: &HwInfo,
+        card_cntr: &mut card_cntr::CardCntr,
+    ) -> Result<(), Error> {
+        self.srcs.extend_from_slice(&hwinfo.clk_srcs);
+        self.rates.extend_from_slice(&hwinfo.clk_rates);
+
+        let labels: Vec<&str> = self
+            .srcs
+            .iter()
+            .map(|src| match *src {
+                ClkSrc::Internal => "Internal",
+                ClkSrc::WordClock => "WordClock",
+                ClkSrc::Spdif => "S/PDIF",
+                ClkSrc::Adat => "ADAT",
+                ClkSrc::Adat2 => "ADAT2",
+                ClkSrc::Continuous => "Continuous",
+                ClkSrc::Unknown(_) => "Unknown",
+            })
+            .collect();
+
+        let elem_id =
+            alsactl::ElemId::new_by_name(alsactl::ElemIfaceType::Mixer, 0, 0, Self::SRC_NAME, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+
+        let labels: Vec<&str> = hwinfo
+            .clk_rates
+            .iter()
+            .map(|rate| match *rate {
+                32000 => "32000",
+                44100 => "44100",
+                48000 => "48000",
+                88200 => "88200",
+                96000 => "96000",
+                176400 => "176400",
+                192000 => "192000",
+                _ => "Unknown",
+            })
+            .collect();
+
+        let elem_id =
+            alsactl::ElemId::new_by_name(alsactl::ElemIfaceType::Mixer, 0, 0, Self::RATE_NAME, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+
+        Ok(())
+    }
+
+    pub fn read(
+        &mut self,
+        unit: &hinawa::SndEfw,
+        elem_id: &alsactl::ElemId,
+        elem_value: &mut alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            Self::SRC_NAME => {
+                let (src, _) = EfwHwCtl::get_clock(unit)?;
+                if let Some(pos) = self.srcs.iter().position(|s| *s == src) {
+                    elem_value.set_enum(&[pos as u32]);
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Self::RATE_NAME => {
+                let (_, rate) = EfwHwCtl::get_clock(unit)?;
+                if let Some(pos) = self.rates.iter().position(|r| *r == rate) {
+                    elem_value.set_enum(&[pos as u32]);
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            _ => Ok(false),
+        }
+    }
+
+    pub fn write(
+        &mut self,
+        unit: &hinawa::SndEfw,
+        elem_id: &alsactl::ElemId,
+        old: &alsactl::ElemValue,
+        new: &alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            Self::SRC_NAME => {
+                if !unit.get_property_streaming() {
+                    let mut vals = [0; 2];
+                    old.get_enum(&mut vals[0..1]);
+                    new.get_enum(&mut vals[1..]);
+                    if vals[0] != vals[1] {
+                        EfwHwCtl::set_clock(unit, Some(ClkSrc::from(vals[0] as usize)), None)?;
+                    }
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Self::RATE_NAME => {
+                if !unit.get_property_streaming() {
+                    let mut vals = [0; 2];
+                    old.get_enum(&mut vals[0..1]);
+                    new.get_enum(&mut vals[1..]);
+                    if vals[0] != vals[1] {
+                        EfwHwCtl::set_clock(unit, None, Some(vals[0]))?;
+                    }
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            _ => Ok(false),
+        }
+    }
+}

src/efw.rs
@@ -3,3 +3,5 @@ pub mod unit;
 mod model;
 
 mod transactions;
+
+mod clk_ctl;

src/efw/model.rs
@@ -8,8 +8,11 @@ use crate::card_cntr;
 use card_cntr::CtlModel;
 
 use super::transactions::EfwInfo;
+use super::clk_ctl;
 
-pub struct EfwModel {}
+pub struct EfwModel {
+    clk_ctl: clk_ctl::ClkCtl,
+}
 
 impl EfwModel {
     pub fn new(data: &[u8]) -> Result<Self, Error> {
@@ -37,7 +40,9 @@ impl EfwModel {
                 (0x00075b, 0x00afb2) |
                 // Gibson, Robot Interface Pack (RIP) for Dark Fire series.
                 (0x00075b, 0x00afb9) => {
-                    let model = EfwModel {};
+                    let model = EfwModel {
+                        clk_ctl: clk_ctl::ClkCtl::new(),
+                    };
                     Ok(model)
                 },
                 _ => {
@@ -54,24 +59,34 @@ impl EfwModel {
 }
 
 impl CtlModel<hinawa::SndEfw> for EfwModel {
-    fn load(&mut self, unit: &hinawa::SndEfw, _: &mut card_cntr::CardCntr) -> Result<(), Error> {
+    fn load(&mut self, unit: &hinawa::SndEfw, card_cntr: &mut card_cntr::CardCntr)
+        -> Result<(), Error> {
         let hwinfo = EfwInfo::get_hwinfo(unit)?;
-
+        self.clk_ctl.load(&hwinfo, card_cntr)?;
         Ok(())
     }
 
-    fn read(&mut self, _: &hinawa::SndEfw, _: &alsactl::ElemId, _: &mut alsactl::ElemValue)
+    fn read(&mut self, unit: &hinawa::SndEfw, elem_id: &alsactl::ElemId,
+            elem_value: &mut alsactl::ElemValue)
         -> Result<bool, Error> {
-        Ok(false)
+        if self.clk_ctl.read(unit, elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(
         &mut self,
-        _: &hinawa::SndEfw,
-        _: &alsactl::ElemId,
-        _: &alsactl::ElemValue,
-        _: &alsactl::ElemValue,
+        unit: &hinawa::SndEfw,
+        elem_id: &alsactl::ElemId,
+        old: &alsactl::ElemValue,
+        new: &alsactl::ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.clk_ctl.write(unit, elem_id, old, new)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }

commit 52b1b4a59fbf772262b2022ce2f98f5d4cd6782c

efw: fix failure to configure sampling rate
In transaction of Fireworks board module, sampling rate is configured
by literal number. On the other hand, the handler for sampling-rate
control element attempts to set index number in model-specific table
for supported sampling rate. As a result, invalid number is going to
be configured.

This commit fixes failure to configure sampling rate.

Fixes: 4b43427b9ebd ("efw: add clock control elements")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/efw/clk_ctl.rs
@@ -129,8 +129,12 @@ impl<'a> ClkCtl {
                 if !unit.get_property_streaming() {
                     let mut vals = [0];
                     new.get_enum(&mut vals);
-                    EfwHwCtl::set_clock(unit, None, Some(vals[0]))?;
-                    Ok(true)
+                    if let Some(&rate) = self.rates.iter().nth(vals[0] as usize) {
+                        EfwHwCtl::set_clock(unit, None, Some(rate))?;
+                        Ok(true)
+                    } else {
+                        Ok(false)
+                    }
                 } else {
                     Ok(false)
                 }