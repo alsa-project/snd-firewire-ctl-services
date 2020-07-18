64337e4f9b31d3388a16ff6c41f4e09195d8cfc2
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-18 10:53:16 +0900

    efw: add unit and service program
    Echo Digital Audio corporation has shipped Fireworks board module. The
    module consists of two processors:
    
    * TI TSB43Cx43A (IceLynx Micro, iCEM)
    * TI TI TMS320C67DSP or Xilinx Spartan XC35250E
    
    Both processors can download firmwares. For the purpose, the board
    module has flash memory. The firmware for iCEM implements a pair of
    request and response transactions specific for the board module. Any
    driver can configure the board module by the transactions.
    
    ALSA fireworks driver supports the board module in a point of packet
    streaming and specific transaction. On the other hand, no application
    has been developed for the clock configuration, digital I/O routing,
    and so on.
    
    This commit adds an executable, snd-fireworks-ctl-service, to dispatch
    events from ALSA control applications to control the functionalities.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/bin/snd-fireworks-ctl-service.rs
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use snd_fw_ctl_services::efw;
+
+use std::env;
+
+fn print_help() {
+    println!("
+Usage:
+  snd-fireworks-ctl-service CARD_ID
+
+  where:
+    CARD_ID: The numerical ID of sound card.
+    ");
+}
+
+fn main() {
+    // Check arguments in command line.
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print_help();
+        std::process::exit(libc::EXIT_FAILURE);
+    }
+
+    let card_id = match args[1].parse::<u32>() {
+        Ok(card_id) => card_id,
+        Err(err) => {
+            println!("{:?}", err);
+            print_help();
+            std::process::exit(libc::EXIT_FAILURE);
+        }
+    };
+
+    let err = match efw::unit::EfwUnit::new(card_id) {
+        Err(err) => {
+            println!("The card {} is not for fireworks device: {}",
+                     card_id, err);
+            Err(err)
+        }
+        Ok(mut unit) => {
+            if let Err(err) = unit.listen() {
+                println!("Fail to listen events: {}", err);
+                Err(err)
+            } else {
+                unit.run();
+                Ok(())
+            }
+        }
+    };
+
+    if err.is_err() {
+        std::process::exit(libc::EXIT_FAILURE)
+    }
+
+    std::process::exit(libc::EXIT_SUCCESS)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,4 @@ pub mod card_cntr;
 
 pub mod dg00x;
 pub mod tascam;
+pub mod efw;
