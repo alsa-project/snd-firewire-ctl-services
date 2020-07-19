8af8e94b1e8cf167ee744b52cf0495e41a01197c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-07-19 22:58:53 +0900

    efw: cache current states of nominal input level for FPGA model
    In FPGA models, the response to read request for nominal input
    level is the same even if any change is actually applied. The response
    always includes the same value.
    
    This commit is for the quirk. The cache is added for current status of
    nominal input level for FPGA model. In the case of FPGA models, nominal
    input level is configured to be the default value so that the cache works
    properly.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/efw/input_ctl.rs
+++ src/efw/input_ctl.rs
@@ -10,6 +10,7 @@ use super::transactions::{HwCap, EfwPhysInput, NominalLevel, HwInfo};
 
 pub struct InputCtl {
     phys_inputs: usize,
+    cache: Option<Vec::<NominalLevel>>,
 }
 
 impl<'a> InputCtl {
@@ -19,10 +20,10 @@ impl<'a> InputCtl {
     const IN_NOMINAL_LEVELS: &'a [NominalLevel] = &[NominalLevel::PlusFour, NominalLevel::MinusTen];
 
     pub fn new() -> Self {
-        InputCtl { phys_inputs: 0 }
+        InputCtl { phys_inputs: 0, cache: None}
     }
 
-    pub fn load(&mut self, hwinfo: &HwInfo, card_cntr: &mut card_cntr::CardCntr)
+    pub fn load(&mut self, unit: &hinawa::SndEfw, hwinfo: &HwInfo, card_cntr: &mut card_cntr::CardCntr)
         -> Result<(), Error>
     {
         self.phys_inputs = hwinfo.phys_inputs.iter().fold(0, |accm, entry| accm + entry.group_count);
@@ -32,6 +33,19 @@ impl<'a> InputCtl {
                 alsactl::ElemIfaceType::Mixer, 0, 0, Self::IN_NOMINAL_NAME, 0);
             let _ = card_cntr.add_enum_elems(&elem_id, 1,
                 self.phys_inputs, Self::IN_NOMINAL_LABELS, None, true)?;
+        }
+
+        // FPGA models return invalid state of nominal level. Here, initialize them and cache the
+        // state instead.
+        let has_fpga = hwinfo.caps.iter().find(|&cap| *cap == HwCap::Fpga).is_some();
+        if has_fpga {
+            let cache = vec![NominalLevel::PlusFour;self.phys_inputs];
+            cache.iter().enumerate()
+                .try_for_each( |(i, &level)| {
+                    EfwPhysInput::set_nominal(unit, i, level)?;
+                    Ok(())
+                })?;
+            self.cache = Some(cache);
         }
 
         Ok(())
@@ -46,13 +60,24 @@ impl<'a> InputCtl {
         match elem_id.get_name().as_str() {
             Self::IN_NOMINAL_NAME => {
                 let mut vals = vec![0; self.phys_inputs];
-                vals.iter_mut().enumerate().try_for_each(|(i, val)| {
-                    let level = EfwPhysInput::get_nominal(unit, i)?;
-                    if let Some(pos) = Self::IN_NOMINAL_LEVELS.iter().position(|&l| l == level) {
-                        *val = pos as u32;
+                match &self.cache {
+                    // For models with FPGA.
+                    Some(cache) => {
+                        vals.iter_mut().zip(cache.iter()).for_each(|(val, &level)| {
+                            *val = u32::from(level)
+                        });
                     }
-                    Ok(())
-                })?;
+                    // For models with DSP.
+                    None => {
+                        vals.iter_mut().enumerate().try_for_each(|(i, val)| {
+                            let level = EfwPhysInput::get_nominal(unit, i)?;
+                            if let Some(pos) = Self::IN_NOMINAL_LEVELS.iter().position(|&l| l == level) {
+                                *val = pos as u32;
+                            }
+                            Ok(())
+                        })?;
+                    }
+                }
                 elem_value.set_enum(&vals);
                 Ok(true)
             }
@@ -80,6 +105,14 @@ impl<'a> InputCtl {
                     }
                     Ok(())
                 })?;
+
+                // For FPGA models.
+                if let Some(cache) = &mut self.cache {
+                    cache.iter_mut().zip(vals[0..self.phys_inputs].iter()).for_each(|(c, &val)| {
+                        *c = NominalLevel::from(val);
+                    });
+                }
+
                 Ok(true)
             }
             _ => Ok(false),
