alsa-project/snd-firewire-ctl-services

commit 2562867ed9715459c157870536fd6228093ec192

efw: add port configuration control elements
This commit adds some control elements for port configuration.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/efw/port_ctl.rs
@@ -1,0 +1,261 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+
+use crate::card_cntr;
+
+use alsactl::{ElemValueExt, ElemValueExtManual};
+
+use super::transactions::{HwCap, DigitalMode, EfwPortConf, PhysGroupType, HwInfo};
+
+pub struct PortCtl {
+    dig_modes: Vec<DigitalMode>,
+    phys_in_pairs: usize,
+    phys_out_pairs: usize,
+    rx_pairs: usize,
+    tx_pairs: usize,
+}
+
+impl<'a> PortCtl {
+    const MIRROR_OUTPUT_NAME: &'a str = "mirror-output";
+    const DIG_MODE_NAME: &'a str = "digital-mode";
+    const PHANTOM_NAME: &'a str = "phantom-powering";
+    const RX_MAP_NAME: &'a str = "stream-playback-routing";
+    const TX_MAP_NAME: &'a str = "stream-capture-routing";
+
+    const DIG_MODES: &'a [(HwCap, DigitalMode)] = &[
+        (HwCap::SpdifCoax, DigitalMode::SpdifCoax),
+        (HwCap::AesebuXlr, DigitalMode::AesebuXlr),
+        (HwCap::SpdifOpt, DigitalMode::SpdifOpt),
+        (HwCap::AdatOpt, DigitalMode::AdatOpt),
+    ];
+
+    pub fn new() -> Self {
+        PortCtl {
+            dig_modes: Vec::new(),
+            phys_in_pairs: 0,
+            phys_out_pairs: 0,
+            tx_pairs: 0,
+            rx_pairs: 0,
+        }
+    }
+
+    fn add_mapping_ctl(
+        &self,
+        card_cntr: &mut card_cntr::CardCntr,
+        name: &'a str,
+        phys_pairs: usize,
+        stream_pairs: usize,
+    ) -> Result<(), Error> {
+        let list: Vec<String> = (0..stream_pairs)
+            .map(|pair| format!("Stream-{}/{}", pair * 2 + 1, pair * 2 + 2))
+            .collect();
+        let labels: Vec<&str> = list.iter().map(|entry| entry.as_str()).collect();
+
+        let elem_id = alsactl::ElemId::new_by_name(alsactl::ElemIfaceType::Mixer, 0, 0, name, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, phys_pairs, &labels, None, true)?;
+
+        Ok(())
+    }
+
+    pub fn load(&mut self, hwinfo: &HwInfo, card_cntr: &mut card_cntr::CardCntr)
+        -> Result<(), Error>
+    {
+        if hwinfo.caps.iter().find(|&cap| *cap == HwCap::MirrorOutput).is_some() {
+            let mut list = Vec::new();
+            hwinfo.phys_outputs.iter().for_each(|entry| {
+                if entry.group_type != PhysGroupType::AnalogMirror {
+                    (0..(entry.group_count / 2)).for_each(|i| {
+                        let name = String::from(&entry.group_type);
+                        list.push(format!("{}-{}/{}", name, i * 2 + 1, i * 2 + 2))
+                    })
+                }
+            });
+            let labels: Vec<&str> = list.iter().map(|entry| entry.as_str()).collect();
+
+            let elem_id = alsactl::ElemId::new_by_name(
+                alsactl::ElemIfaceType::Mixer, 0, 0, Self::MIRROR_OUTPUT_NAME, 0);
+            let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+        }
+
+        Self::DIG_MODES.iter().for_each(|(cap, mode)| {
+            if hwinfo.caps.iter().find(|&c| *c == *cap).is_some() {
+                self.dig_modes.push(*mode);
+            }
+        });
+        if self.dig_modes.len() > 0 {
+            let modes: Vec<String> = self.dig_modes.iter()
+                .map(|mode| String::from(mode))
+                .collect();
+            let labels: Vec<&str> = modes.iter()
+                .map(|label| label.as_str())
+                .collect();
+
+            let elem_id = alsactl::ElemId::new_by_name(
+                alsactl::ElemIfaceType::Mixer, 0, 0, Self::DIG_MODE_NAME, 0);
+            let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+        }
+
+        if hwinfo.caps.iter().position(|cap| *cap == HwCap::PhantomPowering).is_some() {
+            let elem_id = alsactl::ElemId::new_by_name(
+                alsactl::ElemIfaceType::Mixer, 0, 0, Self::PHANTOM_NAME, 0);
+            let _ = card_cntr.add_bool_elems(&elem_id, 1, 1, true)?;
+        }
+
+        if hwinfo.caps.iter().position(|cap| *cap == HwCap::OutputMapping).is_some() {
+            self.phys_out_pairs = hwinfo.phys_outputs.iter()
+                .fold(0, |accm, entry| accm + entry.group_count)
+                / 2;
+            self.rx_pairs = hwinfo.rx_channels[0] / 2;
+
+            self.add_mapping_ctl(card_cntr, Self::RX_MAP_NAME, self.phys_out_pairs, self.rx_pairs)?;
+        }
+
+        if hwinfo.caps.iter().position(|cap| *cap == HwCap::InputMapping).is_some() {
+            self.phys_in_pairs = hwinfo.phys_inputs.iter()
+                .fold(0, |accm, entry| accm + entry.group_count) / 2;
+            self.tx_pairs = hwinfo.tx_channels[0] / 2;
+
+            self.add_mapping_ctl(
+                card_cntr,
+                Self::TX_MAP_NAME,
+                self.phys_in_pairs,
+                self.tx_pairs,
+            )?;
+        }
+
+        Ok(())
+    }
+
+    pub fn read(
+        &mut self,
+        unit: &hinawa::SndEfw,
+        elem_id: &alsactl::ElemId,
+        elem_value: &mut alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            Self::MIRROR_OUTPUT_NAME => {
+                let pair = EfwPortConf::get_output_mirror(unit)?;
+                elem_value.set_enum(&[pair as u32]);
+                Ok(true)
+            }
+            Self::DIG_MODE_NAME => {
+                let mode = EfwPortConf::get_digital_mode(unit)?;
+                if let Some(pos) = self.dig_modes.iter().position(|&m| m == mode) {
+                    elem_value.set_enum(&[pos as u32]);
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Self::PHANTOM_NAME => {
+                let state = EfwPortConf::get_phantom_powering(unit)?;
+                elem_value.set_bool(&[state]);
+                Ok(true)
+            }
+            Self::RX_MAP_NAME => {
+                let (rx_entries, _) = EfwPortConf::get_stream_map(unit)?;
+                let vals: Vec<u32> = rx_entries.iter().map(|entry| *entry as u32).collect();
+                elem_value.set_enum(&vals);
+                Ok(true)
+            }
+            Self::TX_MAP_NAME => {
+                let (_, tx_entries) = EfwPortConf::get_stream_map(unit)?;
+                let vals: Vec<u32> = tx_entries.iter().map(|entry| *entry as u32).collect();
+                elem_value.set_enum(&vals);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    pub fn write(
+        &mut self,
+        unit: &hinawa::SndEfw,
+        elem_id: &alsactl::ElemId,
+        old: &alsactl::ElemValue,
+        new: &alsactl::ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            Self::MIRROR_OUTPUT_NAME => {
+                let mut vals = [0];
+                new.get_enum(&mut vals);
+                EfwPortConf::set_output_mirror(unit, vals[0] as usize)?;
+                Ok(true)
+            }
+            Self::DIG_MODE_NAME => {
+                let mut vals = [0];
+                new.get_enum(&mut vals);
+                if self.dig_modes.len() > vals[0] as usize {
+                    EfwPortConf::set_digital_mode(unit, self.dig_modes[vals[0] as usize])?;
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            Self::PHANTOM_NAME => {
+                let mut vals = [false];
+                new.get_bool(&mut vals);
+                EfwPortConf::set_phantom_powering(unit, vals[0])?;
+                Ok(true)
+            }
+            Self::RX_MAP_NAME => {
+                let mut vals = vec![0; self.rx_pairs * 2];
+                new.get_enum(&mut vals[..self.rx_pairs]);
+                old.get_enum(&mut vals[self.rx_pairs..]);
+                if vals[..self.rx_pairs] != vals[self.rx_pairs..] {
+                    let entries: Vec<usize> = vals[..self.rx_pairs]
+                        .iter()
+                        .map(|entry| *entry as usize)
+                        .collect();
+                    EfwPortConf::set_stream_map(unit, Some(entries), None)?;
+                }
+                Ok(true)
+            }
+            Self::TX_MAP_NAME => {
+                let mut vals = vec![0; self.tx_pairs * 2];
+                new.get_enum(&mut vals[..self.tx_pairs]);
+                old.get_enum(&mut vals[self.tx_pairs..]);
+                if vals[..self.tx_pairs] != vals[self.tx_pairs..] {
+                    let entries: Vec<usize> = vals[..self.tx_pairs]
+                        .iter()
+                        .map(|entry| *entry as usize)
+                        .collect();
+                    EfwPortConf::set_stream_map(unit, None, Some(entries))?;
+                }
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+}
+
+impl From<&PhysGroupType> for String {
+    fn from(group_type: &PhysGroupType) -> String {
+        match group_type {
+            PhysGroupType::Analog       => "Analog",
+            PhysGroupType::Spdif        => "S/PDIF",
+            PhysGroupType::Adat         => "ADAT",
+            PhysGroupType::SpdifOrAdat  => "S/PDIForADAT",
+            PhysGroupType::AnalogMirror => "AnalogMirror",
+            PhysGroupType::Headphones   => "HeadPhones",
+            PhysGroupType::I2s          => "I2S",
+            PhysGroupType::Guitar       => "Guitar",
+            PhysGroupType::PiezoGuitar  => "PiezoGuitar",
+            PhysGroupType::GuitarString => "GuitarString",
+            PhysGroupType::Unknown(_)   => "Unknown",
+        }.to_string()
+    }
+}
+
+impl From<&DigitalMode> for String {
+    fn from(mode: &DigitalMode) -> String {
+        match mode {
+            DigitalMode::SpdifCoax  => "S/PDIF-Coaxial",
+            DigitalMode::AesebuXlr  => "AES/EBU-XLR",
+            DigitalMode::SpdifOpt   => "S/PDIF-Optical",
+            DigitalMode::AdatOpt    => "ADAT-Optical",
+            DigitalMode::Unknown(_) => "Unknown",
+        }.to_string()
+    }
+}

src/efw.rs
@@ -8,3 +8,4 @@ mod clk_ctl;
 mod mixer_ctl;
 mod output_ctl;
 mod input_ctl;
+mod port_ctl;

src/efw/model.rs
@@ -12,12 +12,14 @@ use super::clk_ctl;
 use super::mixer_ctl;
 use super::output_ctl;
 use super::input_ctl;
+use super::port_ctl;
 
 pub struct EfwModel {
     clk_ctl: clk_ctl::ClkCtl,
     mixer_ctl: mixer_ctl::MixerCtl,
     output_ctl: output_ctl::OutputCtl,
     input_ctl: input_ctl::InputCtl,
+    port_ctl: port_ctl::PortCtl,
 }
 
 impl EfwModel {
@@ -51,6 +53,7 @@ impl EfwModel {
                         mixer_ctl: mixer_ctl::MixerCtl::new(),
                         output_ctl: output_ctl::OutputCtl::new(),
                         input_ctl: input_ctl::InputCtl::new(),
+                        port_ctl: port_ctl::PortCtl::new(),
                     };
                     Ok(model)
                 },
@@ -75,6 +78,7 @@ impl CtlModel<hinawa::SndEfw> for EfwModel {
         self.mixer_ctl.load(&hwinfo, card_cntr)?;
         self.output_ctl.load(&hwinfo, card_cntr)?;
         self.input_ctl.load(&hwinfo, card_cntr)?;
+        self.port_ctl.load(&hwinfo, card_cntr)?;
         Ok(())
     }
 
@@ -88,6 +92,8 @@ impl CtlModel<hinawa::SndEfw> for EfwModel {
         } else if self.output_ctl.read(unit, elem_id, elem_value)? {
             Ok(true)
         } else if self.input_ctl.read(unit, elem_id, elem_value)? {
+            Ok(true)
+        } else if self.port_ctl.read(unit, elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -108,6 +114,8 @@ impl CtlModel<hinawa::SndEfw> for EfwModel {
         } else if self.output_ctl.write(unit, elem_id, old, new)? {
             Ok(true)
         } else if self.input_ctl.write(unit, elem_id, old, new)? {
+            Ok(true)
+        } else if self.port_ctl.write(unit, elem_id, old, new)? {
             Ok(true)
         } else {
             Ok(false)

commit 7bfe1258ac08db39dcf7b72febe0add0ba9972bd

efw: add digital-mode control element only when several digital interfaces are available
The digital-mode control element is added for models which has several
digital interfaces. However, the control element is added for models
which has any digital interfaces.

This commit limits the addition of control element just for models with
severl digital interfaces.

Fixes: 2562867ed971 ("efw: add port configuration control elements")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/efw/port_ctl.rs
@@ -83,7 +83,7 @@ impl<'a> PortCtl {
                 self.dig_modes.push(*mode);
             }
         });
-        if self.dig_modes.len() > 0 {
+        if self.dig_modes.len() > 1 {
             let modes: Vec<String> = self.dig_modes.iter()
                 .map(|mode| String::from(mode))
                 .collect();