2e8dc3c79eb001ec9f736cc77b50433f94bef0e0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-08-31 13:16:22 +0900

    motu: add support for MOTU AudioExpress
    This commit adds support for MOTU AudioExpress. Unfortunately,
    the firmware of AudioExpress often return invalid acknowledge
    code for request transaction. Linux driver for 1394 OHCI
    controller reports 'unsolicited response' for the transaction
    and userspace application receives 'send error' result.
    
    This commit also changes workaround to check target register
    so that it has the same value in the transaction.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/motu/model.rs
+++ src/motu/model.rs
@@ -5,6 +5,7 @@ use glib::{Error, FileError};
 use crate::card_cntr::{CardCntr, CtlModel};
 
 use super::ultralite_mk3::UltraLiteMk3;
+use super::audioexpress::AudioExpress;
 use super::h4pre::H4pre;
 
 pub struct MotuModel<'a> {
@@ -14,6 +15,7 @@ pub struct MotuModel<'a> {
 
 enum MotuCtlModel<'a> {
     UltraLiteMk3(UltraLiteMk3<'a>),
+    AudioExpress(AudioExpress<'a>),
     H4pre(H4pre<'a>),
 }
 
@@ -21,6 +23,7 @@ impl<'a> MotuModel<'a> {
     pub fn new(model_id: u32, version: u32) -> Result<Self, Error> {
         let ctl_model = match model_id {
             0x000019 => MotuCtlModel::UltraLiteMk3(UltraLiteMk3::new()),
+            0x000033 => MotuCtlModel::AudioExpress(AudioExpress::new()),
             0x000045 => MotuCtlModel::H4pre(H4pre::new()),
             _ => {
                 let label = format!("Unsupported model ID: 0x{:06x}", model_id);
@@ -39,6 +42,7 @@ impl<'a> MotuModel<'a> {
     {
         match &mut self.ctl_model {
             MotuCtlModel::UltraLiteMk3(m) => m.load(unit, card_cntr),
+            MotuCtlModel::AudioExpress(m) => m.load(unit, card_cntr),
             MotuCtlModel::H4pre(m) => m.load(unit, card_cntr),
         }
     }
@@ -49,6 +53,7 @@ impl<'a> MotuModel<'a> {
     {
         match &mut self.ctl_model {
             MotuCtlModel::UltraLiteMk3(m) => card_cntr.dispatch_elem_event(unit, elem_id, events, m),
+            MotuCtlModel::AudioExpress(m) => card_cntr.dispatch_elem_event(unit, elem_id, events, m),
             MotuCtlModel::H4pre(m) => card_cntr.dispatch_elem_event(unit, elem_id, events, m),
         }
     }
