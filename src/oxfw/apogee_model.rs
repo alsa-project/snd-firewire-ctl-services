fbdc29e4e39dc5e901aea5362f1a23dbd34dcc7b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-10-03 14:20:06 +0900

    oxfw: apogee_model: add support for common control
    This commit adds common control for Apogee Duet FireWire.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/oxfw/apogee_model.rs
+++ src/oxfw/apogee_model.rs
@@ -2,46 +2,72 @@
 // Copyright (c) 2020 Takashi Sakamoto
 use glib::Error;
 
+use hinawa::{SndUnitExt, FwFcpExt};
+
 use crate::card_cntr;
 
-pub struct ApogeeModel;
+use super::common_ctl::CommonCtl;
+
+pub struct ApogeeModel{
+    avc: hinawa::FwFcp,
+    common_ctl: CommonCtl,
+}
 
 impl<'a> ApogeeModel {
+    const FCP_TIMEOUT_MS: u32 = 100;
+
     pub fn new() -> Self {
-        ApogeeModel{}
+        ApogeeModel{
+            avc: hinawa::FwFcp::new(),
+            common_ctl: CommonCtl::new(),
+        }
     }
 }
 
 impl card_cntr::CtlModel<hinawa::SndUnit> for ApogeeModel {
-    fn load(&mut self, _: &hinawa::SndUnit, _: &mut card_cntr::CardCntr) -> Result<(), Error> {
+    fn load(&mut self, unit: &hinawa::SndUnit, card_cntr: &mut card_cntr::CardCntr) -> Result<(), Error> {
+        self.avc.bind(&unit.get_node())?;
+
+        self.common_ctl.load(&self.avc, card_cntr, Self::FCP_TIMEOUT_MS)?;
+
         Ok(())
     }
 
-    fn read(&mut self, _: &hinawa::SndUnit, _: &alsactl::ElemId, _: &mut alsactl::ElemValue)
+    fn read(&mut self, _: &hinawa::SndUnit, elem_id: &alsactl::ElemId,
+            elem_value: &mut alsactl::ElemValue)
         -> Result<bool, Error>
     {
-        Ok(false)
+        if self.common_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
-    fn write(&mut self, _: &hinawa::SndUnit, _: &alsactl::ElemId, _: &alsactl::ElemValue,
-             _: &alsactl::ElemValue)
+    fn write(&mut self, unit: &hinawa::SndUnit, elem_id: &alsactl::ElemId, _: &alsactl::ElemValue,
+             new: &alsactl::ElemValue)
         -> Result<bool, Error>
     {
-        Ok(false)
+        if self.common_ctl.write(unit, &self.avc, elem_id, new, Self::FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
 
 impl card_cntr::NotifyModel<hinawa::SndUnit, bool> for ApogeeModel {
-    fn get_notified_elem_list(&mut self, _: &mut Vec<alsactl::ElemId>) {
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<alsactl::ElemId>) {
+        elem_id_list.extend_from_slice(&self.common_ctl.notified_elem_list);
     }
 
     fn parse_notification(&mut self, _: &hinawa::SndUnit, _: &bool) -> Result<(), Error> {
         Ok(())
     }
 
-    fn read_notified_elem(&mut self, _: &hinawa::SndUnit, _: &alsactl::ElemId, _: &mut alsactl::ElemValue)
+    fn read_notified_elem(&mut self, _: &hinawa::SndUnit, elem_id: &alsactl::ElemId, elem_value: &mut alsactl::ElemValue)
         -> Result<bool, Error>
     {
-        Ok(false)
+        self.common_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)
     }
 }
