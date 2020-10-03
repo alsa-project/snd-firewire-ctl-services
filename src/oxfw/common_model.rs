44afe2653c98b4b9f9d1baedeeb249ef82b81286
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-10-03 14:20:06 +0900

    oxfw: common_model: add common control model for the other devices
    OXFW970 and OXFW971 are used for various models and ALSA oxfw driver
    supports known models. Many of the models don't allow application to
    configure its internal functionalities except for sampling transfer
    frequency. The models are listed below:
    
     * Behringer FCA202 (2005)
     * Mackie Onyx-i series (820i, 1220i, 1620i, 1640i, former design)
     * Tapco Link.FireWire 4x6 (2007)
     * Mackie Onyx Sattelite (2006)
     * Mackie d.2 pro (2007)
     * Mackie d.4 pro (2007)
     * Mackie U.420 (2008)
     * Mackie U.420d (2008)
     * Stanton Controllers & Systems 1 Mixer (SCS.1m, 2008)
    
    This commit adds common model which has control for sampling transfer
    frequency only. ALSA oxfw driver supports Stanton Controllers & Systems 1
    Deck (SCS.1d, 2008). The model doesn't support audio functionality and
    this commit excludes it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/oxfw/common_model.rs
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+
+use hinawa::{SndUnitExt, FwFcpExt};
+
+use crate::card_cntr;
+
+use super::common_ctl::CommonCtl;
+
+pub struct CommonModel {
+    avc: hinawa::FwFcp,
+    common_ctl: CommonCtl,
+}
+
+impl<'a> CommonModel {
+    const FCP_TIMEOUT_MS: u32 = 100;
+
+    pub fn new() -> Self {
+        CommonModel{
+            avc: hinawa::FwFcp::new(),
+            common_ctl: CommonCtl::new(),
+        }
+    }
+}
+
+impl card_cntr::CtlModel<hinawa::SndUnit> for CommonModel {
+    fn load(&mut self, unit: &hinawa::SndUnit, card_cntr: &mut card_cntr::CardCntr) -> Result<(), Error> {
+        self.avc.bind(&unit.get_node())?;
+
+        self.common_ctl.load(&self.avc, card_cntr, Self::FCP_TIMEOUT_MS)?;
+
+        Ok(())
+    }
+
+    fn read(&mut self, _: &hinawa::SndUnit, elem_id: &alsactl::ElemId, elem_value: &mut alsactl::ElemValue)
+        -> Result<bool, Error>
+    {
+        if self.common_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    fn write(&mut self, unit: &hinawa::SndUnit, elem_id: &alsactl::ElemId, _: &alsactl::ElemValue,
+             new: &alsactl::ElemValue) -> Result<bool, Error>
+    {
+        if self.common_ctl.write(unit, &self.avc, elem_id, new, Self::FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl card_cntr::NotifyModel<hinawa::SndUnit, bool> for CommonModel {
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<alsactl::ElemId>) {
+        elem_id_list.extend_from_slice(&self.common_ctl.notified_elem_list);
+    }
+
+    fn parse_notification(&mut self, _: &hinawa::SndUnit, _: &bool) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn read_notified_elem(&mut self, _: &hinawa::SndUnit, elem_id: &alsactl::ElemId, elem_value: &mut alsactl::ElemValue)
+        -> Result<bool, Error>
+    {
+        self.common_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)
+    }
+}
