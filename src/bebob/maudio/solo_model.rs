cd8c063c554aa507708f21893d372c043826811d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-10-04 15:25:52 +0900

    bebob: maudio: add implementation for mixer controls
    This commit adds controls for source of internal mixer.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/bebob/maudio/solo_model.rs
+++ src/bebob/maudio/solo_model.rs
@@ -16,13 +16,14 @@ use crate::bebob::common_ctls::ClkCtl;
 use crate::bebob::BebobAvc;
 
 use super::common_proto::FCP_TIMEOUT_MS;
-use super::normal_ctls::MeterCtl;
+use super::normal_ctls::{MeterCtl, MixerCtl};
 
 pub struct SoloModel<'a>{
     avc: BebobAvc,
     clk_ctl: ClkCtl<'a>,
     req: hinawa::FwReq,
     meter_ctl: MeterCtl<'a>,
+    mixer_ctl: MixerCtl<'a>,
 }
 
 impl<'a> SoloModel<'a> {
@@ -51,6 +52,13 @@ impl<'a> SoloModel<'a> {
         "stream-in-1", "stream-in-2", "stream-in-3", "stream-in-4",
     ];
 
+    const MIXER_DST_FB_IDS: &'a [u8] = &[0x01, 0x01];
+    const MIXER_LABELS: &'a [&'a str] = &["mixer-1/2", "mixer-3/4"];
+    const MIXER_PHYS_SRC_FB_IDS: &'a [u8] = &[0x00, 0x01];
+    const PHYS_IN_LABELS: &'a [&'a str] = &["analog-1/2", "digital-1/2"];
+    const MIXER_STREAM_SRC_FB_IDS: &'a [u8] = &[0x02, 0x03];
+    const STREAM_IN_LABELS: &'a [&'a str] = &["stream-1/2", "stream-1/2"];
+
     pub fn new() -> Self {
         SoloModel{
             avc: BebobAvc::new(),
@@ -58,6 +66,11 @@ impl<'a> SoloModel<'a> {
             clk_ctl: ClkCtl::new(&Self::CLK_DST, Self::CLK_SRCS, Self::CLK_LABELS),
             meter_ctl: MeterCtl::new(Self::IN_METER_LABELS, Self::STREAM_METER_LABELS, Self::OUT_METER_LABELS,
                                      false, 0, true),
+            mixer_ctl: MixerCtl::new(
+                Self::MIXER_DST_FB_IDS, Self::MIXER_LABELS,
+                Self::MIXER_PHYS_SRC_FB_IDS, Self::PHYS_IN_LABELS,
+                Self::MIXER_STREAM_SRC_FB_IDS, Self::STREAM_IN_LABELS,
+            ),
         }
     }
 }
@@ -72,6 +85,7 @@ impl<'a> CtlModel<hinawa::SndUnit> for SoloModel<'a> {
 
         self.clk_ctl.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.meter_ctl.load(unit, &self.avc, &self.req, card_cntr)?;
+        self.mixer_ctl.load(&self.avc, card_cntr)?;
 
         Ok(())
     }
@@ -82,6 +96,8 @@ impl<'a> CtlModel<hinawa::SndUnit> for SoloModel<'a> {
         if self.clk_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.meter_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.mixer_ctl.read(&self.avc, elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -95,6 +111,8 @@ impl<'a> CtlModel<hinawa::SndUnit> for SoloModel<'a> {
         if self.clk_ctl.write(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.meter_ctl.write(&self.avc, elem_id, old, new)? {
+            Ok(true)
+        } else if self.mixer_ctl.write(&self.avc, elem_id, old, new)? {
             Ok(true)
         } else {
             Ok(false)
