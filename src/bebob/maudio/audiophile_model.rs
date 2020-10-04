ae05e2caff09d99b80988d89db4ba7636e65f013
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-10-04 15:25:52 +0900

    bebob: maudio: add implementation for headphone controls
    This commit adds controls for headphone outputs.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/bebob/maudio/audiophile_model.rs
+++ src/bebob/maudio/audiophile_model.rs
@@ -16,7 +16,7 @@ use crate::bebob::common_ctls::ClkCtl;
 use crate::bebob::BebobAvc;
 
 use super::common_proto::FCP_TIMEOUT_MS;
-use super::normal_ctls::{MeterCtl, MixerCtl, InputCtl, AuxCtl, OutputCtl};
+use super::normal_ctls::{MeterCtl, MixerCtl, InputCtl, AuxCtl, OutputCtl, HpCtl};
 
 pub struct AudiophileModel<'a>{
     avc: BebobAvc,
@@ -27,6 +27,7 @@ pub struct AudiophileModel<'a>{
     input_ctl: InputCtl<'a>,
     aux_ctl: AuxCtl<'a>,
     output_ctl: OutputCtl<'a>,
+    hp_ctl: HpCtl<'a>,
 }
 
 impl<'a> AudiophileModel<'a> {
@@ -58,6 +59,7 @@ impl<'a> AudiophileModel<'a> {
     const PHYS_IN_LABELS: &'a [&'a str] = &["analog-1/2", "digital-1/2"];
     const MIXER_STREAM_SRC_FB_IDS: &'a [u8] = &[0x00, 0x01, 0x02];
     const STREAM_IN_LABELS: &'a [&'a str] = &["stream-1/2", "stream-3/4", "stream-5/6"];
+    const HP_SRC_LABELS: &'a [&'a str] = &["mixer-1/2", "mixer-3/4", "mixer-5/6", "aux-1/2"];
 
     const PHYS_IN_FB_IDS: &'a [u8] = &[0x04, 0x05];
     const STREAM_IN_FB_IDS: &'a [u8] = &[0x01, 0x02, 0x03];
@@ -69,6 +71,9 @@ impl<'a> AudiophileModel<'a> {
     const PHYS_OUT_LABELS: &'a [&'a str] = &["analog-1/2", "analog-3/4", "digital-1/2"];
     const PHYS_OUT_FB_IDS: &'a [u8] = &[0x0c, 0x0d, 0x0e];
     const PHYS_OUT_SRC_FB_IDS: &'a [u8] = &[0x01, 0x02, 0x03];
+
+    const HP_SRC_FB_ID: u8 = 0x04;
+    const HP_OUT_FB_ID: u8 = 0x0f;
 
     pub fn new() -> Self {
         AudiophileModel{
@@ -94,6 +99,7 @@ impl<'a> AudiophileModel<'a> {
                 Self::PHYS_OUT_FB_IDS,
                 Self::PHYS_OUT_SRC_FB_IDS,
             ),
+            hp_ctl: HpCtl::new(Self::HP_OUT_FB_ID, Self::HP_SRC_FB_ID, Self::HP_SRC_LABELS),
         }
     }
 }
@@ -112,6 +118,7 @@ impl<'a> CtlModel<hinawa::SndUnit> for AudiophileModel<'a> {
         self.input_ctl.load(&self.avc, card_cntr)?;
         self.aux_ctl.load(&self.avc, card_cntr)?;
         self.output_ctl.load(&self.avc, card_cntr)?;
+        self.hp_ctl.load(&self.avc, card_cntr)?;
 
         Ok(())
     }
@@ -130,6 +137,8 @@ impl<'a> CtlModel<hinawa::SndUnit> for AudiophileModel<'a> {
         } else if self.aux_ctl.read(&self.avc, elem_id, elem_value)? {
             Ok(true)
         } else if self.output_ctl.read(&self.avc, elem_id, elem_value)? {
+            Ok(true)
+        } else if self.hp_ctl.read(&self.avc, elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -151,6 +160,8 @@ impl<'a> CtlModel<hinawa::SndUnit> for AudiophileModel<'a> {
         } else if self.aux_ctl.write(&self.avc, elem_id, old, new)? {
             Ok(true)
         } else if self.output_ctl.write(&self.avc, elem_id, old, new)? {
+            Ok(true)
+        } else if self.hp_ctl.write(&self.avc, elem_id, old, new)? {
             Ok(true)
         } else {
             Ok(false)
