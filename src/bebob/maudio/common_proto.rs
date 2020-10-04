ec78b153c37976f34fb2ef0de368b267aa568a7a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-10-04 15:25:52 +0900

    bebob: maudio: add implementation for common transaction protocol
    All of models by M-Audio with BeBoB solution uses asynchronous read
    transaction for metering.
    
    This commit adds trait for it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/bebob/maudio/common_proto.rs
+++ src/bebob/maudio/common_proto.rs
@@ -1,3 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+
+use hinawa::{FwReqExtManual, SndUnitExt};
+
 pub const FCP_TIMEOUT_MS: u32 = 100;
+
+pub trait CommonProto: FwReqExtManual {
+    const BASE_ADDR: u64 = 0xffc700700000;
+    const METER_ADDR: u64 = 0xffc700600000;
+
+    const TIMEOUT: u32 = 100;
+
+    fn read_meters(&self, unit: &hinawa::SndUnit, frames: &mut [u8]) -> Result<(), Error> {
+        self.transaction_sync(&unit.get_node(), hinawa::FwTcode::ReadBlockRequest,
+                              Self::METER_ADDR, frames.len(), frames, Self::TIMEOUT)
+    }
+
+    fn write_block(&self, unit: &hinawa::SndUnit, offset: u64, frames: &mut [u8]) -> Result<(), Error> {
+        self.transaction_sync(&unit.get_node(), hinawa::FwTcode::WriteBlockRequest,
+                              Self::BASE_ADDR + offset, frames.len(), frames, Self::TIMEOUT)
+    }
+}
