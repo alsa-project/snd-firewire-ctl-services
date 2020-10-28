9f436dd85952966a1a188c9c92c4405245356612
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-10-28 11:45:55 +0900

    oxfw: split into oxfw crate
    The protocol to operate OXFW970/971 ASICs and vendor-specific models is
    implemented just for a service program, however it's convenient to have
    a crate for it.
    
    This commit adds a crate as 'oxfw' and push the implementation into it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/oxfw/src/apogee_ctls.rs
+++ libs/oxfw/src/apogee_ctls.rs
@@ -7,6 +7,7 @@ use alsactl::{ElemValueExt, ElemValueExtManual};
 use core::card_cntr;
 
 use ta1394::{AvcAddr, Ta1394Avc};
+
 use super::apogee_proto::{VendorCmd, ApogeeCmd, ApogeeMeterProtocol};
 
 const TIMEOUT_MS: u32 = 100;
--- libs/src/lib.rs
+++ libs/src/lib.rs
@@ -1,4 +1,3 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-pub mod oxfw;
 pub mod bebob;
