487de8b8a7a64890f48d5f1c093b67a75f44ca0b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-10 19:02:59 +0900

    alsa-ctl-tlv-codec: add trait and error structure to represent data of TLV
    The data of TLV style has three fields; type, length, and value.
    
    This commit adds a trait to retrieve the content of fields. Furthermore,
    the trait has two supertrait to support conversion for vector of u32
    element. For decoder, a structure is added to represent error.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/alsa-ctl-tlv-codec/src/lib.rs
+++ libs/alsa-ctl-tlv-codec/src/lib.rs
@@ -11,3 +11,38 @@
 
 #[allow(dead_code)]
 mod uapi;
+
+/// The error type at failure to convert from array of u32 elements to TLV data.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct InvalidTlvDataError{
+    msg: &'static str,
+}
+
+impl InvalidTlvDataError {
+    pub fn new(msg: &'static str) -> Self {
+        InvalidTlvDataError{msg}
+    }
+}
+
+impl std::fmt::Display for InvalidTlvDataError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl std::error::Error for InvalidTlvDataError {}
+
+/// The trait for common methods to data of TLV (Type-Length-Value) in ALSA control interface.
+/// The TryFrom supertrait should be implemented to parse the array of u32 elements and it
+/// can return InvalidTlvDataError at failure. The Into supertrait should be implemented as well
+/// to build the array of u32 element.
+pub trait TlvData<'a> : std::convert::TryFrom<&'a [u32]> + Into<Vec<u32>> {
+    /// Return the value of type field. It should come from UAPI of Linux kernel.
+    fn value_type(&self) -> u32;
+
+    /// Return the length of value field. It should be in byte unit and multiples of 4 as result.
+    fn value_length(&self) -> usize;
+
+    /// Generate vector with u32 element for raw data.
+    fn value(&self) -> Vec<u32>;
+}
