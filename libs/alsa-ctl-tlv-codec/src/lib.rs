c4f7b00fc36c6c6f051d29eea52f048c980eda11
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-10 19:02:59 +0900

    alsa-ctl-tlv-codec: add crate to encode/decode TLV data in ALSA control interface
    In ALSA control interface, control element set can have data in TLV
    (Type-Length-Value) style. This data is added by kernel drivers or
    userspace applications, then it's referred by the other userspace
    applications.
    
    This commit adds a crate, alsa-ctl-tlv-codec to parse and build the
    data of TLV style. The crate is purely written by Rust and no dependency
    to the other crate or software.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/alsa-ctl-tlv-codec/src/lib.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! The crate is designed to process data represented by TLV (Type-Length-Value) style in
+//! ALSA control interface. The crate produces encoder and decoder for the u32 array of data
+//! as well as structures and enumerations to represent the data.
+//!
+//! The data of TLV style is used for several purposes. As of Linux kernel 5.10, it includes
+//! information about dB representation as well as information about channel mapping in ALSA
+//! PCM substream.
