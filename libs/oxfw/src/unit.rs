aa9ea61a547de82eb49275e262304654132db1c1
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-24 14:20:21 +0900

    oxfw: Ta1394ConfigRom trait implementation to pick up model information
    This commit uses implementation of Ta1394ConfigRom trait instead of
    helper function.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/oxfw/src/unit.rs
+++ libs/oxfw/src/unit.rs
@@ -12,7 +12,12 @@ use alsactl::{CardExt, CardExtManual, ElemValueExtManual};
 use core::dispatcher;
 use core::card_cntr;
 
+use ieee1212_config_rom::ConfigRom;
+use ta1394::config_rom::Ta1394ConfigRom;
+
 use super::model::OxfwModel;
+
+use std::convert::TryFrom;
 
 enum Event {
     Shutdown,
@@ -58,11 +63,20 @@ impl<'a> OxfwUnit {
         }
 
         let node = unit.get_node();
-        let data = node.get_config_rom()?;
-        let (vendor, model) = ta1394::config_rom::parse_entries(&data).ok_or_else(|| {
-            let label = "Fail to detect information of unit";
-            Error::new(FileError::Noent, label)
-        })?;
+        let raw = node.get_config_rom()?;
+        let config_rom = ConfigRom::try_from(raw)
+            .map_err(|e| {
+                let label = format!("Malformed configuration ROM detected: {}", e);
+                Error::new(FileError::Nxio, &label)
+            })?;
+
+        let (vendor, model) = config_rom.get_vendor()
+            .and_then(|vendor| {
+                config_rom.get_model()
+                    .map(|model| (vendor, model))
+            })
+            .ok_or(Error::new(FileError::Nxio, "Configuration ROM is not for 1394TA standard"))?;
+
         let model = OxfwModel::new(vendor.vendor_id, model.model_id)?;
 
         let card_cntr = card_cntr::CardCntr::new();
