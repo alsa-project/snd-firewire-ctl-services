30c89c4b933919b2778830abc93d5eca281f7713
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-24 14:20:21 +0900

    ta1394: code refactoring to use accessor trait implementation
    This commit uses EntryDataAccess for the crate.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/ta1394/src/config_rom.rs
+++ libs/ta1394/src/config_rom.rs
@@ -19,99 +19,53 @@ pub struct UnitData {
 pub fn parse_entries(data: &[u8]) -> Option<(VendorData, UnitData)> {
     let entries = get_root_entry_list(&data);
 
-    match get_vendor_data(&entries) {
-        Some(vendor) => match get_unit_data(&entries, 0) {
-            Some(model) => Some((vendor, model)),
-            None => None,
-        }
-        None => None,
-    }
-}
-
-pub fn get_vendor_data(entries: &[Entry]) -> Option<VendorData> {
-    match detect_desc_text(entries, KeyType::Vendor) {
-        Some((vendor_id, vendor_name)) => Some(VendorData{vendor_id, vendor_name}),
-        None => None,
-    }
+    detect_desc_text(&entries, KeyType::Vendor)
+        .map(|(vendor_id, vendor_name)| VendorData{vendor_id, vendor_name})
+        .and_then(|vendor| {
+            get_unit_data(&entries, 0)
+                .map(|model| (vendor, model))
+        })
 }
 
 pub fn get_unit_data(entries: &[Entry], directory_id: u32) -> Option<UnitData> {
-    match detect_unit_directory(entries, directory_id) {
-        Some(directory) => match detect_immediate_value(directory, KeyType::SpecifierId) {
-            Some(specifier_id) => match detect_immediate_value(directory, KeyType::Version) {
-                Some(version) => match detect_desc_text(directory, KeyType::Model) {
-                    Some((model_id, model_name)) => Some(UnitData{model_id, model_name, specifier_id, version}),
-                    None => None,
-                }
-                None => None,
-            },
-            None => None,
-        },
-        None => None,
-    }
-}
-
-fn detect_immediate_value(entries: &[Entry], key: KeyType) -> Option<u32> {
-    entries.iter().find_map(|entry| {
-        if entry.key == key {
-            match entry.data {
-                EntryData::Immediate(spec_id) => Some(spec_id),
-                _ => None,
-            }
-        } else {
-            None
-        }
+    entries.iter().filter_map(|entry| {
+        EntryDataAccess::<&[Entry]>::get(entry, KeyType::Unit)
+    })
+    .nth(directory_id as usize)
+    .and_then(|entries| {
+        entries.iter().find_map(|entry| {
+            EntryDataAccess::<u32>::get(entry, KeyType::SpecifierId)
+        })
+        .and_then(|specifier_id| {
+            entries.iter().find_map(|entry| {
+                EntryDataAccess::<u32>::get(entry, KeyType::Version)
+            })
+            .and_then(|version| {
+                detect_desc_text(entries, KeyType::Model)
+                    .map(|(model_id, model_name)| {
+                        UnitData{model_id, model_name, specifier_id, version}
+                    })
+            })
+        })
     })
 }
 
-fn detect_unit_directory<'a>(entries: &'a [Entry], directory_id: u32) -> Option<&'a [Entry<'a>]> {
-    let mut count = 0;
-
-    entries.iter().find_map(|entry| {
-        if entry.key == KeyType::Unit {
-            match &entry.data {
-                EntryData::Directory(directory) => {
-                    match count == directory_id {
-                        true => Some(&directory[..]),
-                        false => {
-                            count += 1;
-                            None
-                        }
-                    }
-                },
-                _ => None,
-            }
-        } else {
-            None
-        }
-    })
-}
-
-fn detect_desc_text(entries: &[Entry], key: KeyType) -> Option<(u32, String)> {
+fn detect_desc_text(entries: &[Entry], key_type: KeyType) -> Option<(u32, String)> {
     let mut peekable = entries.iter().peekable();
 
-    while let Some(entry) = peekable.next() {
-        if entry.key != key {
-            continue;
-        }
+    while let Some(entry) = peekable.next()
+    {
+        let result = EntryDataAccess::<u32>::get(entry, key_type)
+            .and_then(|value| {
+                peekable.peek()
+                    .and_then(|&next| {
+                        EntryDataAccess::<&str>::get(next, KeyType::Descriptor)
+                            .map(|name| (value, name.to_string()))
+                    })
+            });
 
-        let next = match peekable.peek() {
-            Some(n) => {
-                if n.key == KeyType::Descriptor {
-                    n
-                } else {
-                    continue
-                }
-            },
-            None => continue,
-        };
-
-        if let EntryData::Immediate(value) = entry.data {
-            if let EntryData::Leaf(leaf) = &next.data {
-                if let Some(name) = parse_leaf_entry_as_text(&leaf) {
-                    return Some((value, name.to_string()));
-                }
-            }
+        if result.is_some() {
+            return result;
         }
     }
 
