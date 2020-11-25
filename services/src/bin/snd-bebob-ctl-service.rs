871f33c8b92245ba49f08e16350aabfe8a2ed9fa
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-25 16:19:35 +0900

    bebob: implement trait for command
    This commit implements trait for command.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- services/src/bin/snd-bebob-ctl-service.rs
+++ services/src/bin/snd-bebob-ctl-service.rs
@@ -1,44 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-use std::env;
+use snd_firewire_ctl_services::*;
 use bebob::runtime::BebobRuntime;
-use core::RuntimeOperation;
+
+struct BebobServiceCmd;
+
+impl<'a> ServiceCmd<'a, u32, BebobRuntime<'a>> for BebobServiceCmd {
+    const CMD_NAME: &'a str = "snd-bebob-ctl-service";
+    const ARGS: &'a [(&'a str, &'a str)] = &[("CARD_ID", "The numeric ID of sound card")];
+
+    fn parse_args(args: &[String]) -> Result<u32, String> {
+        parse_arg_as_u32(&args[0])
+    }
+}
 
 fn main() {
-    // Check arguments in command line.
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("At least, one argument is required for: ");
-        println!("  The numerical ID of sound card.");
-        std::process::exit(1);
-    }
-
-    let card_id = match args[1].parse::<u32>() {
-        Ok(card_id) => card_id,
-        Err(err) => {
-            println!("{:?}", err);
-            std::process::exit(1);
-        }
-    };
-
-    let err = match BebobRuntime::new(card_id) {
-        Err(err) => {
-            println!("The {} is not for BeBoB device: {}", card_id, err);
-            Err(err)
-        }
-        Ok(mut unit) => {
-            if let Err(err) = unit.listen() {
-                println!("Fail to listen events: {}", err);
-                Err(err)
-            } else {
-                unit.run()
-            }
-        }
-    };
-
-    if err.is_err() {
-        std::process::exit(1)
-    }
-
-    std::process::exit(0)
+    BebobServiceCmd::run()
 }
