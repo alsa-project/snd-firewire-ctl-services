52e5e05cf348355382122620aae938826343903c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-25 16:19:35 +0900

    motu: implement trait for command
    This commit implements trait for command.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- services/src/bin/snd-firewire-motu-ctl-service.rs
+++ services/src/bin/snd-firewire-motu-ctl-service.rs
@@ -1,55 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-use std::env;
+use snd_firewire_ctl_services::*;
 use motu::runtime::MotuRuntime;
-use core::RuntimeOperation;
 
-fn print_help() {
-    println!("
-Usage:
-  snd-firewire-motu-ctl-service CARD_ID
+struct MotuServiceCmd;
 
-  where:
-    CARD_ID: The numerical ID of sound card.
-    ");
+impl<'a> ServiceCmd<'a, u32, MotuRuntime<'a>> for MotuServiceCmd {
+    const CMD_NAME: &'a str = "snd-firewire-motu-ctl-service";
+    const ARGS: &'a [(&'a str, &'a str)] = &[("CARD_ID", "The numeric ID of sound card")];
+
+    fn parse_args(args: &[String]) -> Result<u32, String> {
+        parse_arg_as_u32(&args[0])
+    }
 }
 
 fn main() {
-    // Check arguments in command line.
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        print_help();
-        std::process::exit(libc::EXIT_FAILURE);
-    }
-
-    let card_id = match args[1].parse::<u32>() {
-        Ok(card_id) => card_id,
-        Err(err) => {
-            println!("{:?}", err);
-            print_help();
-            std::process::exit(libc::EXIT_FAILURE);
-        }
-    };
-
-    let err = match MotuRuntime::new(card_id) {
-        Err(err) => {
-            println!("The card {} is not for motu device: {}",
-                     card_id, err);
-            Err(err)
-        }
-        Ok(mut unit) => {
-            if let Err(err) = unit.listen() {
-                println!("Fail to listen events: {}", err);
-                Err(err)
-            } else {
-                unit.run()
-            }
-        }
-    };
-
-    if err.is_err() {
-        std::process::exit(libc::EXIT_FAILURE)
-    }
-
-    std::process::exit(libc::EXIT_SUCCESS)
+    MotuServiceCmd::run()
 }
