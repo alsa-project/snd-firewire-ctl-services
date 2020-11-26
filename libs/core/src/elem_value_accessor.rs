alsa-project/snd-firewire-ctl-services

commit 1ada77aa6414a17e337aa6c1e648ddbfd7585cac

core: add ElemValueAccessor trait and implement for each type of element value
At present, element value is processed by each crate without any common
code. This brings duplicated codes.

This commit adds ElemValueAccessor trait and implements it for each type
of element value. Later, the duplicated codes are replaced with the
implementation.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/core/src/elem_value_accessor.rs
@@ -1,0 +1,109 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+use glib::IsA;
+
+use alsactl::{ElemValueExt, ElemValueExtManual};
+
+pub trait ElemValueAccessor<T> : IsA<alsactl::ElemValue>
+    where T: Copy + Clone + Default + Eq + PartialEq
+{
+    fn set(&self, vals: &[T]);
+    fn get(&self, vals: &mut [T]);
+
+    fn set_vals<F>(&self, len: usize, mut cb: F)
+        -> Result<(), Error>
+        where F: FnMut(usize) -> Result<T, Error>
+    {
+        let mut vals = vec![Default::default();len];
+        vals.iter_mut().enumerate().try_for_each(|(ch, v)| {
+            *v = cb(ch)?;
+            Ok(())
+        })?;
+        self.set(&vals);
+        Ok(())
+    }
+
+    fn get_vals<F>(&self, old: &Self, len: usize, mut cb: F)
+        -> Result<(), Error>
+        where F: FnMut(usize, T) -> Result<(), Error>
+    {
+        let mut vals = vec![Default::default();len];
+        self.get(&mut vals[..len]);
+        old.get(&mut vals[len..]);
+        vals[..len].iter().zip(vals[len..].iter()).enumerate()
+            .filter(|(_, (n, o))| *n != *o)
+            .try_for_each(|(ch, (v, _))| {
+                cb(ch, *v)?;
+                Ok(())
+            })?;
+        Ok(())
+    }
+
+    fn set_val<F>(&self, mut cb: F) -> Result<(), Error>
+        where F: FnMut() -> Result<T, Error>
+    {
+        let mut vals = [Default::default()];
+        vals[0] = cb()?;
+        self.set(&vals);
+        Ok(())
+    }
+
+    fn get_val<F>(&self, mut cb: F) -> Result<(), Error>
+        where F: FnMut(T) -> Result<(), Error>
+    {
+        let mut vals = [Default::default()];
+        self.get(&mut vals);
+        cb(vals[0])
+    }
+}
+
+impl ElemValueAccessor<bool> for alsactl::ElemValue {
+    fn set(&self, vals: &[bool]) {
+        self.set_bool(vals);
+    }
+
+    fn get(&self, vals: &mut [bool]) {
+        self.get_bool(vals);
+    }
+}
+
+impl ElemValueAccessor<u8> for alsactl::ElemValue {
+    fn set(&self, vals: &[u8]) {
+        self.set_bytes(vals);
+    }
+
+    fn get(&self, vals: &mut [u8]) {
+        self.get_bytes(vals);
+    }
+}
+
+impl ElemValueAccessor<i32> for alsactl::ElemValue {
+    fn set(&self, vals: &[i32]) {
+        self.set_int(vals);
+    }
+
+    fn get(&self, vals: &mut [i32]) {
+        self.get_int(vals);
+    }
+}
+
+impl ElemValueAccessor<u32> for alsactl::ElemValue {
+    fn set(&self, vals: &[u32]) {
+        self.set_enum(vals);
+    }
+
+    fn get(&self, vals: &mut [u32]) {
+        self.get_enum(vals);
+    }
+}
+
+impl ElemValueAccessor<i64> for alsactl::ElemValue {
+    fn set(&self, vals: &[i64]) {
+        self.set_int64(vals);
+    }
+
+    fn get(&self, vals: &mut [i64]) {
+        self.get_int64(vals);
+    }
+}

libs/core/src/lib.rs
@@ -2,3 +2,4 @@
 // Copyright (c) 2020 Takashi Sakamoto
 pub mod dispatcher;
 pub mod card_cntr;
+pub mod elem_value_accessor;

commit 7918126ee72abf4204eeb7bd0691aaacb8bd4e4f

core: elem_value_accessor: fix to read and compare element value
Enough length of vactor is not allocated to store old and new element value.
This brings partial comparison between them and failure to handle write
request from applications.

This commit fixes the bug.

Fixes: 1ada77aa6414 ("core: add ElemValueAccessor trait and implement for each type of element value")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/core/src/elem_value_accessor.rs
@@ -28,7 +28,7 @@ pub trait ElemValueAccessor<T> : IsA<alsactl::ElemValue>
         -> Result<(), Error>
         where F: FnMut(usize, T) -> Result<(), Error>
     {
-        let mut vals = vec![Default::default();len];
+        let mut vals = vec![Default::default();len * 2];
         self.get(&mut vals[..len]);
         old.get(&mut vals[len..]);
         vals[..len].iter().zip(vals[len..].iter()).enumerate()