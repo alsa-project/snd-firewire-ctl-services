2facf5afb675276575eaf1df587974d904525ca9
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    tcat-protocols: add crate for various protocols for ASICs of DICE
    TC Applied Technologies (TCAT) designed ASICs of Digital Interface
    Communication Engine (DICE) for audio and music units in IEEE 1394 bus.
    The first product of the series; Dice II, were manufactured 2006 by
    Wavefront Semiconductor, formerly known as Alesis Semiconductor.
    TCD2210 (Dice Mini) and TCD2220 (Dice Jr.) were manufactored by TCAT
    2010.
    
    The ASICs were adopted to many models produced by third party vendors. TCAT
    defined general protocol to configure functionality in the ASICs. For
    TCD2210 and TCD2220, the protocol was extended. Furthermore, the vendors
    implemented own protocol for specific purposes.
    
    This commit add a crate as 'dice-protocols' to implement the protocols
    defined by TCAT as well as vendors.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Protocols defined for ASICs of Digital Interface Communication Engine (DICE).
+//!
+//! The crate includes various kind of protocols defined by TC Applied Technologies (TCAT) as
+//! well as hardware vendors for ASICs of Digital Interface Communication Engine (DICE).
