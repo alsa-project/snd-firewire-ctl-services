f817cb3852b7fcbb6ab9e2c9464708c21c96b719
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-protocols: tcat: extension: add trait and its implementation for application section of TCAT protocol extension
    In TCAT protocol extension, the range of CSR in the target unit is
    exposed as application section so that third party vendors can develop
    specific functionalities.
    
    This commit adds trait and its implementation of the section.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcat/extension/appl_section.rs
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Application section in protocol extension defined by TCAT for ASICs of DICE.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for application
+//! section in protocol extension defined by TCAT for ASICs of DICE.
+
+use super::*;
+
+pub trait ApplSectionProtocol<T> : ProtocolExtension<T>
+    where T: AsRef<FwNode>,
+{
+    fn read_appl_data(&self, node: &T, sections: &ExtensionSections, offset: usize,
+                      frames: &mut [u8], timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        ProtocolExtension::read(self, node, sections.application.offset + offset, frames, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Appl, &e.to_string()))
+    }
+
+    fn write_appl_data(&self, node: &T, sections: &ExtensionSections, offset: usize,
+                       frames: &mut [u8], timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        ProtocolExtension::write(self, node, sections.application.offset + offset, frames, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Appl, &e.to_string()))
+    }
+}
+
+impl<O: AsRef<FwReq>, T: AsRef<FwNode>> ApplSectionProtocol<T> for O {}
