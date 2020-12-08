d7ea8fabda292cd6a34711ab11292303d24e8789
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-runtime: pfire_model: add control specific to ProFire series
    All of models in M-Audio ProFire series have master knob and controller
    node can register assignment of the knob from four items.
    
    This commit adds control for the above.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/pfire_model.rs
+++ libs/dice/runtime/src/pfire_model.rs
@@ -2,15 +2,17 @@
 // Copyright (c) 2020 Takashi Sakamoto
 use glib::Error;
 
-use alsactl::{ElemId, ElemValue};
+use alsactl::{ElemId, ElemIfaceType, ElemValue, ElemValueExtManual};
 
 use hinawa::FwReq;
 use hinawa::{SndDice, SndUnitExt};
 
 use core::card_cntr::*;
+use core::elem_value_accessor::*;
 
 use dice_protocols::tcat::{*, global_section::*};
 use dice_protocols::tcat::extension::*;
+use dice_protocols::maudio::*;
 
 use super::common_ctl::*;
 use super::tcd22xx_spec::*;
@@ -25,6 +27,7 @@ pub struct PfireModel<S>
     extension_sections: ExtensionSections,
     ctl: CommonCtl,
     tcd22xx_ctl: Tcd22xxCtl<S>,
+    specific_ctl: SpecificCtl,
 }
 
 pub type Pfire2626Model = PfireModel<Pfire2626State>;
@@ -46,6 +49,7 @@ impl<S> CtlModel<SndDice> for PfireModel<S>
         self.extension_sections = self.proto.read_extension_sections(&node, TIMEOUT_MS)?;
         self.tcd22xx_ctl.load(unit, &self.proto, &self.extension_sections, &caps, &src_labels,
                           TIMEOUT_MS, card_cntr)?;
+        self.specific_ctl.load(unit, &self.proto, &self.extension_sections, TIMEOUT_MS, card_cntr)?;
 
         self.tcd22xx_ctl.cache(unit, &self.proto, &self.sections, &self.extension_sections, TIMEOUT_MS)?;
 
@@ -60,6 +64,8 @@ impl<S> CtlModel<SndDice> for PfireModel<S>
         } else if self.tcd22xx_ctl.read(unit, &self.proto, &self.extension_sections, elem_id,
                                     elem_value, TIMEOUT_MS)? {
             Ok(true)
+        } else if self.specific_ctl.read(elem_id, elem_value)? {
+            Ok(true)
         } else {
             Ok(false)
         }
@@ -72,6 +78,9 @@ impl<S> CtlModel<SndDice> for PfireModel<S>
             Ok(true)
         } else if self.tcd22xx_ctl.write(unit, &self.proto, &self.extension_sections, elem_id,
                                      old, new, TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.specific_ctl.write(unit, &self.proto, &self.extension_sections, elem_id,
+                                          old, new, TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -230,4 +239,61 @@ impl<'a> PfireClkSpec<'a> for Pfire610State {
             ClockSource::Aes1,
             ClockSource::Internal,
     ];
+}
+
+#[derive(Default, Debug)]
+struct SpecificCtl{
+    targets: [bool;KNOB_COUNT],
+}
+
+impl<'a> SpecificCtl {
+    const MASTER_KNOB_NAME: &'a str = "master-knob-target";
+
+    // MEMO: Both models support 'Output{id: DstBlkId::Ins0, count: 8}'.
+    const MASTER_KNOB_TARGET_LABELS: &'a [&'a str] = &[
+        "analog-out-1/2",
+        "analog-out-3/4",
+        "analog-out-5/6",
+        "analog-out-7/8",
+    ];
+
+    fn load(&self, unit: &SndDice, proto: &FwReq, sections: &ExtensionSections, timeout_ms: u32,
+            card_cntr: &mut CardCntr)
+        -> Result<(), Error>
+    {
+        let node = unit.get_node();
+        proto.write_knob_assign(&node, sections, &self.targets, timeout_ms)?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Card, 0, 0, Self::MASTER_KNOB_NAME, 0);
+        let _ = card_cntr.add_bool_elems(&elem_id, 1, Self::MASTER_KNOB_TARGET_LABELS.len(), true)?;
+        Ok(())
+    }
+
+    fn read(&self, elem_id: &ElemId, elem_value: &ElemValue) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            Self::MASTER_KNOB_NAME => {
+                elem_value.set_bool(&self.targets);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    fn write(&mut self, unit: &SndDice, proto: &FwReq, sections: &ExtensionSections,
+             elem_id: &ElemId, old: &ElemValue, new: &ElemValue, timeout_ms: u32)
+        -> Result<bool, Error>
+    {
+        match elem_id.get_name().as_str() {
+            Self::MASTER_KNOB_NAME => {
+                ElemValueAccessor::<bool>::get_vals(new, old, self.targets.len(), |idx, val| {
+                    self.targets[idx] = val;
+                    Ok(())
+                })?;
+                let node = unit.get_node();
+                proto.write_knob_assign(&node, sections, &self.targets, timeout_ms)?;
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
 }
