d49d55b20cdae8674f3c79261f2c163a41ea7a0c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-protocols: tcat: extension: add local trait and its implementation for stream format section of TCAT protocol extension
    In TCAT protocol extension, format of packet transferred and received by
    the target unit is represented in stream format section. The section
    includes information about the format according to the result of
    operation in cmd section. The controller node can get and set format of
    packet, then refrects the change into the target unit by operation of
    cmd section.
    
    This commit adds trait and its implementation of the section.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcat/extension/stream_format_section.rs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Stream format section in protocol extension defined by TCAT for ASICs of DICE.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for stream format
+//! section in protocol extension defined by TCAT for ASICs of DICE.
+
+use super::{*, caps_section::*, stream_format_entry::*};
+
+pub trait StreamFormatSectionProtocol<T> : ProtocolExtension<T>
+    where T: AsRef<FwNode>,
+{
+    fn read_stream_format_entries(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps,
+                                  timeout_ms: u32)
+        -> Result<(Vec<FormatEntryData>, Vec<FormatEntryData>), Error>
+    {
+        StreamFormatEntryProtocol::read_stream_format_entries(&self, node, caps, sections.stream_format.offset,
+                                                              timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::StreamFormat, &e.to_string()))
+    }
+
+    fn write_stream_format_entries(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps,
+                                   pair: &(Vec<FormatEntryData>, Vec<FormatEntryData>), timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        StreamFormatEntryProtocol::write_stream_format_entries(&self, node, caps, sections.stream_format.offset,
+                                                               pair, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::StreamFormat, &e.to_string()))
+    }
+}
+
+impl<O: AsRef<FwReq>, T: AsRef<FwNode>> StreamFormatSectionProtocol<T> for O {}
