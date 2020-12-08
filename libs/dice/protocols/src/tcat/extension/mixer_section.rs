b18583b360ce4cf764a79738c3d0e05bb085a4f0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-protocols: tcat: extension: add trait and its implementation for mixer feature of TCAT protocol extension
    In TCAT protocol extension, mixer section represents multiplexer of
    audio signal. The multiplexer has 18 source port and 16 destination port
    as a default. The coefficiency is represented by 16 bit with 2:14
    fixed-point number up to 12dB.
    
    This commit adds trait and its implementation. The routing to source
    ports and from destination ports can be configured by router section,
    implemented later.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcat/extension/mixer_section.rs
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Mixer section in protocol extension defined by TCAT for ASICs of DICE.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for mixer section
+//! in protocol extension defined by TCAT for ASICs of DICE.
+use super::{*, caps_section::*};
+
+pub trait MixerSectionProtocol<T> : ProtocolExtension<T>
+    where T: AsRef<FwNode>,
+{
+    const SATURATION_OFFSET: usize = 0x00;
+    const COEFF_OFFSET: usize = 0x04;
+
+    fn read_saturation(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps, timeout_ms: u32)
+        -> Result<Vec<bool>, Error>
+    {
+        if !caps.mixer.is_exposed {
+            Err(Error::new(ProtocolExtensionError::Mixer, "Mixer is not available"))?
+        }
+
+        let mut data = [0;4];
+        ProtocolExtension::read(self, node, sections.mixer.offset + Self::SATURATION_OFFSET, &mut data,
+                                timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Mixer, &e.to_string()))
+            .map(|_| {
+                let val = u32::from_be_bytes(data);
+                (0..caps.mixer.output_count)
+                    .map(|i| val & (1 << i) > 0)
+                    .collect::<Vec<_>>()
+            })
+    }
+
+    fn read_coef(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps,
+                 dst: usize, src: usize, timeout_ms: u32)
+        -> Result<u32, Error>
+    {
+        if !caps.mixer.is_exposed {
+            Err(Error::new(ProtocolExtensionError::Mixer, "Mixer is not available"))?
+        }
+
+        let offset = 4 * (src + dst * caps.mixer.input_count as usize);
+        let mut data = [0;4];
+        ProtocolExtension::read(self, node, sections.mixer.offset + Self::COEFF_OFFSET + offset,
+                                &mut data, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Mixer, &e.to_string()))
+            .map(|_|  u32::from_be_bytes(data))
+    }
+
+    fn write_coef(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps,
+                  dst: usize, src: usize, val: u32, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        if caps.mixer.is_readonly {
+            Err(Error::new(ProtocolExtensionError::Mixer, "Mixer is immutable"))?
+        }
+
+        let offset = 4 * (src + dst * caps.mixer.input_count as usize);
+        let mut data = [0;4];
+        data.copy_from_slice(&val.to_be_bytes());
+        ProtocolExtension::write(self, node, sections.mixer.offset + Self::COEFF_OFFSET + offset,
+                                 &mut data, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Mixer, &e.to_string()))
+    }
+}
+
+impl<O: AsRef<FwReq>, T: AsRef<FwNode>> MixerSectionProtocol<T> for O {}
