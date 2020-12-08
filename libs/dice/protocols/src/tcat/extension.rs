041e6ee9620da7e26bcf16c0b544970d770aaceb
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-protocols: tcat: extension: add local trait and its implementation for router entry of TCAT protocol extension
    In TCAT protocol extension, routing information is represented by 4
    bytes entry. It includes source block ID, destination block ID, and
    peak of audio signal in the route.
    
    This commit adds helper trait and its implementation for the router
    entries used internally.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/tcat/extension.rs
+++ libs/dice/protocols/src/tcat/extension.rs
@@ -10,6 +10,8 @@
 pub mod caps_section;
 pub mod cmd_section;
 pub mod mixer_section;
+#[doc(hidden)]
+mod router_entry;
 
 use super::*;
 
@@ -54,6 +56,7 @@ pub enum ProtocolExtensionError {
     Caps,
     Cmd,
     Mixer,
+    RouterEntry,
     Invalid(i32),
 }
 
@@ -63,6 +66,7 @@ impl std::fmt::Display for ProtocolExtensionError {
             ProtocolExtensionError::Caps => "caps",
             ProtocolExtensionError::Cmd => "command",
             ProtocolExtensionError::Mixer => "mixer",
+            ProtocolExtensionError::RouterEntry => "router-entry",
             ProtocolExtensionError::Invalid(_) => "invalid",
         };
 
@@ -80,6 +84,7 @@ impl ErrorDomain for ProtocolExtensionError {
             ProtocolExtensionError::Caps => 0,
             ProtocolExtensionError::Cmd => 1,
             ProtocolExtensionError::Mixer => 2,
+            ProtocolExtensionError::RouterEntry => 3,
             ProtocolExtensionError::Invalid(v) => v,
         }
     }
@@ -89,6 +94,7 @@ impl ErrorDomain for ProtocolExtensionError {
             0 => ProtocolExtensionError::Caps,
             1 => ProtocolExtensionError::Cmd,
             2 => ProtocolExtensionError::Mixer,
+            3 => ProtocolExtensionError::RouterEntry,
             _ => ProtocolExtensionError::Invalid(code),
         };
         Some(enumeration)
@@ -119,10 +125,219 @@ pub trait ProtocolExtension<T: AsRef<FwNode>> : GeneralProtocol<T> {
 
 impl<O: AsRef<FwReq>, T: AsRef<FwNode>> ProtocolExtension<T> for O {}
 
+/// The enumeration to represent ID of destination block.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum DstBlkId {
+    Aes,
+    Adat,
+    MixerTx0,
+    MixerTx1,
+    Ins0,
+    Ins1,
+    ArmApbAudio,
+    Avs0,
+    Avs1,
+    Reserved(u8),
+}
+
+impl Default for DstBlkId {
+    fn default() -> Self {
+        DstBlkId::Reserved(0xff)
+    }
+}
+
+impl From<u8> for DstBlkId {
+    fn from(val: u8) -> Self {
+        match val {
+            0 => Self::Aes,
+            1 => Self::Adat,
+            2 => Self::MixerTx0,
+            3 => Self::MixerTx1,
+            4 => Self::Ins0,
+            5 => Self::Ins1,
+            10 => Self::ArmApbAudio,
+            11 => Self::Avs0,
+            12 => Self::Avs1,
+            _ => Self::Reserved(val),
+        }
+    }
+}
+
+impl From<DstBlkId> for u8 {
+    fn from(id: DstBlkId) -> Self {
+        match id {
+            DstBlkId::Aes => 0,
+            DstBlkId::Adat => 1,
+            DstBlkId::MixerTx0 => 2,
+            DstBlkId::MixerTx1 => 3,
+            DstBlkId::Ins0 => 4,
+            DstBlkId::Ins1 => 5,
+            DstBlkId::ArmApbAudio => 10,
+            DstBlkId::Avs0 => 11,
+            DstBlkId::Avs1 => 12,
+            DstBlkId::Reserved(val) => val,
+        }
+    }
+}
+
+/// The structure to represent destination block.
+#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+pub struct DstBlk {
+    pub id: DstBlkId,
+    pub ch: u8,
+}
+
+impl DstBlk {
+    pub const ID_MASK: u8 = 0xf0;
+    pub const ID_SHIFT: usize = 4;
+    pub const CH_MASK: u8 = 0x0f;
+    pub const CH_SHIFT: usize = 0;
+}
+
+impl From<u8> for DstBlk {
+    fn from(val: u8) -> Self {
+        DstBlk {
+            id: DstBlkId::from((val & DstBlk::ID_MASK) >> DstBlk::ID_SHIFT),
+            ch: (val & DstBlk::CH_MASK) >> DstBlk::CH_SHIFT,
+        }
+    }
+}
+
+impl From<DstBlk> for u8 {
+    fn from(blk: DstBlk) -> Self {
+        (u8::from(blk.id) << DstBlk::ID_SHIFT) | blk.ch
+    }
+}
+
+/// The enumeration to represent ID of source block.
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum SrcBlkId {
+    Aes,
+    Adat,
+    Mixer,
+    Ins0,
+    Ins1,
+    ArmAprAudio,
+    Avs0,
+    Avs1,
+    Mute,
+    Reserved(u8),
+}
+
+impl Default for SrcBlkId {
+    fn default() -> Self {
+        SrcBlkId::Reserved(0xff)
+    }
+}
+
+impl From<u8> for SrcBlkId {
+    fn from(val: u8) -> Self {
+        match val {
+            0 => Self::Aes,
+            1 => Self::Adat,
+            2 => Self::Mixer,
+            4 => Self::Ins0,
+            5 => Self::Ins1,
+            10 => Self::ArmAprAudio,
+            11 => Self::Avs0,
+            12 => Self::Avs1,
+            15 => Self::Mute,
+            _ => Self::Reserved(val),
+        }
+    }
+}
+
+impl From<SrcBlkId> for u8 {
+    fn from(id: SrcBlkId) -> Self {
+        match id {
+            SrcBlkId::Aes => 0,
+            SrcBlkId::Adat => 1,
+            SrcBlkId::Mixer => 2,
+            SrcBlkId::Ins0 => 4,
+            SrcBlkId::Ins1 => 5,
+            SrcBlkId::ArmAprAudio => 10,
+            SrcBlkId::Avs0 => 11,
+            SrcBlkId::Avs1 => 12,
+            SrcBlkId::Mute => 15,
+            SrcBlkId::Reserved(val) => val,
+        }
+    }
+}
+
+/// The structure to represent source block.
+#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+pub struct SrcBlk {
+    pub id: SrcBlkId,
+    pub ch: u8,
+}
+
+impl SrcBlk {
+    pub const ID_MASK: u8 = 0xf0;
+    pub const ID_SHIFT: usize = 4;
+    pub const CH_MASK: u8 = 0x0f;
+    pub const CH_SHIFT: usize = 0;
+}
+
+impl From<u8> for SrcBlk {
+    fn from(val: u8) -> Self {
+        SrcBlk {
+            id: SrcBlkId::from((val & SrcBlk::ID_MASK) >> SrcBlk::ID_SHIFT),
+            ch: (val & SrcBlk::CH_MASK) >> SrcBlk::CH_SHIFT,
+        }
+    }
+}
+
+impl From<SrcBlk> for u8 {
+    fn from(blk: SrcBlk) -> Self {
+        (u8::from(blk.id) << SrcBlk::ID_SHIFT) | blk.ch
+    }
+}
+
+/// The alternative type of data for router entry.
+pub type RouterEntryData = [u8;RouterEntry::SIZE];
+
+/// The structure to represent entry of route.
+#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+pub struct RouterEntry {
+    pub dst: DstBlk,
+    pub src: SrcBlk,
+    pub peak: u16,
+}
+
+impl RouterEntry {
+    const SIZE: usize = 4;
+
+    pub const SRC_OFFSET: usize = 2;
+    pub const DST_OFFSET: usize = 3;
+}
+
+impl From<&RouterEntryData> for RouterEntry {
+    fn from(raw: &RouterEntryData) -> Self {
+        let mut doublet = [0;2];
+        doublet.copy_from_slice(&raw[..2]);
+        RouterEntry {
+            dst: raw[Self::DST_OFFSET].into(),
+            src: raw[Self::SRC_OFFSET].into(),
+            peak: u16::from_be_bytes(doublet),
+        }
+    }
+}
+
+impl From<&RouterEntry> for RouterEntryData {
+    fn from(entry: &RouterEntry) -> RouterEntryData {
+        let mut raw = RouterEntryData::default();
+        raw.copy_from_slice(&entry.peak.to_be_bytes());
+        raw[RouterEntry::SRC_OFFSET] = entry.src.into();
+        raw[RouterEntry::DST_OFFSET] = entry.dst.into();
+        raw
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::Section;
     use super::ExtensionSections;
+    use super::{DstBlk, SrcBlk, DstBlkId, SrcBlkId};
 
     #[test]
     fn section_from() {
@@ -146,5 +361,23 @@ mod test {
             application: Section{offset: 0x04, size: 0x00},
         };
         assert_eq!(space, ExtensionSections::from(&raw[..]));
+    }
+
+    #[test]
+    fn dst_blk_from() {
+        let blk = DstBlk {
+            id: DstBlkId::ArmApbAudio,
+            ch: 0x04,
+        };
+        assert_eq!(blk, DstBlk::from(u8::from(blk)));
+    }
+
+    #[test]
+    fn src_blk_from() {
+        let blk = SrcBlk {
+            id: SrcBlkId::ArmAprAudio,
+            ch: 0x04,
+        };
+        assert_eq!(blk, SrcBlk::from(u8::from(blk)));
     }
 }
---
+++ libs/dice/protocols/src/tcat/extension/router_entry.rs
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+use super::{*, caps_section::*};
+
+pub trait RouterEntryProtocol<T> : ProtocolExtension<T>
+    where T: AsRef<FwNode>,
+{
+    fn read_router_entries(&self, node: &T, caps: &ExtensionCaps, offset: usize,
+                           entry_count: usize, timeout_ms: u32)
+        -> Result<Vec<RouterEntryData>, Error>
+    {
+        if entry_count > caps.router.maximum_entry_count as usize {
+            let msg = format!("Invalid entries to read: {} but greater than {}",
+                              entry_count, caps.router.maximum_entry_count);
+            Err(Error::new(ProtocolExtensionError::RouterEntry, &msg))?
+        }
+
+        let mut data = vec![0;entry_count * RouterEntry::SIZE];
+        ProtocolExtension::read(self, node, offset, &mut data, timeout_ms)?;
+
+        let entries = (0..entry_count)
+            .map(|i| {
+                let mut raw = RouterEntryData::default();
+                let pos = i * RouterEntry::SIZE;
+                raw.copy_from_slice(&data[pos..(pos + RouterEntry::SIZE)]);
+                raw
+            })
+            .collect::<Vec<_>>();
+
+        Ok(entries)
+    }
+
+    fn write_router_entries(&self, node: &T, caps: &ExtensionCaps, offset: usize,
+                            entries: &Vec<RouterEntryData>, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        if entries.len() > caps.router.maximum_entry_count as usize {
+            let msg = format!("Invalid number of entries to read: {} but greater than {}",
+                              entries.len(), caps.router.maximum_entry_count * 4);
+            Err(Error::new(ProtocolExtensionError::RouterEntry, &msg))?
+        }
+
+        let mut data = [0;4];
+        data.copy_from_slice(&(entries.len() as u32).to_be_bytes());
+        ProtocolExtension::write(self, node, offset, &mut data, timeout_ms)?;
+
+        let mut data = Vec::new();
+        entries.iter().for_each(|entry| {
+            data.extend_from_slice(entry);
+        });
+        ProtocolExtension::write(self, node, offset + 4, &mut data, timeout_ms)
+    }
+}
+
+impl<O: AsRef<FwReq>, T: AsRef<FwNode>> RouterEntryProtocol<T> for O {}
