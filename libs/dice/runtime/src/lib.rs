29c9055deacdc6c33a052a6dd681f59117a894d7
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-runtime: add runtime structure and trait implementation
    This commit implements RuntimeOperation trait for runtime service to
    models based on ASICs of DICE.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/lib.rs
+++ libs/dice/runtime/src/lib.rs
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
+use glib::Error;
+
+use core::RuntimeOperation;
+
+pub struct DiceRuntime;
+
+impl RuntimeOperation<u32> for DiceRuntime {
+    fn new(_: u32) -> Result<Self, Error> {
+        Ok(DiceRuntime{})
+    }
+
+    fn listen(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn run(&mut self) -> Result<(), Error> {
+        Ok(())
+    }
+}
