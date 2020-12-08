d2cf39400036272b3fc37e2f8a41b802ee5061d6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-08 16:31:06 +0900

    dice-protocols: tcat: extension: add trait and its implementation for router space of TCAT protocol extension
    In TCAT protocol extension, router entries are represented in router
    section. This section includes current routing information. Controller
    node can change arbitrary entry in the section, then refrect the change
    into the target unit by operating in cmd section.
    
    This commit adds trait and its implementation of the section.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcat/extension/router_section.rs
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Router section in protocol extension defined by TCAT for ASICs of DICE.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for router section
+//! in protocol extension defined by TCAT for ASICs of DICE.
+
+use super::{*, caps_section::*, router_entry::*};
+
+pub trait RouterSectionProtocol<T> : ProtocolExtension<T>
+    where T: AsRef<FwNode>,
+{
+    fn read_router_entries(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps,
+                           timeout_ms: u32)
+        -> Result<Vec<RouterEntryData>, Error>
+    {
+        let mut data = [0;4];
+        ProtocolExtension::read(self, node, sections.router.offset, &mut data, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Router, &e.to_string()))?;
+
+        let entry_count = std::cmp::min(u32::from_be_bytes(data) as usize,
+                                        caps.router.maximum_entry_count as usize);
+        RouterEntryProtocol::read_router_entries(&self, node, caps, sections.router.offset + 4,
+                                                 entry_count, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Router, &e.to_string()))
+    }
+
+    fn write_router_entries(&self, node: &T, sections: &ExtensionSections, caps: &ExtensionCaps,
+                            entries: &Vec<RouterEntryData>, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        RouterEntryProtocol::write_router_entries(&self, node, caps, sections.router.offset,
+                                                  entries, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Router, &e.to_string()))
+    }
+}
+
+impl<O: AsRef<FwReq>, T: AsRef<FwNode>> RouterSectionProtocol<T> for O {}
