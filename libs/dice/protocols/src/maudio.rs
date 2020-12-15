3cf06140fa9b5f082a5916fef2ce8a978a959a59
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-12-15 17:52:16 +0900

    dice-protocols: maudio: add read method for master knob assignment
    In M-Audio ProFire series, the first quadlet in application section of
    TCAT extension protocol is used for assignment of master knob. Although
    the corresponding register is available to read and write transaction,
    current protocol impelementation has no method for read transaction.
    
    This commit adds the method.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/maudio.rs
+++ libs/dice/protocols/src/maudio.rs
@@ -19,6 +19,20 @@ pub trait MaudioPfireApplProtocol<T> : ApplSectionProtocol<T>
 {
     const KNOB_ASSIGN_OFFSET: usize = 0x00;
 
+    fn read_knob_assign(&self, node: &T, sections: &ExtensionSections, targets: &mut [bool;KNOB_COUNT],
+                        timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        let mut data = [0;4];
+        self.read_appl_data(node, sections, Self::KNOB_ASSIGN_OFFSET, &mut data, timeout_ms)
+            .map(|_| {
+                let val = u32::from_be_bytes(data);
+                targets.iter_mut()
+                    .enumerate()
+                    .for_each(|(i, v)| *v = val & (1 << i) > 0)
+            })
+    }
+
     fn write_knob_assign(&self, node: &T, sections: &ExtensionSections,
                          targets: &[bool;KNOB_COUNT], timeout_ms: u32)
         -> Result<(), Error>
