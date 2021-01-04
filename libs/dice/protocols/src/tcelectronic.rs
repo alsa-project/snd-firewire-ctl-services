cd4dc11449d7df41a7d754ac602f541f4669fcb4
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: add structure and trait for segment in protocol for Konnekt series
    The memory space of models in Konnekt series consists of several
    segments which has sub-effect to control hardware by write transaction.
    In the segment, quadlet-aligned data represents reach configuration.
    Current configuration is available by read transaction to the segment.
    
    This commit adds generic structure to represent the segment as well as
    trait implementation to read/write content of the segment.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcelectronic.rs
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Protocol specific to TC Electronic Konnekt series.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for protocol
+//! defined by TC Electronic for Konnekt series.
+//!
+//! In Konnekt series, the accessible memory space is separated to several segments.
+
+use glib::Error;
+
+use hinawa::FwNode;
+
+use super::tcat::*;
+
+pub trait TcKonnektSegmentData : Default {
+    fn build(&self, raw: &mut [u8]);
+    fn parse(&mut self, raw: &[u8]);
+}
+
+/// The trait to represent specification of segment.
+pub trait TcKonnektSegmentSpec {
+    const OFFSET: usize;
+    const SIZE: usize;
+}
+
+/// The structure to represent segment.
+#[derive(Debug)]
+pub struct TcKonnektSegment<U>
+    where U: TcKonnektSegmentData,
+{
+    pub data: U,
+    raw: Vec<u8>,
+}
+
+impl<U> Default for TcKonnektSegment<U>
+    where U: TcKonnektSegmentData,
+          TcKonnektSegment<U>: TcKonnektSegmentSpec,
+{
+    fn default() -> Self {
+        TcKonnektSegment{
+            data: Default::default(),
+            raw: vec![0;Self::SIZE],
+        }
+    }
+}
+
+/// The trait to represent protocol for segment.
+pub trait TcKonnektSegmentProtocol<T, U> : GeneralProtocol<T>
+    where T: AsRef<FwNode>,
+          U: TcKonnektSegmentData,
+          TcKonnektSegment<U>: TcKonnektSegmentSpec,
+{
+    const BASE_OFFSET: usize = 0x00a01000;
+
+    fn read_segment(&self, node: &T, segment: &mut TcKonnektSegment<U>, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        assert_eq!(segment.raw.len(), TcKonnektSegment::<U>::SIZE, "Programming error...");
+
+        self.read(node, Self::BASE_OFFSET + TcKonnektSegment::<U>::OFFSET, &mut segment.raw, timeout_ms)
+            .map(|_| segment.data.parse(&segment.raw))
+    }
+
+    fn write_segment(&self, node: &T, segment: &mut TcKonnektSegment<U>, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        assert_eq!(segment.raw.len(), TcKonnektSegment::<U>::SIZE, "Programming error...");
+
+        let mut raw = segment.raw.clone();
+        segment.data.build(&mut raw);
+
+        (0..(raw.len() / 4))
+            .map(|i| i * 4)
+            .try_for_each(|pos| {
+                if raw[pos..(pos + 4)] != segment.raw[pos..(pos + 4)] {
+                    self.write(node, Self::BASE_OFFSET + TcKonnektSegment::<U>::OFFSET + pos,
+                               &mut raw[pos..(pos + 4)], timeout_ms)
+                        .map(|_| segment.raw[pos..(pos + 4)].copy_from_slice(&raw[pos..(pos + 4)]))
+                } else {
+                    Ok(())
+                }
+            })
+    }
+}
+
+impl<O, T, U> TcKonnektSegmentProtocol<T, U> for O
+    where O: GeneralProtocol<T>,
+          T: AsRef<FwNode>,
+          U: TcKonnektSegmentData,
+          TcKonnektSegment<U>: TcKonnektSegmentSpec,
+{}
