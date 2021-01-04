0110214897de1b6b7c49af095d157b28cef6e0c6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: add trait implementation for mixer segment of Desktop Konnekt 6
    Desktop Konnekt 6 has internal multiplexer with three mode of inputs.
    Each mode takes a pair of inputs to multiplex signals to a pair of
    output. The relevant configuration is in a segment.
    
    This commit implements the segment.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/tcelectronic/desktop.rs
+++ libs/dice/protocols/src/tcelectronic/desktop.rs
@@ -10,15 +10,133 @@ use crate::*;
 
 use crate::tcelectronic::{*, fw_led::*};
 
+const DESKTOP_MIXER_STATE_NOTIFY_FLAG: u32 = 0x00040000;
 const DESKTOP_PANEL_NOTIFY_FLAG: u32 = 0x00080000;
 
 /// The structure to represent segments in memory space of Desktop Konnekt 6.
 #[derive(Default, Debug)]
 pub struct DesktopSegments{
+    /// Segment for state of mixer. 0x00b8..0x0367 (172 quads).
+    pub mixer: TcKonnektSegment<DesktopMixerState>,
     /// Segment for panel. 0x2008..0x2047 (15 quads).
     pub panel: TcKonnektSegment<DesktopPanel>,
     /// Segment for meter. 0x20e4..0x213f (23 quads).
     pub meter: TcKonnektSegment<DesktopMeter>,
+}
+
+/// The enumeration to represent source of headphone.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum DesktopHpSrc {
+    Stream23,
+    Mixer01,
+}
+
+impl Default for DesktopHpSrc {
+    fn default() -> Self {
+        Self::Stream23
+    }
+}
+
+impl From<u32> for DesktopHpSrc {
+    fn from(val: u32) -> Self {
+        match val {
+            0x05 => Self::Stream23,
+            _ => Self::Mixer01,
+        }
+    }
+}
+
+impl From<DesktopHpSrc> for u32 {
+    fn from(src: DesktopHpSrc) -> Self {
+        match src {
+            DesktopHpSrc::Stream23 => 0x05,
+            DesktopHpSrc::Mixer01 => 0x0b,
+        }
+    }
+}
+
+/// The structure to represent state of mixer.
+#[derive(Default, Debug)]
+pub struct DesktopMixerState{
+    /// The input level for ch 0 and 1. -1000..0 (-94.0..0.0 dB)
+    pub mic_inst_level: [i32;2],
+    /// The LR balance for ch 0 and 1. -50..50.
+    pub mic_inst_pan: [i32;2],
+    /// The level to send for ch 0 and 1. -1000..0 (-94.0..0.0 dB)
+    pub mic_inst_send: [i32;2],
+    /// The input level for ch 0 and 1. -1000..0 (-94.0..0.0 dB)
+    pub dual_inst_level: [i32;2],
+    /// The LR balance for ch 0 and 1. -50..50.
+    pub dual_inst_pan: [i32;2],
+    /// The level to send for ch 0 and 1. -1000..0 (-94.0..0.0 dB)
+    pub dual_inst_send: [i32;2],
+    /// The input level for both channels. -1000..0 (-94.0..0.0 dB)
+    pub stereo_in_level: i32,
+    /// The LR balance for both channels. -50..50.
+    pub stereo_in_pan: i32,
+    /// The level to send for both channels. -1000..0 (-94.0..0.0 dB)
+    pub stereo_in_send: i32,
+    /// The source of headphone.
+    pub hp_src: DesktopHpSrc,
+}
+
+impl DesktopMixerState {
+    const SIZE: usize = 688;
+}
+
+impl TcKonnektSegmentData for DesktopMixerState {
+    fn build(&self, raw: &mut [u8]) {
+        self.mic_inst_level[0].build_quadlet(&mut raw[12..16]);
+        self.mic_inst_pan[0].build_quadlet(&mut raw[16..20]);
+        self.mic_inst_send[0].build_quadlet(&mut raw[20..24]);
+        self.mic_inst_level[1].build_quadlet(&mut raw[28..32]);
+        self.mic_inst_pan[1].build_quadlet(&mut raw[32..36]);
+        self.mic_inst_send[1].build_quadlet(&mut raw[40..44]);
+
+        self.dual_inst_level[0].build_quadlet(&mut raw[228..232]);
+        self.dual_inst_pan[0].build_quadlet(&mut raw[232..236]);
+        self.dual_inst_send[0].build_quadlet(&mut raw[240..244]);
+        self.dual_inst_level[1].build_quadlet(&mut raw[248..252]);
+        self.dual_inst_pan[1].build_quadlet(&mut raw[252..256]);
+        self.dual_inst_send[1].build_quadlet(&mut raw[260..264]);
+
+        self.stereo_in_level.build_quadlet(&mut raw[444..448]);
+        self.stereo_in_pan.build_quadlet(&mut raw[448..452]);
+        self.stereo_in_send.build_quadlet(&mut raw[452..456]);
+
+        self.hp_src.build_quadlet(&mut raw[648..652]);
+    }
+
+    fn parse(&mut self, raw: &[u8]) {
+        self.mic_inst_level[0].parse_quadlet(&raw[12..16]);
+        self.mic_inst_pan[0].parse_quadlet(&raw[16..20]);
+        self.mic_inst_send[0].parse_quadlet(&raw[20..24]);
+        self.mic_inst_level[1].parse_quadlet(&raw[28..32]);
+        self.mic_inst_pan[1].parse_quadlet(&raw[32..36]);
+        self.mic_inst_send[1].parse_quadlet(&raw[40..44]);
+
+        self.dual_inst_level[0].parse_quadlet(&raw[228..232]);
+        self.dual_inst_pan[0].parse_quadlet(&raw[232..236]);
+        self.dual_inst_send[0].parse_quadlet(&raw[240..244]);
+        self.dual_inst_level[1].parse_quadlet(&raw[248..252]);
+        self.dual_inst_pan[1].parse_quadlet(&raw[252..256]);
+        self.dual_inst_send[1].parse_quadlet(&raw[260..264]);
+
+        self.stereo_in_level.parse_quadlet(&raw[444..448]);
+        self.stereo_in_pan.parse_quadlet(&raw[448..452]);
+        self.stereo_in_send.parse_quadlet(&raw[452..456]);
+
+        self.hp_src.parse_quadlet(&raw[648..652]);
+    }
+}
+
+impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopMixerState> {
+    const OFFSET: usize = 0x00b8;
+    const SIZE: usize = DesktopMixerState::SIZE;
+}
+
+impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<DesktopMixerState> {
+    const NOTIFY_FLAG: u32 = DESKTOP_MIXER_STATE_NOTIFY_FLAG;
 }
 
 #[derive(Default, Debug)]
