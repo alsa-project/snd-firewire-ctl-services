afc9b7be372fa314a50f22617b46f2b228ea372c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: add common structure in configuration segment of Konnekt series
    The 'shell' type of models in Konnekt series commonly have segment for
    general configuration. Although it largely differs depending on model,
    some common structure locates such as standalone configuration.
    
    This commit adds such structures.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcelectronic/standalone.rs
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Protocol defined by TC Electronic for Konnekt series.
+//!
+//! The module includes enumeration for data of standalone configuration defined by TC Electronic
+//! for Konnekt series.
+
+/// The enumeration to represent available rate for sampling clock in standalone mode.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum TcKonnektStandaloneClkRate{
+    R44100,
+    R48000,
+    R88200,
+    R96000,
+}
+
+impl Default for TcKonnektStandaloneClkRate {
+    fn default() -> Self {
+        Self::R44100
+    }
+}
+
+impl From<TcKonnektStandaloneClkRate> for u32 {
+    fn from(clk: TcKonnektStandaloneClkRate) -> Self {
+        match clk {
+            TcKonnektStandaloneClkRate::R96000 => 4,
+            TcKonnektStandaloneClkRate::R88200 => 3,
+            TcKonnektStandaloneClkRate::R48000 => 2,
+            TcKonnektStandaloneClkRate::R44100 => 1,
+        }
+    }
+}
+
+impl From<u32> for TcKonnektStandaloneClkRate {
+    fn from(val: u32) -> Self {
+        match val {
+            4 => TcKonnektStandaloneClkRate::R96000,
+            3 => TcKonnektStandaloneClkRate::R88200,
+            2 => TcKonnektStandaloneClkRate::R48000,
+            _ => TcKonnektStandaloneClkRate::R44100,
+        }
+    }
+}
