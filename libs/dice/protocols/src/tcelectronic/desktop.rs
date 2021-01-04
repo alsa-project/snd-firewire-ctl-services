0ff683a02ce1ce5f232a04591105385d8e94fd51
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: add trait implementation for meters of Desktop Konnekt 6
    Desktop Konnekt 6 has meter segment so that software can retrieve
    metering information.
    
    This commit implements the segment.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/tcelectronic/desktop.rs
+++ libs/dice/protocols/src/tcelectronic/desktop.rs
@@ -6,6 +6,43 @@
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by TC Electronic for Desktop Konnekt 6.
 
+use crate::*;
+
+use crate::tcelectronic::*;
+
 /// The structure to represent segments in memory space of Desktop Konnekt 6.
 #[derive(Default, Debug)]
-pub struct DesktopSegments;
+pub struct DesktopSegments{
+    /// Segment for meter. 0x20e4..0x213f (23 quads).
+    pub meter: TcKonnektSegment<DesktopMeter>,
+}
+
+#[derive(Default, Debug)]
+pub struct DesktopMeter{
+    pub analog_inputs: [i32;2],
+    pub mixer_outputs: [i32;2],
+    pub stream_inputs: [i32;2],
+}
+
+impl DesktopMeter {
+    const SIZE: usize = 92;
+}
+
+impl TcKonnektSegmentData for DesktopMeter {
+    fn build(&self, raw: &mut [u8]) {
+        self.analog_inputs.build_quadlet_block(&mut raw[..8]);
+        self.mixer_outputs.build_quadlet_block(&mut raw[40..48]);
+        self.stream_inputs.build_quadlet_block(&mut raw[48..56]);
+    }
+
+    fn parse(&mut self, raw: &[u8]) {
+        self.analog_inputs.parse_quadlet_block(&raw[..8]);
+        self.mixer_outputs.parse_quadlet_block(&raw[40..48]);
+        self.stream_inputs.parse_quadlet_block(&raw[48..56]);
+    }
+}
+
+impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopMeter> {
+    const OFFSET: usize = 0x20e4;
+    const SIZE: usize = DesktopMeter::SIZE;
+}
