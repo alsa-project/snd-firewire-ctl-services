947c085540100339e95dedf3caa59fd9fdf4dcf5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: shell/studio: add implementation for ch strip effect
    Konnekt 24d, Konnekt Live, and Impact Twin has channel strip effect for
    2 channels. Studio Konnekt 48 has the effect but for 4 channels.
    
    This commit implements segments of channel strip effect for the above
    models.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/tcelectronic/shell/itwin.rs
+++ libs/dice/protocols/src/tcelectronic/shell/itwin.rs
@@ -6,6 +6,78 @@
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by TC Electronic for Impact Twin.
 
+use super::*;
+use crate::tcelectronic::{*, ch_strip::*};
+
 /// The structure to represent segments in memory space of Impact Twin.
 #[derive(Default, Debug)]
-pub struct ItwinSegments;
+pub struct ItwinSegments{
+    /// Segment for states of channel strip effect. 0x0288..0x03ab (73 quads).
+    pub ch_strip_state: TcKonnektSegment<ItwinChStripStates>,
+    /// Segment for meters of channel strip effect. 0x10e0..0x111b (15 quads).
+    pub ch_strip_meter: TcKonnektSegment<ItwinChStripMeters>,
+}
+
+#[derive(Default, Debug)]
+pub struct ItwinChStripStates([ChStripState;SHELL_CH_STRIP_COUNT]);
+
+impl AsRef<[ChStripState]> for ItwinChStripStates {
+    fn as_ref(&self) -> &[ChStripState] {
+        &self.0
+    }
+}
+
+impl AsMut<[ChStripState]> for ItwinChStripStates {
+    fn as_mut(&mut self) -> &mut [ChStripState] {
+        &mut self.0
+    }
+}
+
+impl TcKonnektSegmentData for ItwinChStripStates {
+    fn build(&self, raw: &mut [u8]) {
+        self.0.build(raw)
+    }
+
+    fn parse(&mut self, raw: &[u8]) {
+        self.0.parse(raw)
+    }
+}
+
+impl TcKonnektSegmentSpec for TcKonnektSegment<ItwinChStripStates> {
+    const OFFSET: usize = 0x0288;
+    const SIZE: usize = ChStripState::SIZE * SHELL_CH_STRIP_COUNT + 4;
+}
+
+impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<ItwinChStripStates> {
+    const NOTIFY_FLAG: u32 = SHELL_CH_STRIP_NOTIFY_FLAG;
+}
+
+#[derive(Default, Debug)]
+pub struct ItwinChStripMeters([ChStripMeter;SHELL_CH_STRIP_COUNT]);
+
+impl AsRef<[ChStripMeter]> for ItwinChStripMeters {
+    fn as_ref(&self) -> &[ChStripMeter] {
+        &self.0
+    }
+}
+
+impl AsMut<[ChStripMeter]> for ItwinChStripMeters {
+    fn as_mut(&mut self) -> &mut [ChStripMeter] {
+        &mut self.0
+    }
+}
+
+impl TcKonnektSegmentData for ItwinChStripMeters {
+    fn build(&self, raw: &mut [u8]) {
+        self.0.build(raw)
+    }
+
+    fn parse(&mut self, raw: &[u8]) {
+        self.0.parse(raw)
+    }
+}
+
+impl TcKonnektSegmentSpec for TcKonnektSegment<ItwinChStripMeters> {
+    const OFFSET: usize = 0x10e0;
+    const SIZE: usize = ChStripMeter::SIZE * SHELL_CH_STRIP_COUNT + 4;
+}
