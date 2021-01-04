4e0d1b2cf38f9d52311613897a91ac270bcce429
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: studio: add module for protocol of TC Electronic Studio Konnekt 48
    Studio Konnekt 48 has unique features than the other models of Konnekt
    series.
    
    This commit adds module to implement protocol for the model, named as
    'studio'.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dice/protocols/src/tcelectronic/studio.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2020 Takashi Sakamoto
+
+//! Protocol defined by TC Electronic for Studio Konnekt 48.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for protocol
+//! defined by TC Electronic for Studio Konnekt 48.
+
+/// The structure to represent segments in memory space of Studio Konnekt 48.
+#[derive(Default, Debug)]
+pub struct StudioSegments;
