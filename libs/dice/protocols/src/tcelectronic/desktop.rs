38b41197e397fc03fa3d40ceececd98f948deab7
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-04 17:10:50 +0900

    dice-protocols: tcelectronic: add trait implementation for panel segment of Desktop Konnekt 6
    Desktop Konnekt 6 has some buttons in its panel. A segment represents
    current state of the items.
    
    This commit implements the segment.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/tcelectronic/desktop.rs
+++ libs/dice/protocols/src/tcelectronic/desktop.rs
@@ -10,6 +10,7 @@ use crate::*;
 
 use crate::tcelectronic::{*, fw_led::*, standalone::*};
 
+const DESKTOP_HW_STATE_NOTIFY_FLAG: u32 = 0x00010000;
 const DESKTOP_CONFIG_NOTIFY_FLAG: u32 = 0x00020000;
 const DESKTOP_MIXER_STATE_NOTIFY_FLAG: u32 = 0x00040000;
 const DESKTOP_PANEL_NOTIFY_FLAG: u32 = 0x00080000;
@@ -17,6 +18,8 @@ const DESKTOP_PANEL_NOTIFY_FLAG: u32 = 0x00080000;
 /// The structure to represent segments in memory space of Desktop Konnekt 6.
 #[derive(Default, Debug)]
 pub struct DesktopSegments{
+    /// Segment for panel. 0x0008..0x0097 (36 quads).
+    pub hw_state: TcKonnektSegment<DesktopHwState>,
     /// Segment for configuration. 0x0098..0x00b7 (8 quads).
     pub config: TcKonnektSegment<DesktopConfig>,
     /// Segment for state of mixer. 0x00b8..0x0367 (172 quads).
@@ -25,6 +28,154 @@ pub struct DesktopSegments{
     pub panel: TcKonnektSegment<DesktopPanel>,
     /// Segment for meter. 0x20e4..0x213f (23 quads).
     pub meter: TcKonnektSegment<DesktopMeter>,
+}
+
+/// The enumeration to represent target of meter.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum MeterTarget{
+    Input,
+    Pre,
+    Post,
+}
+
+impl Default for MeterTarget {
+    fn default() -> Self {
+        Self::Input
+    }
+}
+
+impl From<u32> for MeterTarget {
+    fn from(val: u32) -> Self {
+        match val {
+            2 => Self::Post,
+            1 => Self::Pre,
+            _ => Self::Input,
+        }
+    }
+}
+
+impl From<MeterTarget> for u32 {
+    fn from(target: MeterTarget) -> Self {
+        match target {
+            MeterTarget::Post => 2,
+            MeterTarget::Pre => 1,
+            MeterTarget::Input => 0,
+        }
+    }
+}
+
+/// The enumeration to represent current scene.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum InputScene {
+    MicInst,
+    DualInst,
+    StereoIn,
+}
+
+impl Default for InputScene {
+    fn default() -> Self {
+        Self::MicInst
+    }
+}
+
+impl From<u32> for InputScene {
+    fn from(val: u32) -> Self {
+        match val {
+            2 => Self::StereoIn,
+            1 => Self::DualInst,
+            _ => Self::MicInst,
+        }
+    }
+}
+
+impl From<InputScene> for u32 {
+    fn from(target: InputScene) -> Self {
+        match target {
+            InputScene::StereoIn => 2,
+            InputScene::DualInst => 1,
+            InputScene::MicInst => 0,
+        }
+    }
+}
+
+/// The structure to represent state of panel.
+#[derive(Default, Debug)]
+pub struct DesktopHwState {
+    pub meter_target: MeterTarget,
+    pub mixer_output_monaural: bool,
+    /// Whether to assign main knob to hp.
+    pub knob_assign_to_hp: bool,
+    /// Whether to dim output.
+    pub mixer_output_dim_enabled: bool,
+    /// The volume of dimmed output. -1000..-60. (-94.0..-6.0)
+    pub mixer_output_dim_volume: i32,
+    pub input_scene: InputScene,
+    pub reverb_to_master: bool,
+    pub reverb_to_hp: bool,
+    /// Turn on backlight in master knob.
+    pub master_knob_backlight: bool,
+    /// Phantom powering in mic 0.
+    pub mic_0_phantom: bool,
+    /// Signal boost in mic 0 by 12 dB.
+    pub mic_0_boost: bool,
+}
+
+impl DesktopHwState {
+    const SIZE: usize = 144;
+
+    const REVERB_TO_MAIN_MASK: u32 = 0x00000001;
+    const REVERB_TO_HP_MASK: u32 = 0x00000002;
+}
+
+impl TcKonnektSegmentData for DesktopHwState {
+    fn build(&self, raw: &mut [u8]) {
+        self.meter_target.build_quadlet(&mut raw[..4]);
+        self.mixer_output_monaural.build_quadlet(&mut raw[4..8]);
+        self.knob_assign_to_hp.build_quadlet(&mut raw[8..12]);
+        self.mixer_output_dim_enabled.build_quadlet(&mut raw[12..16]);
+        self.mixer_output_dim_volume.build_quadlet(&mut raw[16..20]);
+        self.input_scene.build_quadlet(&mut raw[20..24]);
+
+        let mut val = 0;
+        if self.reverb_to_master {
+            val |= Self::REVERB_TO_MAIN_MASK;
+        }
+        if self.reverb_to_hp {
+            val |= Self::REVERB_TO_HP_MASK;
+        }
+        val.build_quadlet(&mut raw[28..32]);
+
+        self.master_knob_backlight.build_quadlet(&mut raw[32..36]);
+        self.mic_0_phantom.build_quadlet(&mut raw[52..56]);
+        self.mic_0_boost.build_quadlet(&mut raw[56..60]);
+    }
+
+    fn parse(&mut self, raw: &[u8]) {
+        self.meter_target.parse_quadlet(&raw[..4]);
+        self.mixer_output_monaural.parse_quadlet(&raw[4..8]);
+        self.knob_assign_to_hp.parse_quadlet(&raw[8..12]);
+        self.mixer_output_dim_enabled.parse_quadlet(&raw[12..16]);
+        self.mixer_output_dim_volume.parse_quadlet(&raw[16..20]);
+        self.input_scene.parse_quadlet(&raw[20..24]);
+
+        let mut val = 0;
+        val.parse_quadlet(&raw[28..32]);
+        self.reverb_to_master = val & Self::REVERB_TO_MAIN_MASK > 0;
+        self.reverb_to_hp = val & Self::REVERB_TO_HP_MASK > 0;
+
+        self.master_knob_backlight.parse_quadlet(&raw[32..36]);
+        self.mic_0_phantom.parse_quadlet(&raw[52..56]);
+        self.mic_0_boost.parse_quadlet(&raw[56..60]);
+    }
+}
+
+impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopHwState> {
+    const OFFSET: usize = 0x0008;
+    const SIZE: usize = DesktopHwState::SIZE;
+}
+
+impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<DesktopHwState> {
+    const NOTIFY_FLAG: u32 = DESKTOP_HW_STATE_NOTIFY_FLAG;
 }
 
 /// The structure to represent configuration.
