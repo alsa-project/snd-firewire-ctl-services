0909c56f56769493e487ddfec0b80baa64a3dcbd
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-15 12:51:18 +0900

    dice-protocols: lexicon: add trait implementation for meter protocol
    The status of hardware meter is available by read transaction to
    specific address space. The space consists of quadlet data aligned to
    big endian with three field:
     - level (16 bit, MSB side)
     - source (8 bit)
     - destination (8 bit, LSB side)
    
    The source and destination are encoded according to TCAT prococol
    extension.
    
    This commit adds trait implementation for meter protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/lexicon.rs
+++ libs/dice/protocols/src/lexicon.rs
@@ -6,6 +6,8 @@
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by Lexicon for I-ONIX FW810s.
 
+pub mod meter;
+
 use glib::Error;
 use hinawa::FwNode;
 
