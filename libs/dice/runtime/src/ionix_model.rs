0523309a74b0ab0fa421f689ad42d2cf65cb730b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-15 12:51:18 +0900

    dice-runtime: ionix_model: add mixer controls
    This commit adds mixer controls.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/ionix_model.rs
+++ libs/dice/runtime/src/ionix_model.rs
@@ -10,9 +10,10 @@ use hinawa::{SndDice, SndUnitExt};
 use alsa_ctl_tlv_codec::items::DbInterval;
 
 use core::card_cntr::*;
+use core::elem_value_accessor::*;
 
 use dice_protocols::tcat::{*, global_section::*};
-use dice_protocols::lexicon::meter::*;
+use dice_protocols::lexicon::{meter::*, mixer::*};
 
 use crate::common_ctl::*;
 
@@ -22,6 +23,7 @@ pub struct IonixModel{
     sections: GeneralSections,
     ctl: CommonCtl,
     meter_ctl: MeterCtl,
+    mixer_ctl: MixerCtl,
 }
 
 const TIMEOUT_MS: u32 = 20;
@@ -36,6 +38,7 @@ impl CtlModel<SndDice> for IonixModel {
         self.ctl.load(card_cntr, &caps, &src_labels)?;
 
         self.meter_ctl.load(card_cntr)?;
+        self.mixer_ctl.load(card_cntr)?;
 
         Ok(())
     }
@@ -43,13 +46,25 @@ impl CtlModel<SndDice> for IonixModel {
     fn read(&mut self, unit: &SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.ctl.read(unit, &self.proto, &self.sections, elem_id, elem_value, TIMEOUT_MS)
+        if self.ctl.read(unit, &self.proto, &self.sections, elem_id, elem_value, TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_ctl.read(unit, &self.proto, elem_id, elem_value, TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(&mut self, unit: &SndDice, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        self.ctl.write(unit, &self.proto, &self.sections, elem_id, old, new, TIMEOUT_MS)
+        if self.ctl.write(unit, &self.proto, &self.sections, elem_id, old, new, TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_ctl.write(unit, &self.proto, elem_id, old, new, TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
 
@@ -155,6 +170,128 @@ impl<'a> MeterCtl {
             Self::MAIN_OUTPUT_NAME => {
                 elem_value.set_int(&self.meters.main_outputs);
                 Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+}
+
+fn mixer_src_to_string(src: &MixerSrc) -> String {
+    match src {
+        MixerSrc::Stream(ch) => format!("Stream-{}", ch + 1),
+        MixerSrc::Spdif(ch) => format!("S/PDIF-{}", ch + 1),
+        MixerSrc::Analog(ch) => format!("Analog-{}", ch + 1),
+    }
+}
+
+#[derive(Default, Debug)]
+struct MixerCtl;
+
+impl<'a> MixerCtl {
+    const BUS_SRC_GAIN_NAME: &'a str = "mixer-bus-input-gain";
+    const MAIN_SRC_GAIN_NAME: &'a str = "mixer-main-input-gain";
+    const REVERB_SRC_GAIN_NAME: &'a str = "mixer-reverb-input-gain";
+
+    const GAIN_MIN: i32 = 0;
+    const GAIN_MAX: i32 = 0x00007fff;
+    const GAIN_STEP: i32 = 1;
+    const GAIN_TLV: DbInterval = DbInterval{min: -6000, max: 0, linear: false, mute_avail: false};
+
+    const MIXER_SRCS: [MixerSrc;20] = [
+        MixerSrc::Analog(0), MixerSrc::Analog(1), MixerSrc::Analog(2), MixerSrc::Analog(3),
+        MixerSrc::Analog(4), MixerSrc::Analog(5), MixerSrc::Analog(6), MixerSrc::Analog(7),
+        MixerSrc::Spdif(0), MixerSrc::Spdif(1),
+        MixerSrc::Stream(0), MixerSrc::Stream(1), MixerSrc::Stream(2), MixerSrc::Stream(3),
+        MixerSrc::Stream(4), MixerSrc::Stream(5), MixerSrc::Stream(6), MixerSrc::Stream(7),
+        MixerSrc::Stream(8), MixerSrc::Stream(9),
+    ];
+
+    fn load(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
+        let labels: Vec<String> = Self::MIXER_SRCS.iter()
+            .map(|s| mixer_src_to_string(s))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::BUS_SRC_GAIN_NAME, 0);
+        let _ = card_cntr.add_int_elems(&elem_id, MIXER_BUS_CHANNEL_COUNT,
+                                        Self::GAIN_MIN, Self::GAIN_MAX, Self::GAIN_STEP, labels.len(),
+                                        Some(&Vec::<u32>::from(Self::GAIN_TLV)), true)?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::MAIN_SRC_GAIN_NAME, 0);
+        let _ = card_cntr.add_int_elems(&elem_id, MIXER_MAIN_CHANNEL_COUNT,
+                                        Self::GAIN_MIN, Self::GAIN_MAX, Self::GAIN_STEP, labels.len(),
+                                        Some(&Vec::<u32>::from(Self::GAIN_TLV)), true)?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::REVERB_SRC_GAIN_NAME, 0);
+        let _ = card_cntr.add_int_elems(&elem_id, MIXER_REVERB_CHANNEL_COUNT,
+                                        Self::GAIN_MIN, Self::GAIN_MAX, Self::GAIN_STEP, labels.len(),
+                                        Some(&Vec::<u32>::from(Self::GAIN_TLV)), true)?;
+
+        Ok(())
+    }
+
+    fn read(&self, unit: &SndDice, proto: &FwReq, elem_id: &ElemId, elem_value: &mut ElemValue,
+            timeout_ms: u32)
+        -> Result<bool, Error>
+    {
+        match elem_id.get_name().as_str() {
+            Self::BUS_SRC_GAIN_NAME => {
+                let mixer = elem_id.get_index() as usize;
+                let node = unit.get_node();
+                ElemValueAccessor::<i32>::set_vals(elem_value, Self::MIXER_SRCS.len(), |idx| {
+                    proto.read_mixer_bus_src_gain(&node, mixer, Self::MIXER_SRCS[idx], timeout_ms)
+                        .map(|val| val as i32)
+                })
+                .map(|_| true)
+            }
+            Self::MAIN_SRC_GAIN_NAME => {
+                let mixer = elem_id.get_index() as usize;
+                let node = unit.get_node();
+                ElemValueAccessor::<i32>::set_vals(elem_value, Self::MIXER_SRCS.len(), |idx| {
+                    proto.read_mixer_main_src_gain(&node, mixer, Self::MIXER_SRCS[idx], timeout_ms)
+                        .map(|val| val as i32)
+                })
+                .map(|_| true)
+            }
+            Self::REVERB_SRC_GAIN_NAME => {
+                let mixer = elem_id.get_index() as usize;
+                let node = unit.get_node();
+                ElemValueAccessor::<i32>::set_vals(elem_value, Self::MIXER_SRCS.len(), |idx| {
+                    proto.read_mixer_reverb_src_gain(&node, mixer, Self::MIXER_SRCS[idx], timeout_ms)
+                        .map(|val| val as i32)
+                })
+                .map(|_| true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    fn write(&mut self, unit: &SndDice, proto: &FwReq, elem_id: &ElemId, old: &ElemValue, new: &ElemValue,
+             timeout_ms: u32)
+        -> Result<bool, Error>
+    {
+        match elem_id.get_name().as_str() {
+            Self::BUS_SRC_GAIN_NAME => {
+                let mixer = elem_id.get_index() as usize;
+                let node = unit.get_node();
+                ElemValueAccessor::<i32>::get_vals(new, old, Self::MIXER_SRCS.len(), |idx, val| {
+                    proto.write_mixer_bus_src_gain(&node, mixer, Self::MIXER_SRCS[idx], val as u32, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            Self::MAIN_SRC_GAIN_NAME => {
+                let mixer = elem_id.get_index() as usize;
+                let node = unit.get_node();
+                ElemValueAccessor::<i32>::get_vals(new, old, Self::MIXER_SRCS.len(), |idx, val| {
+                    proto.write_mixer_main_src_gain(&node, mixer, Self::MIXER_SRCS[idx], val as u32, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            Self::REVERB_SRC_GAIN_NAME => {
+                let mixer = elem_id.get_index() as usize;
+                let node = unit.get_node();
+                ElemValueAccessor::<i32>::get_vals(new, old, Self::MIXER_SRCS.len(), |idx, val| {
+                    proto.write_mixer_reverb_src_gain(&node, mixer, Self::MIXER_SRCS[idx], val as u32, timeout_ms)
+                })
+                .map(|_| true)
             }
             _ => Ok(false),
         }
