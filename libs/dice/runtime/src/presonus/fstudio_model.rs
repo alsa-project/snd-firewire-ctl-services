bb23c5af989a9ddcd76e9dee1c31c2958be54e91
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-29 22:08:19 +0900

    dice-runtime: presonus: add meter controls for FireStudio
    This commit adds meter controls for FireStudio.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/presonus/fstudio_model.rs
+++ libs/dice/runtime/src/presonus/fstudio_model.rs
@@ -2,9 +2,11 @@
 // Copyright (c) 2021 Takashi Sakamoto
 use glib::Error;
 
-use alsactl::{ElemId, ElemValue};
+use alsactl::{ElemId, ElemIfaceType, ElemValue, ElemValueExt};
 
 use hinawa::{SndDice, SndUnitExt};
+
+use alsa_ctl_tlv_codec::items::DbInterval;
 
 use core::card_cntr::*;
 
@@ -19,6 +21,7 @@ pub struct FStudioModel{
     proto: FStudioProto,
     sections: GeneralSections,
     ctl: CommonCtl,
+    meter_ctl: MeterCtl,
 }
 
 const TIMEOUT_MS: u32 = 20;
@@ -51,6 +54,8 @@ impl CtlModel<SndDice> for FStudioModel {
             .collect();
         let src_labels = ClockSourceLabels{entries};
         self.ctl.load(card_cntr, &caps, &src_labels)?;
+
+        self.meter_ctl.load(card_cntr, unit, &self.proto, TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -87,15 +92,96 @@ impl NotifyModel<SndDice, u32> for FStudioModel {
 impl MeasureModel<hinawa::SndDice> for FStudioModel {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.ctl.measured_elem_list);
+        elem_id_list.extend_from_slice(&self.meter_ctl.measured_elem_list);
     }
 
     fn measure_states(&mut self, unit: &SndDice) -> Result<(), Error> {
-        self.ctl.measure_states(unit, &self.proto, &self.sections, TIMEOUT_MS)
+        self.ctl.measure_states(unit, &self.proto, &self.sections, TIMEOUT_MS)?;
+        self.meter_ctl.measure_states(unit, &self.proto, TIMEOUT_MS)?;
+        Ok(())
     }
 
     fn measure_elem(&mut self, _: &SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.ctl.measure_elem(elem_id, elem_value)
+        if self.ctl.measure_elem(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.meter_ctl.read_measured_elem(elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+struct MeterCtl{
+    meter: FStudioMeter,
+    measured_elem_list: Vec<ElemId>,
+}
+
+impl<'a> MeterCtl {
+    const ANALOG_INPUT_NAME: &'a str = "analog-input-meter";
+    const STREAM_INPUT_NAME: &'a str = "stream-input-meter";
+    const MIXER_OUTPUT_NAME: &'a str = "mixer-output-meter";
+
+    const LEVEL_MIN: i32 = 0x00;
+    const LEVEL_MAX: i32 = 0xff;
+    const LEVEL_STEP: i32 = 1;
+    const LEVEL_TLV: DbInterval = DbInterval{min: -9600, max: 0, linear: false, mute_avail: false};
+
+    fn load(&mut self, card_cntr: &mut CardCntr, unit: &SndDice, proto: &FStudioProto, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        proto.read_meter(&unit.get_node(), &mut self.meter, timeout_ms)?;
+
+        [
+            (Self::ANALOG_INPUT_NAME, self.meter.analog_inputs.len()),
+            (Self::STREAM_INPUT_NAME, self.meter.stream_inputs.len()),
+            (Self::MIXER_OUTPUT_NAME, self.meter.mixer_outputs.len()),
+        ].iter()
+            .try_for_each(|&(name, count)| {
+                let elem_id = ElemId::new_by_name(ElemIfaceType::Card, 0, 0, name, 0);
+                card_cntr.add_int_elems(&elem_id, 1, Self::LEVEL_MIN, Self::LEVEL_MAX, Self::LEVEL_STEP,
+                                        count, Some(&Into::<Vec<u32>>::into(Self::LEVEL_TLV)), false)
+                    .map(|mut elem_id_list| self.measured_elem_list.append(&mut elem_id_list))
+            })?;
+
+        Ok(())
+    }
+
+    fn measure_states(&mut self, unit: &SndDice, proto: &FStudioProto, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        proto.read_meter(&unit.get_node(), &mut self.meter, timeout_ms)
+    }
+
+    fn read_measured_elem(&mut self, elem_id: &ElemId, elem_value: &mut ElemValue)
+        -> Result<bool, Error>
+    {
+        match elem_id.get_name().as_str() {
+            Self::ANALOG_INPUT_NAME => {
+                let vals: Vec<i32> = self.meter.analog_inputs.iter()
+                    .map(|&l| l as i32)
+                    .collect();
+                elem_value.set_int(&vals);
+                Ok(true)
+            }
+            Self::STREAM_INPUT_NAME => {
+                let vals: Vec<i32> = self.meter.stream_inputs.iter()
+                    .map(|&l| l as i32)
+                    .collect();
+                elem_value.set_int(&vals);
+                Ok(true)
+            }
+            Self::MIXER_OUTPUT_NAME => {
+                let vals: Vec<i32> = self.meter.mixer_outputs.iter()
+                    .map(|&l| l as i32)
+                    .collect();
+                elem_value.set_int(&vals);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
     }
 }
