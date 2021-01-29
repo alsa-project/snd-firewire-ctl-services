f3b62675d52982e19297ae60fe4a3cde718400df
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-29 09:39:35 +0900

    dice-runtime: add support for PreSonus FireStudio Mobile
    FireStudio Mobile was shipped by PreSonus Audio Electronics, Inc. 2010.
    The model uses TCAT TCD2210 as its communication engine and supports TCAT
    protocol extension.
    
    This commit adds support for the model. No unique protocol is used for the
    model.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/presonus.rs
+++ libs/dice/protocols/src/presonus.rs
@@ -1,3 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
 pub mod fstudioproject;
+pub mod fstudiomobile;
--- libs/dice/runtime/src/presonus.rs
+++ libs/dice/runtime/src/presonus.rs
@@ -1,3 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
 pub mod fstudioproject_model;
+pub mod fstudiomobile_model;
