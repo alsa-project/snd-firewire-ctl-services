20022751c668664df422a6062931bf12a4d2b796
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-01-29 22:08:19 +0900

    dice-protocols: presonus: add trait implementation for protocol of FireStudio
    FireStudio is configured by unique protocol. IEEE 1394 asynchronous
    transaction to offset 0x'ffff'e070'0000 is used.
    
    This commit adds trait implementation for the protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/presonus/fstudio.rs
+++ libs/dice/protocols/src/presonus/fstudio.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
-use hinawa::FwReq;
+use glib::Error;
+
+use hinawa::{FwNode, FwReq};
+
+use crate::tcat::*;
 
 #[derive(Default, Debug)]
 pub struct FStudioProto(FwReq);
@@ -10,3 +14,20 @@ impl AsRef<FwReq> for FStudioProto {
         &self.0
     }
 }
+
+/// The trait to represent protocol specific to FireStudio.
+pub trait PresonusFStudioProto<T> : GeneralProtocol<T>
+    where T: AsRef<FwNode>,
+{
+    const OFFSET: usize = 0x00700000;
+
+    fn read(&self, node: &T, offset: usize, raw: &mut [u8], timeout_ms: u32) -> Result<(), Error> {
+        GeneralProtocol::read(self, node, Self::OFFSET + offset, raw, timeout_ms)
+    }
+
+    fn write(&self, node: &T, offset: usize, raw: &mut [u8], timeout_ms: u32) -> Result<(), Error> {
+        GeneralProtocol::write(self, node, Self::OFFSET + offset, raw, timeout_ms)
+    }
+}
+
+impl<T: AsRef<FwNode>> PresonusFStudioProto<T> for FStudioProto {}
