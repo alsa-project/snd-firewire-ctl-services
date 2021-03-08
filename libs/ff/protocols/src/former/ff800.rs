07d91d275be8606181de0eed6ebf50aad8e4d832
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-08 16:02:31 +0900

    ff-protocols: former: add trait implementation for output protocol of Fireface 800
    In RME Fireface series, adjustment of volume for hardware output is
    available.
    
    This commit adds trait implementation for output protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/ff/protocols/src/former/ff800.rs
+++ libs/ff/protocols/src/former/ff800.rs
@@ -16,6 +16,8 @@ impl AsRef<FwReq> for Ff800Protocol {
     }
 }
 
+
+const OUTPUT_OFFSET: usize      = 0x000080081f80;
 const METER_OFFSET: usize       = 0x000080100000;
 
 const ANALOG_INPUT_COUNT: usize = 10;
@@ -65,4 +67,33 @@ impl<T, O> RmeFfFormerMeterProtocol<T, Ff800MeterState> for O
           O: AsRef<FwReq>,
 {
     const METER_OFFSET: usize = METER_OFFSET;
+}
+
+/// The structure to represent volume of outputs for Fireface 800.
+///
+/// The value for volume is between 0x00000000 and 0x00010000 through 0x00000001 and 0x00080000 to
+/// represent the range from negative infinite to 6.00 dB through -90.30 dB and 0.00 dB.
+#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+pub struct Ff800OutputVolumeState([i32;ANALOG_OUTPUT_COUNT + SPDIF_OUTPUT_COUNT + ADAT_OUTPUT_COUNT]);
+
+impl AsRef<[i32]> for Ff800OutputVolumeState {
+    fn as_ref(&self) -> &[i32] {
+        &self.0
+    }
+}
+
+impl AsMut<[i32]> for Ff800OutputVolumeState {
+    fn as_mut(&mut self) -> &mut [i32] {
+        &mut self.0
+    }
+}
+
+impl<T: AsRef<FwNode>> RmeFormerOutputProtocol<T, Ff800OutputVolumeState> for Ff800Protocol {
+    fn write_output_vol(&self, node: &T, ch: usize, vol: i32, timeout_ms: u32) -> Result<(), Error> {
+        let mut raw = [0;4];
+        raw.copy_from_slice(&vol.to_le_bytes());
+        self.as_ref().transaction_sync(node.as_ref(), FwTcode::WriteBlockRequest,
+                                       (OUTPUT_OFFSET + ch * 4) as u64, raw.len(), &mut raw,
+                                       timeout_ms)
+    }
 }
