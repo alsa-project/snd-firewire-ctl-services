73674added494fd1c6c239e26b161cf7e135abb0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-08 16:02:31 +0900

    ff-protocols: add trait implementation for parser of configuration rom by unique protocol
    In RME Fireface series, the version field of unit directory in
    configuration rom represents actual model ID.
    
    This commit adds trait implementation to pick it up.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/ff/protocols/src/lib.rs
+++ libs/ff/protocols/src/lib.rs
@@ -5,3 +5,31 @@
 //!
 //! The crate includes various kind of protocols defined by RME GmbH for models of its Fireface
 //! series. The protocols are categorized by two generations; i.e. former and latter.
+
+use ieee1212_config_rom::{*, entry::*};
+
+const RME_OUI: u32 = 0x00000a35;
+
+/// The trait to represent parser of configuration rom for RME Fireface series.
+pub trait FfConfigRom {
+    fn get_model_id(&self) -> Option<u32>;
+}
+
+impl<'a> FfConfigRom for ConfigRom<'a> {
+    fn get_model_id(&self) -> Option<u32> {
+        self.root.iter().find_map(|entry| {
+            EntryDataAccess::<u32>::get(entry, KeyType::Vendor)
+        })
+        .filter(|vendor_id| vendor_id.eq(&RME_OUI))
+        .and_then(|_| {
+            self.root.iter().find_map(|entry| {
+                EntryDataAccess::<&[Entry]>::get(entry, KeyType::Unit)
+            })
+            .and_then(|entries| {
+                entries.iter().find_map(|entry| {
+                    EntryDataAccess::<u32>::get(entry, KeyType::Version)
+                })
+            })
+        })
+    }
+}
