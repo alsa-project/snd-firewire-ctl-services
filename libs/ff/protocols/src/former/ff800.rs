e85bd8652ff4333e50003807125238ff6a682b72
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-08 16:02:31 +0900

    ff-protocols: former: add trait implementation for meter protocol of Fireface 800
    In RME Fireface series, hardware metering is available by read block
    transaction. The read data consists of some octlets and quadlets. The
    octlet meter seems to be for the result to processing audio signal
    in DSP and the quadlet meter seems to be for direct hardware inputs and
    outputs.
    
    This commit adds trait implementation for the meter protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/ff/protocols/src/former/ff800.rs
+++ libs/ff/protocols/src/former/ff800.rs
@@ -2,8 +2,9 @@
 // Copyright (c) 2021 Takashi Sakamoto
 
 //! Protocol defined by RME GmbH for Fireface 800.
+use hinawa::{FwNode, FwReq};
 
-use hinawa::FwReq;
+use super::*;
 
 /// The structure to represent unique protocol for Fireface 800.
 #[derive(Default, Debug, Clone, Eq, PartialEq)]
@@ -13,4 +14,55 @@ impl AsRef<FwReq> for Ff800Protocol {
     fn as_ref(&self) -> &FwReq {
         &self.0
     }
+}
+
+const METER_OFFSET: usize       = 0x000080100000;
+
+const ANALOG_INPUT_COUNT: usize = 10;
+const SPDIF_INPUT_COUNT: usize = 2;
+const ADAT_INPUT_COUNT: usize = 16;
+const STREAM_INPUT_COUNT: usize = 28;
+
+const ANALOG_OUTPUT_COUNT: usize = 10;
+const SPDIF_OUTPUT_COUNT: usize = 2;
+const ADAT_OUTPUT_COUNT: usize = 16;
+
+/// The structure to represent state of hardware meter for Fireface 400.
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Ff800MeterState(FormerMeterState);
+
+impl AsRef<FormerMeterState> for Ff800MeterState {
+    fn as_ref(&self) -> &FormerMeterState {
+        &self.0
+    }
+}
+
+impl AsMut<FormerMeterState> for Ff800MeterState {
+    fn as_mut(&mut self) -> &mut FormerMeterState {
+        &mut self.0
+    }
+}
+
+impl FormerMeterSpec for Ff800MeterState {
+    const ANALOG_INPUT_COUNT: usize = ANALOG_INPUT_COUNT;
+    const SPDIF_INPUT_COUNT: usize = SPDIF_INPUT_COUNT;
+    const ADAT_INPUT_COUNT: usize = ADAT_INPUT_COUNT;
+    const STREAM_INPUT_COUNT: usize = STREAM_INPUT_COUNT;
+
+    const ANALOG_OUTPUT_COUNT: usize = ANALOG_OUTPUT_COUNT;
+    const SPDIF_OUTPUT_COUNT: usize = SPDIF_OUTPUT_COUNT;
+    const ADAT_OUTPUT_COUNT: usize = ADAT_OUTPUT_COUNT;
+}
+
+impl Default for Ff800MeterState {
+    fn default() -> Self {
+        Self(Self::create_meter_state())
+    }
+}
+
+impl<T, O> RmeFfFormerMeterProtocol<T, Ff800MeterState> for O
+    where T: AsRef<FwNode>,
+          O: AsRef<FwReq>,
+{
+    const METER_OFFSET: usize = METER_OFFSET;
 }
