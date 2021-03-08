e94923236a07569702f42608b867beb77f67af25
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-08 16:02:31 +0900

    ff-protocols: former: add trait implementation for output protocol of Fireface 800
    In RME Fireface series, adjustment of volume for hardware output is
    available.
    
    This commit adds trait implementation for output protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/ff/protocols/src/former.rs
+++ libs/ff/protocols/src/former.rs
@@ -97,3 +97,31 @@ pub trait RmeFfFormerMeterProtocol<T, U> : AsRef<FwReq>
             })
     }
 }
+
+/// The trait to represent output protocol specific to former models of RME Fireface.
+///
+/// The value for volume is between 0x00000000 and 0x00010000 through 0x00000001 and 0x00080000 to
+/// represent the range from negative infinite to 6.00 dB through -90.30 dB and 0.00 dB.
+pub trait RmeFormerOutputProtocol<T, U> : AsRef<FwReq>
+    where T: AsRef<FwNode>,
+          U: AsRef<[i32]> + AsMut<[i32]>,
+{
+    fn write_output_vol(&self, node: &T, ch: usize, vol: i32, timeout_ms: u32) -> Result<(), Error>;
+
+    fn init_output_vols(&self, node: &T, state: &U, timeout_ms: u32) -> Result<(), Error> {
+        state.as_ref().iter()
+            .enumerate()
+            .try_for_each(|(i, vol)| self.write_output_vol(node, i, *vol, timeout_ms))
+    }
+
+    fn write_output_vols(&self, node: &T, state: &mut U, vols: &[i32], timeout_ms: u32) -> Result<(), Error> {
+        state.as_mut().iter_mut()
+            .zip(vols.iter())
+            .enumerate()
+            .filter(|(_, (o, n))| !o.eq(n))
+            .try_for_each(|(i, (o, n))| {
+                self.write_output_vol(node, i, *n, timeout_ms)
+                    .map(|_| *o = *n)
+            })
+    }
+}
