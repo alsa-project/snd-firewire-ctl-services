c7e3ca39b947f5a3a06782298d455d53efa4628f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-08 16:02:31 +0900

    ff-protocols: former: add trait implementation for input amplifier protocol of Fireface 400
    This commit adds trait implementation of input amplifier protocol of
    Fireface 400.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/ff/protocols/src/former/ff400.rs
+++ libs/ff/protocols/src/former/ff400.rs
@@ -31,6 +31,9 @@ const ANALOG_OUTPUT_COUNT: usize = 8;
 const SPDIF_OUTPUT_COUNT: usize = 2;
 const ADAT_OUTPUT_COUNT: usize = 8;
 
+const AMP_MIC_IN_CH_OFFSET: u8 = 0;
+const AMP_LINE_IN_CH_OFFSET: u8 = 2;
+
 /// The structure to represent state of hardware meter for Fireface 400.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Ff400MeterState(FormerMeterState);
@@ -83,3 +86,73 @@ pub trait RmeFf400AmpProtocol<T: AsRef<FwNode>> : AsRef<FwReq> {
 }
 
 impl<T: AsRef<FwNode>> RmeFf400AmpProtocol<T> for Ff400Protocol {}
+
+/// The structure to represent status of input gains of Fireface 400.
+#[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
+pub struct Ff400InputGainStatus{
+    /// The level of gain for input 1 and 2. The value is between 0 and 65 by step 1 to represent
+    /// the range from 0 to 65 dB.
+    pub mic: [i8;2],
+    /// The level of gain for input 3 and 4. The value is between 0 and 36 by step 1 to represent
+    /// the range from 0 to 18 dB.
+    pub line: [i8;2],
+}
+
+/// The trait to represent amplifier protocol of Fireface 400.
+pub trait RmeFf400InputGainProtocol<T: AsRef<FwNode>> : RmeFf400AmpProtocol<T> {
+    fn write_input_mic_gain(&self, node: &T, ch: usize, gain: i8, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        self.write_amp_cmd(node, AMP_MIC_IN_CH_OFFSET + ch as u8, gain, timeout_ms)
+    }
+
+    fn write_input_line_gain(&self, node: &T, ch: usize, gain: i8, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        self.write_amp_cmd(node, AMP_LINE_IN_CH_OFFSET + ch as u8, gain, timeout_ms)
+    }
+
+    fn init_input_gains(&self, node: &T, status: &Ff400InputGainStatus, timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        status.mic.iter()
+            .enumerate()
+            .try_for_each(|(i, gain)| self.write_input_mic_gain(node, i, *gain, timeout_ms))?;
+
+        status.line.iter()
+            .enumerate()
+            .try_for_each(|(i, gain)| self.write_input_line_gain(node, i, *gain, timeout_ms))?;
+
+        Ok(())
+    }
+
+    fn write_input_mic_gains(&self, node: &T, status: &mut Ff400InputGainStatus, gains: &[i8],
+                             timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        status.mic.iter_mut()
+            .zip(gains.iter())
+            .enumerate()
+            .filter(|(_, (o, n))| !o.eq(n))
+            .try_for_each(|(i, (o, n))| {
+                self.write_input_mic_gain(node, i, *n, timeout_ms)
+                    .map(|_| *o = *n)
+            })
+    }
+
+    fn write_input_line_gains(&self, node: &T, status: &mut Ff400InputGainStatus, gains: &[i8],
+                             timeout_ms: u32)
+        -> Result<(), Error>
+    {
+        status.line.iter_mut()
+            .zip(gains.iter())
+            .enumerate()
+            .filter(|(_, (o, n))| !o.eq(n))
+            .try_for_each(|(i, (o, n))| {
+                self.write_input_line_gain(node, i, *n, timeout_ms)
+                    .map(|_| *o = *n)
+            })
+    }
+}
+
+impl<T: AsRef<FwNode>, O: RmeFf400AmpProtocol<T>> RmeFf400InputGainProtocol<T> for O {}
