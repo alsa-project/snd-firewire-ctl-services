alsa-project/snd-firewire-ctl-services

commit 44fb62a97364deaa72cba9de242174b1363c418d

efw: use alsa-ctl-tlv-codec to generate data of TLV
This commit uses DbInterval in alsa-ctl-tlv-codec crate to build data of
TLV.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/efw/src/mixer_ctl.rs
@@ -2,6 +2,8 @@
 // Copyright (c) 2020 Takashi Sakamoto
 use glib::Error;
 
+use alsa_ctl_tlv_codec::items::DbInterval;
+
 use core::card_cntr;
 use core::elem_value_accessor::ElemValueAccessor;
 
@@ -29,7 +31,7 @@ impl<'a> MixerCtl {
     const COEF_MIN: i32 = 0x00000000;
     const COEF_MAX: i32 = 0x02000000;
     const COEF_STEP: i32 = 0x00000001;
-    const COEF_TLV: &'a [u32] = &[5, 8, -14400i32 as u32, 6];
+    const COEF_TLV: DbInterval = DbInterval{min: -14400, max: 6, linear: false, mute_avail: false};
 
     const PAN_MIN: i32 = 0;
     const PAN_MAX: i32 = 255;
@@ -55,7 +57,7 @@ impl<'a> MixerCtl {
             alsactl::ElemIfaceType::Mixer, 0, 0, Self::PLAYBACK_VOL_NAME, 0);
         let _ = card_cntr.add_int_elems(&elem_id, 1,
             Self::COEF_MIN, Self::COEF_MAX, Self::COEF_STEP,
-            self.playbacks, Some(Self::COEF_TLV), true)?;
+            self.playbacks, Some(&Into::<Vec<u32>>::into(Self::COEF_TLV)), true)?;
 
         let elem_id = alsactl::ElemId::new_by_name(
             alsactl::ElemIfaceType::Mixer, 0, 0, Self::PLAYBACK_MUTE_NAME, 0);
@@ -69,7 +71,7 @@ impl<'a> MixerCtl {
             alsactl::ElemIfaceType::Mixer, 0, 0, Self::MONITOR_GAIN_NAME, 0);
         let _ = card_cntr.add_int_elems(&elem_id, self.playbacks,
             Self::COEF_MIN, Self::COEF_MAX, Self::COEF_STEP,
-            self.captures, Some(Self::COEF_TLV), true)?;
+            self.captures, Some(&Into::<Vec<u32>>::into(Self::COEF_TLV)), true)?;
 
         let elem_id = alsactl::ElemId::new_by_name(
             alsactl::ElemIfaceType::Mixer, 0, 0, Self::MONITOR_MUTE_NAME, 0);

libs/efw/src/output_ctl.rs
@@ -2,6 +2,8 @@
 // Copyright (c) 2020 Takashi Sakamoto
 use glib::{Error, FileError};
 
+use alsa_ctl_tlv_codec::items::DbInterval;
+
 use core::card_cntr;
 use core::elem_value_accessor::ElemValueAccessor;
 
@@ -20,7 +22,7 @@ impl<'a> OutputCtl {
     const COEF_MIN: i32 = 0x00000000;
     const COEF_MAX: i32 = 0x02000000;
     const COEF_STEP: i32 = 0x00000001;
-    const COEF_TLV: &'a [u32] = &[5, 8, -14400i32 as u32, 6];
+    const COEF_TLV: DbInterval = DbInterval{min: -14400, max: 6, linear: false, mute_avail: false};
 
     const OUT_NOMINAL_LABELS: &'a [&'a str] = &["+4dBu", "-10dBV"];
     const OUT_NOMINAL_LEVELS: &'a [NominalLevel] = &[NominalLevel::PlusFour, NominalLevel::MinusTen];
@@ -44,7 +46,7 @@ impl<'a> OutputCtl {
             alsactl::ElemIfaceType::Mixer, 0, 0, Self::OUT_VOL_NAME, 0);
         let _ = card_cntr.add_int_elems(&elem_id, 1,
             Self::COEF_MIN, Self::COEF_MAX, Self::COEF_STEP,
-            self.phys_outputs, Some(Self::COEF_TLV), true)?;
+            self.phys_outputs, Some(&Into::<Vec<u32>>::into(Self::COEF_TLV)), true)?;
 
         let elem_id = alsactl::ElemId::new_by_name(
             alsactl::ElemIfaceType::Mixer, 0, 0, Self::OUT_MUTE_NAME, 0);

libs/efw/Cargo.toml
@@ -15,5 +15,6 @@ nix = "0.17"
 glib = "0.10"
 hinawa = { git = "https://github.com/alsa-project/hinawa-rs.git", tag = "v0.3.0", version = "0.3" }
 alsactl = { git = "https://github.com/alsa-project/alsa-gobject-rs.git", tag = "v0.1.1", version = "0.1" }
+alsa-ctl-tlv-codec = { path = "../alsa-ctl-tlv-codec" }
 core = { path = "../core" }
 ta1394 = { path = "../ta1394" }

commit 12dea448b4ed5ef676548045229efae322bcb218

efw: fix maximum value for dB interval
In TLV-style data, dB value is in 0.01 unit.

This commit fixes invalid declaration in the above.

Fixes: 44fb62a97364 ('efw: use alsa-ctl-tlv-codec to generate data of TLV')
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/efw/src/mixer_ctl.rs
@@ -31,7 +31,7 @@ impl<'a> MixerCtl {
     const COEF_MIN: i32 = 0x00000000;
     const COEF_MAX: i32 = 0x02000000;
     const COEF_STEP: i32 = 0x00000001;
-    const COEF_TLV: DbInterval = DbInterval{min: -14400, max: 6, linear: false, mute_avail: false};
+    const COEF_TLV: DbInterval = DbInterval{min: -14400, max: 600, linear: false, mute_avail: false};
 
     const PAN_MIN: i32 = 0;
     const PAN_MAX: i32 = 255;

libs/efw/src/output_ctl.rs
@@ -22,7 +22,7 @@ impl<'a> OutputCtl {
     const COEF_MIN: i32 = 0x00000000;
     const COEF_MAX: i32 = 0x02000000;
     const COEF_STEP: i32 = 0x00000001;
-    const COEF_TLV: DbInterval = DbInterval{min: -14400, max: 6, linear: false, mute_avail: false};
+    const COEF_TLV: DbInterval = DbInterval{min: -14400, max: 600, linear: false, mute_avail: false};
 
     const OUT_NOMINAL_LABELS: &'a [&'a str] = &["+4dBu", "-10dBV"];
     const OUT_NOMINAL_LEVELS: &'a [NominalLevel] = &[NominalLevel::PlusFour, NominalLevel::MinusTen];