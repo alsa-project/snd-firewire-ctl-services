e4a183ba1f0a8a5b8124c0b68a45d3f517ea4302
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-17 17:04:26 +0900

    oxfw-runtime: tapco_model: add control to switch stream source between analog and S/PDIF inputs
    Tapco Link.FireWire 4x6 allows software to configure source of capture
    from two options; 2ch analog inputs and 2ch S/PDIF inputs.
    
    This commit adds control element for the feature.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/oxfw/runtime/src/loud_model.rs
+++ libs/oxfw/runtime/src/loud_model.rs
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
-use glib::Error;
+use glib::{Error, FileError};
 
 use hinawa::{SndUnit, SndUnitExt, FwFcp, FwFcpExt};
 
-use alsactl::{ElemId, ElemValue};
+use alsactl::{ElemId, ElemIfaceType, ElemValue, ElemValueExt, ElemValueExtManual};
 
 use core::card_cntr::{CardCntr, CtlModel, NotifyModel};
 
 use super::common_ctl::CommonCtl;
 
+use ta1394::{*, ccm::*};
+
 #[derive(Default, Debug)]
 pub struct LinkFwModel {
     avc: FwFcp,
     common_ctl: CommonCtl,
+    specific_ctl: SpecificCtl,
 }
 
 const FCP_TIMEOUT_MS: u32 = 100;
@@ -23,6 +26,7 @@ impl CtlModel<SndUnit> for LinkFwModel {
         self.avc.bind(&unit.get_node())?;
 
         self.common_ctl.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
+        self.specific_ctl.load(card_cntr)?;
 
         Ok(())
     }
@@ -30,13 +34,25 @@ impl CtlModel<SndUnit> for LinkFwModel {
     fn read(&mut self, _: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.common_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
+        if self.common_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.specific_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(&mut self, unit: &SndUnit, elem_id: &ElemId, _: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        self.common_ctl.write(unit, &self.avc, elem_id, new, FCP_TIMEOUT_MS)
+        if self.common_ctl.write(unit, &self.avc, elem_id, new, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.specific_ctl.write(&self.avc, elem_id, new, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
 
@@ -53,5 +69,79 @@ impl NotifyModel<SndUnit, bool> for LinkFwModel {
         -> Result<bool, Error>
     {
         self.common_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
+    }
+}
+
+#[derive(Default, Debug)]
+struct SpecificCtl;
+
+impl<'a> SpecificCtl {
+    const CAPTURE_SOURCE_NAME: &'a str = "capture-source";
+
+    const CAPTURE_SOURCES: [&'a str;2] = [
+        "Analog-input",
+        "S/PDIF-input",
+    ];
+
+    const SIG_DST: SignalAddr = SignalAddr::Subunit(
+        SignalSubunitAddr{
+            subunit: AvcAddrSubunit{
+                subunit_type: AvcSubunitType::Audio,
+                subunit_id: 0,
+            },
+            plug_id: 1,
+        }
+    );
+
+    const SIG_SRCS: [SignalAddr;2] = [
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x00)),
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x01)),
+    ];
+
+    fn load(&self, card_cntr: &mut CardCntr) -> Result<(), Error> {
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::CAPTURE_SOURCE_NAME, 0);
+        card_cntr.add_enum_elems(&elem_id, 1, 1, &Self::CAPTURE_SOURCES, None, true)
+            .map(|_| ())
+    }
+
+    fn read(&self, avc: &FwFcp, elem_id: &ElemId, elem_value: &mut ElemValue, timeout_ms: u32)
+        -> Result<bool, Error>
+    {
+        match elem_id.get_name().as_str() {
+            Self::CAPTURE_SOURCE_NAME => {
+                let mut op = SignalSource::new(&Self::SIG_DST);
+                avc.status(&AvcAddr::Unit, &mut op, timeout_ms)?;
+                let idx = Self::SIG_SRCS.iter()
+                    .position(|src| src.eq(&op.src))
+                    .unwrap();
+                elem_value.set_enum(&[idx as u32]);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    fn write(&self, avc: &FwFcp, elem_id: &ElemId, elem_value: &ElemValue, timeout_ms: u32)
+        -> Result<bool, Error>
+    {
+        match elem_id.get_name().as_str() {
+            Self::CAPTURE_SOURCE_NAME => {
+                let mut vals = [0];
+                elem_value.get_enum(&mut vals);
+                let src = Self::SIG_SRCS.iter()
+                    .nth(vals[0] as usize)
+                    .ok_or_else(|| {
+                        let msg = format!("Invalid value for index of signal source: {}", vals[0]);
+                        Error::new(FileError::Inval, &msg)
+                    })?;
+                let mut op = SignalSource{
+                    src: *src,
+                    dst: Self::SIG_DST,
+                };
+                avc.control(&AvcAddr::Unit, &mut op, timeout_ms)
+                    .map(|_| true)
+            }
+            _ => Ok(false),
+        }
     }
 }
