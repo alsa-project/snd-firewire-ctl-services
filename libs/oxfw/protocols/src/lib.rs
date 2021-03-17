0fc68feef0468b4628ff56384c0ede1dc681a5b9
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-03-17 17:04:26 +0900

    oxfw-protocols: add protocols crate for Oxford Semiconductor FW970/971 ASICs
    This commit adds oxfw-protocol crate, which includes implementations of
    any vendor-dependent protocols.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/oxfw/protocols/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocols defined for Oxford Semiconductor FW970/971 chipset.
+//!
+//! The crate includes various kind of protocols defined by Oxford Semiconductor as well as
+//! hardware vendors for FW970/971 ASICs.
