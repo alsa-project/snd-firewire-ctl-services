a54e36cd0339c95b300fcd9bae768968efbb472e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-06-05 20:42:13 +0900

    motu-protocols: add crate of protocols for MOTU FireWire series
    This commit adds crate for protocols in MOTU FireWire series.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/motu/protocols/src/lib.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocols defined for Mark of the Unicorn FireWire series.
+//!
+//! The crate includes protocols defined by Mark of the Unicorn for its FireWire series.
