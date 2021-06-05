f59cc9cb55e2ad9cfaf5eeb487f7eefdb43bef04
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-06-05 20:42:13 +0900

    motu-runtime: minor code refactoring for runtime structure
    This commit moves the code of entry point for service runtime.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- services/src/bin/snd-firewire-motu-ctl-service.rs
+++ services/src/bin/snd-firewire-motu-ctl-service.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
 use snd_firewire_ctl_services::*;
-use motu_runtime::runtime::MotuRuntime;
+use motu_runtime::MotuRuntime;
 
 struct MotuServiceCmd;
 
