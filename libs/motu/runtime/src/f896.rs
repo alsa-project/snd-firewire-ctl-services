d86be04ec65fabf1797b33cee39a87881124f69c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-06-23 18:31:08 +0900

    motu-runtime: add controls of sampling clock for version 1 models
    This commit adds implementation of clock control for 828/896.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/motu/runtime/src/f896.rs
+++ libs/motu/runtime/src/f896.rs
@@ -10,29 +10,52 @@ use core::card_cntr::{CardCntr, CtlModel};
 
 use motu_protocols::version_1::*;
 
+use super::v1_ctls::*;
+
 const TIMEOUT_MS: u32 = 100;
 
 #[derive(Default)]
 pub struct F896 {
     proto: F896Protocol,
+    clk_ctls: V1ClkCtl,
 }
 
 impl CtlModel<SndMotu> for F896 {
-    fn load(&mut self, _: &SndMotu, _: &mut CardCntr) -> Result<(), Error> {
+    fn load(&mut self, _: &SndMotu, card_cntr: &mut CardCntr) -> Result<(), Error> {
+        self.clk_ctls.load(&self.proto, card_cntr)?;
         Ok(())
     }
 
-    fn read(&mut self, _: &SndMotu, _: &ElemId, _: &mut ElemValue) -> Result<bool, Error> {
-        Ok(false)
+    fn read(
+        &mut self,
+        unit: &SndMotu,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        if self
+            .clk_ctls
+            .read(unit, &self.proto, elem_id, elem_value, TIMEOUT_MS)?
+        {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(
         &mut self,
-        _: &SndMotu,
-        _: &ElemId,
-        _: &ElemValue,
-        _: &ElemValue,
+        unit: &SndMotu,
+        elem_id: &ElemId,
+        old: &ElemValue,
+        new: &ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self
+            .clk_ctls
+            .write(unit, &self.proto, elem_id, old, new, TIMEOUT_MS)?
+        {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
