2fead11e43ffa64aa948a30c7489d83ebff1f4f9
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-06 15:56:53 +0900

    efw-protocols: add trait for protocol
    Near future, the operation related to on-board flash memory will be
    implemented in protocols crate. For the case, the implementation
    should work without any assist of ALSA fireworks driver. On the
    other hand, current implementation for the rest of protocol heavily
    depends on the driver.
    
    This commit adds a trait as wrapper as quick solution.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/efw/protocols/src/lib.rs
+++ libs/efw/protocols/src/lib.rs
@@ -7,3 +7,30 @@
 //! module.
 
 pub mod transactions;
+
+use hinawa::SndEfwExtManual;
+
+/// The trait to represent protocol for Echo Audio Fireworks board module.
+pub trait EfwProtocol {
+    fn transaction_sync(
+        &mut self,
+        category: u32,
+        command: u32,
+        args: Option<&[u32]>,
+        params: Option<&mut [u32]>,
+        timeout_ms: u32,
+    ) -> Result<(), glib::Error>;
+}
+
+impl<O: SndEfwExtManual> EfwProtocol for O {
+    fn transaction_sync(
+        &mut self,
+        category: u32,
+        command: u32,
+        args: Option<&[u32]>,
+        params: Option<&mut [u32]>,
+        timeout_ms: u32,
+    ) -> Result<(), glib::Error> {
+        O::transaction_sync(self, category, command, args, params, timeout_ms).map(|_| ())
+    }
+}
