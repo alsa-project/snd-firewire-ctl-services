f0f6c1ecc4161041df4c6794fcd169882e858a4e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-06 15:56:53 +0900

    efw-protocols: add crate for protocol in Fireworks board module
    This commit adds protocols crate to which any implementation for
    protocol is put.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/efw/protocols/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol defined by Echo Audio Digital Corporation for Fireworks board module.
+//!
+//! The crate includes protocols defined by Echo Audio Digital Corporation for Fireworks board
+//! module.
