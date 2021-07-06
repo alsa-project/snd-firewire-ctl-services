alsa-project/snd-firewire-ctl-services

commit 30bb610c0e1cb8e0e558a8ecf970cc4712d01ecd

efw: use ElemValueAccessor to handle element values
The implementation of ElemValueAccessor for each type of element value is
used to handle element event.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/efw/src/output_ctl.rs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-use glib::Error;
+use glib::{Error, FileError};
 
 use core::card_cntr;
-
-use alsactl::{ElemValueExt, ElemValueExtManual};
+use core::elem_value_accessor::ElemValueAccessor;
 
 use super::transactions::{HwCap, PhysGroupType, NominalLevel, EfwPhysOutput, HwInfo};
 
@@ -69,33 +68,28 @@ impl<'a> OutputCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::OUT_VOL_NAME => {
-                let mut vals = vec![0; self.phys_outputs];
-                vals.iter_mut().enumerate().try_for_each(|(i, val)| {
-                    *val = EfwPhysOutput::get_vol(unit, i)?;
-                    Ok(())
+                ElemValueAccessor::<i32>::set_vals(elem_value, self.phys_outputs, |idx| {
+                    let val = EfwPhysOutput::get_vol(unit, idx)?;
+                    Ok(val)
                 })?;
-                elem_value.set_int(&vals);
                 Ok(true)
             }
             Self::OUT_MUTE_NAME => {
-                let mut vals = vec![false; self.phys_outputs];
-                vals.iter_mut().enumerate().try_for_each(|(i, val)| {
-                    *val = EfwPhysOutput::get_mute(unit, i)?;
-                    Ok(())
+                ElemValueAccessor::<bool>::set_vals(elem_value, self.phys_outputs, |idx| {
+                    let val = EfwPhysOutput::get_mute(unit, idx)?;
+                    Ok(val)
                 })?;
-                elem_value.set_bool(&vals);
                 Ok(true)
             }
             Self::OUT_NOMINAL_NAME => {
-                let mut vals = vec![0; self.phys_outputs];
-                vals.iter_mut().enumerate().try_for_each(|(i, val)| {
-                    let level = EfwPhysOutput::get_nominal(unit, i)?;
+                ElemValueAccessor::<u32>::set_vals(elem_value, self.phys_outputs, |idx| {
+                    let level = EfwPhysOutput::get_nominal(unit, idx)?;
                     if let Some(pos) = Self::OUT_NOMINAL_LEVELS.iter().position(|&l| l == level) {
-                        *val = pos as u32;
+                        Ok(pos as u32)
+                    } else {
+                        unreachable!();
                     }
-                    Ok(())
                 })?;
-                elem_value.set_enum(&vals);
                 Ok(true)
             }
             _ => Ok(false),
@@ -111,40 +105,25 @@ impl<'a> OutputCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::OUT_VOL_NAME => {
-                let mut vals = vec![0; self.phys_outputs * 2];
-                new.get_int(&mut vals[..self.phys_outputs]);
-                old.get_int(&mut vals[self.phys_outputs..]);
-                (0..self.phys_outputs).try_for_each(|i| {
-                    if vals[i] != vals[self.phys_outputs + i] {
-                        EfwPhysOutput::set_vol(unit, i, vals[i])?;
-                    }
-                    Ok(())
+                ElemValueAccessor::<i32>::get_vals(new, old, self.phys_outputs, |idx, val| {
+                    EfwPhysOutput::set_vol(unit, idx, val)
                 })?;
                 Ok(true)
             }
             Self::OUT_MUTE_NAME => {
-                let mut vals = vec![false; self.phys_outputs * 2];
-                new.get_bool(&mut vals[..self.phys_outputs]);
-                old.get_bool(&mut vals[self.phys_outputs..]);
-                (0..self.phys_outputs).try_for_each(|i| {
-                    if vals[i] != vals[self.phys_outputs + i] {
-                        EfwPhysOutput::set_mute(unit, i, vals[i])?;
-                    }
-                    Ok(())
+                ElemValueAccessor::<bool>::get_vals(new, old, self.phys_outputs, |idx, val| {
+                    EfwPhysOutput::set_mute(unit, idx, val)
                 })?;
                 Ok(true)
             }
             Self::OUT_NOMINAL_NAME => {
-                let mut vals = vec![0; self.phys_outputs * 2];
-                new.get_enum(&mut vals[..self.phys_outputs]);
-                old.get_enum(&mut vals[self.phys_outputs..]);
-                (0..self.phys_outputs).try_for_each(|i| {
-                    if vals[i] != vals[self.phys_outputs + i] {
-                        if let Some(&level) = Self::OUT_NOMINAL_LEVELS.iter().nth(vals[i] as usize) {
-                            EfwPhysOutput::set_nominal(unit, i, level)?;
-                        }
+                ElemValueAccessor::<bool>::get_vals(new, old, self.phys_outputs, |idx, val| {
+                    if let Some(&level) = Self::OUT_NOMINAL_LEVELS.iter().nth(val as usize) {
+                        EfwPhysOutput::set_nominal(unit, idx, level)
+                    } else {
+                        let label = "Invalid value for nominal level of output";
+                        Err(Error::new(FileError::Io, &label))
                     }
-                    Ok(())
                 })?;
                 Ok(true)
             }

libs/efw/src/clk_ctl.rs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-use glib::Error;
+use glib::{Error, FileError};
 
 use core::card_cntr;
+use core::elem_value_accessor::ElemValueAccessor;
 
 use hinawa::SndUnitExt;
-
-use alsactl::{ElemValueExt, ElemValueExtManual};
 
 use super::transactions::{ClkSrc, HwInfo, EfwHwCtl};
 
@@ -61,22 +60,28 @@ impl<'a> ClkCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::SRC_NAME => {
-                let (src, _) = EfwHwCtl::get_clock(unit)?;
-                if let Some(pos) = self.srcs.iter().position(|s| *s == src) {
-                    elem_value.set_enum(&[pos as u32]);
-                    Ok(true)
-                } else {
-                    Ok(false)
-                }
+                ElemValueAccessor::<u32>::set_val(elem_value, || {
+                    let (src, _) = EfwHwCtl::get_clock(unit)?;
+                    if let Some(pos) = self.srcs.iter().position(|s| *s == src) {
+                        Ok(pos as u32)
+                    } else {
+                        let label = "Unexpected value for source of clock";
+                        Err(Error::new(FileError::Io, &label))
+                    }
+                })?;
+                Ok(true)
             }
             Self::RATE_NAME => {
-                let (_, rate) = EfwHwCtl::get_clock(unit)?;
-                if let Some(pos) = self.rates.iter().position(|r| *r == rate) {
-                    elem_value.set_enum(&[pos as u32]);
-                    Ok(true)
-                } else {
-                    Ok(false)
-                }
+                ElemValueAccessor::<u32>::set_val(elem_value, || {
+                    let (_, rate) = EfwHwCtl::get_clock(unit)?;
+                    if let Some(pos) = self.rates.iter().position(|r| *r == rate) {
+                        Ok(pos as u32)
+                    } else {
+                        let label = "Unexpected value for rate of clock";
+                        Err(Error::new(FileError::Io, &label))
+                    }
+                })?;
+                Ok(true)
             }
             _ => Ok(false),
         }
@@ -91,34 +96,32 @@ impl<'a> ClkCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::SRC_NAME => {
-                let mut vals = [0];
-                new.get_enum(&mut vals);
-                if let Some(&src) = self.srcs.iter().nth(vals[0] as usize) {
-                    unit.lock()?;
-                    let res = EfwHwCtl::set_clock(unit, Some(src), None);
-                    let _ = unit.unlock();
-                    match res {
-                        Err(err) => Err(err),
-                        Ok(()) => Ok(true),
+                ElemValueAccessor::<u32>::get_val(new, |val| {
+                    if let Some(&src) = self.srcs.iter().nth(val as usize) {
+                        unit.lock()?;
+                        let res = EfwHwCtl::set_clock(unit, Some(src), None);
+                        let _ = unit.unlock();
+                        res
+                    } else {
+                        let label = "Invalid value for source of clock";
+                        Err(Error::new(FileError::Io, &label))
                     }
-                } else {
-                    Ok(false)
-                }
+                })?;
+                Ok(true)
             }
             Self::RATE_NAME => {
-                let mut vals = [0];
-                new.get_enum(&mut vals);
-                if let Some(&rate) = self.rates.iter().nth(vals[0] as usize) {
-                    unit.lock()?;
-                    let res = EfwHwCtl::set_clock(unit, None, Some(rate));
-                    let _ = unit.unlock();
-                    match res {
-                        Err(err) => Err(err),
-                        Ok(()) => Ok(true),
+                ElemValueAccessor::<u32>::get_val(new, |val| {
+                    if let Some(&rate) = self.rates.iter().nth(val as usize) {
+                        unit.lock()?;
+                        let res = EfwHwCtl::set_clock(unit, None, Some(rate));
+                        let _ = unit.unlock();
+                        res
+                    } else {
+                        let label = "Invalid value for rate of clock";
+                        Err(Error::new(FileError::Io, &label))
                     }
-                } else {
-                    Ok(false)
-                }
+                })?;
+                Ok(true)
             }
             _ => Ok(false),
         }

libs/efw/src/guitar_ctl.rs
@@ -2,9 +2,8 @@
 // Copyright (c) 2020 Takashi Sakamoto
 use glib::Error;
 
-use alsactl::{ElemValueExt, ElemValueExtManual};
-
 use core::card_cntr;
+use core::elem_value_accessor::ElemValueAccessor;
 
 use super::transactions::{HwInfo, HwCap, EfwGuitar};
 
@@ -54,18 +53,24 @@ impl<'a> GuitarCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::MANUAL_CHARGE_NAME => {
-                let state = EfwGuitar::get_charge_state(unit)?;
-                elem_value.set_bool(&[state.manual_charge]);
+                ElemValueAccessor::<bool>::set_val(elem_value, || {
+                    let state = EfwGuitar::get_charge_state(unit)?;
+                    Ok(state.manual_charge)
+                })?;
                 Ok(true)
             }
             Self::AUTO_CHARGE_NAME => {
-                let state = EfwGuitar::get_charge_state(unit)?;
-                elem_value.set_bool(&[state.auto_charge]);
+                ElemValueAccessor::<bool>::set_val(elem_value, || {
+                    let state = EfwGuitar::get_charge_state(unit)?;
+                    Ok(state.auto_charge)
+                })?;
                 Ok(true)
             }
             Self::SUSPEND_TO_CHARGE => {
-                let state = EfwGuitar::get_charge_state(unit)?;
-                elem_value.set_int(&[state.suspend_to_charge as i32]);
+                ElemValueAccessor::<i32>::set_val(elem_value, || {
+                    let state = EfwGuitar::get_charge_state(unit)?;
+                    Ok(state.suspend_to_charge as i32)
+                })?;
                 Ok(true)
             }
             _ => Ok(false),
@@ -81,27 +86,27 @@ impl<'a> GuitarCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::MANUAL_CHARGE_NAME => {
-                let mut vals = [false];
-                new.get_bool(&mut vals);
-                let mut state = EfwGuitar::get_charge_state(unit)?;
-                state.manual_charge = vals[0];
-                EfwGuitar::set_charge_state(unit, &state)?;
+                ElemValueAccessor::<bool>::get_val(new, |val| {
+                    let mut state = EfwGuitar::get_charge_state(unit)?;
+                    state.manual_charge = val;
+                    EfwGuitar::set_charge_state(unit, &state)
+                })?;
                 Ok(true)
             }
             Self::AUTO_CHARGE_NAME => {
-                let mut vals = [false];
-                new.get_bool(&mut vals);
-                let mut state = EfwGuitar::get_charge_state(unit)?;
-                state.auto_charge = vals[0];
-                EfwGuitar::set_charge_state(unit, &state)?;
+                ElemValueAccessor::<bool>::get_val(new, |val| {
+                    let mut state = EfwGuitar::get_charge_state(unit)?;
+                    state.auto_charge = val;
+                    EfwGuitar::set_charge_state(unit, &state)
+                })?;
                 Ok(true)
             }
             Self::SUSPEND_TO_CHARGE => {
-                let mut vals = [0];
-                new.get_int(&mut vals);
-                let mut state = EfwGuitar::get_charge_state(unit)?;
-                state.suspend_to_charge = vals[0] as u32;
-                EfwGuitar::set_charge_state(unit, &state)?;
+                ElemValueAccessor::<i32>::get_val(new, |val| {
+                    let mut state = EfwGuitar::get_charge_state(unit)?;
+                    state.suspend_to_charge = val as u32;
+                    EfwGuitar::set_charge_state(unit, &state)
+                })?;
                 Ok(true)
             }
             _ => Ok(false),

libs/efw/src/input_ctl.rs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-use glib::Error;
+use glib::{Error, FileError};
 
 use core::card_cntr;
-
-use alsactl::{ElemValueExt, ElemValueExtManual};
+use core::elem_value_accessor::ElemValueAccessor;
 
 use super::transactions::{HwCap, EfwPhysInput, NominalLevel, HwInfo};
 
@@ -59,26 +58,20 @@ impl<'a> InputCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::IN_NOMINAL_NAME => {
-                let mut vals = vec![0; self.phys_inputs];
-                match &self.cache {
-                    // For models with FPGA.
-                    Some(cache) => {
-                        vals.iter_mut().zip(cache.iter()).for_each(|(val, &level)| {
-                            *val = u32::from(level)
-                        });
+                ElemValueAccessor::<u32>::set_vals(elem_value, self.phys_inputs, |idx| {
+                    if let Some(cache) = &self.cache {
+                        // For models with FPGA.
+                        Ok(u32::from(cache[idx]))
+                    } else {
+                        // For models with DSP.
+                        let level = EfwPhysInput::get_nominal(unit, idx)?;
+                        if let Some(pos) = Self::IN_NOMINAL_LEVELS.iter().position(|&l| l == level) {
+                            Ok(pos as u32)
+                        } else {
+                            unreachable!();
+                        }
                     }
-                    // For models with DSP.
-                    None => {
-                        vals.iter_mut().enumerate().try_for_each(|(i, val)| {
-                            let level = EfwPhysInput::get_nominal(unit, i)?;
-                            if let Some(pos) = Self::IN_NOMINAL_LEVELS.iter().position(|&l| l == level) {
-                                *val = pos as u32;
-                            }
-                            Ok(())
-                        })?;
-                    }
-                }
-                elem_value.set_enum(&vals);
+                })?;
                 Ok(true)
             }
             _ => Ok(false),
@@ -94,25 +87,19 @@ impl<'a> InputCtl {
     ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::IN_NOMINAL_NAME => {
-                let mut vals = vec![0; self.phys_inputs * 2];
-                new.get_enum(&mut vals[..self.phys_inputs]);
-                old.get_enum(&mut vals[self.phys_inputs..]);
-                (0..self.phys_inputs).try_for_each(|i| {
-                    if vals[i] != vals[self.phys_inputs + i] {
-                        if let Some(&level) = Self::IN_NOMINAL_LEVELS.iter().nth(vals[i] as usize) {
-                            EfwPhysInput::set_nominal(unit, i, level)?;
+                ElemValueAccessor::<u32>::get_vals(new, old, self.phys_inputs, |idx, val| {
+                    if let Some(&level) = Self::IN_NOMINAL_LEVELS.iter().nth(val as usize) {
+                        EfwPhysInput::set_nominal(unit, idx, level)?;
+                        if let Some(cache) = &mut self.cache {
+                            // For FPGA models.
+                            cache[idx] = level;
                         }
+                        Ok(())
+                    } else {
+                        let label = "Invalid value for nominal level of input";
+                        Err(Error::new(FileError::Inval, &label))
                     }
-                    Ok(())
                 })?;
-
-                // For FPGA models.
-                if let Some(cache) = &mut self.cache {
-                    cache.iter_mut().zip(vals[0..self.phys_inputs].iter()).for_each(|(c, &val)| {
-                        *c = NominalLevel::from(val);
-                    });
-                }
-
                 Ok(true)
             }
             _ => Ok(false),

libs/efw/src/meter_ctl.rs
@@ -136,10 +136,7 @@ impl<'a> MeterCtl {
             }
             Self::MIDI_IN_DETECT => {
                 if let Some(meters) = &self.meters {
-                    let vals: Vec<bool> = (0..self.midi_inputs)
-                        .map(|i| meters.detected_midi_inputs[i])
-                        .collect();
-                    elem_value.set_bool(&vals);
+                    elem_value.set_bool(&meters.detected_midi_inputs[..self.midi_inputs]);
                     Ok(true)
                 } else {
                     Ok(false)
@@ -147,10 +144,7 @@ impl<'a> MeterCtl {
             }
             Self::MIDI_OUT_DETECT => {
                 if let Some(meters) = &self.meters {
-                    let vals: Vec<bool> = (0..self.midi_outputs)
-                        .map(|i| meters.detected_midi_inputs[i])
-                        .collect();
-                    elem_value.set_bool(&vals);
+                    elem_value.set_bool(&meters.detected_midi_outputs[..self.midi_outputs]);
                     Ok(true)
                 } else {
                     Ok(false)

commit acc6b74465ee4fa0e585aac997826703375d3b4e

efw: fix invalid access to enumerated element
The control element named as "output-nominal" delivers enumerated type of
value. However, when handling change, it's accessed as boolean type of
value. This brings unexpected operation to target device.

Fixes: 30bb610c0e1c ("efw: use ElemValueAccessor to handle element values")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/efw/src/output_ctl.rs
@@ -119,7 +119,7 @@ impl<'a> OutputCtl {
                 Ok(true)
             }
             Self::OUT_NOMINAL_NAME => {
-                ElemValueAccessor::<bool>::get_vals(new, old, self.phys_outputs, |idx, val| {
+                ElemValueAccessor::<u32>::get_vals(new, old, self.phys_outputs, |idx, val| {
                     if let Some(&level) = Self::OUT_NOMINAL_LEVELS.iter().nth(val as usize) {
                         EfwPhysOutput::set_nominal(unit, idx, level)
                     } else {