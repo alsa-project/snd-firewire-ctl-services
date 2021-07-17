aa86f8af2ea1bab0bab749c8773fa7ec81c58756
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-17 13:01:28 +0900

    bebob-runtime: esi: minor code refactoring for ESI  Quatafire 610
    While Ego Systems shipped several models with BeBoB solution, the models
    has less controls adjustable from software. This commit keeps single
    file for the models but rename stucture for Quatafire610 model.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/esi.rs
+++ libs/bebob/runtime/src/esi.rs
@@ -17,16 +17,16 @@ use bebob_protocols::*;
 
 use super::common_ctls::ClkCtl;
 
-pub struct QuatafireModel<'a>{
+const FCP_TIMEOUT_MS: u32 = 100;
+
+pub struct Quatafire610Model<'a>{
     avc: BebobAvc,
     clk_ctl: ClkCtl<'a>,
     input_ctl: InputCtl,
     output_ctl: OutputCtl,
 }
 
-impl<'a> QuatafireModel<'a> {
-    const FCP_TIMEOUT_MS: u32 = 100;
-
+impl<'a> Quatafire610Model<'a> {
     const CLK_DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr{
         subunit: MUSIC_SUBUNIT_0,
         plug_id: 0x01,
@@ -43,7 +43,7 @@ impl<'a> QuatafireModel<'a> {
     ];
 }
 
-impl<'a> Default for QuatafireModel<'a> {
+impl<'a> Default for Quatafire610Model<'a> {
     fn default() -> Self {
         Self{
             avc: Default::default(),
@@ -54,10 +54,10 @@ impl<'a> Default for QuatafireModel<'a> {
     }
 }
 
-impl<'a> CtlModel<SndUnit> for QuatafireModel<'a> {
+impl<'a> CtlModel<SndUnit> for Quatafire610Model<'a> {
     fn load(&mut self, unit: &mut SndUnit, card_cntr: &mut CardCntr) -> Result<(), Error> {
         self.avc.as_ref().bind(&unit.get_node())?;
-        self.clk_ctl.load(&self.avc, card_cntr, Self::FCP_TIMEOUT_MS)?;
+        self.clk_ctl.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.input_ctl.load(card_cntr)?;
         self.output_ctl.load(card_cntr)?;
         Ok(())
@@ -66,11 +66,11 @@ impl<'a> CtlModel<SndUnit> for QuatafireModel<'a> {
     fn read(&mut self, _: &mut SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        if self.clk_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)? {
+        if self.clk_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
-        } else if self.input_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)? {
+        } else if self.input_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
-        } else if self.output_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)? {
+        } else if self.output_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -80,11 +80,11 @@ impl<'a> CtlModel<SndUnit> for QuatafireModel<'a> {
     fn write(&mut self, unit: &mut SndUnit, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        if self.clk_ctl.write(unit, &self.avc, elem_id, old, new, Self::FCP_TIMEOUT_MS)? {
+        if self.clk_ctl.write(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
-        } else if self.input_ctl.write(&self.avc, elem_id, old, new, Self::FCP_TIMEOUT_MS)? {
+        } else if self.input_ctl.write(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
-        } else if self.output_ctl.write(&self.avc, elem_id, old, new, Self::FCP_TIMEOUT_MS)? {
+        } else if self.output_ctl.write(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -92,7 +92,7 @@ impl<'a> CtlModel<SndUnit> for QuatafireModel<'a> {
     }
 }
 
-impl<'a> NotifyModel<SndUnit, bool> for QuatafireModel<'a> {
+impl<'a> NotifyModel<SndUnit, bool> for Quatafire610Model<'a> {
     fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.clk_ctl.notified_elem_list);
     }
@@ -104,7 +104,7 @@ impl<'a> NotifyModel<SndUnit, bool> for QuatafireModel<'a> {
     fn read_notified_elem(&mut self, _: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.clk_ctl.read(&self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)
+        self.clk_ctl.read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
     }
 }
 
