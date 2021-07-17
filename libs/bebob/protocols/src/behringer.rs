8082e023f7df0659ad51ad02f4ebaf36dd154a9b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-17 13:42:22 +0900

    bebob-protocols: add trait and its implementations for source of sampling clock
    BeBoB solution gives standard way to configure source of sampling clock
    by some AV/C commands. This commit adds trait and its implementation for
    it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/behringer.rs
+++ libs/bebob/protocols/src/behringer.rs
@@ -8,10 +8,32 @@
 
 use super::*;
 
+use ta1394::ccm::{SignalAddr, SignalSubunitAddr, SignalUnitAddr};
+use ta1394::MUSIC_SUBUNIT_0;
+
 /// The protocol implementation for media and sampling clock of FCA 610.
 #[derive(Default)]
 pub struct Fca610ClkProtocol;
 
 impl MediaClockFrequencyOperation for Fca610ClkProtocol {
     const FREQ_LIST: &'static [u32] = &[44100, 48000, 88200, 96000];
+}
+
+impl SamplingClockSourceOperation for Fca610ClkProtocol {
+    const DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: MUSIC_SUBUNIT_0,
+        plug_id: 0x05,
+    });
+
+    const SRC_LIST: &'static [SignalAddr] = &[
+        // Device internal clock
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x04)),
+        // S/PDIF
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x03)),
+        // Firewire-bus. This is the same source as Internal in former BeBoB models.
+        SignalAddr::Subunit(SignalSubunitAddr {
+            subunit: MUSIC_SUBUNIT_0,
+            plug_id: 0x07,
+        }),
+    ];
 }
--- libs/bebob/protocols/src/maudio/normal.rs
+++ libs/bebob/protocols/src/maudio/normal.rs
@@ -8,12 +8,32 @@
 
 use crate::*;
 
+use ta1394::ccm::{SignalAddr, SignalSubunitAddr, SignalUnitAddr};
+use ta1394::MUSIC_SUBUNIT_0;
+
 /// The protocol implementation for media and sampling clock of FireWire 410.
 #[derive(Default)]
 pub struct Fw410ClkProtocol;
 
 impl MediaClockFrequencyOperation for Fw410ClkProtocol {
     const FREQ_LIST: &'static [u32] = &[44100, 48000, 88200, 96000, 176400, 192000];
+}
+
+impl SamplingClockSourceOperation for Fw410ClkProtocol {
+    const DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: MUSIC_SUBUNIT_0,
+        plug_id: 0x01,
+    });
+
+    const SRC_LIST: &'static [SignalAddr] = &[
+        // Internal
+        SignalAddr::Subunit(SignalSubunitAddr {
+            subunit: MUSIC_SUBUNIT_0,
+            plug_id: 0x01,
+        }),
+        // S/PDIF
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x02)),
+    ];
 }
 
 /// The protocol implementation for media and sampling clock of FireWire Solo.
@@ -24,6 +44,23 @@ impl MediaClockFrequencyOperation for SoloClkProtocol {
     const FREQ_LIST: &'static [u32] = &[44100, 48000, 88200, 96000];
 }
 
+impl SamplingClockSourceOperation for SoloClkProtocol {
+    const DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: MUSIC_SUBUNIT_0,
+        plug_id: 0x01,
+    });
+
+    const SRC_LIST: &'static [SignalAddr] = &[
+        // Internal
+        SignalAddr::Subunit(SignalSubunitAddr {
+            subunit: MUSIC_SUBUNIT_0,
+            plug_id: 0x01,
+        }),
+        // S/PDIF
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x01)),
+    ];
+}
+
 /// The protocol implementation for media and sampling clock of FireWire Audiophile.
 #[derive(Default)]
 pub struct AudiophileClkProtocol;
@@ -32,10 +69,42 @@ impl MediaClockFrequencyOperation for AudiophileClkProtocol {
     const FREQ_LIST: &'static [u32] = &[44100, 48000, 88200, 96000];
 }
 
+impl SamplingClockSourceOperation for AudiophileClkProtocol {
+    const DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: MUSIC_SUBUNIT_0,
+        plug_id: 0x01,
+    });
+
+    const SRC_LIST: &'static [SignalAddr] = &[
+        // Internal
+        SignalAddr::Subunit(SignalSubunitAddr {
+            subunit: MUSIC_SUBUNIT_0,
+            plug_id: 0x01,
+        }),
+        // S/PDIF
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x02)),
+    ];
+}
+
 /// The protocol implementation for media and sampling clock of Ozonic.
 #[derive(Default)]
 pub struct OzonicClkProtocol;
 
 impl MediaClockFrequencyOperation for OzonicClkProtocol {
     const FREQ_LIST: &'static [u32] = &[44100, 48000, 88200, 96000];
+}
+
+impl SamplingClockSourceOperation for OzonicClkProtocol {
+    const DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: MUSIC_SUBUNIT_0,
+        plug_id: 0x05,
+    });
+
+    const SRC_LIST: &'static [SignalAddr] = &[
+        // Internal
+        SignalAddr::Subunit(SignalSubunitAddr {
+            subunit: MUSIC_SUBUNIT_0,
+            plug_id: 0x05,
+        }),
+    ];
 }
