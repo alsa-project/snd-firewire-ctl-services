62f9d432314e2bb2b733aebbe59f987fda478a32
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-18 09:52:37 +0900

    bebob-protocols: add trait and its implementations for source of sampling clock
    BeBoB solution gives standard way to configure source of sampling clock
    by some AV/C commands. This commit adds trait and its implementation for
    it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/stanton.rs
+++ libs/bebob/protocols/src/stanton.rs
@@ -8,10 +8,28 @@
 
 use super::*;
 
+use ta1394::ccm::{SignalAddr, SignalSubunitAddr};
+use ta1394::MUSIC_SUBUNIT_0;
+
 /// The protocol implementation for media and sampling clock of Scratchamp.
 #[derive(Default)]
 pub struct ScratchampClkProtocol;
 
 impl MediaClockFrequencyOperation for ScratchampClkProtocol {
     const FREQ_LIST: &'static [u32] = &[44100, 48000, 88200, 96000];
+}
+
+impl SamplingClockSourceOperation for ScratchampClkProtocol {
+    const DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: MUSIC_SUBUNIT_0,
+        plug_id: 0x05,
+    });
+
+    const SRC_LIST: &'static [SignalAddr] = &[
+        // Internal
+        SignalAddr::Subunit(SignalSubunitAddr {
+            subunit: MUSIC_SUBUNIT_0,
+            plug_id: 0x05,
+        }),
+    ];
 }
