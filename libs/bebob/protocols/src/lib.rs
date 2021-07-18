b26bdf190c1295244edabfed2175f8407898b6d3
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-18 09:52:37 +0900

    bebob-protocols: add crate for protocol for BeBoB solution
    This commit adds protocols crate to which any implementation is put for
    DM1000, DM1100, DM1500 ASICs for BridgeCo. Enhanced Break Out Box (BeBoB)
    solution.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/bebob/protocols/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocols defined for BridgeCo. Enhanced Break Out Box (BeBoB) solution.
+//!
+//! The crate includes various kind of protocols defined by BridgeCo. AG and application vendors
+//! for DM1000, DM1100, and DM1500 ASICs with its BridgeCo. Enhanced Break Out Box (BeBoB) solution.
