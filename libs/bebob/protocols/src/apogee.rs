49f9087a8c4d404972755ed834a5faf3ef1541d0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-18 09:52:37 +0900

    bebob-protocols: add modules for protocol implementations of supported models
    This commit adds skeleton of some modules for supported moels.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/bebob/protocols/src/apogee.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol implementation for Apogee Electronics FireWire models.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for protocol
+//! defined by Apogee Electronics for FireWire models.
+
+pub mod ensemble;
---
+++ libs/bebob/protocols/src/esi.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol implementation for ESI Quatafire series.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for protocol
+//! defined by Ego Systems (ESI) for Quatafire series.
