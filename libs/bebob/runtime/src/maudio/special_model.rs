3722e751258df73b6dc826762d1b8c74a115c10e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-18 09:52:37 +0900

    bebob-runtime: maudio: differentiate FireWire 1814 from ProjectMix I/O using generic structure
    M-Audio FireWire 1814 and ProjectMix I/O are different in a point of
    supported frequencies of media clock.
    
    This commit differenciates these models by using generic structure.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/maudio/special_model.rs
+++ libs/bebob/runtime/src/maudio/special_model.rs
@@ -10,35 +10,48 @@ use alsactl::{ElemId, ElemValue};
 
 use core::card_cntr::*;
 
-use bebob_protocols::*;
+use bebob_protocols::{*, maudio::special::*};
 
-use super::special_ctls::{ClkCtl, MeterCtl, StateCache, MixerCtl, InputCtl, OutputCtl, AuxCtl, HpCtl};
+use crate::common_ctls::*;
+use super::special_ctls::{MeterCtl, StateCache, MixerCtl, InputCtl, OutputCtl, AuxCtl, HpCtl};
 
-pub struct SpecialModel {
+pub type Fw1814Model = SpecialModel<Fw1814ClkProtocol>;
+pub type ProjectMixModel = SpecialModel<ProjectMixClkProtocol>;
+
+pub struct SpecialModel<T: MediaClockFrequencyOperation + Default> {
     avc: BebobAvc,
     req: FwReq,
-    clk_ctl: ClkCtl,
+    clk_ctl: ClkCtl<T>,
     meter_ctl: MeterCtl,
     cache: StateCache,
 }
 
-impl SpecialModel {
-    pub fn new(is_fw1814: bool) -> Self {
-        SpecialModel {
+const FCP_TIMEOUT_MS: u32 = 200;
+
+#[derive(Default)]
+struct ClkCtl<T: MediaClockFrequencyOperation + Default>(Vec<ElemId>, T);
+
+impl<T: MediaClockFrequencyOperation + Default> MediaClkFreqCtlOperation<T> for ClkCtl<T> {}
+
+impl<T: MediaClockFrequencyOperation + Default> Default for SpecialModel<T> {
+    fn default() -> Self {
+        Self {
             avc: Default::default(),
             req: Default::default(),
-            clk_ctl: ClkCtl::new(is_fw1814),
+            clk_ctl: Default::default(),
             meter_ctl: MeterCtl::new(),
             cache: StateCache::new(),
         }
     }
 }
 
-impl CtlModel<SndUnit> for SpecialModel {
+impl<T: MediaClockFrequencyOperation + Default> CtlModel<SndUnit> for SpecialModel<T> {
     fn load(&mut self, unit: &mut SndUnit, card_cntr: &mut CardCntr) -> Result<(), Error> {
         self.avc.as_ref().bind(&unit.get_node())?;
 
-        self.clk_ctl.load(card_cntr)?;
+        self.clk_ctl.load_freq(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
+
         self.meter_ctl.load(unit, &self.req, &self.avc, card_cntr)?;
 
         MixerCtl::load(&mut self.cache, card_cntr)?;
@@ -55,7 +68,7 @@ impl CtlModel<SndUnit> for SpecialModel {
     fn read(&mut self, _: &mut SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        if self.clk_ctl.read(&self.avc, elem_id, elem_value)? {
+        if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if MixerCtl::read(&mut self.cache, elem_id, elem_value)? {
             Ok(true)
@@ -75,7 +88,7 @@ impl CtlModel<SndUnit> for SpecialModel {
     fn write(&mut self, unit: &mut SndUnit, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        if self.clk_ctl.write(unit, &self.avc, elem_id, old, new)? {
+        if self.clk_ctl.write_freq(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS * 3)? {
             Ok(true)
         } else if MixerCtl::write(&mut self.cache, unit, &self.req, elem_id, old, new)? {
             Ok(true)
@@ -93,7 +106,7 @@ impl CtlModel<SndUnit> for SpecialModel {
     }
 }
 
-impl MeasureModel<SndUnit> for SpecialModel {
+impl<T: MediaClockFrequencyOperation + Default> MeasureModel<SndUnit> for SpecialModel<T> {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.meter_ctl.measure_elems);
     }
@@ -109,9 +122,9 @@ impl MeasureModel<SndUnit> for SpecialModel {
     }
 }
 
-impl NotifyModel<SndUnit, bool> for SpecialModel {
+impl<T: MediaClockFrequencyOperation + Default> NotifyModel<SndUnit, bool> for SpecialModel<T> {
     fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
-        elem_id_list.extend_from_slice(&self.clk_ctl.notified_elem_list);
+        elem_id_list.extend_from_slice(&self.clk_ctl.0);
     }
 
     fn parse_notification(&mut self, _: &mut SndUnit, _: &bool) -> Result<(), Error> {
@@ -121,6 +134,26 @@ impl NotifyModel<SndUnit, bool> for SpecialModel {
     fn read_notified_elem(&mut self, _: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.clk_ctl.read(&self.avc, elem_id, elem_value)
+        self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use alsactl::CardError;
+
+    #[test]
+    fn test_clk_ctl_definition() {
+        let mut card_cntr = CardCntr::new();
+        let mut ctl = ClkCtl::<Fw1814ClkProtocol>::default();
+
+        let error = ctl.load_freq(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let mut ctl = ClkCtl::<ProjectMixClkProtocol>::default();
+
+        let error = ctl.load_freq(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }
