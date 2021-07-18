abc1d7f6c472cffbd3e57e7f6b6ba8783522c6f8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-18 09:52:37 +0900

    bebob-protocols: maudio: add implementation of meter protocol for normal models
    M-Audio utilizes specific range of offset in node for metering
    information. This commit adds trait and implementation for it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/maudio.rs
+++ libs/bebob/protocols/src/maudio.rs
@@ -10,4 +10,28 @@ pub mod normal;
 pub mod pfl;
 pub mod special;
 
+use glib::Error;
+
+use hinawa::{FwNode, FwReq, FwReqExtManual};
+
 const MAUDIO_OUI: [u8; 3] = [0x00, 0x0d, 0x6c];
+
+const BASE_OFFSET: u64 = 0xffc700000000;
+const METER_OFFSET: u64 = 0x00600000;
+
+fn read_block(
+    req: &FwReq,
+    node: &FwNode,
+    offset: u64,
+    frames: &mut [u8],
+    timeout_ms: u32,
+) -> Result<(), Error> {
+    req.transaction_sync(
+        node,
+        hinawa::FwTcode::ReadBlockRequest,
+        BASE_OFFSET + offset,
+        frames.len(),
+        frames,
+        timeout_ms,
+    )
+}
--- libs/bebob/protocols/src/maudio/normal.rs
+++ libs/bebob/protocols/src/maudio/normal.rs
@@ -38,6 +38,19 @@ impl SamplingClockSourceOperation for Fw410ClkProtocol {
     ];
 }
 
+/// The protocol implementation for meter in FireWire 410.
+#[derive(Default)]
+pub struct Fw410MeterProtocol;
+
+impl MaudioNormalMeterProtocol for Fw410MeterProtocol {
+    const PHYS_INPUT_COUNT: usize = 4;
+    const STREAM_INPUT_COUNT: usize = 0;
+    const PHYS_OUTPUT_COUNT: usize = 10;
+    const ROTARY_COUNT: usize = 1;
+    const HAS_SWITCH: bool = false;
+    const HAS_SYNC_STATUS: bool = true;
+}
+
 /// The protocol implementation for media and sampling clock of FireWire Solo.
 #[derive(Default)]
 pub struct SoloClkProtocol;
@@ -61,6 +74,19 @@ impl SamplingClockSourceOperation for SoloClkProtocol {
         // S/PDIF
         SignalAddr::Unit(SignalUnitAddr::Ext(0x01)),
     ];
+}
+
+/// The protocol implementation for meter in FireWire Solo.
+#[derive(Default)]
+pub struct SoloMeterProtocol;
+
+impl MaudioNormalMeterProtocol for SoloMeterProtocol {
+    const PHYS_INPUT_COUNT: usize = 4;
+    const STREAM_INPUT_COUNT: usize = 4;
+    const PHYS_OUTPUT_COUNT: usize = 4;
+    const ROTARY_COUNT: usize = 0;
+    const HAS_SWITCH: bool = false;
+    const HAS_SYNC_STATUS: bool = true;
 }
 
 /// The protocol implementation for media and sampling clock of FireWire Audiophile.
@@ -88,6 +114,19 @@ impl SamplingClockSourceOperation for AudiophileClkProtocol {
     ];
 }
 
+/// The protocol implementation for meter in FireWire Audiophile.
+#[derive(Default)]
+pub struct AudiophileMeterProtocol;
+
+impl MaudioNormalMeterProtocol for AudiophileMeterProtocol {
+    const PHYS_INPUT_COUNT: usize = 4;
+    const STREAM_INPUT_COUNT: usize = 0;
+    const PHYS_OUTPUT_COUNT: usize = 6;
+    const ROTARY_COUNT: usize = 2;
+    const HAS_SWITCH: bool = true;
+    const HAS_SYNC_STATUS: bool = true;
+}
+
 /// The protocol implementation for media and sampling clock of Ozonic.
 #[derive(Default)]
 pub struct OzonicClkProtocol;
@@ -109,6 +148,19 @@ impl SamplingClockSourceOperation for OzonicClkProtocol {
             plug_id: 0x05,
         }),
     ];
+}
+
+/// The protocol implementation for meter in Ozonic.
+#[derive(Default)]
+pub struct OzonicMeterProtocol;
+
+impl MaudioNormalMeterProtocol for OzonicMeterProtocol {
+    const PHYS_INPUT_COUNT: usize = 4;
+    const STREAM_INPUT_COUNT: usize = 4;
+    const PHYS_OUTPUT_COUNT: usize = 4;
+    const ROTARY_COUNT: usize = 0;
+    const HAS_SWITCH: bool = false;
+    const HAS_SYNC_STATUS: bool = false;
 }
 
 /// The state of switch with LED specific to FireWire Audiophile.
@@ -184,5 +236,183 @@ impl AvcControl for AudiophileLedSwitch {
 
     fn parse_operands(&mut self, addr: &AvcAddr, operands: &[u8]) -> Result<(), Error> {
         AvcControl::parse_operands(&mut self.op, addr, operands)
+    }
+}
+
+/// The structure to express metering information.
+pub struct MaudioNormalMeter {
+    pub phys_inputs: Vec<i32>,
+    pub stream_inputs: Option<Vec<i32>>,
+    pub phys_outputs: Vec<i32>,
+    pub headphone: Option<[i32; 2]>,
+    pub aux_output: Option<[i32; 2]>,
+    pub rotaries: Option<[i32; 2]>,
+    pub switch: Option<AudiophileSwitchState>,
+    pub sync_status: Option<bool>,
+
+    cache: Vec<u8>,
+}
+
+/// The trait for meter protocol specific to M-Audio FireWire series.
+pub trait MaudioNormalMeterProtocol {
+    const PHYS_INPUT_COUNT: usize;
+    const STREAM_INPUT_COUNT: usize;
+    const PHYS_OUTPUT_COUNT: usize;
+    const ROTARY_COUNT: usize;
+    const HAS_SWITCH: bool;
+    const HAS_SYNC_STATUS: bool;
+
+    const LEVEL_MIN: i32 = 0;
+    const LEVEL_MAX: i32 = i32::MAX;
+    const LEVEL_STEP: i32 = 0x100;
+
+    const ROTARY_MIN: i32 = 0;
+    const ROTARY_MAX: i32 = i16::MAX as i32;
+    const ROTARY_STEP: i32 = 0x200;
+
+    fn create_meter() -> MaudioNormalMeter {
+        let mut meter = MaudioNormalMeter {
+            phys_inputs: vec![0; Self::PHYS_INPUT_COUNT],
+            stream_inputs: Default::default(),
+            phys_outputs: vec![0; Self::PHYS_OUTPUT_COUNT],
+            headphone: Default::default(),
+            aux_output: Default::default(),
+            rotaries: Default::default(),
+            switch: Default::default(),
+            sync_status: Default::default(),
+            cache: vec![0; Self::calculate_meter_frame_size()],
+        };
+
+        if Self::STREAM_INPUT_COUNT > 0 {
+            meter.stream_inputs = Some(vec![0; Self::STREAM_INPUT_COUNT]);
+        } else {
+            meter.headphone = Some(Default::default());
+            meter.aux_output = Some(Default::default());
+        }
+
+        if Self::ROTARY_COUNT > 0 {
+            meter.rotaries = Some(Default::default());
+        }
+
+        if Self::HAS_SWITCH {
+            meter.switch = Some(Default::default());
+        }
+
+        if Self::HAS_SYNC_STATUS {
+            meter.sync_status = Some(Default::default());
+        }
+
+        meter
+    }
+
+    fn calculate_meter_frame_size() -> usize {
+        let mut size = Self::PHYS_INPUT_COUNT + Self::PHYS_OUTPUT_COUNT;
+
+        if Self::STREAM_INPUT_COUNT > 0 {
+            size += Self::STREAM_INPUT_COUNT;
+        } else {
+            // Plus headphone-1 and -2, aux-1 and -2.
+            size += 4;
+        }
+
+        if Self::ROTARY_COUNT > 0 || Self::HAS_SWITCH || Self::HAS_SYNC_STATUS {
+            size += 1;
+        }
+
+        size * 4
+    }
+
+    fn read_meter(
+        req: &FwReq,
+        node: &FwNode,
+        meter: &mut MaudioNormalMeter,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let frame = &mut meter.cache;
+
+        // For rotaries, switch, and sync_status if available.
+        let mut bitmap = [0; 4];
+        let pos = frame.len() - 4;
+        bitmap.copy_from_slice(&frame[pos..]);
+
+        read_block(req, node, METER_OFFSET, frame, timeout_ms)?;
+
+        let mut quadlet = [0; 4];
+
+        meter.phys_inputs.iter_mut().enumerate().for_each(|(i, m)| {
+            let pos = i * 4;
+            quadlet.copy_from_slice(&frame[pos..(pos + 4)]);
+            *m = i32::from_be_bytes(quadlet);
+        });
+
+        if let Some(stream_inputs) = &mut meter.stream_inputs {
+            stream_inputs.iter_mut().enumerate().for_each(|(i, m)| {
+                let pos = (Self::PHYS_INPUT_COUNT + i) * 4;
+                quadlet.copy_from_slice(&frame[pos..(pos + 4)]);
+                *m = i32::from_be_bytes(quadlet);
+            });
+        }
+
+        meter
+            .phys_outputs
+            .iter_mut()
+            .enumerate()
+            .for_each(|(i, m)| {
+                let pos = (Self::PHYS_INPUT_COUNT + Self::STREAM_INPUT_COUNT + i) * 4;
+                quadlet.copy_from_slice(&frame[pos..(pos + 4)]);
+                *m = i32::from_be_bytes(quadlet);
+            });
+
+        if let Some(headphone) = &mut meter.headphone {
+            headphone.iter_mut().enumerate().for_each(|(i, m)| {
+                let pos = (Self::PHYS_INPUT_COUNT + Self::PHYS_OUTPUT_COUNT + i) * 4;
+                quadlet.copy_from_slice(&frame[pos..(pos + 4)]);
+                *m = i32::from_be_bytes(quadlet);
+            });
+        }
+
+        if let Some(aux_output) = &mut meter.aux_output {
+            aux_output.iter_mut().enumerate().for_each(|(i, m)| {
+                let pos = (Self::PHYS_INPUT_COUNT + Self::PHYS_OUTPUT_COUNT + 2 + i) * 4;
+                quadlet.copy_from_slice(&frame[pos..(pos + 4)]);
+                *m = i32::from_be_bytes(quadlet);
+            });
+        }
+
+        if let Some(rotaries) = &mut meter.rotaries {
+            rotaries.iter_mut().enumerate().for_each(|(i, r)| {
+                let shift = i * 4;
+                if (bitmap[1] ^ frame[frame.len() - 3]) & (0x0f << shift) > 0 {
+                    let flag = (frame[frame.len() - 3] >> shift) & 0x0f;
+                    if flag == 0x01 {
+                        if *r <= Self::ROTARY_MAX - Self::ROTARY_STEP {
+                            *r += Self::ROTARY_STEP;
+                        }
+                    } else if flag == 0x02 {
+                        if *r >= Self::ROTARY_MIN + Self::ROTARY_STEP {
+                            *r -= Self::ROTARY_STEP;
+                        }
+                    }
+                }
+            });
+        }
+
+        if let Some(switch) = &mut meter.switch {
+            if bitmap[0] ^ frame[frame.len() - 4] & 0xf0 > 0 {
+                if bitmap[0] & 0xf0 > 0 {
+                    *switch = match *switch {
+                        AudiophileSwitchState::Off => AudiophileSwitchState::A,
+                        AudiophileSwitchState::A => AudiophileSwitchState::B,
+                        AudiophileSwitchState::B => AudiophileSwitchState::Off,
+                    };
+                }
+            }
+        }
+
+        if let Some(sync_status) = &mut meter.sync_status {
+            *sync_status = frame[frame.len() - 1] > 0;
+        }
+
+        Ok(())
     }
 }
