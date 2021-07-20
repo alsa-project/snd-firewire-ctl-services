db9537527d8c766340167938bd66949d68a97a4e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-runtime: presonus: use implementation of clock operation for Inspire 1394
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/presonus/inspire1394_model.rs
+++ libs/bebob/runtime/src/presonus/inspire1394_model.rs
@@ -10,29 +10,77 @@ use alsactl::{ElemId, ElemValue};
 
 use core::card_cntr::*;
 
-use bebob_protocols::*;
+use bebob_protocols::{*, presonus::inspire1394::*};
+
+use crate::common_ctls::*;
 
 #[derive(Default)]
 pub struct Inspire1394Model {
     avc: BebobAvc,
+    clk_ctl: ClkCtl,
+}
+
+const FCP_TIMEOUT_MS: u32 = 100;
+
+#[derive(Default)]
+struct ClkCtl(Vec<ElemId>);
+
+impl MediaClkFreqCtlOperation<Inspire1394ClkProtocol> for ClkCtl {}
+
+impl SamplingClkSrcCtlOperation<Inspire1394ClkProtocol> for ClkCtl {
+    const SRC_LABELS: &'static [&'static str] = &["Internal"];
 }
 
 impl CtlModel<SndUnit> for Inspire1394Model {
-    fn load(&mut self, unit: &mut SndUnit, _: &mut CardCntr) -> Result<(), Error> {
+    fn load(&mut self, unit: &mut SndUnit, card_cntr: &mut CardCntr) -> Result<(), Error> {
         self.avc.as_ref().bind(&unit.get_node())?;
+
+        self.clk_ctl.load_freq(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
+
+        self.clk_ctl.load_src(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
         Ok(())
     }
 
-    fn read(&mut self, _: &mut SndUnit, _: &ElemId, _: &mut ElemValue)
+    fn read(&mut self, _: &mut SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        Ok(false)
+        if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.clk_ctl.read_src(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
-    fn write(&mut self, _: &mut SndUnit, _: &ElemId, _: &ElemValue, _: &ElemValue)
+    fn write(&mut self, unit: &mut SndUnit, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        Ok(false)
+        if self.clk_ctl.write_freq(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS * 3)? {
+            Ok(true)
+        } else if self.clk_ctl.write_src(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl NotifyModel<SndUnit, bool> for Inspire1394Model {
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
+        elem_id_list.extend_from_slice(&self.clk_ctl.0);
+    }
+
+    fn parse_notification(&mut self, _: &mut SndUnit, _: &bool) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn read_notified_elem(&mut self, _: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
+        -> Result<bool, Error>
+    {
+        self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
     }
 }
