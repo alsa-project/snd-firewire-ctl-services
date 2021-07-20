e314e95e7c532fa70aead6d4638750eb54562893
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-protocols: terratec: add protocol implementation of level operation for Aureon
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/terratec/aureon.rs
+++ libs/bebob/protocols/src/terratec/aureon.rs
@@ -5,6 +5,27 @@
 //!
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by Terratec for Aureon 7.1 FW.
+//!
+//! ## Diagram of internal signal flow
+//!
+//! ```text
+//! analog-input-1/2 ----------+
+//! analog-input-3/4 ----------|-+
+//! analog-input-5/6 ----------|-|-+
+//! digital-input-1/2 ---------|-|-|-+
+//!                            | | | |
+//!                       (one source only)
+//!                       monitor-source-1/2 -------------------> stream-output-1/2
+//!                                |
+//!                                v
+//!                          ++=========++
+//! stream-input-1/2 --------||         || -> mixer-output-7/8 -> analog-output-7/8
+//! stream-input-3/4 --------|| 10 x  8 || -> mixer-output-5/6 -> analog-output-5/6
+//! stream-input-5/6 --------||  mixer  || -> mixer-output-2/3 -> analog-output-3/4
+//! stream-input-7/8 --------||         || -> mixer-output-1/2 -> analog-output-1/2
+//!        |                 +==========++           |
+//!        +----------------------------------------or----------> digital-output-1/2
+//! ```
 
 use crate::*;
 
@@ -25,5 +46,43 @@ impl SamplingClockSourceOperation for AureonClkProtocol {
     const SRC_LIST: &'static [SignalAddr] = &[
         // Internal.
         SignalAddr::Subunit(SignalSubunitAddr{subunit: MUSIC_SUBUNIT_0, plug_id: 0x03}),
+    ];
+}
+
+/// The protocol implementation of mixer output.
+#[derive(Default)]
+pub struct AureonMixerOutputProtocol;
+
+impl AvcLevelOperation for AureonMixerOutputProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x01, AudioCh::Each(0)), // mixer-output-1
+        (0x01, AudioCh::Each(1)), // mixer-output-2
+        (0x01, AudioCh::Each(2)), // mixer-output-3
+        (0x01, AudioCh::Each(3)), // mixer-output-4
+        (0x01, AudioCh::Each(4)), // mixer-output-5
+        (0x01, AudioCh::Each(5)), // mixer-output-6
+        (0x01, AudioCh::Each(6)), // mixer-output-7
+        (0x01, AudioCh::Each(7)), // mixer-output-8
+    ];
+}
+
+/// The protocol implementation of analog input.
+#[derive(Default)]
+pub struct AureonPhysInputProtocol;
+
+impl AvcLevelOperation for AureonPhysInputProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x02, AudioCh::All), // analog-input-1/2
+        (0x03, AudioCh::All), // analog-input-3/4
+    ];
+}
+
+/// The protocol implementation of monitor output.
+#[derive(Default)]
+pub struct AureonMonitorOutputProtocol;
+
+impl AvcLevelOperation for AureonMonitorOutputProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x04, AudioCh::All), // monitor-output-1/2
     ];
 }
