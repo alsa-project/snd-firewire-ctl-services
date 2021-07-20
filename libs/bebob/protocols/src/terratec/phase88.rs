2eeac9f165d7d8f3ef5a99dc3ade23bd7bb75092
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-protocols: terratec: add protocol implementation of level operations
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/terratec/phase88.rs
+++ libs/bebob/protocols/src/terratec/phase88.rs
@@ -5,6 +5,35 @@
 //!
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by Terratec for Phase 88 FW.
+//!
+//! ## Diagram of internal signal flow
+//!
+//! ```text
+//!                        analog-input-1/2 ------+------------> stream-output-1/2
+//!                        analog-input-3/4 ------|------------> stream-output-3/4
+//!                        analog-input-5/6 ------|-|-+--------> stream-output-5/6
+//! line-input-7/8 ---or-> analog-input-7/8 ------|-|-|-+------> stream-output-7/8
+//! mic-input-7/8  ---+                           | | | |
+//!                        digital-input-1/2 -----|-|-|-|-+----> stream-output-9/10
+//!                                               | | | | |
+//!                                               v v v v v
+//!                                             ++=========++
+//!                                             ||         ||
+//!                        stream-source-1/2 -> || 12 x  2 ||
+//!                        (one source only)    ||  mixer  ||
+//!                            ^ ^ ^ ^ ^        ||         ||
+//!                            | | | | |        ++---------++
+//!                            | | | | |              v
+//!                            | | | | |       mixer-output-1/2
+//!                            | | | | |              v
+//!                            | | | | |     (one destination only)
+//!                            | | | | |          | | | | |
+//! stream-input-1/2 ----------+-|-|-|-|---------or-|-|-|-|----> analog-output-1/2
+//! stream-input-3/4 ------------+-|-|-|-----------or-|-|-|----> analog-output-3/4
+//! stream-input-5/6 --------------+-|-|-------------or-|-|----> analog-output-5/6
+//! stream-input-7/8 ----------------+-|---------------or-|----> analog-output-7/8
+//! stream-input-9/10 -----------------+-----------------or----> digital-output-1/2
+//! ```
 
 use crate::*;
 
@@ -72,4 +101,45 @@ impl SamplingClockSourceOperation for Phase88ClkProtocol {
 
         Ok(())
     }
+}
+
+/// The protocol implementation of source of physical input to mixer.
+#[derive(Default)]
+pub struct Phase88MixerPhysSourceProtocol;
+
+impl AvcLevelOperation for Phase88MixerPhysSourceProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x02, AudioCh::Each(0)), // analog-input-1
+        (0x02, AudioCh::Each(1)), // analog-input-2
+        (0x03, AudioCh::Each(0)), // analog-input-3
+        (0x03, AudioCh::Each(1)), // analog-input-4
+        (0x04, AudioCh::Each(0)), // analog-input-5
+        (0x04, AudioCh::Each(1)), // analog-input-6
+        (0x05, AudioCh::Each(0)), // analog-input-7
+        (0x05, AudioCh::Each(1)), // analog-input-8
+        (0x06, AudioCh::Each(0)), // digital-input-1
+        (0x06, AudioCh::Each(1)), // digital-input-2
+    ];
+}
+
+/// The protocol implementation of source of stream input to mixer.
+#[derive(Default)]
+pub struct Phase88MixerStreamSourceProtocol;
+
+impl AvcLevelOperation for Phase88MixerStreamSourceProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x07, AudioCh::Each(0)), // stream-source-1
+        (0x07, AudioCh::Each(1)), // stream-source-2
+    ];
+}
+
+/// The protocol implementation of mixer output.
+#[derive(Default)]
+pub struct Phase88MixerOutputProtocol;
+
+impl AvcLevelOperation for Phase88MixerOutputProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x00, AudioCh::Each(0)),
+        (0x01, AudioCh::Each(1)),
+    ];
 }
