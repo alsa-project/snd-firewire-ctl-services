388b72b9d101c209a8fa272afee229cc3ebaaf21
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-protocols: apogee: add diagram for Ensemble
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/apogee/ensemble.rs
+++ libs/bebob/protocols/src/apogee/ensemble.rs
@@ -5,6 +5,55 @@
 //!
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by Apogee Electronics Ensemble FireWire.
+//!
+//! ## Diagram of internal signal flow for FireWire 410
+//!
+//! ```text
+//!                                ++==========++
+//! analog-inputs (8 channels) --> ||  36x36   ||
+//! spdif-inputs (2 channels) ---> ||  capture || --> stream-outputs (18 channels)
+//! adat-inputs (8 channels) ----> ||  router  ||
+//!                                ++==========++
+//!
+//!                                ++==========++
+//! analog-inputs (8 channels) --> ||  36x4    ||
+//! spdif-inputs (2 channels) ---> ||          || --> mixer-outputs (4 channels)
+//! adat-inputs (8 channels) ----> ||  mixer   ||
+//! stream-inputs (18 channels) -> ||          ||
+//!                                ++==========++
+//!
+//!                                ++==========++
+//! analog-inputs (8 channels) --> ||  40x18   ||
+//! spdif-inputs (2 channels) ---> ||          || --> analog-outputs (8 channels)
+//! adat-inputs (8 channels) ----> || playback || --> spdif-outputs (2 channels)
+//! stream-inputs (18 channels) -> ||          || --> adat-outputs (8 channels)
+//! mixer-outputs (4 channels) --> ||  router  ||
+//!                                ++==========++
+//!
+//! (source) ----------------------------> spdif-output-1/2
+//!                           ^
+//!                           |
+//!                 ++================++
+//!                 || rate converter || (optional)
+//!                 ++================++
+//!                           |
+//!                           v
+//! spdif-input-1/2 ------------------------> (destination)
+//! 
+//! analog-input-1/2 ------------------------> (destination)
+//! analog-input-3/4 ------------------------> (destination)
+//! analog-input-5/6 ------------------------> (destination)
+//! analog-input-7/8 ------------------------> (destination)
+//! spdif-input-1/2 -------------------------> (destination)
+//!                           ^
+//!                           |
+//!                ++==================++
+//!                || format converter || (optoinal)
+//!                ++==================++
+//!                           |
+//!                           v
+//! (source) ------------------------------> spdif-output-1/2
+//! ```
 
 use crate::*;
 
