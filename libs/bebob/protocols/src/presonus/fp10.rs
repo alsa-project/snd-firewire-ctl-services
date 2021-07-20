d9bfd0f8acc1ba7612d9bf637f5a2aa4907b65f6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-protocols: presonus: add protocol implementation of level operation for FP10
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/presonus/fp10.rs
+++ libs/bebob/protocols/src/presonus/fp10.rs
@@ -5,6 +5,27 @@
 //!
 //! The module includes structure, enumeration, and trait and its implementation for protocol
 //! defined by PreSonus for Firepod/FP10.
+//!
+//! ## Diagram of internal signal flow
+//!
+//! ```text
+//! analog-input-1/2 -----+-----------> stream-output-1/2
+//! analog-input-3/4 -----|-+---------> stream-output-3/4
+//! analog-input-5/6 -----|-|-+-------> stream-output-5/6
+//! analog-input-7/8 -----|-|-|-+-----> stream-output-7/8
+//! digital-input-1/2 ----|-|-|-|-----> stream-output-9/10
+//!                       v v v v
+//!                     ++=======++
+//!                     || 10x2  ||
+//!                     || mixer ||
+//!                     ++=======++
+//!                        ^   |
+//! stream-input-1/2 ------+   +------> analog-output-1/2
+//! stream-input-3/4 -----------------> analog-output-3/4
+//! stream-input-5/6 -----------------> analog-output-5/6
+//! stream-input-7/8 -----------------> analog-output-7/8
+//! stream-input-9/10 ----------------> digital-output-1/2
+//! ```
 
 use crate::*;
 
@@ -27,5 +48,21 @@ impl SamplingClockSourceOperation for Fp10ClkProtocol {
         SignalAddr::Subunit(SignalSubunitAddr{subunit: MUSIC_SUBUNIT_0, plug_id: 0x07}),
         // S/PDIF
         SignalAddr::Unit(SignalUnitAddr::Ext(0x01)),
+    ];
+}
+
+/// The protocol implementation for physical output.
+pub struct Fp10PhysOutputProtocol;
+
+impl AvcLevelOperation for Fp10PhysOutputProtocol {
+    const ENTRIES: &'static [(u8, AudioCh)] = &[
+        (0x01, AudioCh::Each(0)),
+        (0x01, AudioCh::Each(1)),
+        (0x02, AudioCh::Each(0)),
+        (0x02, AudioCh::Each(1)),
+        (0x03, AudioCh::Each(0)),
+        (0x03, AudioCh::Each(1)),
+        (0x04, AudioCh::Each(0)),
+        (0x04, AudioCh::Each(1)),
     ];
 }
