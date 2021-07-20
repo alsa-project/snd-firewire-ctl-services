0a98eb179ac114438d02358b1b56842bc4382098
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-protocols: presonus: add implementation of clock operations
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/bebob/protocols/src/presonus.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol implementation for some PreSonus models.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for protocol
+//! defined by PreSonus for some FireWire models.
+
+pub mod inspire1394;
