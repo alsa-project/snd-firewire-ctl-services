c7ca17a165609b68d84d1ee203ec76cb7ec06501
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-runtime: yamaha_terratec: use implementations of level operation
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/yamaha_terratec.rs
+++ libs/bebob/runtime/src/yamaha_terratec.rs
@@ -16,12 +16,17 @@ use super::common_ctls::*;
 pub struct GoPhase24CoaxModel {
     avc: BebobAvc,
     clk_ctl: ClkCtl,
+    mixer_src_ctl: MixerSourceCtl,
+    mixer_out_ctl: CoaxMixerOutputCtl,
 }
 
 #[derive(Default)]
 pub struct GoPhase24OptModel {
     avc: BebobAvc,
     clk_ctl: ClkCtl,
+    phys_out_ctl: OptPhysOutputCtl,
+    mixer_src_ctl: MixerSourceCtl,
+    mixer_out_ctl: OptMixerOutputCtl,
 }
 
 const FCP_TIMEOUT_MS: u32 = 100;
@@ -35,6 +40,46 @@ impl SamplingClkSrcCtlOperation<GoPhase24ClkProtocol> for ClkCtl {
     const SRC_LABELS: &'static [&'static str] = &["Internal", "S/PDIF"];
 }
 
+#[derive(Default)]
+struct MixerSourceCtl;
+
+impl AvcLevelCtlOperation<GoPhase24MixerSourceProtocol> for MixerSourceCtl {
+    const LEVEL_NAME: &'static str = "mixer-source-gain";
+    const PORT_LABELS: &'static [&'static str] = &[
+        "analog-input-1", "analog-input-2",
+        "digital-input-1", "digital-input-2",
+        "stream-input-1", "stream-input-2",
+        "stream-input-3", "stream-input-4",
+        "stream-input-5", "stream-input-6",
+    ];
+}
+
+#[derive(Default)]
+struct OptPhysOutputCtl;
+
+impl AvcLevelCtlOperation<GoPhase24OptPhysOutputProtocol> for OptPhysOutputCtl {
+    const LEVEL_NAME: &'static str = "phy-output-volume";
+    const PORT_LABELS: &'static [&'static str] = &[
+        "analog-output-1", "analog-output-2", "analog-output-3", "analog-output-4",
+    ];
+}
+
+#[derive(Default)]
+struct CoaxMixerOutputCtl;
+
+impl AvcLevelCtlOperation<GoPhase24CoaxMixerOutputProtocol> for CoaxMixerOutputCtl {
+    const LEVEL_NAME: &'static str = "mixer-output-volume";
+    const PORT_LABELS: &'static [&'static str] = &["mixer-output-1", "mixer-output-2"];
+}
+
+#[derive(Default)]
+struct OptMixerOutputCtl;
+
+impl AvcLevelCtlOperation<GoPhase24OptMixerOutputProtocol> for OptMixerOutputCtl {
+    const LEVEL_NAME: &'static str = "mixer-output-volume";
+    const PORT_LABELS: &'static [&'static str] = &["mixer-output-1", "mixer-output-2"];
+}
+
 impl CtlModel<SndUnit> for GoPhase24CoaxModel {
     fn load(&mut self, unit: &mut SndUnit, card_cntr: &mut CardCntr) -> Result<(), Error> {
         self.avc.as_ref().bind(&unit.get_node())?;
@@ -45,6 +90,9 @@ impl CtlModel<SndUnit> for GoPhase24CoaxModel {
         self.clk_ctl.load_src(card_cntr)
             .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
+        self.mixer_src_ctl.load_level(card_cntr)?;
+        self.mixer_out_ctl.load_level(card_cntr)?;
+
         Ok(())
     }
 
@@ -54,6 +102,10 @@ impl CtlModel<SndUnit> for GoPhase24CoaxModel {
         if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.clk_ctl.read_src(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_src_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_out_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -67,6 +119,10 @@ impl CtlModel<SndUnit> for GoPhase24CoaxModel {
             Ok(true)
         } else if self.clk_ctl.write_src(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
+        } else if self.mixer_src_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+             Ok(true)
+        } else if self.mixer_out_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+             Ok(true)
         } else {
             Ok(false)
         }
@@ -99,6 +155,10 @@ impl CtlModel<SndUnit> for GoPhase24OptModel {
         self.clk_ctl.load_src(card_cntr)
             .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
+        self.phys_out_ctl.load_level(card_cntr)?;
+        self.mixer_src_ctl.load_level(card_cntr)?;
+        self.mixer_out_ctl.load_level(card_cntr)?;
+
         Ok(())
     }
 
@@ -108,6 +168,12 @@ impl CtlModel<SndUnit> for GoPhase24OptModel {
         if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.clk_ctl.read_src(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.phys_out_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_src_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_out_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -121,6 +187,12 @@ impl CtlModel<SndUnit> for GoPhase24OptModel {
             Ok(true)
         } else if self.clk_ctl.write_src(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
+        } else if self.phys_out_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+             Ok(true)
+        } else if self.mixer_src_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+             Ok(true)
+        } else if self.mixer_out_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+             Ok(true)
         } else {
             Ok(false)
         }
@@ -154,6 +226,27 @@ mod test {
         let mut ctl = ClkCtl::default();
 
         let error = ctl.load_freq(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+    }
+
+    #[test]
+    fn test_level_ctl_definition() {
+        let mut card_cntr = CardCntr::new();
+
+        let ctl = OptPhysOutputCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let ctl = CoaxMixerOutputCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let ctl = MixerSourceCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let ctl = OptMixerOutputCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }
