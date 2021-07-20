a57e7fa813ad5eebb466c542e7ae5aede01264e8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-runtime: terratec: use protocol implementaion of level operation for Phase 88
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/terratec/phase88_model.rs
+++ libs/bebob/runtime/src/terratec/phase88_model.rs
@@ -18,6 +18,9 @@ use crate::common_ctls::*;
 pub struct Phase88Model {
     avc: BebobAvc,
     clk_ctl: ClkCtl,
+    mixer_phys_src_ctl: MixerPhysSrcCtl,
+    mixer_stream_src_ctl: MixerStreamSrcCtl,
+    mixer_out_ctl: MixerOutputCtl,
 }
 
 const FCP_TIMEOUT_MS: u32 = 100;
@@ -29,6 +32,36 @@ impl MediaClkFreqCtlOperation<Phase88ClkProtocol> for ClkCtl {}
 
 impl SamplingClkSrcCtlOperation<Phase88ClkProtocol> for ClkCtl {
     const SRC_LABELS: &'static [&'static str] = &["Internal", "S/PDIF", "Word-clock"];
+}
+
+#[derive(Default)]
+struct MixerPhysSrcCtl;
+
+impl AvcLevelCtlOperation<Phase88MixerPhysSourceProtocol> for MixerPhysSrcCtl {
+    const LEVEL_NAME: &'static str = "mixer-phys-source-gain";
+    const PORT_LABELS: &'static [&'static str] = &[
+        "analog-input-1", "analog-input-2",
+        "analog-input-3", "analog-input-4",
+        "analog-input-5", "analog-input-6",
+        "analog-input-7", "analog-input-8",
+        "digital-input-1", "digital-input-2",
+    ];
+}
+
+#[derive(Default)]
+struct MixerStreamSrcCtl;
+
+impl AvcLevelCtlOperation<Phase88MixerStreamSourceProtocol> for MixerStreamSrcCtl {
+    const LEVEL_NAME: &'static str = "mixer-stream-source-gain";
+    const PORT_LABELS: &'static [&'static str] = &["stream-source-1", "stream-source-2"];
+}
+
+#[derive(Default)]
+struct MixerOutputCtl;
+
+impl AvcLevelCtlOperation<Phase88MixerOutputProtocol> for MixerOutputCtl {
+    const LEVEL_NAME: &'static str = "mixer-output-volume";
+    const PORT_LABELS: &'static [&'static str] = &["mixer-output-1", "mixer-output-2"];
 }
 
 impl CtlModel<SndUnit> for Phase88Model {
@@ -45,6 +78,10 @@ impl CtlModel<SndUnit> for Phase88Model {
         self.clk_ctl.load_src(card_cntr)
             .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
+        self.mixer_phys_src_ctl.load_level(card_cntr)?;
+        self.mixer_stream_src_ctl.load_level(card_cntr)?;
+        self.mixer_out_ctl.load_level(card_cntr)?;
+
         Ok(())
     }
 
@@ -57,6 +94,12 @@ impl CtlModel<SndUnit> for Phase88Model {
         if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.clk_ctl.read_src(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_phys_src_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_stream_src_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_out_ctl.read_level(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -73,6 +116,12 @@ impl CtlModel<SndUnit> for Phase88Model {
         if self.clk_ctl.write_freq(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS * 3)? {
             Ok(true)
         } else if self.clk_ctl.write_src(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_phys_src_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_stream_src_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.mixer_out_ctl.write_level(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
@@ -110,6 +159,23 @@ mod test {
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
 
         let error = ctl.load_src(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+    }
+
+    #[test]
+    fn test_level_ctl_definition() {
+        let mut card_cntr = CardCntr::new();
+
+        let ctl = MixerPhysSrcCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let ctl = MixerStreamSrcCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let ctl = MixerOutputCtl::default();
+        let error = ctl.load_level(&mut card_cntr).unwrap_err();
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }
