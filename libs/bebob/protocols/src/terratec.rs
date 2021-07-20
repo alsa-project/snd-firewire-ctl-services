9854b2f0e1c4843ace2deeee8ad0bfa7ca78ed22
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-07-20 10:15:46 +0900

    bebob-protocols: terratec: add protocol implementation for clock operation for Aureon
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/bebob/protocols/src/terratec.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol implementation for some Terratec models.
+//!
+//! The module includes structure, enumeration, and trait and its implementation for protocol
+//! defined by Terratec for some FireWire models.
+
+pub mod aureon;
