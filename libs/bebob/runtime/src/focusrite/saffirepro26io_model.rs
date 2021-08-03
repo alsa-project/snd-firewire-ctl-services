dbd6027a930736e6cd375d3376ff99fe7eb3c21e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-03 18:58:07 +0900

    bebob-runtime: focusrite: use protocol implementation of clock operation for Pro 10/26 i/o
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/focusrite/saffirepro26io_model.rs
+++ libs/bebob/runtime/src/focusrite/saffirepro26io_model.rs
@@ -3,47 +3,94 @@
 
 use glib::Error;
 
-use hinawa::FwFcpExt;
+use hinawa::{FwFcpExt, FwReq};
 use hinawa::{SndUnit, SndUnitExt};
 
 use alsactl::{ElemId, ElemValue};
 
 use core::card_cntr::*;
 
-use bebob_protocols::*;
+use bebob_protocols::{*, focusrite::saffireproio::*};
+
+use super::*;
 
 #[derive(Default)]
 pub struct SaffirePro26ioModel {
+    req: FwReq,
     avc: BebobAvc,
+    clk_ctl: ClkCtl,
 }
+
+const TIMEOUT_MS: u32 = 50;
+
+#[derive(Default)]
+struct ClkCtl(Vec<ElemId>);
+
+impl SaffireProMediaClkFreqCtlOperation<SaffirePro26ioClkProtocol> for ClkCtl {}
+
+impl SaffireProSamplingClkSrcCtlOperation<SaffirePro26ioClkProtocol> for ClkCtl {}
 
 impl CtlModel<SndUnit> for SaffirePro26ioModel {
     fn load(
         &mut self,
         unit: &mut SndUnit,
-        _: &mut CardCntr,
+        card_cntr: &mut CardCntr,
     ) -> Result<(), Error> {
         self.avc.as_ref().bind(&unit.get_node())?;
+
+        self.clk_ctl.load_freq(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
+
+        self.clk_ctl.load_src(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
         Ok(())
     }
 
     fn read(
         &mut self,
-        _: &mut SndUnit,
-        _: &ElemId,
-        _: &mut ElemValue,
+        unit: &mut SndUnit,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.clk_ctl.read_freq(unit, &self.req, elem_id, elem_value, TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.clk_ctl.read_src(unit, &self.req, elem_id, elem_value, TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(
         &mut self,
-        _: &mut SndUnit,
-        _: &ElemId,
+        unit: &mut SndUnit,
+        elem_id: &ElemId,
         _: &ElemValue,
-        _: &ElemValue,
+        new: &ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.clk_ctl.write_freq(unit, &self.req, elem_id, new, TIMEOUT_MS * 3)? {
+            Ok(true)
+        } else if self.clk_ctl.write_src(unit, &self.req, elem_id, new, TIMEOUT_MS * 3)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl NotifyModel<SndUnit, bool> for SaffirePro26ioModel {
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
+        elem_id_list.extend_from_slice(&self.clk_ctl.0);
+    }
+
+    fn parse_notification(&mut self, _: &mut SndUnit, _: &bool) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn read_notified_elem(&mut self, unit: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
+        -> Result<bool, Error>
+    {
+        self.clk_ctl.read_freq(unit, &self.req, elem_id, elem_value, TIMEOUT_MS)
     }
 }
