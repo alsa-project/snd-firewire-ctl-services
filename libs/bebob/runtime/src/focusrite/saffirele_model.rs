3126c0d1b49692f6c9a5dfb20528e8e7fed9ce72
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-03 18:58:07 +0900

    bebob-runtime: focusrite: use protocol implementation of clock operation for Saffire and LE
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/focusrite/saffirele_model.rs
+++ libs/bebob/runtime/src/focusrite/saffirele_model.rs
@@ -10,21 +10,41 @@ use alsactl::{ElemId, ElemValue};
 
 use core::card_cntr::*;
 
-use bebob_protocols::*;
+use bebob_protocols::{*, focusrite::saffire::*};
+
+use crate::common_ctls::*;
 
 #[derive(Default)]
 pub struct SaffireLeModel {
     avc: BebobAvc,
+    clk_ctl: ClkCtl,
+}
+
+// NOTE: At 88.2/96.0 kHz, AV/C transaction takes more time than 44.1/48.0 kHz.
+const FCP_TIMEOUT_MS: u32 = 200;
+
+#[derive(Default)]
+struct ClkCtl(Vec<ElemId>);
+
+impl MediaClkFreqCtlOperation<SaffireLeClkProtocol> for ClkCtl {}
+
+impl SamplingClkSrcCtlOperation<SaffireLeClkProtocol> for ClkCtl {
+    const SRC_LABELS: &'static [&'static str] = &["Internal", "S/PDIF"];
 }
 
 impl CtlModel<SndUnit> for SaffireLeModel {
     fn load(
         &mut self,
         unit: &mut SndUnit,
-        _: &mut CardCntr,
+        card_cntr: &mut CardCntr,
     ) -> Result<(), Error> {
         self.avc.as_ref().bind(&unit.get_node())?;
 
+        self.clk_ctl.load_freq(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
+
+        self.clk_ctl.load_src(card_cntr)
+            .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
         Ok(())
     }
@@ -32,19 +52,65 @@ impl CtlModel<SndUnit> for SaffireLeModel {
     fn read(
         &mut self,
         _: &mut SndUnit,
-        _: &ElemId,
-        _: &mut ElemValue,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.clk_ctl.read_src(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     fn write(
         &mut self,
-        _: &mut SndUnit,
-        _: &ElemId,
-        _: &ElemValue,
-        _: &ElemValue,
+        unit: &mut SndUnit,
+        elem_id: &ElemId,
+        old: &ElemValue,
+        new: &ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.clk_ctl.write_freq(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS * 3)? {
+            Ok(true)
+        } else if self.clk_ctl.write_src(unit, &self.avc, elem_id, old, new, FCP_TIMEOUT_MS * 3)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl NotifyModel<SndUnit, bool> for SaffireLeModel {
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
+        elem_id_list.extend_from_slice(&self.clk_ctl.0);
+    }
+
+    fn parse_notification(&mut self, _: &mut SndUnit, _: &bool) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn read_notified_elem(&mut self, _: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
+        -> Result<bool, Error>
+    {
+        self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use alsactl::CardError;
+
+    #[test]
+    fn test_clk_ctl_definition() {
+        let mut card_cntr = CardCntr::new();
+        let mut ctl = ClkCtl::default();
+
+        let error = ctl.load_freq(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
+
+        let error = ctl.load_src(&mut card_cntr).unwrap_err();
+        assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }
