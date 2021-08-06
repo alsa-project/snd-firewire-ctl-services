fabcabaff85f74760677577b0c0abc1bc0d699d8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-06 16:41:15 +0900

    bebob-runtime: apogee: use protocol implementation of meter information in Ensemble
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/runtime/src/apogee/ensemble_model.rs
+++ libs/bebob/runtime/src/apogee/ensemble_model.rs
@@ -6,20 +6,26 @@ use glib::Error;
 use hinawa::FwFcpExt;
 use hinawa::{SndUnit, SndUnitExt};
 
-use alsactl::{ElemId, ElemValue};
+use alsactl::{ElemId, ElemIfaceType, ElemValue, ElemValueExt};
 
 use core::card_cntr::*;
+use core::elem_value_accessor::ElemValueAccessor;
+
+use alsa_ctl_tlv_codec::items::DbInterval;
 
 use bebob_protocols::{*, apogee::ensemble::*};
 
+use crate::model::{IN_METER_NAME, OUT_METER_NAME};
+
 use crate::common_ctls::*;
-use super::apogee_ctls::{HwCtl, DisplayCtl, OpticalCtl, InputCtl, OutputCtl, MixerCtl, RouteCtl, ResamplerCtl, MeterCtl};
+use super::apogee_ctls::{HwCtl, DisplayCtl, OpticalCtl, InputCtl, OutputCtl, MixerCtl, RouteCtl, ResamplerCtl};
 
 const FCP_TIMEOUT_MS: u32 = 100;
 
 pub struct EnsembleModel {
     avc: BebobAvc,
     clk_ctl: ClkCtl,
+    meter_ctl: MeterCtl,
     hw_ctls: HwCtl,
     display_ctls: DisplayCtl,
     opt_iface_ctls: OpticalCtl,
@@ -28,7 +34,6 @@ pub struct EnsembleModel {
     mixer_ctls: MixerCtl,
     route_ctls: RouteCtl,
     resampler_ctls: ResamplerCtl,
-    meter_ctls: MeterCtl,
 }
 
 #[derive(Default)]
@@ -50,6 +55,7 @@ impl Default for EnsembleModel {
         Self {
             avc: Default::default(),
             clk_ctl: Default::default(),
+            meter_ctl: Default::default(),
             hw_ctls: HwCtl::new(),
             display_ctls: DisplayCtl::new(),
             opt_iface_ctls: OpticalCtl::new(),
@@ -58,7 +64,6 @@ impl Default for EnsembleModel {
             mixer_ctls: MixerCtl::new(),
             route_ctls: RouteCtl::new(),
             resampler_ctls: ResamplerCtl::new(),
-            meter_ctls: MeterCtl::new(),
         }
     }
 }
@@ -75,6 +80,8 @@ impl CtlModel<SndUnit> for EnsembleModel {
         self.clk_ctl.load_src(card_cntr)
             .map(|mut elem_id_list| self.clk_ctl.0.append(&mut elem_id_list))?;
 
+        self.meter_ctl.load_state(card_cntr, &mut self.avc, FCP_TIMEOUT_MS)?;
+
         self.hw_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.display_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.opt_iface_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
@@ -83,7 +90,6 @@ impl CtlModel<SndUnit> for EnsembleModel {
         self.mixer_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.route_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.resampler_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
-        self.meter_ctls.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -94,6 +100,8 @@ impl CtlModel<SndUnit> for EnsembleModel {
         if self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.clk_ctl.read_src(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.meter_ctl.read_state(elem_id, elem_value)? {
             Ok(true)
         } else if self.hw_ctls.read(elem_id, elem_value)? {
             Ok(true)
@@ -110,8 +118,6 @@ impl CtlModel<SndUnit> for EnsembleModel {
         } else if self.route_ctls.read(elem_id, elem_value)? {
             Ok(true)
         } else if self.resampler_ctls.read(elem_id, elem_value)? {
-            Ok(true)
-        } else if self.meter_ctls.read(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -141,8 +147,6 @@ impl CtlModel<SndUnit> for EnsembleModel {
             Ok(true)
         } else if self.resampler_ctls.write(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
             Ok(true)
-        } else if self.meter_ctls.write(&self.avc, elem_id, old, new, FCP_TIMEOUT_MS)? {
-            Ok(true)
         } else {
             Ok(true)
         }
@@ -151,17 +155,17 @@ impl CtlModel<SndUnit> for EnsembleModel {
 
 impl MeasureModel<SndUnit> for EnsembleModel {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
-        elem_id_list.extend_from_slice(&self.meter_ctls.measure_elem_list);
+        elem_id_list.extend_from_slice(&self.meter_ctl.1);
     }
 
     fn measure_states(&mut self, _: &mut SndUnit) -> Result<(), Error> {
-        self.meter_ctls.measure_states(&self.avc, FCP_TIMEOUT_MS)
+        self.meter_ctl.measure_state(&mut self.avc, FCP_TIMEOUT_MS)
     }
 
     fn measure_elem(&mut self, _: &SndUnit, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.meter_ctls.measure_elem(elem_id, elem_value)
+        self.meter_ctl.read_state(elem_id, elem_value)
     }
 }
 
@@ -178,6 +182,159 @@ impl NotifyModel<SndUnit, bool> for EnsembleModel {
         -> Result<bool, Error>
     {
         self.clk_ctl.read_freq(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)
+    }
+}
+
+#[derive(Default)]
+struct MeterCtl(EnsembleMeter, Vec<ElemId>);
+
+const KNOB_IN_TARGET_NAME: &str = "knob-input-target";
+const KNOB_OUT_TARGET_NAME: &str = "knob-output-target";
+
+fn knob_input_target_to_str(target: &KnobInputTarget) -> &str {
+    match target {
+        KnobInputTarget::Mic0 => "mic-1",
+        KnobInputTarget::Mic1 => "mic-2",
+        KnobInputTarget::Mic2 => "mic-3",
+        KnobInputTarget::Mic3 => "mic-4",
+    }
+}
+
+fn knob_output_target_to_str(target: &KnobOutputTarget) -> &str {
+    match target {
+        KnobOutputTarget::AnalogOutputPair0 => "main",
+        KnobOutputTarget::HeadphonePair0 => "headphone-1/2",
+        KnobOutputTarget::HeadphonePair1 => "headphone-3/4",
+    }
+}
+
+impl MeterCtl {
+    const METER_TLV: DbInterval = DbInterval{min: -4800, max: 0, linear: false, mute_avail: false};
+
+    const IN_METER_LABELS: [&'static str; 18] = [
+        "analog-input-1", "analog-input-2", "analog-input-3", "analog-input-4",
+        "analog-input-5", "analog-input-6", "analog-input-7", "analog-input-8",
+        "spdif-input-1", "spdif-input-2",
+        "adat-input-1", "adat-input-2", "adat-input-3", "adat-input-4",
+        "adat-input-5", "adat-input-6", "adat-input-7", "adat-input-8",
+    ];
+
+    const OUT_METER_LABELS: [&'static str; 16] = [
+        "analog-output-1", "analog-output-2", "analog-output-3", "analog-output-4",
+        "analog-output-5", "analog-output-6", "analog-output-7", "analog-output-8",
+        "spdif-output-1", "spdif-output-2",
+        "adat-output-1", "adat-output-2", "adat-output-3", "adat-output-4",
+        "adat-output-5", "adat-output-6",
+        //"adat-output-7", "adat-output-8",
+    ];
+
+    const KNOB_INPUT_TARGETS: [KnobInputTarget; 4] = [
+        KnobInputTarget::Mic0,
+        KnobInputTarget::Mic1,
+        KnobInputTarget::Mic2,
+        KnobInputTarget::Mic3,
+    ];
+
+    const KNOB_OUTPUT_TARGETS: [KnobOutputTarget; 3] = [
+        KnobOutputTarget::AnalogOutputPair0,
+        KnobOutputTarget::HeadphonePair0,
+        KnobOutputTarget::HeadphonePair1,
+    ];
+
+    const LEVEL_MIN: i32 = EnsembleMeterProtocol::LEVEL_MIN as i32;
+    const LEVEL_MAX: i32 = EnsembleMeterProtocol::LEVEL_MAX as i32;
+    const LEVEL_STEP: i32 = EnsembleMeterProtocol::LEVEL_STEP as i32;
+
+    fn load_state(
+        &mut self,
+        card_cntr: &mut CardCntr,
+        avc: &mut BebobAvc,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let labels: Vec<&str> = Self::KNOB_INPUT_TARGETS.iter()
+            .map(|t| knob_input_target_to_str(t))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Card, 0, 0, KNOB_IN_TARGET_NAME, 0);
+        card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)
+            .map(|mut elem_id_list| self.1.append(&mut elem_id_list))?;
+
+        let labels: Vec<&str> = Self::KNOB_OUTPUT_TARGETS.iter()
+            .map(|t| knob_output_target_to_str(t))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Card, 0, 0, KNOB_OUT_TARGET_NAME, 0);
+        card_cntr
+            .add_enum_elems(&elem_id, 1, 1, &labels, None, true)
+            .map(|mut elem_id_list| self.1.append(&mut elem_id_list))?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, IN_METER_NAME, 0);
+        card_cntr
+            .add_int_elems(
+                &elem_id,
+                1,
+                Self::LEVEL_MIN,
+                Self::LEVEL_MAX,
+                Self::LEVEL_STEP,
+                Self::IN_METER_LABELS.len(),
+                Some(&Into::<Vec<u32>>::into(Self::METER_TLV)),
+                false,
+            )
+            .map(|mut elem_id_list| self.1.append(&mut elem_id_list))?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUT_METER_NAME, 0);
+        card_cntr
+            .add_int_elems(
+                &elem_id,
+                1,
+                Self::LEVEL_MIN,
+                Self::LEVEL_MAX,
+                Self::LEVEL_STEP,
+                Self::OUT_METER_LABELS.len(),
+                Some(&Into::<Vec<u32>>::into(Self::METER_TLV)),
+                false,
+            )
+            .map(|mut elem_id_list| self.1.append(&mut elem_id_list))?;
+
+        self.measure_state(avc, timeout_ms)
+    }
+
+    fn measure_state(&mut self, avc: &mut BebobAvc, timeout_ms: u32) -> Result<(), Error> {
+        EnsembleMeterProtocol::measure_meter(avc, &mut self.0, timeout_ms)
+    }
+
+    fn read_state(
+        &self,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            KNOB_IN_TARGET_NAME => {
+                let idx = Self::KNOB_INPUT_TARGETS.iter()
+                    .position(|t| t.eq(&self.0.knob_input_target))
+                    .unwrap();
+                elem_value.set_enum(&[idx as u32]);
+                Ok(true)
+            }
+            KNOB_OUT_TARGET_NAME => {
+                let idx = Self::KNOB_OUTPUT_TARGETS.iter()
+                    .position(|t| t.eq(&self.0.knob_output_target))
+                    .unwrap();
+                elem_value.set_enum(&[idx as u32]);
+                Ok(true)
+            }
+            IN_METER_NAME => {
+                ElemValueAccessor::<i32>::set_vals(elem_value, 18, |idx| {
+                    Ok(self.0.phys_inputs[idx] as i32)
+                })?;
+                Ok(true)
+            }
+            OUT_METER_NAME => {
+                ElemValueAccessor::<i32>::set_vals(elem_value, 16, |idx| {
+                    Ok(self.0.phys_outputs[idx] as i32)
+                })?;
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
     }
 }
 
