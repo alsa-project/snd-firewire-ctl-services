8e9a3323f5700ced0a8aa1d9b2371e84f2ad414b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-15 16:45:59 +0900

    oxfw-protocols: lacie: add protocol implementation for FireWire Speakers
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/oxfw/protocols/src/lacie.rs
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol defined by La Cie for FireWire Speakers.
+//!
+//! The module includes protocol implementation defined by La Cie for FireWire Speackers.
+
+use glib::Error;
+
+use hinawa::FwFcp;
+
+use ta1394::{audio::*, *};
+
+/// The protocol implementation for FireWire Speackers.
+#[derive(Default, Debug)]
+pub struct FwSpeakersProtocol;
+
+impl FwSpeakersProtocol {
+    pub const VOLUME_MIN: i16 = i16::MIN;
+    pub const VOLUME_MAX: i16 = 0;
+    pub const VOLUME_STEP: i16 = 1;
+
+    const FB_ID: u8 = 0x01;
+
+    pub fn read_volume(avc: &mut FwFcp, volume: &mut i16, timeout_ms: u32) -> Result<(), Error> {
+        let mut op = AudioFeature::new(
+            Self::FB_ID,
+            CtlAttr::Current,
+            AudioCh::All,
+            FeatureCtl::Volume(vec![-1]),
+        );
+        avc.status(&AUDIO_SUBUNIT_0_ADDR, &mut op, timeout_ms)
+            .map(|_| {
+                if let FeatureCtl::Volume(data) = op.ctl {
+                    *volume = data[0]
+                }
+            })
+    }
+
+    pub fn write_volume(avc: &mut FwFcp, volume: i16, timeout_ms: u32) -> Result<(), Error> {
+        let mut op = AudioFeature::new(
+            Self::FB_ID,
+            CtlAttr::Current,
+            AudioCh::All,
+            FeatureCtl::Volume(vec![volume]),
+        );
+        avc.control(&AUDIO_SUBUNIT_0_ADDR, &mut op, timeout_ms)
+    }
+
+    pub fn read_mute(avc: &mut FwFcp, mute: &mut bool, timeout_ms: u32) -> Result<(), Error> {
+        let mut op = AudioFeature::new(
+            Self::FB_ID,
+            CtlAttr::Current,
+            AudioCh::All,
+            FeatureCtl::Mute(vec![false]),
+        );
+        avc.status(&AUDIO_SUBUNIT_0_ADDR, &mut op, timeout_ms)
+            .map(|_| {
+                if let FeatureCtl::Mute(data) = op.ctl {
+                    *mute = data[0]
+                }
+            })
+    }
+
+    pub fn write_mute(avc: &mut FwFcp, mute: bool, timeout_ms: u32) -> Result<(), Error> {
+        let mut op = AudioFeature::new(
+            Self::FB_ID,
+            CtlAttr::Current,
+            AudioCh::All,
+            FeatureCtl::Mute(vec![mute]),
+        );
+        avc.control(&AUDIO_SUBUNIT_0_ADDR, &mut op, timeout_ms)
+    }
+}
