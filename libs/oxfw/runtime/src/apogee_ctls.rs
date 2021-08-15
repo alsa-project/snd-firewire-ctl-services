cfc0afbbaa75eb3942f5f4b433458f0e255154ed
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-15 16:45:59 +0900

    oxfw-runtime: use protocol implementation of output parameter
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/oxfw/runtime/src/apogee_ctls.rs
+++ libs/oxfw/runtime/src/apogee_ctls.rs
@@ -15,163 +15,6 @@ use oxfw_protocols::apogee::{VendorCmd, ApogeeCmd};
 const TIMEOUT_MS: u32 = 100;
 
 #[derive(Default, Debug)]
-pub struct OutputCtl;
-
-impl OutputCtl {
-    const SRC_NAME: &'static str = "output-source";
-    const LEVEL_NAME: &'static str = "output-level";
-    const MUTE_FOR_LINE_OUT: &'static str = "mute-for-line-out";
-    const MUTE_FOR_HP_OUT: &'static str = "mute-for-hp-out";
-
-    const SRC_LABELS: [&'static str; 2] = ["stream-1/2", "mixer-1/2"];
-    const LEVEL_LABELS: [&'static str; 2] = ["instrument", "-10dB"];
-
-    const MUTE_LABELS: [&'static str; 3] = [
-        "never",
-        "normal",
-        "swapped"
-    ];
-
-    pub fn load(&mut self, _: &FwFcp, card_cntr: &mut CardCntr) -> Result<(), Error> {
-        // For source of analog outputs.
-        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::SRC_NAME, 0);
-        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &Self::SRC_LABELS, None, true)?;
-
-        // For level of analog outputs.
-        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::LEVEL_NAME, 0);
-        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &Self::LEVEL_LABELS, None, true)?;
-
-        // For association of mute state to line output.
-        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::MUTE_FOR_LINE_OUT, 0);
-        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &Self::MUTE_LABELS, None, true)?;
-
-        // For association of mute state to hp output.
-        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::MUTE_FOR_HP_OUT, 0);
-        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &Self::MUTE_LABELS, None, true)?;
-
-        Ok(())
-    }
-
-    fn parse_mute_mode(mute_mode: bool, unmute_mode: bool) -> u32 {
-        let mut idx = 0;
-        if !mute_mode {
-            idx |= 0x01;
-        }
-        if !unmute_mode {
-            idx |= 0x02;
-        }
-        if idx > 2 {
-            idx = 0
-        }
-        idx
-    }
-
-    pub fn read(&mut self, avc: &FwFcp, company_id: &[u8;3], elem_id: &ElemId, elem_value: &mut ElemValue)
-        -> Result<bool, Error>
-    {
-        match elem_id.get_name().as_str() {
-            Self::SRC_NAME => {
-                ElemValueAccessor::<u32>::set_val(elem_value, || {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::UseMixerOut);
-                    avc.status(&AvcAddr::Unit, &mut op, TIMEOUT_MS)?;
-                    Ok(op.get_enum())
-                })?;
-                Ok(true)
-            }
-            Self::LEVEL_NAME => {
-                ElemValueAccessor::<u32>::set_val(elem_value, || {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::OutAttr);
-                    avc.status(&AvcAddr::Unit, &mut op, TIMEOUT_MS)?;
-                    Ok(op.get_enum())
-                })?;
-                Ok(true)
-            }
-            Self::MUTE_FOR_LINE_OUT => {
-                ElemValueAccessor::<u32>::set_val(elem_value, || {
-                    let mut mute_op = ApogeeCmd::new(company_id, VendorCmd::MuteForLineOut);
-                    avc.status(&AvcAddr::Unit, &mut mute_op, TIMEOUT_MS)?;
-
-                    let mut unmute_op = ApogeeCmd::new(company_id, VendorCmd::UnmuteForLineOut);
-                    avc.status(&AvcAddr::Unit, &mut unmute_op, TIMEOUT_MS)?;
-
-                    Ok(Self::parse_mute_mode(mute_op.get_enum() > 0, unmute_op.get_enum() > 0))
-                })?;
-                Ok(true)
-            }
-            Self::MUTE_FOR_HP_OUT => {
-                ElemValueAccessor::<u32>::set_val(elem_value, || {
-                    let mut mute_op = ApogeeCmd::new(company_id, VendorCmd::MuteForHpOut);
-                    avc.status(&AvcAddr::Unit, &mut mute_op, TIMEOUT_MS)?;
-                    let mut unmute_op = ApogeeCmd::new(company_id, VendorCmd::UnmuteForHpOut);
-                    avc.status(&AvcAddr::Unit, &mut unmute_op, TIMEOUT_MS)?;
-
-                    Ok(Self::parse_mute_mode(mute_op.get_enum() > 0, unmute_op.get_enum() > 0))
-                })?;
-                Ok(true)
-            }
-            _ => Ok(false),
-        }
-    }
-
-    fn build_mute_mode(idx: u32) -> (bool, bool) {
-        (idx & 0x01 == 0, idx & 0x02 == 0)
-    }
-
-    pub fn write(&mut self, avc: &FwFcp, company_id: &[u8;3], elem_id: &ElemId, _: &ElemValue,
-                 new: &ElemValue)
-        -> Result<bool, Error>
-    {
-        match elem_id.get_name().as_str() {
-            Self::SRC_NAME => {
-                ElemValueAccessor::<u32>::get_val(new, |val| {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::UseMixerOut);
-                    op.put_enum(val);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)
-                })?;
-                Ok(true)
-            }
-            Self::LEVEL_NAME => {
-                ElemValueAccessor::<u32>::get_val(new, |val| {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::OutAttr);
-                    op.put_enum(val);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)
-                })?;
-                Ok(true)
-            }
-            Self::MUTE_FOR_LINE_OUT => {
-                ElemValueAccessor::<u32>::get_val(new, |val| {
-                    let (mute_mode, unmute_mode) = Self::build_mute_mode(val);
-
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::MuteForLineOut);
-                    op.put_enum(mute_mode as u32);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)?;
-
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::UnmuteForLineOut);
-                    op.put_enum(unmute_mode as u32);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)
-                })?;
-                Ok(true)
-            }
-            Self::MUTE_FOR_HP_OUT => {
-                ElemValueAccessor::<u32>::get_val(new, |val| {
-                    let (mute_mode, unmute_mode) = Self::build_mute_mode(val);
-
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::MuteForHpOut);
-                    op.put_enum(mute_mode as u32);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)?;
-
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::UnmuteForHpOut);
-                    op.put_enum(unmute_mode as u32);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)
-                })?;
-                Ok(true)
-            }
-            _ => Ok(false),
-        }
-    }
-}
-
-#[derive(Default, Debug)]
 pub struct MixerCtl;
 
 impl MixerCtl {
@@ -498,8 +341,6 @@ pub struct HwState {
 }
 
 impl HwState {
-    const OUT_MUTE_NAME: &'static str = "output-mute";
-    const OUT_VOLUME_NAME: &'static str = "output-volume";
     const IN_GAIN_NAME: &'static str = "input-gain";
 
     const INPUT_LABELS: [&'static str; 2] = [
@@ -507,27 +348,11 @@ impl HwState {
         "analog-input-2",
     ];
 
-    const DIAL_OUT_MIN: i32 = 0;
-    const DIAL_OUT_MAX: i32 = 75;
-    const DIAL_OUT_STEP: i32 = 1;
-
     const DIAL_IN_MIN: i32 = 10;
     const DIAL_IN_MAX: i32 = 75;
     const DIAL_IN_STEP: i32 = 1;
 
     pub fn load(&mut self, _: &FwFcp, card_cntr: &mut CardCntr) -> Result<(), Error> {
-        // For mute of analog outputs.
-        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::OUT_MUTE_NAME, 0);
-        let elem_id_list = card_cntr.add_bool_elems(&elem_id, 1, 1, true)?;
-        self.measure_elems.extend_from_slice(&elem_id_list);
-
-        // For output volume.
-        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::OUT_VOLUME_NAME, 0);
-        let elem_id_list = card_cntr.add_int_elems(&elem_id, 1,
-                                    Self::DIAL_OUT_MIN, Self::DIAL_OUT_MAX, Self::DIAL_OUT_STEP,
-                                    1, None, true)?;
-        self.measure_elems.extend_from_slice(&elem_id_list);
-
         // For input gain.
         let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::IN_GAIN_NAME, 0);
         let elem_id_list = card_cntr.add_int_elems(&elem_id, 1,
@@ -542,22 +367,6 @@ impl HwState {
         -> Result<bool, Error>
     {
         match elem_id.get_name().as_str() {
-            Self::OUT_MUTE_NAME => {
-                ElemValueAccessor::<bool>::set_val(elem_value, || {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::OutMute);
-                    avc.status(&AvcAddr::Unit, &mut op, TIMEOUT_MS)?;
-                    Ok(op.get_enum() > 0)
-                })?;
-                Ok(true)
-            }
-            Self::OUT_VOLUME_NAME => {
-                ElemValueAccessor::<i32>::set_val(elem_value, || {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::OutVolume);
-                    avc.status(&AvcAddr::Unit, &mut op, TIMEOUT_MS)?;
-                    Ok(Self::DIAL_OUT_MAX - (op.get_u8() as i32))
-                })?;
-                Ok(true)
-            }
             Self::IN_GAIN_NAME => {
                 ElemValueAccessor::<i32>::set_vals(elem_value, 2, |idx| {
                     let mut op = ApogeeCmd::new(company_id, VendorCmd::InGain(idx as u8));
@@ -575,22 +384,6 @@ impl HwState {
         -> Result<bool, Error>
     {
         match elem_id.get_name().as_str() {
-            Self::OUT_MUTE_NAME => {
-                ElemValueAccessor::<bool>::get_val(new, |val| {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::OutMute);
-                    op.put_enum(val as u32);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)
-                })?;
-                Ok(true)
-            }
-            Self::OUT_VOLUME_NAME => {
-                ElemValueAccessor::<i32>::get_val(new, |val| {
-                    let mut op = ApogeeCmd::new(company_id, VendorCmd::OutVolume);
-                    op.put_u8((Self::DIAL_OUT_MAX - val) as u8);
-                    avc.control(&AvcAddr::Unit, &mut op, TIMEOUT_MS)
-                })?;
-                Ok(true)
-            }
             Self::IN_GAIN_NAME => {
                 ElemValueAccessor::<i32>::get_vals(new, old, 2, |idx, val| {
                     let mut op = ApogeeCmd::new(company_id, VendorCmd::InGain(idx as u8));
@@ -607,14 +400,6 @@ impl HwState {
         -> Result<bool, Error>
     {
         match elem_id.get_name().as_str() {
-            Self::OUT_MUTE_NAME => {
-                ElemValueAccessor::<bool>::set_val(elem_value, || Ok(self.states[0] > 0))?;
-                Ok(true)
-            }
-            Self::OUT_VOLUME_NAME => {
-                ElemValueAccessor::<i32>::set_val(elem_value, || Ok(self.states[3] as i32))?;
-                Ok(true)
-            }
             Self::IN_GAIN_NAME => {
                 ElemValueAccessor::<i32>::set_vals(elem_value, 2, |idx| Ok(self.states[4 + idx] as i32))?;
                 Ok(true)
--- libs/oxfw/runtime/src/apogee_model.rs
+++ libs/oxfw/runtime/src/apogee_model.rs
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
-use glib::Error;
+use glib::{Error, FileError};
 
 use hinawa::{FwReq, FwFcp, FwFcpExt};
 use hinawa::{SndUnit, SndUnitExt};
 
-use alsactl::{ElemId, ElemIfaceType, ElemValue, ElemValueExt};
+use alsactl::{ElemId, ElemIfaceType, ElemValue, ElemValueExt, ElemValueExtManual};
 
 use core::card_cntr::*;
+use core::elem_value_accessor::*;
 
 use ta1394::{Ta1394Avc, AvcAddr, AvcSubunitType};
 use ta1394::general::UnitInfo;
@@ -15,7 +16,7 @@ use ta1394::general::UnitInfo;
 use oxfw_protocols::apogee::*;
 
 use super::common_ctl::CommonCtl;
-use super::apogee_ctls::{OutputCtl, MixerCtl, InputCtl, DisplayCtl, HwState};
+use super::apogee_ctls::{MixerCtl, InputCtl, DisplayCtl, HwState};
 
 #[derive(Default, Debug)]
 pub struct ApogeeModel{
@@ -53,7 +54,7 @@ impl CtlModel<hinawa::SndUnit> for ApogeeModel {
         self.common_ctl.load(&self.avc, card_cntr, Self::FCP_TIMEOUT_MS)?;
         self.meter_ctl.load_state(card_cntr, unit, &mut self.req, TIMEOUT_MS)?;
         self.knob_ctl.load_state(card_cntr, &mut self.avc, Self::FCP_TIMEOUT_MS)?;
-        self.output_ctl.load(&self.avc, card_cntr)?;
+        self.output_ctl.load_params(card_cntr, &mut self.avc, Self::FCP_TIMEOUT_MS)?;
         self.mixer_ctl.load(&self.avc, card_cntr)?;
         self.input_ctl.load(&self.avc, card_cntr)?;
         self.display_ctl.load(&self.avc, card_cntr)?;
@@ -72,7 +73,7 @@ impl CtlModel<hinawa::SndUnit> for ApogeeModel {
             Ok(true)
         } else if self.knob_ctl.read_state(elem_id, elem_value)? {
             Ok(true)
-        } else if self.output_ctl.read(&self.avc, &self.company_id, elem_id, elem_value)? {
+        } else if self.output_ctl.read_params(&mut self.avc, elem_id, elem_value, Self::FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.mixer_ctl.read(&self.avc, &self.company_id, elem_id, elem_value)? {
             Ok(true)
@@ -93,7 +94,7 @@ impl CtlModel<hinawa::SndUnit> for ApogeeModel {
     {
         if self.common_ctl.write(unit, &self.avc, elem_id, new, Self::FCP_TIMEOUT_MS)? {
             Ok(true)
-        } else if self.output_ctl.write(&self.avc, &self.company_id, elem_id, old, new)? {
+        } else if self.output_ctl.write_params(&mut self.avc, elem_id, new, Self::FCP_TIMEOUT_MS)? {
             Ok(true)
         } else if self.mixer_ctl.write(&self.avc, &self.company_id, elem_id, old, new)? {
             Ok(true)
@@ -113,6 +114,7 @@ impl MeasureModel<hinawa::SndUnit> for ApogeeModel {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<alsactl::ElemId>) {
         elem_id_list.extend_from_slice(&self.meter_ctl.2);
         elem_id_list.extend_from_slice(&self.knob_ctl.1);
+        elem_id_list.extend_from_slice(&self.output_ctl.2);
         elem_id_list.extend_from_slice(&self.hwstate.measure_elems);
     }
 
@@ -120,8 +122,8 @@ impl MeasureModel<hinawa::SndUnit> for ApogeeModel {
         self.meter_ctl.measure_state(unit, &mut self.req, TIMEOUT_MS)?;
         self.knob_ctl.measure_state(&mut self.avc, TIMEOUT_MS)?;
 
-        self.hwstate.states[0] = self.knob_ctl.0.output_mute as u8;
-        self.hwstate.states[3] = self.knob_ctl.0.output_volume;
+        self.output_ctl.0 = self.knob_ctl.0.output_mute;
+        self.output_ctl.1 = self.knob_ctl.0.output_volume;
         self.hwstate.states[4] = self.knob_ctl.0.input_gains[0];
         self.hwstate.states[5] = self.knob_ctl.0.input_gains[1];
 
@@ -135,6 +137,8 @@ impl MeasureModel<hinawa::SndUnit> for ApogeeModel {
         if self.meter_ctl.read_state(elem_id, elem_value)? {
             Ok(true)
         } else if self.knob_ctl.read_state(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.measure_params(elem_id, elem_value)? {
             Ok(true)
         } else if self.hwstate.measure_elems(elem_id, elem_value)? {
             Ok(true)
@@ -303,6 +307,245 @@ impl KnobCtl {
                     .unwrap();
                 elem_value.set_enum(&[pos as u32]);
                 Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+struct OutputCtl(bool, u8, Vec<ElemId>);
+
+fn output_source_to_str(src: &DuetFwOutputSource) -> &str {
+    match src {
+        DuetFwOutputSource::StreamInputPair0 => "stream-input-1/2",
+        DuetFwOutputSource::MixerOutputPair0 => "mixer-output-1/2",
+    }
+}
+
+fn output_nominal_level_to_str(level: &DuetFwOutputNominalLevel) -> &str {
+    match level {
+        DuetFwOutputNominalLevel::Instrument => "instrument",
+        DuetFwOutputNominalLevel::Consumer => "-10dB",
+    }
+}
+
+fn output_mute_mode_to_str(mode: &DuetFwOutputMuteMode) -> &str {
+    match mode {
+        DuetFwOutputMuteMode::Never => "never",
+        DuetFwOutputMuteMode::Normal => "normal",
+        DuetFwOutputMuteMode::Swapped => "swapped",
+    }
+}
+
+const OUTPUT_MUTE_NAME: &str = "output-mute";
+const OUTPUT_VOLUME_NAME: &str = "output-volume";
+const OUTPUT_SRC_NAME: &str = "output-source";
+const OUTPUT_NOMINAL_LEVEL_NAME: &str = "output-level";
+const OUTPUT_MUTE_FOR_LINE_OUT: &str = "mute-for-line-out";
+const OUTPUT_MUTE_FOR_HP_OUT: &str = "mute-for-hp-out";
+
+impl OutputCtl {
+    const SOURCES: [DuetFwOutputSource; 2] = [
+        DuetFwOutputSource::StreamInputPair0,
+        DuetFwOutputSource::MixerOutputPair0,
+    ];
+
+    const NOMINAL_LEVELS: [DuetFwOutputNominalLevel; 2] = [
+        DuetFwOutputNominalLevel::Instrument,
+        DuetFwOutputNominalLevel::Consumer,
+    ];
+
+    const MUTE_MODES: [DuetFwOutputMuteMode; 3] = [
+        DuetFwOutputMuteMode::Never,
+        DuetFwOutputMuteMode::Normal,
+        DuetFwOutputMuteMode::Swapped,
+    ];
+
+    fn load_params(
+        &mut self,
+        card_cntr: &mut CardCntr,
+        avc: &mut FwFcp,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUTPUT_MUTE_NAME, 0);
+        card_cntr.add_bool_elems(&elem_id, 1, 1, true)
+            .map(|mut elem_id_list| self.2.append(&mut elem_id_list))?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUTPUT_VOLUME_NAME, 0);
+        card_cntr
+            .add_int_elems(
+                &elem_id,
+                1,
+                DuetFwOutputProtocol::VOLUME_MIN as i32,
+                DuetFwOutputProtocol::VOLUME_MAX as i32,
+                DuetFwOutputProtocol::VOLUME_STEP as i32,
+                1,
+                None,
+                true,
+            )
+            .map(|mut elem_id_list| self.2.append(&mut elem_id_list))?;
+
+        let labels: Vec<&str> = Self::SOURCES.iter()
+            .map(|s| output_source_to_str(s))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUTPUT_SRC_NAME, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+
+        let labels: Vec<&str> = Self::NOMINAL_LEVELS.iter()
+            .map(|l| output_nominal_level_to_str(l))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUTPUT_NOMINAL_LEVEL_NAME, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+
+        let labels: Vec<&str> = Self::MUTE_MODES.iter()
+            .map(|m| output_mute_mode_to_str(m))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUTPUT_MUTE_FOR_LINE_OUT, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OUTPUT_MUTE_FOR_HP_OUT, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+
+        DuetFwOutputProtocol::read_mute(avc, &mut self.0, timeout_ms)?;
+        DuetFwOutputProtocol::read_volume(avc, &mut self.1, timeout_ms)?;
+
+        Ok(())
+    }
+
+    fn read_params(
+        &mut self,
+        avc: &mut FwFcp,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+        timeout_ms: u32,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            OUTPUT_SRC_NAME => {
+                let mut src = DuetFwOutputSource::default();
+                DuetFwOutputProtocol::read_src(avc, &mut src, timeout_ms)?;
+                let pos = Self::SOURCES.iter()
+                    .position(|s| s.eq(&src))
+                    .unwrap();
+                elem_value.set_enum(&[pos as u32]);
+                Ok(true)
+            }
+            OUTPUT_NOMINAL_LEVEL_NAME => {
+                let mut level = DuetFwOutputNominalLevel::default();
+                DuetFwOutputProtocol::read_nominal_level(avc, &mut level, timeout_ms)?;
+                let pos = Self::NOMINAL_LEVELS.iter()
+                    .position(|l| l.eq(&level))
+                    .unwrap();
+                elem_value.set_enum(&[pos as u32]);
+                Ok(true)
+            }
+            OUTPUT_MUTE_FOR_LINE_OUT => {
+                let mut mode = DuetFwOutputMuteMode::default();
+                DuetFwOutputProtocol::read_mute_mode_for_analog_output(avc, &mut mode, timeout_ms)?;
+                let pos = Self::MUTE_MODES.iter()
+                    .position(|m| m.eq(&mode))
+                    .unwrap();
+                elem_value.set_enum(&[pos as u32]);
+                Ok(true)
+            }
+            OUTPUT_MUTE_FOR_HP_OUT => {
+                let mut mode = DuetFwOutputMuteMode::default();
+                DuetFwOutputProtocol::read_mute_mode_for_hp(avc, &mut mode, timeout_ms)?;
+                let pos = Self::MUTE_MODES.iter()
+                    .position(|m| m.eq(&mode))
+                    .unwrap();
+                elem_value.set_enum(&[pos as u32]);
+                Ok(true)
+            }
+            _ => self.measure_params(elem_id, elem_value),
+        }
+    }
+
+    fn measure_params(
+        &mut self,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            OUTPUT_MUTE_NAME => {
+                elem_value.set_bool(&[self.0]);
+                Ok(true)
+            }
+            OUTPUT_VOLUME_NAME => {
+                elem_value.set_int(&[self.1 as i32]);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    fn write_params(
+        &mut self,
+        avc: &mut FwFcp,
+        elem_id: &ElemId,
+        elem_value: &ElemValue,
+        timeout_ms: u32,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            OUTPUT_MUTE_NAME => {
+                ElemValueAccessor::<bool>::get_val(elem_value, |val| {
+                    DuetFwOutputProtocol::write_mute(avc, val, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            OUTPUT_VOLUME_NAME => {
+                ElemValueAccessor::<i32>::get_val(elem_value, |val| {
+                    DuetFwOutputProtocol::write_volume(avc, val as u8, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            OUTPUT_SRC_NAME => {
+                ElemValueAccessor::<u32>::get_val(elem_value, |val| {
+                    let &src = Self::SOURCES.iter()
+                        .nth(val as usize)
+                        .ok_or_else(|| {
+                            let msg = format!("Invalid index for source of output: {}", val);
+                            Error::new(FileError::Inval, &msg)
+                        })?;
+                    DuetFwOutputProtocol::write_src(avc, src, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            OUTPUT_NOMINAL_LEVEL_NAME => {
+                ElemValueAccessor::<u32>::get_val(elem_value, |val| {
+                    let &level = Self::NOMINAL_LEVELS.iter()
+                        .nth(val as usize)
+                        .ok_or_else(|| {
+                            let msg = format!("Invalid index for source of output: {}", val);
+                            Error::new(FileError::Inval, &msg)
+                        })?;
+                    DuetFwOutputProtocol::write_nominal_level(avc, level, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            OUTPUT_MUTE_FOR_LINE_OUT => {
+                ElemValueAccessor::<u32>::get_val(elem_value, |val| {
+                    let &mode = Self::MUTE_MODES.iter()
+                        .nth(val as usize)
+                        .ok_or_else(|| {
+                            let msg = format!("Invalid index for mute modes: {}", val);
+                            Error::new(FileError::Inval, &msg)
+                        })?;
+                    DuetFwOutputProtocol::write_mute_mode_for_analog_output(avc, mode, timeout_ms)
+                })
+                .map(|_| true)
+            }
+            OUTPUT_MUTE_FOR_HP_OUT => {
+                ElemValueAccessor::<u32>::get_val(elem_value, |val| {
+                    let &mode = Self::MUTE_MODES.iter()
+                        .nth(val as usize)
+                        .ok_or_else(|| {
+                            let msg = format!("Invalid index for mute modes: {}", val);
+                            Error::new(FileError::Inval, &msg)
+                        })?;
+                    DuetFwOutputProtocol::write_mute_mode_for_hp(avc, mode, timeout_ms)
+                })
+                .map(|_| true)
             }
             _ => Ok(false),
         }
