291ab97c380958a993a3d192ffbe31d0b1aed935
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-19 18:20:59 +0900

    dg00x-protocols: add protocols crate for Digi 00x family
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/dg00x/protocols/src/lib.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocols defined by Digidesign for Digi 00x family
+//!
+//! The crate includes implementation of protocol defined by Digidesign for Digi 00x family.
