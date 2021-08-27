a1b114d890fffed7ca43e4031169e0984d6f3d9f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-27 23:05:46 +0900

    tascam-protocols: add protocols crate
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/tascam/protocols/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! The implementation for protocol defined by Tascam specific to FireWire series.
+//!
+//! The crate includes traits, structures, and enumerations for protocol defined by Tascam specific
+//! to its FireWire series.
