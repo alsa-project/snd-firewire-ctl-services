690d0580d6415045c12573ed88c9862bd9e11828
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-27 23:05:46 +0900

    tascam-protocols: config_rom: add parser for content of unit directory in configuration ROM
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/tascam/protocols/src/bin/tascam-config-rom-parser.rs
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+use glib::{FileError, MainContext, MainLoop};
+
+use hinawa::{FwNode, FwNodeError, FwNodeExt, FwNodeExtManual};
+
+use ieee1212_config_rom::*;
+
+use tascam_protocols::config_rom::*;
+
+use std::convert::TryFrom;
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    let result: Result<(), String> = (|| {
+        let path = std::env::args().nth(1).ok_or_else(|| {
+            let msg =
+                "One argument is required for path to special file of FireWire character device";
+            msg.to_string()
+        })?;
+
+        let node = FwNode::default();
+        node.open(&path).map_err(|e| {
+            let cause = if let Some(error) = e.kind::<FileError>() {
+                match error {
+                    FileError::Isdir => "is directory",
+                    FileError::Acces => "access permission",
+                    FileError::Noent => "not exists",
+                    _ => "unknown",
+                }
+                .to_string()
+            } else if let Some(error) = e.kind::<FwNodeError>() {
+                match error {
+                    FwNodeError::Disconnected => "disconnected",
+                    FwNodeError::Failed => "ioctl error",
+                    _ => "unknown",
+                }
+                .to_string()
+            } else {
+                e.to_string()
+            };
+            format!(
+                "Fail to open firewire character device {}: {} {}",
+                path, cause, e
+            )
+        })?;
+
+        let src = node.create_source().map_err(|e| e.to_string())?;
+
+        let ctx = MainContext::new();
+        let _ = src.attach(Some(&ctx));
+        let dispatcher = Arc::new(MainLoop::new(Some(&ctx), false));
+        let d = dispatcher.clone();
+        let th = thread::spawn(move || d.run());
+
+        let result = {
+            let data = node.get_config_rom().map_err(|e| e.to_string())?;
+            let config_rom = ConfigRom::try_from(data).map_err(|e| e.to_string())?;
+
+            config_rom
+                .get_unit_data()
+                .map(|unit_data| {
+                    println!("Unit data:");
+                    println!("  specifier_id: 0x{:06x}", unit_data.specifier_id);
+                    println!("  version:      0x{:06x}", unit_data.version);
+                    println!("  vendor_name:  {}", unit_data.vendor_name);
+                    println!("  model_name:   {}", unit_data.model_name);
+                })
+                .map_err(|e| format!("Fail to parse content of configuration ROM: {}", e))?;
+
+            Ok(())
+        };
+
+        dispatcher.quit();
+        th.join().unwrap();
+        result
+    })();
+
+    let code = match result {
+        Ok(_) => 0,
+        Err(msg) => {
+            println!("{}", msg);
+            print_help();
+            1
+        }
+    };
+
+    std::process::exit(code)
+}
+
+fn print_help() {
+    print!(
+        r###"
+Usage:
+  tascam-config-rom-parser CDEV
+
+  where:
+    CDEV:       The path to special file of firewire character device, typically '/dev/fw1'.
+"###
+    );
+}
