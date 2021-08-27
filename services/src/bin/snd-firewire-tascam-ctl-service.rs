dc0a804f876e9df7ed1a50dab4c1c0fd80e50567
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-27 23:05:46 +0900

    tascam-runtime: rename runtime crate
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- services/src/bin/snd-firewire-tascam-ctl-service.rs
+++ services/src/bin/snd-firewire-tascam-ctl-service.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2020 Takashi Sakamoto
 use snd_firewire_ctl_services::*;
-use tascam::runtime::TascamRuntime;
+use tascam_runtime::runtime::TascamRuntime;
 
 struct TascamServiceCmd;
 
