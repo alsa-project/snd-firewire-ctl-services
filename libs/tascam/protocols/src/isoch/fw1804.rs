209aa10d5f87727f42bff7642f88d1b0d5120781
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-08-27 23:05:46 +0900

    tascam-protocols: isoch: add protocol implementation of meter operation
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ libs/tascam/protocols/src/isoch/fw1804.rs
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol implementation for FW-1804.
+//!
+//! The module includes protocol implementation defined by Tascam for FW-1804.
+
+use super::*;
+
+/// The protocol implementation of FW-1804.
+#[derive(Default)]
+pub struct Fw1804Protocol;
+
+impl IsochMeterOperation for Fw1804Protocol {
+    const INPUT_COUNT: usize = 18;
+    const OUTPUT_COUNT: usize = 18;
+    const HAS_SOLO: bool = false;
+}
