902b835e9bf1a217f0ab39b004f3292146c591bf
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-09-12 17:41:48 +0900

    dice-runtime: blackbird_model: code refactoring for rename of structure member
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/blackbird_model.rs
+++ libs/dice/runtime/src/blackbird_model.rs
@@ -18,7 +18,7 @@ use super::tcd22xx_ctl::*;
 
 #[derive(Default)]
 pub struct BlackbirdModel {
-    proto: FwReq,
+    req: FwReq,
     sections: GeneralSections,
     extension_sections: ExtensionSections,
     ctl: CommonCtl,
@@ -29,18 +29,18 @@ const TIMEOUT_MS: u32 = 20;
 
 impl CtlModel<SndDice> for BlackbirdModel {
     fn load(&mut self, unit: &mut SndDice, card_cntr: &mut CardCntr) -> Result<(), Error> {
-        let node = unit.get_node();
+        let mut node = unit.get_node();
 
-        self.sections = self.proto.read_general_sections(&node, TIMEOUT_MS)?;
-        let caps = self.proto.read_clock_caps(&node, &self.sections, TIMEOUT_MS)?;
-        let src_labels = self.proto.read_clock_source_labels(&node, &self.sections, TIMEOUT_MS)?;
+        self.sections = self.req.read_general_sections(&mut node, TIMEOUT_MS)?;
+        let caps = self.req.read_clock_caps(&mut node, &self.sections, TIMEOUT_MS)?;
+        let src_labels = self.req.read_clock_source_labels(&mut node, &self.sections, TIMEOUT_MS)?;
         self.ctl.load(card_cntr, &caps, &src_labels)?;
 
-        self.extension_sections = self.proto.read_extension_sections(&node, TIMEOUT_MS)?;
-        self.tcd22xx_ctl.load(unit, &self.proto, &self.extension_sections, &caps, &src_labels,
+        self.extension_sections = self.req.read_extension_sections(&mut node, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.load(unit, &mut self.req, &self.extension_sections, &caps, &src_labels,
                           TIMEOUT_MS, card_cntr)?;
 
-        self.tcd22xx_ctl.cache(unit, &self.proto, &self.sections, &self.extension_sections, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.cache(unit, &mut self.req, &self.sections, &self.extension_sections, TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -48,9 +48,9 @@ impl CtlModel<SndDice> for BlackbirdModel {
     fn read(&mut self, unit: &mut SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        if self.ctl.read(unit, &self.proto, &self.sections, elem_id, elem_value, TIMEOUT_MS)? {
+        if self.ctl.read(unit, &mut self.req, &self.sections, elem_id, elem_value, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.tcd22xx_ctl.read(unit, &self.proto, &self.extension_sections, elem_id,
+        } else if self.tcd22xx_ctl.read(unit, &mut self.req, &self.extension_sections, elem_id,
                                     elem_value, TIMEOUT_MS)? {
             Ok(true)
         } else {
@@ -61,9 +61,9 @@ impl CtlModel<SndDice> for BlackbirdModel {
     fn write(&mut self, unit: &mut SndDice, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        if self.ctl.write(unit, &self.proto, &self.sections, elem_id, old, new, TIMEOUT_MS)? {
+        if self.ctl.write(unit, &mut self.req, &self.sections, elem_id, old, new, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.tcd22xx_ctl.write(unit, &self.proto, &self.extension_sections, elem_id,
+        } else if self.tcd22xx_ctl.write(unit, &mut self.req, &self.extension_sections, elem_id,
                                      old, new, TIMEOUT_MS)? {
             Ok(true)
         } else {
@@ -79,8 +79,8 @@ impl NotifyModel<SndDice, u32> for BlackbirdModel {
     }
 
     fn parse_notification(&mut self, unit: &mut SndDice, msg: &u32) -> Result<(), Error> {
-        self.ctl.parse_notification(unit, &self.proto, &self.sections, *msg, TIMEOUT_MS)?;
-        self.tcd22xx_ctl.parse_notification(unit, &self.proto, &self.sections,
+        self.ctl.parse_notification(unit, &mut self.req, &self.sections, *msg, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.parse_notification(unit, &mut self.req, &self.sections,
                                         &self.extension_sections, TIMEOUT_MS, *msg)?;
         Ok(())
     }
@@ -105,8 +105,8 @@ impl MeasureModel<hinawa::SndDice> for BlackbirdModel {
     }
 
     fn measure_states(&mut self, unit: &mut SndDice) -> Result<(), Error> {
-        self.ctl.measure_states(unit, &self.proto, &self.sections, TIMEOUT_MS)?;
-        self.tcd22xx_ctl.measure_states(unit, &self.proto, &self.extension_sections, TIMEOUT_MS)?;
+        self.ctl.measure_states(unit, &mut self.req, &self.sections, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.measure_states(unit, &mut self.req, &self.extension_sections, TIMEOUT_MS)?;
         Ok(())
     }
 
