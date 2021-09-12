29ff7aa56421cfdee1ca9c6143f8f9f5827d052b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-09-12 17:41:49 +0900

    dice-protocols: tcat: extension: code refactoring for application section protocol
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/protocols/src/tcat/extension/appl_section.rs
+++ libs/dice/protocols/src/tcat/extension/appl_section.rs
@@ -8,30 +8,44 @@
 
 use super::*;
 
-pub trait ApplSectionProtocol: ProtocolExtension {
-    fn read_appl_data(
-        &self,
+/// The structure for protocol implementation of application section.
+#[derive(Default)]
+pub struct ApplSectionProtocol;
+
+impl ApplSectionProtocol {
+    pub fn read_appl_data(
+        req: &mut FwReq,
         node: &mut FwNode,
         sections: &ExtensionSections,
         offset: usize,
         frames: &mut [u8],
         timeout_ms: u32
     ) -> Result<(), Error> {
-        ProtocolExtension::read(self, node, sections.application.offset + offset, frames, timeout_ms)
-            .map_err(|e| Error::new(ProtocolExtensionError::Appl, &e.to_string()))
+        ProtocolExtension::read(
+            req,
+            node,
+            sections.application.offset + offset,
+            frames,
+            timeout_ms,
+        )
+        .map_err(|e| Error::new(ProtocolExtensionError::Appl, &e.to_string()))
     }
 
-    fn write_appl_data(
-        &self,
+    pub fn write_appl_data(
+        req: &mut FwReq,
         node: &mut FwNode,
         sections: &ExtensionSections,
         offset: usize,
         frames: &mut [u8],
         timeout_ms: u32
     ) -> Result<(), Error> {
-        ProtocolExtension::write(self, node, sections.application.offset + offset, frames, timeout_ms)
-            .map_err(|e| Error::new(ProtocolExtensionError::Appl, &e.to_string()))
+        ProtocolExtension::write(
+            req,
+            node,
+            sections.application.offset + offset,
+            frames,
+            timeout_ms,
+        )
+        .map_err(|e| Error::new(ProtocolExtensionError::Appl, &e.to_string()))
     }
 }
-
-impl<O: AsRef<FwReq>> ApplSectionProtocol for O {}
