60f02d3b2a45956e4e0ee96cf7594fc6a6e8d53f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-09-12 17:41:48 +0900

    dice-runtime: minimal_model: code refactoring for rename of structure member
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/minimal_model.rs
+++ libs/dice/runtime/src/minimal_model.rs
@@ -15,7 +15,7 @@ use super::common_ctl::*;
 
 #[derive(Default)]
 pub struct MinimalModel{
-    proto: FwReq,
+    req: FwReq,
     sections: GeneralSections,
     ctl: CommonCtl,
 }
@@ -24,24 +24,24 @@ const TIMEOUT_MS: u32 = 20;
 
 impl CtlModel<SndDice> for MinimalModel {
     fn load(&mut self, unit: &mut SndDice, card_cntr: &mut CardCntr) -> Result<(), Error> {
-        let node = unit.get_node();
+        let mut node = unit.get_node();
 
-        self.sections = self.proto.read_general_sections(&node, TIMEOUT_MS)?;
-        let caps = self.proto.read_clock_caps(&node, &self.sections, TIMEOUT_MS)?;
-        let src_labels = self.proto.read_clock_source_labels(&node, &self.sections, TIMEOUT_MS)?;
+        self.sections = self.req.read_general_sections(&mut node, TIMEOUT_MS)?;
+        let caps = self.req.read_clock_caps(&mut node, &self.sections, TIMEOUT_MS)?;
+        let src_labels = self.req.read_clock_source_labels(&mut node, &self.sections, TIMEOUT_MS)?;
         self.ctl.load(card_cntr, &caps, &src_labels)
     }
 
     fn read(&mut self, unit: &mut SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        self.ctl.read(unit, &self.proto, &self.sections, elem_id, elem_value, TIMEOUT_MS)
+        self.ctl.read(unit, &mut self.req, &self.sections, elem_id, elem_value, TIMEOUT_MS)
     }
 
     fn write(&mut self, unit: &mut SndDice, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        self.ctl.write(unit, &self.proto, &self.sections, elem_id, old, new, TIMEOUT_MS)
+        self.ctl.write(unit, &mut self.req, &self.sections, elem_id, old, new, TIMEOUT_MS)
     }
 }
 
@@ -51,7 +51,7 @@ impl NotifyModel<SndDice, u32> for MinimalModel {
     }
 
     fn parse_notification(&mut self, unit: &mut SndDice, msg: &u32) -> Result<(), Error> {
-        self.ctl.parse_notification(unit, &self.proto, &self.sections, *msg, TIMEOUT_MS)
+        self.ctl.parse_notification(unit, &mut self.req, &self.sections, *msg, TIMEOUT_MS)
     }
 
     fn read_notified_elem(&mut self, _: &SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
@@ -61,13 +61,13 @@ impl NotifyModel<SndDice, u32> for MinimalModel {
     }
 }
 
-impl MeasureModel<hinawa::SndDice> for MinimalModel {
+impl MeasureModel<SndDice> for MinimalModel {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.ctl.measured_elem_list);
     }
 
     fn measure_states(&mut self, unit: &mut SndDice) -> Result<(), Error> {
-        self.ctl.measure_states(unit, &self.proto, &self.sections, TIMEOUT_MS)
+        self.ctl.measure_states(unit, &mut self.req, &self.sections, TIMEOUT_MS)
     }
 
     fn measure_elem(&mut self, _: &SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
