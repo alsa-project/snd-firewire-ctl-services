db62716c247e1cc907a20f972a4fa6d5882d5bd0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-09-12 17:41:48 +0900

    dice-runtime: focusrite: code refactoring for rename of structure member and code format
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/dice/runtime/src/focusrite/spro40_model.rs
+++ libs/dice/runtime/src/focusrite/spro40_model.rs
@@ -16,11 +16,11 @@ use dice_protocols::focusrite::spro40::*;
 use crate::common_ctl::*;
 use crate::tcd22xx_ctl::*;
 
-use super::out_grp_ctl::*;
+use super::*;
 
 #[derive(Default)]
 pub struct SPro40Model {
-    proto: FwReq,
+    req: FwReq,
     sections: GeneralSections,
     extension_sections: ExtensionSections,
     ctl: CommonCtl,
@@ -33,20 +33,20 @@ const TIMEOUT_MS: u32 = 20;
 
 impl CtlModel<SndDice> for SPro40Model {
     fn load(&mut self, unit: &mut SndDice, card_cntr: &mut CardCntr) -> Result<(), Error> {
-        let node = unit.get_node();
+        let mut node = unit.get_node();
 
-        self.sections = self.proto.read_general_sections(&node, TIMEOUT_MS)?;
-        let caps = self.proto.read_clock_caps(&node, &self.sections, TIMEOUT_MS)?;
-        let src_labels = self.proto.read_clock_source_labels(&node, &self.sections, TIMEOUT_MS)?;
+        self.sections = self.req.read_general_sections(&mut node, TIMEOUT_MS)?;
+        let caps = self.req.read_clock_caps(&mut node, &self.sections, TIMEOUT_MS)?;
+        let src_labels = self.req.read_clock_source_labels(&mut node, &self.sections, TIMEOUT_MS)?;
         self.ctl.load(card_cntr, &caps, &src_labels)?;
 
-        self.extension_sections = self.proto.read_extension_sections(&node, TIMEOUT_MS)?;
-        self.tcd22xx_ctl.load(unit, &self.proto, &self.extension_sections, &caps, &src_labels,
+        self.extension_sections = self.req.read_extension_sections(&mut node, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.load(unit, &mut self.req, &self.extension_sections, &caps, &src_labels,
                           TIMEOUT_MS, card_cntr)?;
 
-        self.tcd22xx_ctl.cache(unit, &self.proto, &self.sections, &self.extension_sections, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.cache(unit, &mut self.req, &self.sections, &self.extension_sections, TIMEOUT_MS)?;
 
-        self.out_grp_ctl.load(card_cntr, unit, &self.proto, &self.extension_sections,
+        self.out_grp_ctl.load(card_cntr, unit, &mut self.req, &self.extension_sections,
                               &mut self.tcd22xx_ctl.state, TIMEOUT_MS)?;
         self.specific_ctl.load(card_cntr)?;
 
@@ -56,14 +56,14 @@ impl CtlModel<SndDice> for SPro40Model {
     fn read(&mut self, unit: &mut SndDice, elem_id: &ElemId, elem_value: &mut ElemValue)
         -> Result<bool, Error>
     {
-        if self.ctl.read(unit, &self.proto, &self.sections, elem_id, elem_value, TIMEOUT_MS)? {
+        if self.ctl.read(unit, &mut self.req, &self.sections, elem_id, elem_value, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.tcd22xx_ctl.read(unit, &self.proto, &self.extension_sections, elem_id,
+        } else if self.tcd22xx_ctl.read(unit, &mut self.req, &self.extension_sections, elem_id,
                                     elem_value, TIMEOUT_MS)? {
             Ok(true)
         } else if self.out_grp_ctl.read(&self.tcd22xx_ctl.state, elem_id, elem_value)? {
             Ok(true)
-        } else if self.specific_ctl.read(unit, &self.proto, &self.extension_sections, elem_id, elem_value,
+        } else if self.specific_ctl.read(unit, &mut self.req, &self.extension_sections, elem_id, elem_value,
                                          TIMEOUT_MS)? {
             Ok(true)
         } else {
@@ -74,15 +74,15 @@ impl CtlModel<SndDice> for SPro40Model {
     fn write(&mut self, unit: &mut SndDice, elem_id: &ElemId, old: &ElemValue, new: &ElemValue)
         -> Result<bool, Error>
     {
-        if self.ctl.write(unit, &self.proto, &self.sections, elem_id, old, new, TIMEOUT_MS)? {
+        if self.ctl.write(unit, &mut self.req, &self.sections, elem_id, old, new, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.tcd22xx_ctl.write(unit, &self.proto, &self.extension_sections, elem_id,
+        } else if self.tcd22xx_ctl.write(unit, &mut self.req, &self.extension_sections, elem_id,
                                      old, new, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.out_grp_ctl.write(unit, &self.proto, &self.extension_sections,
+        } else if self.out_grp_ctl.write(unit, &mut self.req, &self.extension_sections,
                                          &mut self.tcd22xx_ctl.state, elem_id, new, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.specific_ctl.write(unit, &self.proto, &self.extension_sections, elem_id,
+        } else if self.specific_ctl.write(unit, &mut self.req, &self.extension_sections, elem_id,
                                           new, TIMEOUT_MS)? {
             Ok(true)
         } else {
@@ -99,10 +99,10 @@ impl NotifyModel<SndDice, u32> for SPro40Model {
     }
 
     fn parse_notification(&mut self, unit: &mut SndDice, msg: &u32) -> Result<(), Error> {
-        self.ctl.parse_notification(unit, &self.proto, &self.sections, *msg, TIMEOUT_MS)?;
-        self.tcd22xx_ctl.parse_notification(unit, &self.proto, &self.sections,
+        self.ctl.parse_notification(unit, &mut self.req, &self.sections, *msg, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.parse_notification(unit, &mut self.req, &self.sections,
                                         &self.extension_sections, TIMEOUT_MS, *msg)?;
-        self.out_grp_ctl.parse_notification(unit, &self.proto, &self.extension_sections,
+        self.out_grp_ctl.parse_notification(unit, &mut self.req, &self.extension_sections,
                                             &mut self.tcd22xx_ctl.state, *msg, TIMEOUT_MS)?;
         Ok(())
     }
@@ -122,15 +122,15 @@ impl NotifyModel<SndDice, u32> for SPro40Model {
     }
 }
 
-impl MeasureModel<hinawa::SndDice> for SPro40Model {
+impl MeasureModel<SndDice> for SPro40Model {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.ctl.measured_elem_list);
         self.tcd22xx_ctl.get_measured_elem_list(elem_id_list);
     }
 
     fn measure_states(&mut self, unit: &mut SndDice) -> Result<(), Error> {
-        self.ctl.measure_states(unit, &self.proto, &self.sections, TIMEOUT_MS)?;
-        self.tcd22xx_ctl.measure_states(unit, &self.proto, &self.extension_sections, TIMEOUT_MS)?;
+        self.ctl.measure_states(unit, &mut self.req, &self.sections, TIMEOUT_MS)?;
+        self.tcd22xx_ctl.measure_states(unit, &mut self.req, &self.extension_sections, TIMEOUT_MS)?;
         Ok(())
     }
 
@@ -166,7 +166,7 @@ impl SpecificCtl {
         OptOutIfaceMode::Spdif,
     ];
 
-    pub fn load(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
+    fn load(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
         let elem_id = ElemId::new_by_name(ElemIfaceType::Card, 0, 0, Self::ANALOG_OUT_0_1_PAD_NAME, 0);
         card_cntr.add_bool_elems(&elem_id, 1, 1, true)?;
 
@@ -179,21 +179,32 @@ impl SpecificCtl {
         Ok(())
     }
 
-    pub fn read(&mut self, unit: &SndDice, proto: &FwReq, sections: &ExtensionSections, elem_id: &ElemId,
-                elem_value: &mut ElemValue, timeout_ms: u32)
-        -> Result<bool, Error>
-    {
+    fn read(
+        &mut self,
+        unit: &mut SndDice,
+        req: &mut FwReq,
+        sections: &ExtensionSections,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+        timeout_ms: u32
+    ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::ANALOG_OUT_0_1_PAD_NAME => {
-                let enabled = proto.read_analog_out_0_1_pad_offset(&unit.get_node(), sections, timeout_ms)?;
+                let enabled = req.read_analog_out_0_1_pad_offset(
+                    &mut unit.get_node(),
+                    sections,
+                    timeout_ms
+                )?;
                 elem_value.set_bool(&[enabled]);
                 Ok(true)
             }
             Self::OPT_OUT_IFACE_MODE_NAME => {
-                let mode = proto.read_opt_out_iface_mode(&unit.get_node(), sections, timeout_ms)?;
-                let pos = Self::OPT_OUT_IFACE_MODES.iter()
-                    .position(|m| m.eq(&mode))
-                    .unwrap();
+                let mode = req.read_opt_out_iface_mode(
+                    &mut unit.get_node(),
+                    sections,
+                    timeout_ms
+                )?;
+                let pos = Self::OPT_OUT_IFACE_MODES.iter().position(|m| m.eq(&mode)).unwrap();
                 elem_value.set_enum(&[pos as u32]);
                 Ok(true)
             }
@@ -201,27 +212,43 @@ impl SpecificCtl {
         }
     }
 
-    pub fn write(&mut self, unit: &SndDice, proto: &FwReq, sections: &ExtensionSections, elem_id: &ElemId,
-                 elem_value: &ElemValue, timeout_ms: u32)
-        -> Result<bool, Error>
-    {
+    fn write(
+        &mut self,
+        unit: &mut SndDice,
+        req: &mut FwReq,
+        sections: &ExtensionSections,
+        elem_id: &ElemId,
+        elem_value: &ElemValue,
+        timeout_ms: u32
+    ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
             Self::ANALOG_OUT_0_1_PAD_NAME => {
                 let mut vals = [false];
                 elem_value.get_bool(&mut vals);
-                proto.write_analog_out_0_1_pad_offset(&unit.get_node(), sections, vals[0], timeout_ms)
+                req.write_analog_out_0_1_pad_offset(
+                    &mut unit.get_node(),
+                    sections,
+                    vals[0],
+                    timeout_ms
+                )
                     .map(|_| true)
             }
             Self::OPT_OUT_IFACE_MODE_NAME => {
                 let mut vals = [0];
                 elem_value.get_enum(&mut vals);
-                let mode = Self::OPT_OUT_IFACE_MODES.iter()
+                let &mode = Self::OPT_OUT_IFACE_MODES.iter()
                     .nth(vals[0] as usize)
                     .ok_or_else(|| {
-                        let msg = format!("Invalid value for index of optical output interface mode: {}", vals[0]);
+                        let msg = format!("Invalid index of optical output interface mode: {}",
+                                          vals[0]);
                         Error::new(FileError::Inval, &msg)
                     })?;
-                proto.write_opt_out_iface_mode(&unit.get_node(), sections, *mode, timeout_ms)
+                req.write_opt_out_iface_mode(
+                    &mut unit.get_node(),
+                    sections,
+                    mode,
+                    timeout_ms
+                )
                     .map(|_| true)
             }
             _ => Ok(false),
