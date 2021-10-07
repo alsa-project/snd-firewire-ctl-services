04751d803d2d7e5986cc15532d6add115f100cf7
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2021-10-07 09:49:51 +0900

    motu-runtime: use protocol implementation for input controls of Audio Express and 4 pre
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/motu/runtime/src/audioexpress.rs
+++ libs/motu/runtime/src/audioexpress.rs
@@ -23,6 +23,7 @@ pub struct AudioExpress {
     mixer_output_ctl: MixerOutputCtl,
     mixer_return_ctl: MixerReturnCtl,
     mixer_source_ctl: MixerSourceCtl,
+    input_ctl: InputCtl,
     output_ctl: OutputCtl,
 }
 
@@ -88,6 +89,19 @@ impl RegisterDspOutputCtlOperation<AudioExpressProtocol> for OutputCtl {
     }
 }
 
+#[derive(Default)]
+struct InputCtl(Audioexpress4preInputState, Vec<ElemId>);
+
+impl Audioexpress4preInputCtlOperation<AudioExpressProtocol> for InputCtl {
+    fn state(&self) -> &Audioexpress4preInputState {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut Audioexpress4preInputState {
+        &mut self.0
+    }
+}
+
 impl CtlModel<SndMotu> for AudioExpress {
     fn load(&mut self, unit: &mut SndMotu, card_cntr: &mut CardCntr) -> Result<(), Error> {
         self.clk_ctls.load(card_cntr)?;
@@ -100,6 +114,8 @@ impl CtlModel<SndMotu> for AudioExpress {
             .map(|elem_id_list| self.mixer_source_ctl.1 = elem_id_list)?;
         self.output_ctl.load(card_cntr, unit, &mut self.req, TIMEOUT_MS)
             .map(|elem_id_list| self.output_ctl.1 = elem_id_list)?;
+        self.input_ctl.load(card_cntr, unit, &mut self.req, TIMEOUT_MS)
+            .map(|elem_id_list| self.input_ctl.1 = elem_id_list)?;
         Ok(())
     }
 
@@ -120,6 +136,8 @@ impl CtlModel<SndMotu> for AudioExpress {
         } else if self.mixer_source_ctl.read(elem_id, elem_value)? {
             Ok(true)
         } else if self.output_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -144,6 +162,8 @@ impl CtlModel<SndMotu> for AudioExpress {
         } else if self.mixer_source_ctl.write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)? {
             Ok(true)
         } else if self.output_ctl.write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)? {
+            Ok(true)
+        } else if self.input_ctl.write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)? {
             Ok(true)
         } else {
             Ok(false)
--- libs/motu/runtime/src/register_dsp_ctls.rs
+++ libs/motu/runtime/src/register_dsp_ctls.rs
@@ -792,3 +792,145 @@ pub trait Traveler828mk2LineInputCtlOperation<T: Traveler828mk2LineInputOperatio
         }
     }
 }
+
+const INPUT_GAIN_NAME: &str = "input-gain";
+const INPUT_INVERT_NAME: &str = "input-invert";
+const MIC_PHANTOM_NAME: &str = "mic-phantom";
+const MIC_PAD_NAME: &str = "mic-pad";
+
+pub trait Audioexpress4preInputCtlOperation<T: Audioexpress4preInputOperation> {
+    fn state(&self) -> &Audioexpress4preInputState;
+    fn state_mut(&mut self) -> &mut Audioexpress4preInputState;
+
+    const GAIN_TLV: DbInterval = DbInterval {
+        min: -6400,
+        max: 0,
+        linear: true,
+        mute_avail: false,
+    };
+
+    fn load(
+        &mut self,
+        card_cntr: &mut CardCntr,
+        unit: &mut SndMotu,
+        req: &mut FwReq,
+        timeout_ms: u32,
+    ) -> Result<Vec<ElemId>, Error> {
+        let mut state = T::create_input_state();
+        T::read_input_state(req, &mut unit.get_node(), &mut state, timeout_ms)?;
+        *self.state_mut() = state;
+
+        let mut notified_elem_id_list = Vec::new();
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, INPUT_GAIN_NAME, 0);
+        card_cntr.add_int_elems(
+            &elem_id,
+            1,
+            T::INPUT_GAIN_MIN as i32,
+            T::INPUT_GAIN_MAX as i32,
+            T::INPUT_GAIN_STEP as i32,
+            T::INPUT_COUNT,
+            Some(&Vec::<u32>::from(&Self::GAIN_TLV)),
+            true,
+        )
+            .map(|mut elem_id_list| notified_elem_id_list.append(&mut elem_id_list))?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, INPUT_INVERT_NAME, 0);
+        card_cntr.add_bool_elems(&elem_id, 1, T::INPUT_COUNT, true)
+            .map(|mut elem_id_list| notified_elem_id_list.append(&mut elem_id_list))?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, MIC_PHANTOM_NAME, 0);
+        card_cntr.add_bool_elems(&elem_id, 1, T::MIC_COUNT, true)
+            .map(|mut elem_id_list| notified_elem_id_list.append(&mut elem_id_list))?;
+
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, MIC_PAD_NAME, 0);
+        card_cntr.add_bool_elems(&elem_id, 1, T::MIC_COUNT, true)
+            .map(|mut elem_id_list| notified_elem_id_list.append(&mut elem_id_list))?;
+
+        Ok(notified_elem_id_list)
+    }
+
+    fn read(&mut self, elem_id: &ElemId, elem_value: &mut ElemValue) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            INPUT_GAIN_NAME => {
+                copy_int_to_elem_value(elem_value, &self.state().gain);
+                Ok(true)
+            }
+            INPUT_INVERT_NAME => {
+                elem_value.set_bool(&self.state().invert);
+                Ok(true)
+            }
+            MIC_PHANTOM_NAME => {
+                elem_value.set_bool(&self.state().phantom);
+                Ok(true)
+            }
+            MIC_PAD_NAME => {
+                elem_value.set_bool(&self.state().pad);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    fn write(
+        &mut self,
+        unit: &mut SndMotu,
+        req: &mut FwReq,
+        elem_id: &ElemId,
+        elem_value: &ElemValue,
+        timeout_ms: u32,
+    ) -> Result<bool, Error> {
+        match elem_id.get_name().as_str() {
+            INPUT_GAIN_NAME => {
+                let mut vals = vec![0; T::INPUT_COUNT];
+                elem_value.get_int(&mut vals);
+                let gain: Vec<u8> = vals.iter().map(|&val| val as u8).collect();
+                T::write_input_gain(
+                    req,
+                    &mut unit.get_node(),
+                    &gain,
+                    self.state_mut(),
+                    timeout_ms
+                )
+                    .map(|_| true)
+            }
+            INPUT_INVERT_NAME => {
+                let mut invert = vec![false; T::INPUT_COUNT];
+                elem_value.get_bool(&mut invert);
+                T::write_input_invert(
+                    req,
+                    &mut unit.get_node(),
+                    &invert,
+                    self.state_mut(),
+                    timeout_ms
+                )
+                    .map(|_| true)
+            }
+            MIC_PHANTOM_NAME => {
+                let mut phantom = vec![false; T::MIC_COUNT];
+                elem_value.get_bool(&mut phantom);
+                T::write_mic_phantom(
+                    req,
+                    &mut unit.get_node(),
+                    &phantom,
+                    self.state_mut(),
+                    timeout_ms
+                )
+                    .map(|_| true)
+            }
+            MIC_PAD_NAME => {
+                let mut pad = vec![false; T::MIC_COUNT];
+                elem_value.get_bool(&mut pad);
+                T::write_mic_pad(
+                    req,
+                    &mut unit.get_node(),
+                    &pad,
+                    self.state_mut(),
+                    timeout_ms
+                )
+                    .map(|_| true)
+            }
+            _ => Ok(false),
+        }
+    }
+}
