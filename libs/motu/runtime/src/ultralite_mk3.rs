267860a9afa5a1ca2b0ad50dbc791476036d6c91
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-20 15:07:15 +0900

    motu-runtime: add timer dispatcher for metering in command DSP models
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/motu/runtime/src/ultralite_mk3.rs
+++ libs/motu/runtime/src/ultralite_mk3.rs
@@ -405,6 +405,18 @@ impl NotifyModel<SndMotu, Vec<DspCmd>> for UltraLiteMk3 {
     }
 }
 
+impl MeasureModel<SndMotu> for UltraLiteMk3 {
+    fn get_measure_elem_list(&mut self, _: &mut Vec<ElemId>) {}
+
+    fn measure_states(&mut self, _: &mut SndMotu) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn measure_elem(&mut self, _: &SndMotu, _: &ElemId, _: &mut ElemValue) -> Result<bool, Error> {
+        Ok(false)
+    }
+}
+
 impl CommandDspModel for UltraLiteMk3 {
     fn prepare_message_handler<F>(&mut self, unit: &mut SndMotu, handler: F) -> Result<(), Error>
     where
