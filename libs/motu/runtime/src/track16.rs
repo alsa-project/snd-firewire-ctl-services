cc956d6c67fef30f10bd5f0cc27bd0f656faafa8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-20 15:07:15 +0900

    motu-runtime: add controls to operate command in Track 16
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/motu/runtime/src/track16.rs
+++ libs/motu/runtime/src/track16.rs
@@ -14,6 +14,12 @@ pub struct Track16 {
     opt_iface_ctl: OptIfaceCtl,
     phone_assign_ctl: PhoneAssignCtl,
     sequence_number: u8,
+    reverb_ctl: ReverbCtl,
+    monitor_ctl: MonitorCtl,
+    mixer_ctl: MixerCtl,
+    input_ctl: InputCtl,
+    output_ctl: OutputCtl,
+    resource_ctl: ResourceCtl,
 }
 
 #[derive(Default)]
@@ -52,6 +58,84 @@ struct OptIfaceCtl;
 
 impl V3OptIfaceCtlOperation<Track16Protocol> for OptIfaceCtl {}
 
+#[derive(Default)]
+struct ReverbCtl(CommandDspReverbState, Vec<ElemId>);
+
+impl CommandDspReverbCtlOperation<Track16Protocol> for ReverbCtl {
+    fn state(&self) -> &CommandDspReverbState {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut CommandDspReverbState {
+        &mut self.0
+    }
+}
+
+#[derive(Default)]
+struct MonitorCtl(CommandDspMonitorState, Vec<ElemId>);
+
+impl CommandDspMonitorCtlOperation<Track16Protocol> for MonitorCtl {
+    fn state(&self) -> &CommandDspMonitorState {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut CommandDspMonitorState {
+        &mut self.0
+    }
+}
+
+#[derive(Default)]
+struct MixerCtl(CommandDspMixerState, Vec<ElemId>);
+
+impl CommandDspMixerCtlOperation<Track16Protocol> for MixerCtl {
+    fn state(&self) -> &CommandDspMixerState {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut CommandDspMixerState {
+        &mut self.0
+    }
+}
+
+#[derive(Default)]
+struct InputCtl(CommandDspInputState, Vec<ElemId>);
+
+impl CommandDspInputCtlOperation<Track16Protocol> for InputCtl {
+    fn state(&self) -> &CommandDspInputState {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut CommandDspInputState {
+        &mut self.0
+    }
+}
+
+#[derive(Default)]
+struct OutputCtl(CommandDspOutputState, Vec<ElemId>);
+
+impl CommandDspOutputCtlOperation<Track16Protocol> for OutputCtl {
+    fn state(&self) -> &CommandDspOutputState {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut CommandDspOutputState {
+        &mut self.0
+    }
+}
+
+#[derive(Default)]
+struct ResourceCtl(u32, Vec<ElemId>);
+
+impl CommandDspResourcebCtlOperation for ResourceCtl {
+    fn state(&self) -> &u32 {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut u32 {
+        &mut self.0
+    }
+}
+
 impl CtlModel<SndMotu> for Track16 {
     fn load(&mut self, unit: &mut SndMotu, card_cntr: &mut CardCntr) -> Result<(), Error> {
         self.clk_ctls.load(card_cntr)?;
@@ -62,6 +146,36 @@ impl CtlModel<SndMotu> for Track16 {
         self.phone_assign_ctl
             .load(card_cntr, unit, &mut self.req, TIMEOUT_MS)
             .map(|mut elem_id_list| self.phone_assign_ctl.1.append(&mut elem_id_list))?;
+        self.reverb_ctl
+            .load(card_cntr)
+            .map(|mut elem_id_list| self.reverb_ctl.1.append(&mut elem_id_list))?;
+        self.monitor_ctl
+            .load(card_cntr)
+            .map(|mut elem_id_list| self.monitor_ctl.1.append(&mut elem_id_list))?;
+        self.mixer_ctl
+            .load(card_cntr)
+            .map(|mut elem_id_list| self.mixer_ctl.1.append(&mut elem_id_list))?;
+        self.input_ctl
+            .load(card_cntr)
+            .map(|mut elem_id_list| self.input_ctl.1.append(&mut elem_id_list))?;
+        self.input_ctl
+            .load_equalizer(card_cntr)
+            .map(|mut elem_id_list| self.input_ctl.1.append(&mut elem_id_list))?;
+        self.input_ctl
+            .load_dynamics(card_cntr)
+            .map(|mut elem_id_list| self.input_ctl.1.append(&mut elem_id_list))?;
+        self.output_ctl
+            .load(card_cntr)
+            .map(|mut elem_id_list| self.output_ctl.1.append(&mut elem_id_list))?;
+        self.output_ctl
+            .load_equalizer(card_cntr)
+            .map(|mut elem_id_list| self.output_ctl.1.append(&mut elem_id_list))?;
+        self.output_ctl
+            .load_dynamics(card_cntr)
+            .map(|mut elem_id_list| self.output_ctl.1.append(&mut elem_id_list))?;
+        self.resource_ctl
+            .load(card_cntr)
+            .map(|mut elem_id_list| self.resource_ctl.1.append(&mut elem_id_list))?;
         Ok(())
     }
 
@@ -84,6 +198,26 @@ impl CtlModel<SndMotu> for Track16 {
         {
             Ok(true)
         } else if self.phone_assign_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.reverb_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.monitor_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.mixer_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read_equalizer(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read_dynamics(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.read_equalizer(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.read_dynamics(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.resource_ctl.read(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -116,6 +250,87 @@ impl CtlModel<SndMotu> for Track16 {
             .phone_assign_ctl
             .write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)?
         {
+            Ok(true)
+        } else if self.reverb_ctl.write(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.monitor_ctl.write(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.mixer_ctl.write(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.input_ctl.write(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.input_ctl.write_equalizer(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.input_ctl.write_dynamics(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.output_ctl.write(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.output_ctl.write_equalizer(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.output_ctl.write_dynamics(
+            &mut self.sequence_number,
+            unit,
+            &mut self.req,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
             Ok(true)
         } else {
             Ok(false)
@@ -156,19 +371,54 @@ impl NotifyModel<SndMotu, u32> for Track16 {
 }
 
 impl NotifyModel<SndMotu, Vec<DspCmd>> for Track16 {
-    fn get_notified_elem_list(&mut self, _: &mut Vec<ElemId>) {}
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
+        elem_id_list.extend_from_slice(&self.reverb_ctl.1);
+        elem_id_list.extend_from_slice(&self.monitor_ctl.1);
+        elem_id_list.extend_from_slice(&self.mixer_ctl.1);
+        elem_id_list.extend_from_slice(&self.input_ctl.1);
+        elem_id_list.extend_from_slice(&self.output_ctl.1);
+        elem_id_list.extend_from_slice(&self.resource_ctl.1);
+    }
 
-    fn parse_notification(&mut self, _: &mut SndMotu, _: &Vec<DspCmd>) -> Result<(), Error> {
+    fn parse_notification(&mut self, _: &mut SndMotu, cmds: &Vec<DspCmd>) -> Result<(), Error> {
+        self.reverb_ctl.parse_commands(&cmds[..]);
+        self.monitor_ctl.parse_commands(&cmds[..]);
+        self.mixer_ctl.parse_commands(&cmds[..]);
+        self.input_ctl.parse_commands(&cmds[..]);
+        self.output_ctl.parse_commands(&cmds[..]);
+        self.resource_ctl.parse_commands(&cmds[..]);
         Ok(())
     }
 
     fn read_notified_elem(
         &mut self,
         _: &SndMotu,
-        _: &ElemId,
-        _: &mut ElemValue,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
-        Ok(false)
+        if self.reverb_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.monitor_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.mixer_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read_equalizer(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.input_ctl.read_dynamics(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.read_equalizer(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.output_ctl.read_dynamics(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.resource_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
 
