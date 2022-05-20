alsa-project/snd-firewire-ctl-services

commit b37f34b666239639d904fef348670cc26be66fd1

motu-runtime: code refactoring for controls of optical interface in version 3 models
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/motu/runtime/src/v3_ctls.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
-use glib::Error;
+use glib::{Error, FileError};
 
 use hinawa::FwReq;
 use hinawa::{SndMotu, SndUnitExt};
@@ -192,135 +192,125 @@ pub trait V3PortAssignCtlOperation<T: V3PortAssignProtocol> {
     }
 }
 
-#[derive(Default)]
-pub struct V3OptIfaceCtl;
+fn opt_iface_mode_to_str(mode: &V3OptIfaceMode) -> &'static str {
+    match mode {
+        V3OptIfaceMode::Disabled => "Disabled",
+        V3OptIfaceMode::Adat => "ADAT",
+        V3OptIfaceMode::Spdif => "S/PDIF",
+    }
+}
 
-impl V3OptIfaceCtl {
-    const OPT_IFACE_IN_MODE_NAME: &'static str = "optical-iface-in-mode";
-    const OPT_IFACE_OUT_MODE_NAME: &'static str = "optical-iface-out-mode";
+const OPT_IFACE_IN_MODE_NAME: &str = "optical-iface-in-mode";
+const OPT_IFACE_OUT_MODE_NAME: &str = "optical-iface-out-mode";
 
-    const OPT_IFACE_MODE_LABELS: &'static [&'static str] = &["None", "ADAT", "S/PDIF"];
+pub trait V3OptIfaceCtlOperation<T: V3OptIfaceProtocol> {
+    const MODES: [V3OptIfaceMode; 3] = [
+        V3OptIfaceMode::Disabled,
+        V3OptIfaceMode::Adat,
+        V3OptIfaceMode::Spdif,
+    ];
+    const TARGETS: [V3OptIfaceTarget; 2] = [V3OptIfaceTarget::A, V3OptIfaceTarget::B];
 
-    pub fn load<O>(&mut self, _: &O, card_cntr: &mut CardCntr) -> Result<(), Error>
-    where
-        O: V3OptIfaceProtocol,
-    {
-        let elem_id =
-            ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::OPT_IFACE_IN_MODE_NAME, 0);
-        let _ =
-            card_cntr.add_enum_elems(&elem_id, 1, 2, Self::OPT_IFACE_MODE_LABELS, None, true)?;
+    fn load(&self, card_cntr: &mut CardCntr) -> Result<(), Error> {
+        let labels: Vec<&str> = Self::MODES.iter()
+            .map(|m| opt_iface_mode_to_str(m))
+            .collect();
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OPT_IFACE_IN_MODE_NAME, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, Self::TARGETS.len(), &labels, None, true)?;
 
-        let elem_id =
-            ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, Self::OPT_IFACE_OUT_MODE_NAME, 0);
-        let _ =
-            card_cntr.add_enum_elems(&elem_id, 1, 2, Self::OPT_IFACE_MODE_LABELS, None, true)?;
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OPT_IFACE_OUT_MODE_NAME, 0);
+        let _ = card_cntr.add_enum_elems(&elem_id, 1, Self::TARGETS.len(), &labels, None, true)?;
 
         Ok(())
     }
 
-    fn get_opt_iface_mode<O>(
-        &mut self,
+    fn read(
+        &self,
         unit: &mut SndMotu,
         req: &mut FwReq,
-        _: &O,
-        is_out: bool,
-        is_b: bool,
-        timeout_ms: u32,
-    ) -> Result<u32, Error>
-    where
-        O: V3OptIfaceProtocol,
-    {
-        O::get_opt_iface_mode(req, &mut unit.get_node(), is_out, is_b, timeout_ms)
-            .map(|(enabled, no_adat)| {
-                if enabled {
-                    0
-                } else {
-                    if no_adat {
-                        2
-                    } else {
-                        1
-                    }
-                }
-            })
-    }
-
-    fn set_opt_iface_mode<O>(
-        &mut self,
-        unit: &mut SndMotu,
-        req: &mut FwReq,
-        _: &O,
-        is_out: bool,
-        is_b: bool,
-        mode: u32,
-        timeout_ms: u32,
-    ) -> Result<(), Error>
-    where
-        O: V3OptIfaceProtocol,
-    {
-        let (enabled, no_adat) = match mode {
-            0 => (false, false),
-            1 => (true, false),
-            2 => (true, true),
-            _ => unreachable!(),
-        };
-        O::set_opt_iface_mode(req, &mut unit.get_node(), is_out, is_b, enabled, no_adat, timeout_ms)
-    }
-
-    pub fn read<O>(
-        &mut self,
-        unit: &mut SndMotu,
-        req: &mut FwReq,
-        proto: &O,
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
         timeout_ms: u32,
-    ) -> Result<bool, Error>
-    where
-        O: V3OptIfaceProtocol,
-    {
+    ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
-            Self::OPT_IFACE_IN_MODE_NAME => {
-                ElemValueAccessor::<u32>::set_vals(elem_value, 2, |idx| {
-                    self.get_opt_iface_mode(unit, req, proto, false, idx > 0, timeout_ms)
-                })?;
-                Ok(true)
+            OPT_IFACE_IN_MODE_NAME => {
+                ElemValueAccessor::<u32>::set_vals(elem_value, Self::TARGETS.len(), |idx| {
+                    T::get_opt_input_iface_mode(
+                        req,
+                        &mut unit.get_node(),
+                        Self::TARGETS[idx],
+                        timeout_ms
+                    )
+                        .map(|mode| {
+                            Self::MODES.iter().position(|m| m.eq(&mode)).unwrap() as u32
+                        })
+                })
+                .map(|_| true)
             }
-            Self::OPT_IFACE_OUT_MODE_NAME => {
-                ElemValueAccessor::<u32>::set_vals(elem_value, 2, |idx| {
-                    self.get_opt_iface_mode(unit, req, proto, true, idx > 0, timeout_ms)
-                })?;
-                Ok(true)
+            OPT_IFACE_OUT_MODE_NAME => {
+                ElemValueAccessor::<u32>::set_vals(elem_value, Self::TARGETS.len(), |idx| {
+                    T::get_opt_output_iface_mode(
+                        req,
+                        &mut unit.get_node(),
+                        Self::TARGETS[idx],
+                        timeout_ms
+                    )
+                        .map(|mode| {
+                            Self::MODES.iter().position(|m| m.eq(&mode)).unwrap() as u32
+                        })
+                })
+                .map(|_| true)
             }
             _ => Ok(false),
         }
     }
 
-    pub fn write<O>(
-        &mut self,
+    fn write(
+        &self,
         unit: &mut SndMotu,
         req: &mut FwReq,
-        proto: &O,
         elem_id: &ElemId,
         old: &ElemValue,
         new: &ElemValue,
         timeout_ms: u32,
-    ) -> Result<bool, Error>
-    where
-        O: V3OptIfaceProtocol,
-    {
+    ) -> Result<bool, Error> {
         match elem_id.get_name().as_str() {
-            Self::OPT_IFACE_IN_MODE_NAME => {
+            OPT_IFACE_IN_MODE_NAME => {
                 unit.lock()?;
-                let res = ElemValueAccessor::<u32>::get_vals(new, old, 2, |idx, val| {
-                    self.set_opt_iface_mode(unit, req, proto, false, idx > 0, val, timeout_ms)
+                let res = ElemValueAccessor::<u32>::get_vals(new, old, Self::TARGETS.len(), |idx, val| {
+                    let &mode = Self::MODES.iter()
+                        .nth(val as usize)
+                        .ok_or_else(|| {
+                            let msg = format!("Invalid index for mode of opt interface: {}", val);
+                            Error::new(FileError::Inval, &msg)
+                        })?;
+                    T::set_opt_input_iface_mode(
+                        req,
+                        &mut unit.get_node(),
+                        Self::TARGETS[idx],
+                        mode,
+                        timeout_ms,
+                    )
                 });
                 let _ = unit.unlock();
                 res.and(Ok(true))
             }
-            Self::OPT_IFACE_OUT_MODE_NAME => {
+            OPT_IFACE_OUT_MODE_NAME => {
                 unit.lock()?;
-                let res = ElemValueAccessor::<u32>::get_vals(new, old, 2, |idx, val| {
-                    self.set_opt_iface_mode(unit, req, proto, true, idx > 0, val, timeout_ms)
+                let res = ElemValueAccessor::<u32>::get_vals(new, old, Self::TARGETS.len(), |idx, val| {
+                    let &mode = Self::MODES.iter()
+                        .nth(val as usize)
+                        .ok_or_else(|| {
+                            let msg = format!("Invalid index for mode of opt interface: {}", val);
+                            Error::new(FileError::Inval, &msg)
+                        })?;
+                    T::set_opt_input_iface_mode(
+                        req,
+                        &mut unit.get_node(),
+                        Self::TARGETS[idx],
+                        mode,
+                        timeout_ms,
+                    )
                 });
                 let _ = unit.unlock();
                 res.and(Ok(true))

libs/motu/protocols/src/version_3.rs
@@ -214,68 +214,139 @@ pub trait V3PortAssignProtocol: AssignProtocol {
     }
 }
 
+/// The enumeration for direction of optical interface.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum V3OptIfaceTarget {
+    A,
+    B,
+}
+
+impl Default for V3OptIfaceTarget {
+    fn default() -> Self {
+        Self::A
+    }
+}
+
+/// The enumeration for mode of optical interface.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum V3OptIfaceMode {
+    Disabled,
+    Adat,
+    Spdif,
+}
+
+impl Default for V3OptIfaceMode {
+    fn default() -> Self {
+        Self::Disabled
+    }
+}
+
+fn get_opt_iface_masks(target: V3OptIfaceTarget, is_out: bool) -> (u32, u32) {
+    let mut enabled_mask = 0x00000001;
+    if is_out {
+        enabled_mask <<= 8;
+    }
+    if target == V3OptIfaceTarget::B {
+        enabled_mask <<= 1;
+    }
+
+    let mut no_adat_mask = 0x00010000;
+    if is_out {
+        no_adat_mask <<= 2;
+    }
+    if target == V3OptIfaceTarget::B {
+        no_adat_mask <<= 4;
+    }
+
+    (enabled_mask, no_adat_mask)
+}
+
 const OFFSET_OPT: u32 = 0x0c94;
+
+fn set_opt_iface_mode(
+    req: &mut FwReq,
+    node: &mut FwNode,
+    target: V3OptIfaceTarget,
+    is_out: bool,
+    mode: V3OptIfaceMode,
+    timeout_ms: u32,
+) -> Result<(), Error> {
+    let (enabled_mask, no_adat_mask) = get_opt_iface_masks(target, is_out);
+    read_quad(req, node, OFFSET_OPT, timeout_ms).and_then(|mut quad| {
+        match mode {
+            V3OptIfaceMode::Disabled => {
+                quad &= !enabled_mask;
+                quad &= !no_adat_mask;
+            }
+            V3OptIfaceMode::Adat => {
+                quad |= enabled_mask;
+                quad &= !no_adat_mask;
+            }
+            V3OptIfaceMode::Spdif => {
+                quad |= enabled_mask;
+                quad |= no_adat_mask;
+            }
+        }
+        write_quad(req, node, OFFSET_OPT, quad, timeout_ms)
+    })
+}
+
+fn get_opt_iface_mode(
+    req: &mut FwReq,
+    node: &mut FwNode,
+    target: V3OptIfaceTarget,
+    is_out: bool,
+    timeout_ms: u32,
+) -> Result<V3OptIfaceMode, Error> {
+    read_quad(req, node, OFFSET_OPT, timeout_ms).map(|quad| {
+        let (enabled_mask, no_adat_mask) = get_opt_iface_masks(target, is_out);
+        match (quad & enabled_mask > 0, quad & no_adat_mask > 0) {
+            (false, false) |
+            (false, true) => V3OptIfaceMode::Disabled,
+            (true, false) => V3OptIfaceMode::Adat,
+            (true, true) => V3OptIfaceMode::Spdif,
+        }
+    })
+}
 
 /// The trait for optical interface protocol in version 3.
 pub trait V3OptIfaceProtocol {
-    fn get_opt_iface_masks(is_out: bool, is_b: bool) -> (u32, u32) {
-        let mut enabled_mask = 0x00000001;
-        if is_out {
-            enabled_mask <<= 8;
-        }
-        if is_b {
-            enabled_mask <<= 1;
-        }
-
-        let mut no_adat_mask = 0x00010000;
-        if is_out {
-            no_adat_mask <<= 2;
-        }
-        if is_b {
-            no_adat_mask <<= 4;
-        }
-
-        (enabled_mask, no_adat_mask)
-    }
-
-    fn set_opt_iface_mode(
+    fn set_opt_input_iface_mode(
         req: &mut FwReq,
         node: &mut FwNode,
-        is_out: bool,
-        is_b: bool,
-        enable: bool,
-        no_adat: bool,
+        target: V3OptIfaceTarget,
+        mode: V3OptIfaceMode,
         timeout_ms: u32,
     ) -> Result<(), Error> {
-        let (enabled_mask, no_adat_mask) = Self::get_opt_iface_masks(is_out, is_b);
-        read_quad(req, node, OFFSET_OPT, timeout_ms)
-            .and_then(|mut quad| {
-                quad &= !enabled_mask;
-                quad &= !no_adat_mask;
-                if enable {
-                    quad |= enabled_mask;
-                }
-                if no_adat {
-                    quad |= no_adat_mask;
-                }
-                write_quad(req, node, OFFSET_OPT, quad, timeout_ms)
-            })
+        set_opt_iface_mode(req, node, target, false, mode, timeout_ms)
     }
 
-    fn get_opt_iface_mode(
+    fn get_opt_input_iface_mode(
         req: &mut FwReq,
         node: &mut FwNode,
-        is_out: bool,
-        is_b: bool,
+        target: V3OptIfaceTarget,
         timeout_ms: u32,
-    ) -> Result<(bool, bool), Error> {
-        read_quad(req, node, OFFSET_OPT, timeout_ms).map(|quad| {
-            let (enabled_mask, no_adat_mask) = Self::get_opt_iface_masks(is_out, is_b);
-            let enabled = (quad & enabled_mask) > 0;
-            let no_adat = (quad & no_adat_mask) > 0;
+    ) -> Result<V3OptIfaceMode, Error> {
+        get_opt_iface_mode(req, node, target, false, timeout_ms)
+    }
 
-            (enabled, no_adat)
-        })
+    fn set_opt_output_iface_mode(
+        req: &mut FwReq,
+        node: &mut FwNode,
+        target: V3OptIfaceTarget,
+        mode: V3OptIfaceMode,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        set_opt_iface_mode(req, node, target, true, mode, timeout_ms)
+    }
+
+    fn get_opt_output_iface_mode(
+        req: &mut FwReq,
+        node: &mut FwNode,
+        target: V3OptIfaceTarget,
+        timeout_ms: u32,
+    ) -> Result<V3OptIfaceMode, Error> {
+        get_opt_iface_mode(req, node, target, true, timeout_ms)
     }
 }
 

libs/motu/runtime/src/f828mk3.rs
@@ -19,10 +19,9 @@ const TIMEOUT_MS: u32 = 100;
 #[derive(Default)]
 pub struct F828mk3 {
     req: FwReq,
-    proto: F828mk3Protocol,
     clk_ctls: ClkCtl,
     port_assign_ctl: PortAssignCtl,
-    opt_iface_ctl: V3OptIfaceCtl,
+    opt_iface_ctl: OptIfaceCtl,
     phone_assign_ctl: PhoneAssignCtl,
     word_clk_ctl: WordClkCtl,
     msg_cache: u32,
@@ -48,6 +47,11 @@ struct PortAssignCtl(Vec<ElemId>);
 
 impl V3PortAssignCtlOperation<F828mk3Protocol> for PortAssignCtl {}
 
+#[derive(Default)]
+struct OptIfaceCtl;
+
+impl V3OptIfaceCtlOperation<F828mk3Protocol> for OptIfaceCtl {}
+
 impl F828mk3 {
     const NOTIFY_OPERATED: u32 = 0x40000000;
     const NOTIFY_COMPLETED: u32 = 0x00000002;
@@ -61,7 +65,7 @@ impl CtlModel<SndMotu> for F828mk3 {
         self.clk_ctls.load(card_cntr)?;
         self.port_assign_ctl.load(card_cntr)
             .map(|mut elem_id_list| self.port_assign_ctl.0.append(&mut elem_id_list))?;
-        self.opt_iface_ctl.load(&self.proto, card_cntr)?;
+        self.opt_iface_ctl.load(card_cntr)?;
         self.phone_assign_ctl.load(card_cntr)
             .map(|mut elem_id_list| self.phone_assign_ctl.0.append(&mut elem_id_list))?;
         self.word_clk_ctl.load(card_cntr)
@@ -77,7 +81,7 @@ impl CtlModel<SndMotu> for F828mk3 {
             Ok(true)
         } else if self.port_assign_ctl.read(unit, &mut self.req,  elem_id, elem_value, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.opt_iface_ctl.read(unit, &mut self.req, &self.proto, elem_id, elem_value, TIMEOUT_MS)? {
+        } else if self.opt_iface_ctl.read(unit, &mut self.req, elem_id, elem_value, TIMEOUT_MS)? {
             Ok(true)
         } else if self.phone_assign_ctl.read(unit, &mut self.req, elem_id, elem_value, TIMEOUT_MS)? {
             Ok(true)
@@ -96,7 +100,7 @@ impl CtlModel<SndMotu> for F828mk3 {
             Ok(true)
         } else if self.port_assign_ctl.write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)? {
             Ok(true)
-        } else if self.opt_iface_ctl.write(unit, &mut self.req, &self.proto, elem_id, old, new, TIMEOUT_MS)? {
+        } else if self.opt_iface_ctl.write(unit, &mut self.req, elem_id, old, new, TIMEOUT_MS)? {
             Ok(true)
         } else if self.phone_assign_ctl.write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)? {
             Ok(true)

commit f1347ef35df15a42fabd9ddf4f3eee56ef13b4a0

motu-runtime: fix invalid operation for output optical interface
It's wrongly programmed to use function for input optical interface.

Fixes: b37f34b66623 ("motu-runtime: code refactoring for controls of optical interface in version 3 models")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/motu/runtime/src/v3_ctls.rs
@@ -321,7 +321,7 @@ pub trait V3OptIfaceCtlOperation<T: V3OptIfaceOperation> {
                             let msg = format!("Invalid index for mode of opt interface: {}", val);
                             Error::new(FileError::Inval, &msg)
                         })?;
-                    T::set_opt_input_iface_mode(
+                    T::set_opt_output_iface_mode(
                         req,
                         &mut unit.get_node(),
                         Self::TARGETS[idx],