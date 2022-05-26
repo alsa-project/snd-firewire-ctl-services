430d61f66a00a1ccfb38ad526c337964c0c4c7c3
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-26 19:49:09 +0900

    motu-runtime: handle tuple of unit and node in v2 controls
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/motu/runtime/src/f8pre.rs
+++ libs/motu/runtime/src/f8pre.rs
@@ -126,7 +126,7 @@ impl CtlModel<(SndMotu, FwNode)> for F8pre {
     ) -> Result<(), Error> {
         self.clk_ctls.load(card_cntr)?;
         self.opt_iface_ctl
-            .load(card_cntr, &mut unit.0, &mut self.req, TIMEOUT_MS)
+            .load(card_cntr, unit, &mut self.req, TIMEOUT_MS)
             .map(|mut elem_id_list| self.opt_iface_ctl.1.append(&mut elem_id_list))?;
         self.phone_assign_ctl
             .load(card_cntr, unit, &mut self.req, TIMEOUT_MS)
@@ -153,7 +153,7 @@ impl CtlModel<(SndMotu, FwNode)> for F8pre {
     ) -> Result<bool, Error> {
         if self
             .clk_ctls
-            .read(&mut unit.0, &mut self.req, elem_id, elem_value, TIMEOUT_MS)?
+            .read(unit, &mut self.req, elem_id, elem_value, TIMEOUT_MS)?
         {
             Ok(true)
         } else if self.opt_iface_ctl.read(elem_id, elem_value)? {
@@ -182,12 +182,12 @@ impl CtlModel<(SndMotu, FwNode)> for F8pre {
     ) -> Result<bool, Error> {
         if self
             .clk_ctls
-            .write(&mut unit.0, &mut self.req, elem_id, new, TIMEOUT_MS)?
+            .write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)?
         {
             Ok(true)
         } else if self
             .opt_iface_ctl
-            .write(&mut unit.0, &mut self.req, elem_id, new, TIMEOUT_MS)?
+            .write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)?
         {
             Ok(true)
         } else if self
--- libs/motu/runtime/src/ultralite.rs
+++ libs/motu/runtime/src/ultralite.rs
@@ -159,7 +159,7 @@ impl CtlModel<(SndMotu, FwNode)> for UltraLite {
     ) -> Result<bool, Error> {
         if self
             .clk_ctls
-            .read(&mut unit.0, &mut self.req, elem_id, elem_value, TIMEOUT_MS)?
+            .read(unit, &mut self.req, elem_id, elem_value, TIMEOUT_MS)?
         {
             Ok(true)
         } else if self.main_assign_ctl.read(elem_id, elem_value)? {
@@ -190,7 +190,7 @@ impl CtlModel<(SndMotu, FwNode)> for UltraLite {
     ) -> Result<bool, Error> {
         if self
             .clk_ctls
-            .write(&mut unit.0, &mut self.req, elem_id, new, TIMEOUT_MS)?
+            .write(unit, &mut self.req, elem_id, new, TIMEOUT_MS)?
         {
             Ok(true)
         } else if self
