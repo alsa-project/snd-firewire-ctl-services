c7e0d4d68387f722e1b0f8191a628bdb766d45a5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-26 19:49:09 +0900

    services: code refactoring to remove explicit lifetime
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- services/src/bin/snd-bebob-ctl-service.rs
+++ services/src/bin/snd-bebob-ctl-service.rs
@@ -5,9 +5,10 @@ use {bebob_runtime::BebobRuntime, snd_firewire_ctl_services::*};
 
 struct BebobServiceCmd;
 
-impl<'a> ServiceCmd<'a, u32, BebobRuntime> for BebobServiceCmd {
-    const CMD_NAME: &'a str = "snd-bebob-ctl-service";
-    const ARGS: &'a [(&'a str, &'a str)] = &[("CARD_ID", "The numeric ID of sound card")];
+impl ServiceCmd<u32, BebobRuntime> for BebobServiceCmd {
+    const CMD_NAME: &'static str = "snd-bebob-ctl-service";
+    const ARGS: &'static [(&'static str, &'static str)] =
+        &[("CARD_ID", "The numeric ID of sound card")];
 
     fn parse_args(args: &[String]) -> Result<u32, String> {
         parse_arg_as_u32(&args[0])
