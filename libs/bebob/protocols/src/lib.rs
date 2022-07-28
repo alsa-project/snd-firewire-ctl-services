f785eb6c998e0038806cd8165692c775f0ad6c1f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-07-28 18:42:59 +0900

    bebob-protocols: localize implementation of AV/C transaction
    This commit is a preparation to remove trait general implementation from
    ta1394 crate.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/bebob/protocols/src/lib.rs
+++ libs/bebob/protocols/src/lib.rs
@@ -24,7 +24,7 @@ pub mod yamaha_terratec;
 use {
     self::bridgeco::{ExtendedStreamFormatSingle, *},
     glib::{Error, FileError},
-    hinawa::{prelude::FwReqExtManual, FwFcp, FwNode, FwReq, FwTcode},
+    hinawa::{prelude::{FwFcpExtManual, FwReqExtManual}, FwFcp, FwNode, FwReq, FwTcode},
     ta1394::{amdtp::*, audio::*, ccm::*, general::*, *},
 };
 
@@ -46,6 +46,42 @@ impl AsRef<FwFcp> for BebobAvc {
 }
 
 impl Ta1394Avc for BebobAvc {
+    fn transaction(
+        &self,
+        ctype: AvcCmdType,
+        addr: &AvcAddr,
+        opcode: u8,
+        operands: &[u8],
+        timeout_ms: u32,
+    ) -> Result<(AvcRespCode, Vec<u8>), Error> {
+        let mut cmd = Vec::new();
+        cmd.push(ctype.into());
+        cmd.push(addr.into());
+        cmd.push(opcode);
+        cmd.extend_from_slice(operands);
+
+        let mut resp = vec![0; Self::FRAME_SIZE];
+        self.0
+            .avc_transaction(&cmd, &mut resp, timeout_ms)
+            .and_then(|len| {
+                if resp[1] != addr.into() {
+                    let label = format!("Unexpected address in response: {}", resp[1]);
+                    Err(Error::new(Ta1394AvcError::UnexpectedRespCode, &label))
+                } else if resp[2] != opcode {
+                    let label =
+                        format!("Unexpected opcode in response: {} but {}", opcode, resp[2]);
+                    Err(Error::new(Ta1394AvcError::UnexpectedRespCode, &label))
+                } else {
+                    let rcode = AvcRespCode::from(resp[0] & Self::RESP_CODE_MASK);
+
+                    resp.truncate(len);
+                    let operands = resp.split_off(3);
+
+                    Ok((rcode, operands))
+                }
+            })
+    }
+
     fn control<O: AvcOp + AvcControl>(
         &self,
         addr: &AvcAddr,
