63bccc1998ed653dc0d360d90124b465216ab37c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-08-03 17:13:42 +0900

    oxfw-protocols: change crate license to LGPL v3.0 or later
    With respect to clause for reverse engineering.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/oxfw/protocols/src/lib.rs
+++ libs/oxfw/protocols/src/lib.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
 
 //! Protocols defined for Oxford Semiconductor FW970/971 chipset.
