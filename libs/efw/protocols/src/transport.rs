alsa-project/snd-firewire-ctl-services

commit 5a77d94660e5eb73491c79d0e599baa5b3a0621a

efw-protocols: add implementation for protocol about packet transmission
This commit adds implementation for protocol about packet transmission.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/efw/protocols/src/transport.rs
@@ -1,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2021 Takashi Sakamoto
+
+//! Protocol about hardware information.
+//!
+//! The crate includes protocol about hardware information defined by Echo Audio Digital Corporation
+//! for Fireworks board module.
+
+use glib::Error;
+
+use super::EfwProtocol;
+
+const CATEGORY_TRANSPORT: u32 = 2;
+
+const CMD_SET_TRANSMIT_MODE: u32 = 0;
+
+/// The format of packet in transmission.
+pub enum TxPacketFormat {
+    /// Unique.
+    Unique,
+    /// Compliant to IEC 61883-1/6, actually with some quirks.
+    Iec61883,
+}
+
+impl From<TxPacketFormat> for u32 {
+    fn from(fmt: TxPacketFormat) -> Self {
+        match fmt {
+            TxPacketFormat::Unique => 0,
+            TxPacketFormat::Iec61883 => 1,
+        }
+    }
+}
+
+/// Protocol about transmission for Fireworks board module.
+pub trait TransportProtocol: EfwProtocol {
+    fn get_hw_info(&mut self, fmt: TxPacketFormat, timeout_ms: u32) -> Result<(), Error> {
+        let args = [u32::from(fmt)];
+        self.transaction_sync(
+            CATEGORY_TRANSPORT,
+            CMD_SET_TRANSMIT_MODE,
+            Some(&args),
+            None,
+            timeout_ms,
+        )
+    }
+}
+
+impl<O: EfwProtocol> TransportProtocol for O {}

libs/efw/protocols/src/lib.rs
@@ -9,6 +9,7 @@
 pub mod flash;
 pub mod hw_info;
 pub mod transactions;
+pub mod transport;
 
 use hinawa::SndEfwExtManual;
 

commit e9c9290e73a9e9f944fdb459a5e1cb6209fdb5bb

efw-protocols: fix suspicous copy-and-paste mistake
TransportProtocol::get_hw_info() seems to be copy-and-paste mistake.

This commit renames to TransportProtocol::get_format()

Fixes: 5a77d94660e5 ("efw-protocols: add implementation for protocol about packet transmission")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

libs/efw/protocols/src/transport.rs
@@ -31,7 +31,7 @@ impl From<TxPacketFormat> for u32 {
 
 /// Protocol about transmission for Fireworks board module.
 pub trait TransportProtocol: EfwProtocolExtManual {
-    fn get_hw_info(&mut self, fmt: TxPacketFormat, timeout_ms: u32) -> Result<(), Error> {
+    fn get_packet_format(&mut self, fmt: TxPacketFormat, timeout_ms: u32) -> Result<(), Error> {
         let args = [u32::from(fmt)];
         self.transaction(
             CATEGORY_TRANSPORT,