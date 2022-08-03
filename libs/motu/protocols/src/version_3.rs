14c20758acd621f62a52e956de14a98c36933106
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-08-03 17:13:42 +0900

    motu-protocols: change crate license to LGPL v3.0 or later
    With respect to clause for reverse engineering.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/motu/protocols/src/version_3.rs
+++ libs/motu/protocols/src/version_3.rs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
 
 //! Protocol used in version 3 devices of MOTU FireWire series.
