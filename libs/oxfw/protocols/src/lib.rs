a02fec49023d49765eee66566f6a7f756e450ad0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-08-05 15:11:50 +0900

    oxfw/protocols: add README
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- libs/oxfw/protocols/src/lib.rs
+++ libs/oxfw/protocols/src/lib.rs
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (c) 2021 Takashi Sakamoto
 
-//! Protocols defined for Oxford Semiconductor FW970/971 chipset.
-//!
-//! The crate includes various kind of protocols defined by Oxford Semiconductor as well as
-//! hardware vendors for FW970/971 ASICs.
+#![doc = include_str!("../README.md")]
 
 pub mod apogee;
 pub mod griffin;
