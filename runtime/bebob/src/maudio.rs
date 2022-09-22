alsa-project/snd-firewire-ctl-services

commit a3ee6a671b9d498c3ded3c91a12701ec9c784d26

runtime/bebob: maudio: use state cache for mixer parameters
This commit uses the parameter structure added in the former commit.

The ASICs in BeBoB solution are identified to have heavy load from
AV/C status request for mixer parameters. This commit updates whole
the parameters by consecutive AV/C control requests.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/bebob/src/maudio.rs
@@ -308,12 +308,10 @@ pub trait MaudioNormalMixerCtlOperation<O: MaudioNormalMixerOperation> {
     const DST_COUNT: usize = O::DST_FUNC_BLOCK_ID_LIST.len();
     const SRC_COUNT: usize = O::SRC_FUNC_BLOCK_ID_LIST.len();
 
-    fn load_src_state(
-        &self,
-        card_cntr: &mut CardCntr,
-        avc: &BebobAvc,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
+    fn state(&self) -> &MaudioNormalMixerParameters;
+    fn state_mut(&mut self) -> &mut MaudioNormalMixerParameters;
+
+    fn load_src_state(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
         assert_eq!(
             Self::DST_COUNT,
             Self::DST_LABELS.len(),
@@ -334,47 +332,59 @@ pub trait MaudioNormalMixerCtlOperation<O: MaudioNormalMixerOperation> {
 
         // For convenicence, make connection between mixer destination and stream source.
         if Self::DST_COUNT > 1 {
-            (0..Self::DST_COUNT).try_for_each(|dst_idx| {
-                let src_idx = Self::SRC_COUNT - Self::DST_COUNT + dst_idx;
-                O::write_mixer_src(avc, dst_idx, src_idx, true, timeout_ms)
-            })?;
+            self.state_mut()
+                .0
+                .iter_mut()
+                .enumerate()
+                .for_each(|(dst_idx, srcs)| {
+                    let src_idx = Self::SRC_COUNT - Self::DST_COUNT + dst_idx;
+                    srcs[src_idx] = true;
+                });
         }
 
         Ok(())
     }
 
+    fn cache(&mut self, avc: &BebobAvc, timeout_ms: u32) -> Result<(), Error> {
+        // NOTE: Due to quirk of the ASIC, it should be avoided to request AV/C status request
+        // for mixer parameters. Alternatively, initiate AV/C control request for them.
+        let mut params = self.state().clone();
+        params.0.iter_mut().for_each(|levels| {
+            levels.iter_mut().for_each(|level| *level = !(*level));
+        });
+        let res = O::update(avc, self.state(), &mut params, timeout_ms);
+        debug!(params = ?self.state(), ?res);
+        res
+    }
+
     fn read_src_state(
-        &self,
-        avc: &BebobAvc,
+        &mut self,
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
-        timeout_ms: u32,
     ) -> Result<bool, Error> {
         if elem_id.name().as_str() == Self::MIXER_NAME {
             let dst_idx = elem_id.index() as usize;
-            ElemValueAccessor::<bool>::set_vals(elem_value, Self::SRC_COUNT, |src_idx| {
-                O::read_mixer_src(avc, dst_idx, src_idx, timeout_ms)
-            })
-            .map(|_| true)
+            elem_value.set_bool(&self.state().0[dst_idx]);
+            Ok(true)
         } else {
             Ok(false)
         }
     }
 
     fn write_src_state(
-        &self,
+        &mut self,
         avc: &BebobAvc,
         elem_id: &ElemId,
-        old: &ElemValue,
+        _: &ElemValue,
         new: &ElemValue,
         timeout_ms: u32,
     ) -> Result<bool, Error> {
         if elem_id.name().as_str() == Self::MIXER_NAME {
             let dst_idx = elem_id.index() as usize;
-            ElemValueAccessor::<bool>::get_vals(new, old, Self::SRC_COUNT, |src_idx, val| {
-                O::write_mixer_src(avc, dst_idx, src_idx, val, timeout_ms)
-            })
-            .map(|_| true)
+            let mut params = self.state().clone();
+            let vals = &new.boolean()[..Self::SRC_COUNT];
+            params.0[dst_idx].copy_from_slice(&vals);
+            O::update(avc, &params, self.state_mut(), timeout_ms).map(|_| true)
         } else {
             Ok(false)
         }

protocols/bebob/src/maudio/normal.rs
@@ -973,8 +973,9 @@ impl MaudioNormalMixerOperation for OzonicMixerProtocol {
     ];
 }
 
-/// The parameter of mixer.
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
+/// The parameter of mixer. The `Default` trait should be implemented to call
+/// `MaudioNormalMixerOperation::create_mixer_parameters()`.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MaudioNormalMixerParameters(pub Vec<Vec<bool>>);
 
 /// The trait for mixer operation.
@@ -995,6 +996,8 @@ pub trait MaudioNormalMixerOperation {
         ])
     }
 
+    /// The ASIC get heavy load from AV/C status request for mixer parameters, thus this method
+    /// often brings timeout.
     fn cache(
         avc: &BebobAvc,
         params: &mut MaudioNormalMixerParameters,
@@ -1029,26 +1032,6 @@ pub trait MaudioNormalMixerOperation {
             })
     }
 
-    fn read_mixer_src(
-        avc: &BebobAvc,
-        dst_idx: usize,
-        src_idx: usize,
-        timeout_ms: u32,
-    ) -> Result<bool, Error> {
-        if dst_idx >= Self::DST_FUNC_BLOCK_ID_LIST.len() {
-            let msg = format!("Invalid index of destination ID list: {}", dst_idx);
-            Err(Error::new(FileError::Inval, &msg))?;
-        }
-
-        if src_idx >= Self::SRC_FUNC_BLOCK_ID_LIST.len() {
-            let msg = format!("Invalid index of source ID list: {}", src_idx);
-            Err(Error::new(FileError::Inval, &msg))?;
-        }
-
-        let mut params = Self::create_mixer_parameters();
-        Self::cache(avc, &mut params, timeout_ms).map(|_| params.0[dst_idx][src_idx])
-    }
-
     fn update(
         avc: &BebobAvc,
         params: &MaudioNormalMixerParameters,
@@ -1081,30 +1064,5 @@ pub trait MaudioNormalMixerOperation {
                         })
                 },
             )
-    }
-
-    fn write_mixer_src(
-        avc: &BebobAvc,
-        dst_idx: usize,
-        src_idx: usize,
-        state: bool,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
-        if dst_idx >= Self::DST_FUNC_BLOCK_ID_LIST.len() {
-            let msg = format!("Invalid index of destination ID list: {}", dst_idx);
-            Err(Error::new(FileError::Inval, &msg))?;
-        }
-
-        if src_idx >= Self::SRC_FUNC_BLOCK_ID_LIST.len() {
-            let msg = format!("Invalid index of source ID list: {}", src_idx);
-            Err(Error::new(FileError::Inval, &msg))?;
-        }
-
-        let mut params = Self::create_mixer_parameters();
-        params.0[dst_idx][src_idx] = state;
-        let mut p = Self::create_mixer_parameters();
-        p.0[dst_idx][src_idx] = !state;
-
-        Self::update(avc, &params, &mut p, timeout_ms)
     }
 }

runtime/bebob/src/maudio/audiophile_model.rs
@@ -258,8 +258,14 @@ impl AvcSelectorCtlOperation<AudiophileHeadphoneProtocol> for HeadphoneCtl {
     }
 }
 
-#[derive(Default)]
-struct MixerCtl;
+#[derive(Debug)]
+struct MixerCtl(MaudioNormalMixerParameters);
+
+impl Default for MixerCtl {
+    fn default() -> Self {
+        Self(AudiophileMixerProtocol::create_mixer_parameters())
+    }
+}
 
 impl MaudioNormalMixerCtlOperation<AudiophileMixerProtocol> for MixerCtl {
     const MIXER_NAME: &'static str = "mixer-source";
@@ -273,6 +279,14 @@ impl MaudioNormalMixerCtlOperation<AudiophileMixerProtocol> for MixerCtl {
         "stream-input-3/4",
         "stream-input-5/6",
     ];
+
+    fn state(&self) -> &MaudioNormalMixerParameters {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut MaudioNormalMixerParameters {
+        &mut self.0
+    }
 }
 
 impl CtlModel<(SndUnit, FwNode)> for AudiophileModel {
@@ -303,8 +317,7 @@ impl CtlModel<(SndUnit, FwNode)> for AudiophileModel {
         self.phys_output_ctl.load_selector(card_cntr)?;
         self.hp_ctl.load_level(card_cntr)?;
         self.hp_ctl.load_selector(card_cntr)?;
-        self.mixer_ctl
-            .load_src_state(card_cntr, &self.avc, TIMEOUT_MS)?;
+        self.mixer_ctl.load_src_state(card_cntr)?;
 
         self.clk_ctl.cache_freq(&self.avc, FCP_TIMEOUT_MS)?;
         self.clk_ctl.cache_src(&self.avc, FCP_TIMEOUT_MS)?;
@@ -321,6 +334,7 @@ impl CtlModel<(SndUnit, FwNode)> for AudiophileModel {
         self.phys_output_ctl
             .cache_selectors(&self.avc, FCP_TIMEOUT_MS)?;
         self.hp_ctl.cache_selectors(&self.avc, FCP_TIMEOUT_MS)?;
+        self.mixer_ctl.cache(&self.avc, FCP_TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -353,10 +367,7 @@ impl CtlModel<(SndUnit, FwNode)> for AudiophileModel {
             Ok(true)
         } else if self.hp_ctl.read_selectors(elem_id, elem_value)? {
             Ok(true)
-        } else if self
-            .mixer_ctl
-            .read_src_state(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)?
-        {
+        } else if self.mixer_ctl.read_src_state(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -543,11 +554,10 @@ mod test {
 
     #[test]
     fn test_mixer_ctl_definition() {
-        let avc = BebobAvc::default();
         let mut card_cntr = CardCntr::default();
 
-        let ctl = MixerCtl::default();
-        let error = ctl.load_src_state(&mut card_cntr, &avc, 100).unwrap_err();
+        let mut ctl = MixerCtl::default();
+        let error = ctl.load_src_state(&mut card_cntr).unwrap_err();
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }

runtime/bebob/src/maudio/fw410_model.rs
@@ -226,13 +226,18 @@ impl AvcSelectorCtlOperation<Fw410PhysOutputProtocol> for PhysOutputCtl {
 }
 
 #[derive(Debug)]
-struct HeadphoneCtl(AvcLevelParameters, AvcSelectorParameters);
+struct HeadphoneCtl(
+    AvcLevelParameters,
+    AvcSelectorParameters,
+    MaudioNormalMixerParameters,
+);
 
 impl Default for HeadphoneCtl {
     fn default() -> Self {
         Self(
             Fw410HeadphoneProtocol::create_level_parameters(),
             Fw410HeadphoneProtocol::create_selector_parameters(),
+            Fw410HeadphoneProtocol::create_mixer_parameters(),
         )
     }
 }
@@ -276,6 +281,14 @@ impl MaudioNormalMixerCtlOperation<Fw410HeadphoneProtocol> for HeadphoneCtl {
         "mixer-output-7/8",
         "mixer-output-9/10",
     ];
+
+    fn state(&self) -> &MaudioNormalMixerParameters {
+        &self.2
+    }
+
+    fn state_mut(&mut self) -> &mut MaudioNormalMixerParameters {
+        &mut self.2
+    }
 }
 
 #[derive(Debug)]
@@ -301,8 +314,14 @@ impl AvcSelectorCtlOperation<Fw410SpdifOutputProtocol> for SpdifInputCtl {
     }
 }
 
-#[derive(Default)]
-struct MixerCtl;
+#[derive(Debug)]
+struct MixerCtl(MaudioNormalMixerParameters);
+
+impl Default for MixerCtl {
+    fn default() -> Self {
+        Self(Fw410MixerProtocol::create_mixer_parameters())
+    }
+}
 
 impl MaudioNormalMixerCtlOperation<Fw410MixerProtocol> for MixerCtl {
     const MIXER_NAME: &'static str = "mixer-source";
@@ -324,6 +343,14 @@ impl MaudioNormalMixerCtlOperation<Fw410MixerProtocol> for MixerCtl {
         "analog-input-7/8",
         "digital-input-1/2",
     ];
+
+    fn state(&self) -> &MaudioNormalMixerParameters {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut MaudioNormalMixerParameters {
+        &mut self.0
+    }
 }
 
 impl CtlModel<(SndUnit, FwNode)> for Fw410Model {
@@ -354,12 +381,10 @@ impl CtlModel<(SndUnit, FwNode)> for Fw410Model {
         self.phys_output_ctl.load_selector(card_cntr)?;
         self.hp_ctl.load_level(card_cntr)?;
         self.hp_ctl.load_selector(card_cntr)?;
-        self.hp_ctl
-            .load_src_state(card_cntr, &self.avc, TIMEOUT_MS)?;
+        self.hp_ctl.load_src_state(card_cntr)?;
         self.spdif_input_ctl.load_selector(card_cntr)?;
 
-        self.mixer_ctl
-            .load_src_state(card_cntr, &self.avc, TIMEOUT_MS)?;
+        self.mixer_ctl.load_src_state(card_cntr)?;
 
         self.clk_ctl.cache_freq(&self.avc, FCP_TIMEOUT_MS)?;
         self.clk_ctl.cache_src(&self.avc, FCP_TIMEOUT_MS)?;
@@ -378,6 +403,7 @@ impl CtlModel<(SndUnit, FwNode)> for Fw410Model {
         self.hp_ctl.cache_selectors(&self.avc, FCP_TIMEOUT_MS)?;
         self.spdif_input_ctl
             .cache_selectors(&self.avc, FCP_TIMEOUT_MS)?;
+        self.mixer_ctl.cache(&self.avc, FCP_TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -414,10 +440,7 @@ impl CtlModel<(SndUnit, FwNode)> for Fw410Model {
             Ok(true)
         } else if self.spdif_input_ctl.read_selectors(elem_id, elem_value)? {
             Ok(true)
-        } else if self
-            .mixer_ctl
-            .read_src_state(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)?
-        {
+        } else if self.mixer_ctl.read_src_state(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -616,11 +639,10 @@ mod test {
 
     #[test]
     fn test_mixer_ctl_definition() {
-        let avc = BebobAvc::default();
         let mut card_cntr = CardCntr::default();
 
-        let ctl = MixerCtl::default();
-        let error = ctl.load_src_state(&mut card_cntr, &avc, 100).unwrap_err();
+        let mut ctl = MixerCtl::default();
+        let error = ctl.load_src_state(&mut card_cntr).unwrap_err();
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }

runtime/bebob/src/maudio/ozonic_model.rs
@@ -133,8 +133,14 @@ impl AvcLevelCtlOperation<OzonicStreamInputProtocol> for StreamInputCtl {
     }
 }
 
-#[derive(Default)]
-struct MixerCtl;
+#[derive(Debug)]
+struct MixerCtl(MaudioNormalMixerParameters);
+
+impl Default for MixerCtl {
+    fn default() -> Self {
+        Self(OzonicMixerProtocol::create_mixer_parameters())
+    }
+}
 
 impl MaudioNormalMixerCtlOperation<OzonicMixerProtocol> for MixerCtl {
     const MIXER_NAME: &'static str = "mixer-source";
@@ -145,6 +151,14 @@ impl MaudioNormalMixerCtlOperation<OzonicMixerProtocol> for MixerCtl {
         "stream-input-1/2",
         "stream-input-3/4",
     ];
+
+    fn state(&self) -> &MaudioNormalMixerParameters {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut MaudioNormalMixerParameters {
+        &mut self.0
+    }
 }
 
 impl CtlModel<(SndUnit, FwNode)> for OzonicModel {
@@ -171,8 +185,7 @@ impl CtlModel<(SndUnit, FwNode)> for OzonicModel {
         self.phys_input_ctl.load_balance(card_cntr)?;
         self.stream_input_ctl.load_level(card_cntr)?;
 
-        self.mixer_ctl
-            .load_src_state(card_cntr, &self.avc, TIMEOUT_MS)?;
+        self.mixer_ctl.load_src_state(card_cntr)?;
 
         self.clk_ctl.cache_freq(&self.avc, FCP_TIMEOUT_MS)?;
         self.clk_ctl.cache_src(&self.avc, FCP_TIMEOUT_MS)?;
@@ -182,6 +195,7 @@ impl CtlModel<(SndUnit, FwNode)> for OzonicModel {
             .cache_levels(&self.avc, FCP_TIMEOUT_MS)?;
         self.phys_input_ctl
             .cache_balances(&self.avc, FCP_TIMEOUT_MS)?;
+        self.mixer_ctl.cache(&self.avc, FCP_TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -204,10 +218,7 @@ impl CtlModel<(SndUnit, FwNode)> for OzonicModel {
             Ok(true)
         } else if self.stream_input_ctl.read_levels(elem_id, elem_value)? {
             Ok(true)
-        } else if self
-            .mixer_ctl
-            .read_src_state(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)?
-        {
+        } else if self.mixer_ctl.read_src_state(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -333,11 +344,10 @@ mod test {
 
     #[test]
     fn test_mixer_ctl_definition() {
-        let avc = BebobAvc::default();
         let mut card_cntr = CardCntr::default();
 
-        let ctl = MixerCtl::default();
-        let error = ctl.load_src_state(&mut card_cntr, &avc, 100).unwrap_err();
+        let mut ctl = MixerCtl::default();
+        let error = ctl.load_src_state(&mut card_cntr).unwrap_err();
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }

runtime/bebob/src/maudio/solo_model.rs
@@ -157,8 +157,14 @@ impl AvcSelectorCtlOperation<SoloSpdifOutputProtocol> for SpdifOutputCtl {
     }
 }
 
-#[derive(Default)]
-struct MixerCtl;
+#[derive(Debug)]
+struct MixerCtl(MaudioNormalMixerParameters);
+
+impl Default for MixerCtl {
+    fn default() -> Self {
+        Self(SoloMixerProtocol::create_mixer_parameters())
+    }
+}
 
 impl MaudioNormalMixerCtlOperation<SoloMixerProtocol> for MixerCtl {
     const MIXER_NAME: &'static str = "mixer-source";
@@ -169,6 +175,14 @@ impl MaudioNormalMixerCtlOperation<SoloMixerProtocol> for MixerCtl {
         "stream-input-1/2",
         "stream-input-3/4",
     ];
+
+    fn state(&self) -> &MaudioNormalMixerParameters {
+        &self.0
+    }
+
+    fn state_mut(&mut self) -> &mut MaudioNormalMixerParameters {
+        &mut self.0
+    }
 }
 
 impl CtlModel<(SndUnit, FwNode)> for SoloModel {
@@ -197,8 +211,7 @@ impl CtlModel<(SndUnit, FwNode)> for SoloModel {
 
         self.spdif_output_ctl.load_selector(card_cntr)?;
 
-        self.mixer_ctl
-            .load_src_state(card_cntr, &self.avc, TIMEOUT_MS)?;
+        self.mixer_ctl.load_src_state(card_cntr)?;
 
         self.clk_ctl.cache_freq(&self.avc, FCP_TIMEOUT_MS)?;
         self.clk_ctl.cache_src(&self.avc, FCP_TIMEOUT_MS)?;
@@ -210,6 +223,7 @@ impl CtlModel<(SndUnit, FwNode)> for SoloModel {
             .cache_balances(&self.avc, FCP_TIMEOUT_MS)?;
         self.spdif_output_ctl
             .cache_selectors(&self.avc, FCP_TIMEOUT_MS)?;
+        self.mixer_ctl.cache(&self.avc, FCP_TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -234,10 +248,7 @@ impl CtlModel<(SndUnit, FwNode)> for SoloModel {
             Ok(true)
         } else if self.spdif_output_ctl.read_selectors(elem_id, elem_value)? {
             Ok(true)
-        } else if self
-            .mixer_ctl
-            .read_src_state(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)?
-        {
+        } else if self.mixer_ctl.read_src_state(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -383,11 +394,10 @@ mod test {
 
     #[test]
     fn test_mixer_ctl_definition() {
-        let avc = BebobAvc::default();
         let mut card_cntr = CardCntr::default();
 
-        let ctl = MixerCtl::default();
-        let error = ctl.load_src_state(&mut card_cntr, &avc, 100).unwrap_err();
+        let mut ctl = MixerCtl::default();
+        let error = ctl.load_src_state(&mut card_cntr).unwrap_err();
         assert_eq!(error.kind::<CardError>(), Some(CardError::Failed));
     }
 }

commit 9a1f8c2878f6a6e72dc9f8257233e7d1ed5f0cc0

runtime/bebob: maudio: fix unsupported debug macro
It's planned to use in future commits.

Fixes: a3ee6a671b9d ("runtime/bebob: maudio: use state cache for mixer parameters")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/bebob/src/maudio.rs
@@ -352,9 +352,7 @@ pub trait MaudioNormalMixerCtlOperation<O: MaudioNormalMixerOperation> {
         params.0.iter_mut().for_each(|levels| {
             levels.iter_mut().for_each(|level| *level = !(*level));
         });
-        let res = O::update(avc, self.state(), &mut params, timeout_ms);
-        debug!(params = ?self.state(), ?res);
-        res
+        O::update(avc, self.state(), &mut params, timeout_ms)
     }
 
     fn read_src_state(