19803b04acecdfa0a0d776046d48e387bde8fa87
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-09-29 18:05:19 +0900

    runtime/dice: implementat of common controls for extension model
    This commit implements traits to use its default implementation for
    common controls.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- runtime/dice/src/extension_model.rs
+++ runtime/dice/src/extension_model.rs
@@ -11,7 +11,7 @@ pub struct ExtensionModel {
     req: FwReq,
     sections: GeneralSections,
     extension_sections: ExtensionSections,
-    ctl: CommonCtl,
+    common_ctl: CommonCtl,
     tcd22xx_ctl: ExtensionTcd22xxCtl,
 }
 
@@ -19,8 +19,15 @@ const TIMEOUT_MS: u32 = 20;
 
 impl ExtensionModel {
     pub fn cache(&mut self, unit: &mut (SndDice, FwNode)) -> Result<(), Error> {
-        self.sections =
-            GeneralProtocol::read_general_sections(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
+        ExtensionProtocol::read_general_sections(
+            &self.req,
+            &unit.1,
+            &mut self.sections,
+            TIMEOUT_MS,
+        )?;
+
+        self.common_ctl
+            .whole_cache(&self.req, &unit.1, &mut self.sections, TIMEOUT_MS)?;
 
         Ok(())
     }
@@ -32,29 +39,12 @@ impl CtlModel<(SndDice, FwNode)> for ExtensionModel {
         unit: &mut (SndDice, FwNode),
         card_cntr: &mut CardCntr,
     ) -> Result<(), Error> {
-        let caps = GlobalSectionProtocol::read_clock_caps(
-            &mut self.req,
-            &mut unit.1,
-            &self.sections,
-            TIMEOUT_MS,
+        self.common_ctl.load(card_cntr, &self.sections).map(
+            |(measured_elem_id_list, notified_elem_id_list)| {
+                self.common_ctl.0 = measured_elem_id_list;
+                self.common_ctl.1 = notified_elem_id_list;
+            },
         )?;
-        let src_labels = GlobalSectionProtocol::read_clock_source_labels(
-            &mut self.req,
-            &mut unit.1,
-            &self.sections,
-            TIMEOUT_MS,
-        )?;
-        self.ctl.load(card_cntr, &caps, &src_labels)?;
-
-        let avail_rates = caps.get_rate_entries();
-        let avail_sources = caps.get_src_entries(&src_labels);
-        let clock_source_labels: Vec<(ClockSource, String)> = avail_sources
-            .iter()
-            .filter_map(|&src| {
-                src.get_label(&src_labels, false)
-                    .map(|l| (src, l.to_string()))
-            })
-            .collect();
 
         self.extension_sections =
             ProtocolExtension::read_extension_sections(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
@@ -62,9 +52,9 @@ impl CtlModel<(SndDice, FwNode)> for ExtensionModel {
             unit,
             &mut self.req,
             &self.extension_sections,
-            &avail_rates,
-            &avail_sources,
-            &clock_source_labels,
+            &self.sections.global.params.avail_rates,
+            &self.sections.global.params.avail_sources,
+            &self.sections.global.params.clock_source_labels,
             TIMEOUT_MS,
             card_cntr,
         )?;
@@ -85,14 +75,7 @@ impl CtlModel<(SndDice, FwNode)> for ExtensionModel {
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
-        if self.ctl.read(
-            unit,
-            &mut self.req,
-            &self.sections,
-            elem_id,
-            elem_value,
-            TIMEOUT_MS,
-        )? {
+        if self.common_ctl.read(&self.sections, elem_id, elem_value)? {
             Ok(true)
         } else if self.tcd22xx_ctl.read(
             unit,
@@ -115,12 +98,12 @@ impl CtlModel<(SndDice, FwNode)> for ExtensionModel {
         old: &ElemValue,
         new: &ElemValue,
     ) -> Result<bool, Error> {
-        if self.ctl.write(
-            unit,
-            &mut self.req,
-            &self.sections,
+        if self.common_ctl.write(
+            &unit.0,
+            &self.req,
+            &unit.1,
+            &mut self.sections,
             elem_id,
-            old,
             new,
             TIMEOUT_MS,
         )? {
@@ -143,13 +126,18 @@ impl CtlModel<(SndDice, FwNode)> for ExtensionModel {
 
 impl NotifyModel<(SndDice, FwNode), u32> for ExtensionModel {
     fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
-        elem_id_list.extend_from_slice(&self.ctl.notified_elem_list);
+        elem_id_list.extend_from_slice(&self.common_ctl.1);
         self.tcd22xx_ctl.get_notified_elem_list(elem_id_list);
     }
 
     fn parse_notification(&mut self, unit: &mut (SndDice, FwNode), msg: &u32) -> Result<(), Error> {
-        self.ctl
-            .parse_notification(unit, &mut self.req, &self.sections, *msg, TIMEOUT_MS)?;
+        self.common_ctl.parse_notification(
+            &self.req,
+            &unit.1,
+            &mut self.sections,
+            *msg,
+            TIMEOUT_MS,
+        )?;
         self.tcd22xx_ctl.parse_notification(
             unit,
             &mut self.req,
@@ -167,7 +155,7 @@ impl NotifyModel<(SndDice, FwNode), u32> for ExtensionModel {
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
-        if self.ctl.read_notified_elem(elem_id, elem_value)? {
+        if self.common_ctl.read(&self.sections, elem_id, elem_value)? {
             Ok(true)
         } else if self.tcd22xx_ctl.read_notified_elem(elem_id, elem_value)? {
             Ok(true)
@@ -179,13 +167,13 @@ impl NotifyModel<(SndDice, FwNode), u32> for ExtensionModel {
 
 impl MeasureModel<(SndDice, FwNode)> for ExtensionModel {
     fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
-        elem_id_list.extend_from_slice(&self.ctl.measured_elem_list);
+        elem_id_list.extend_from_slice(&self.common_ctl.0);
         self.tcd22xx_ctl.get_measured_elem_list(elem_id_list);
     }
 
     fn measure_states(&mut self, unit: &mut (SndDice, FwNode)) -> Result<(), Error> {
-        self.ctl
-            .measure_states(unit, &mut self.req, &self.sections, TIMEOUT_MS)?;
+        self.common_ctl
+            .measure(&self.req, &unit.1, &mut self.sections, TIMEOUT_MS)?;
         self.tcd22xx_ctl.measure_states(
             unit,
             &mut self.req,
@@ -201,7 +189,7 @@ impl MeasureModel<(SndDice, FwNode)> for ExtensionModel {
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
-        if self.ctl.measure_elem(elem_id, elem_value)? {
+        if self.common_ctl.read(&self.sections, elem_id, elem_value)? {
             Ok(true)
         } else if self.tcd22xx_ctl.measure_elem(elem_id, elem_value)? {
             Ok(true)
@@ -211,8 +199,12 @@ impl MeasureModel<(SndDice, FwNode)> for ExtensionModel {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct ExtensionProtocol;
+
+impl TcatOperation for ExtensionProtocol {}
+
+impl TcatGlobalSectionSpecification for ExtensionProtocol {}
 
 impl Tcd22xxSpecOperation for ExtensionProtocol {
     const INPUTS: &'static [Input] = &[
@@ -351,6 +343,11 @@ impl Tcd22xxSpecOperation for ExtensionProtocol {
     ];
 }
 
+#[derive(Default, Debug)]
+struct CommonCtl(Vec<ElemId>, Vec<ElemId>);
+
+impl CommonCtlOperation<ExtensionProtocol> for CommonCtl {}
+
 #[derive(Default)]
 struct ExtensionTcd22xxCtl(Tcd22xxCtl);
 
@@ -371,7 +368,8 @@ pub fn detect_extended_model(node: &mut FwNode) -> bool {
     // MEMO: workaround for old firmware. Invalidate the negative effect by failure of
     // previous transaction.
     if res.is_err() {
-        let _ = GeneralProtocol::read_general_sections(&mut req, node, 100);
+        let mut sections = GeneralSections::default();
+        let _ = ExtensionProtocol::read_general_sections(&mut req, node, &mut sections, 100);
     }
 
     res.is_ok()
