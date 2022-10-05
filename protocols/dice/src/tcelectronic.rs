1e5898dd86dbdbbe419602ffd6498cd6cfb1935e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-10-05 11:52:23 +0900

    protocols/dice: tcelectronic: tcelectronic: add trait to revise current implementation
    Current implementation has issue that intermediate data expression has
    parameters to operate protocol. It's preferable to move these parameters
    to protocol implementations.
    
    This commit is a first step for the issue. Some traits are newly added
    to revise current implementation.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/dice/src/tcelectronic.rs
+++ protocols/dice/src/tcelectronic.rs
@@ -58,6 +58,134 @@ where
     }
 }
 
+/// Serialize and deserialize for segment in TC Konnekt protocol.
+pub trait TcKonnektSegmentSerdes<T> {
+    /// The name of segment.
+    const NAME: &'static str;
+
+    /// The offset of segment.
+    const OFFSET: usize;
+
+    /// The size of segment.
+    const SIZE: usize;
+
+    /// Serialize for parameter.
+    fn serialize(params: &T, raw: &mut [u8]) -> Result<(), String>;
+
+    /// Deserialize for parameter.
+    fn deserialize(params: &mut T, raw: &[u8]) -> Result<(), String>;
+}
+
+fn generate_error(segment_name: &str, cause: &str, raw: &[u8]) -> Error {
+    let msg = format!(
+        "segment: {}, cause: '{}', raw: {:02x?}",
+        segment_name, cause, raw
+    );
+    Error::new(GeneralProtocolError::VendorDependent, &msg)
+}
+
+/// Operation to cache content of segment in TC Electronic Konnekt series.
+pub trait TcKonnektSegmentOperation<T>: TcatOperation + TcKonnektSegmentSerdes<T>
+where
+    T: TcKonnektSegmentData,
+{
+    /// Cache whole segment and deserialize for parameters.
+    fn cache_whole_segment(
+        req: &FwReq,
+        node: &FwNode,
+        segment: &mut TcKonnektSegment<T>,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        assert_eq!(segment.raw.len(), Self::SIZE);
+
+        Self::read(
+            req,
+            node,
+            BASE_OFFSET + Self::OFFSET,
+            &mut segment.raw,
+            timeout_ms,
+        )?;
+
+        Self::deserialize(&mut segment.data, &segment.raw).map_err(|cause| {
+            generate_error(Self::NAME, &cause, &segment.raw)
+        })
+    }
+}
+
+impl<O: TcatOperation + TcKonnektSegmentSerdes<T>, T: TcKonnektSegmentData>
+    TcKonnektSegmentOperation<T> for O
+{
+}
+
+/// Operation to update content of segment in TC Electronic Konnekt series.
+pub trait TcKonnektMutableSegmentOperation<T>: TcatOperation + TcKonnektSegmentSerdes<T>
+where
+    T: TcKonnektSegmentData,
+{
+    /// Update part of segment for any change at the parameters.
+    fn update_partial_segment(
+        req: &FwReq,
+        node: &FwNode,
+        params: &T,
+        segment: &mut TcKonnektSegment<T>,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        assert_eq!(segment.raw.len(), Self::SIZE);
+
+        let mut raw = segment.raw.clone();
+        Self::serialize(params, &mut raw).map_err(|cause| {
+            generate_error(Self::NAME, &cause, &segment.raw)
+        })?;
+
+        (0..Self::SIZE).step_by(4).try_for_each(|pos| {
+            let new = &mut raw[pos..(pos + 4)];
+            if new != &segment.raw[pos..(pos + 4)] {
+                Self::write(req, node, BASE_OFFSET + Self::OFFSET + pos, new, timeout_ms)
+                    .map(|_| segment.raw[pos..(pos + 4)].copy_from_slice(new))
+            } else {
+                Ok(())
+            }
+        })?;
+
+        Self::deserialize(&mut segment.data, &raw).map_err(|cause| {
+            generate_error(Self::NAME, &cause, &segment.raw)
+        })
+    }
+
+    /// Update whole segment by the parameters.
+    fn update_whole_segment(
+        req: &FwReq,
+        node: &FwNode,
+        params: &T,
+        segment: &mut TcKonnektSegment<T>,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        assert_eq!(segment.raw.len(), Self::SIZE);
+
+        let mut raw = segment.raw.clone();
+        Self::serialize(&params, &mut raw).map_err(|cause| {
+            generate_error(Self::NAME, &cause, &segment.raw)
+        })?;
+
+        Self::write(req, node, BASE_OFFSET + Self::OFFSET, &mut raw, timeout_ms)?;
+
+        segment.raw.copy_from_slice(&raw);
+        Self::deserialize(&mut segment.data, &segment.raw).map_err(|cause| {
+            generate_error(Self::NAME, &cause, &segment.raw)
+        })
+    }
+}
+
+/// Operation for segment in which any change is notified in TC Electronic Konnekt series.
+pub trait TcKonnektNotifiedSegmentOperation<T: TcKonnektSegmentData> {
+    const NOTIFY_FLAG: u32;
+
+    /// Check message to be notified or not.
+    fn is_notified_segment(_: &TcKonnektSegment<T>, msg: u32) -> bool {
+        msg & Self::NOTIFY_FLAG > 0
+    }
+}
+
 /// Operation of segment in TC Electronics Konnekt series.
 pub trait SegmentOperation<T>
 where
