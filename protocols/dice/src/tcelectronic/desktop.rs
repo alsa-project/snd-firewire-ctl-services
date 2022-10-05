2867a28482fd86560358d84e7d37fac1be3b91f0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-10-05 11:52:23 +0900

    protocols/dice: tcelectronic: use renewed trait for Desktop Konnekt 6
    This commit implements renewed traits to protocol implementation for
    Desktop Konnekt 6.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/dice/src/tcelectronic/desktop.rs
+++ protocols/dice/src/tcelectronic/desktop.rs
@@ -42,7 +42,7 @@ pub type Desktopk6MeterSegment = TcKonnektSegment<DesktopMeter>;
 impl SegmentOperation<DesktopMeter> for Desktopk6Protocol {}
 
 /// Target of meter.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum MeterTarget {
     Input,
     Pre,
@@ -76,7 +76,7 @@ impl From<MeterTarget> for u32 {
 }
 
 /// Current scene.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum InputScene {
     MicInst,
     DualInst,
@@ -110,7 +110,7 @@ impl From<InputScene> for u32 {
 }
 
 /// State of panel.
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct DesktopHwState {
     pub meter_target: MeterTarget,
     pub mixer_output_monaural: bool,
@@ -132,95 +132,140 @@ pub struct DesktopHwState {
 }
 
 impl DesktopHwState {
-    const SIZE: usize = 144;
-
     const REVERB_TO_MAIN_MASK: u32 = 0x00000001;
     const REVERB_TO_HP_MASK: u32 = 0x00000002;
 }
 
-impl TcKonnektSegmentData for DesktopHwState {
-    fn build(&self, raw: &mut [u8]) {
-        self.meter_target.build_quadlet(&mut raw[..4]);
-        self.mixer_output_monaural.build_quadlet(&mut raw[4..8]);
-        self.knob_assign_to_hp.build_quadlet(&mut raw[8..12]);
-        self.mixer_output_dim_enabled
+impl TcKonnektSegmentSerdes<DesktopHwState> for Desktopk6Protocol {
+    const NAME: &'static str = "hardware-state";
+    const OFFSET: usize = 0x0008;
+    const SIZE: usize = 144;
+
+    fn serialize(params: &DesktopHwState, raw: &mut [u8]) -> Result<(), String> {
+        params.meter_target.build_quadlet(&mut raw[..4]);
+        params.mixer_output_monaural.build_quadlet(&mut raw[4..8]);
+        params.knob_assign_to_hp.build_quadlet(&mut raw[8..12]);
+        params
+            .mixer_output_dim_enabled
             .build_quadlet(&mut raw[12..16]);
-        self.mixer_output_dim_volume.build_quadlet(&mut raw[16..20]);
-        self.input_scene.build_quadlet(&mut raw[20..24]);
+        params
+            .mixer_output_dim_volume
+            .build_quadlet(&mut raw[16..20]);
+        params.input_scene.build_quadlet(&mut raw[20..24]);
 
         let mut val = 0;
-        if self.reverb_to_master {
-            val |= Self::REVERB_TO_MAIN_MASK;
+        if params.reverb_to_master {
+            val |= DesktopHwState::REVERB_TO_MAIN_MASK;
         }
-        if self.reverb_to_hp {
-            val |= Self::REVERB_TO_HP_MASK;
+        if params.reverb_to_hp {
+            val |= DesktopHwState::REVERB_TO_HP_MASK;
         }
         val.build_quadlet(&mut raw[28..32]);
 
-        self.master_knob_backlight.build_quadlet(&mut raw[32..36]);
-        self.mic_0_phantom.build_quadlet(&mut raw[52..56]);
-        self.mic_0_boost.build_quadlet(&mut raw[56..60]);
+        params.master_knob_backlight.build_quadlet(&mut raw[32..36]);
+        params.mic_0_phantom.build_quadlet(&mut raw[52..56]);
+        params.mic_0_boost.build_quadlet(&mut raw[56..60]);
+
+        Ok(())
     }
 
-    fn parse(&mut self, raw: &[u8]) {
-        self.meter_target.parse_quadlet(&raw[..4]);
-        self.mixer_output_monaural.parse_quadlet(&raw[4..8]);
-        self.knob_assign_to_hp.parse_quadlet(&raw[8..12]);
-        self.mixer_output_dim_enabled.parse_quadlet(&raw[12..16]);
-        self.mixer_output_dim_volume.parse_quadlet(&raw[16..20]);
-        self.input_scene.parse_quadlet(&raw[20..24]);
+    fn deserialize(params: &mut DesktopHwState, raw: &[u8]) -> Result<(), String> {
+        params.meter_target.parse_quadlet(&raw[..4]);
+        params.mixer_output_monaural.parse_quadlet(&raw[4..8]);
+        params.knob_assign_to_hp.parse_quadlet(&raw[8..12]);
+        params.mixer_output_dim_enabled.parse_quadlet(&raw[12..16]);
+        params.mixer_output_dim_volume.parse_quadlet(&raw[16..20]);
+        params.input_scene.parse_quadlet(&raw[20..24]);
 
         let mut val = 0;
         val.parse_quadlet(&raw[28..32]);
-        self.reverb_to_master = val & Self::REVERB_TO_MAIN_MASK > 0;
-        self.reverb_to_hp = val & Self::REVERB_TO_HP_MASK > 0;
+        params.reverb_to_master = val & DesktopHwState::REVERB_TO_MAIN_MASK > 0;
+        params.reverb_to_hp = val & DesktopHwState::REVERB_TO_HP_MASK > 0;
 
-        self.master_knob_backlight.parse_quadlet(&raw[32..36]);
-        self.mic_0_phantom.parse_quadlet(&raw[52..56]);
-        self.mic_0_boost.parse_quadlet(&raw[56..60]);
+        params.master_knob_backlight.parse_quadlet(&raw[32..36]);
+        params.mic_0_phantom.parse_quadlet(&raw[52..56]);
+        params.mic_0_boost.parse_quadlet(&raw[56..60]);
+
+        Ok(())
+    }
+}
+
+impl TcKonnektMutableSegmentOperation<DesktopHwState> for Desktopk6Protocol {}
+
+impl TcKonnektNotifiedSegmentOperation<DesktopHwState> for Desktopk6Protocol {
+    const NOTIFY_FLAG: u32 = DESKTOP_HW_STATE_NOTIFY_FLAG;
+}
+
+impl TcKonnektSegmentData for DesktopHwState {
+    fn build(&self, raw: &mut [u8]) {
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::serialize(self, raw);
+    }
+
+    fn parse(&mut self, raw: &[u8]) {
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::deserialize(self, raw);
     }
 }
 
 impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopHwState> {
-    const OFFSET: usize = 0x0008;
-    const SIZE: usize = DesktopHwState::SIZE;
+    const OFFSET: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopHwState>>::OFFSET;
+    const SIZE: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopHwState>>::SIZE;
 }
 
 impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<DesktopHwState> {
-    const NOTIFY_FLAG: u32 = DESKTOP_HW_STATE_NOTIFY_FLAG;
+    const NOTIFY_FLAG: u32 =
+        <Desktopk6Protocol as TcKonnektNotifiedSegmentOperation<DesktopHwState>>::NOTIFY_FLAG;
 }
 
 /// Configuration.
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct DesktopConfig {
     pub standalone_rate: TcKonnektStandaloneClkRate,
 }
 
-impl DesktopConfig {
+impl TcKonnektSegmentSerdes<DesktopConfig> for Desktopk6Protocol {
+    const NAME: &'static str = "configuration";
+    const OFFSET: usize = 0x0098;
     const SIZE: usize = 32;
+
+    fn serialize(params: &DesktopConfig, raw: &mut [u8]) -> Result<(), String> {
+        params.standalone_rate.build_quadlet(&mut raw[4..8]);
+        Ok(())
+    }
+
+    fn deserialize(params: &mut DesktopConfig, raw: &[u8]) -> Result<(), String> {
+        params.standalone_rate.parse_quadlet(&raw[4..8]);
+        Ok(())
+    }
+}
+
+impl TcKonnektMutableSegmentOperation<DesktopConfig> for Desktopk6Protocol {}
+
+impl TcKonnektNotifiedSegmentOperation<DesktopConfig> for Desktopk6Protocol {
+    const NOTIFY_FLAG: u32 = DESKTOP_CONFIG_NOTIFY_FLAG;
 }
 
 impl TcKonnektSegmentData for DesktopConfig {
     fn build(&self, raw: &mut [u8]) {
-        self.standalone_rate.build_quadlet(&mut raw[4..8]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::serialize(self, raw);
     }
 
     fn parse(&mut self, raw: &[u8]) {
-        self.standalone_rate.parse_quadlet(&raw[4..8]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::deserialize(self, raw);
     }
 }
 
 impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopConfig> {
-    const OFFSET: usize = 0x0098;
-    const SIZE: usize = DesktopConfig::SIZE;
+    const OFFSET: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopConfig>>::OFFSET;
+    const SIZE: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopConfig>>::SIZE;
 }
 
 impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<DesktopConfig> {
-    const NOTIFY_FLAG: u32 = DESKTOP_CONFIG_NOTIFY_FLAG;
+    const NOTIFY_FLAG: u32 =
+        <Desktopk6Protocol as TcKonnektNotifiedSegmentOperation<DesktopConfig>>::NOTIFY_FLAG;
 }
 
 /// Source of headphone.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum DesktopHpSrc {
     Stream23,
     Mixer01,
@@ -251,7 +296,7 @@ impl From<DesktopHpSrc> for u32 {
 }
 
 /// State of mixer.
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct DesktopMixerState {
     /// The input level for ch 0 and 1. -1000..0 (-94.0..0.0 dB)
     pub mic_inst_level: [i32; 2],
@@ -275,66 +320,85 @@ pub struct DesktopMixerState {
     pub hp_src: DesktopHpSrc,
 }
 
-impl DesktopMixerState {
+impl TcKonnektSegmentSerdes<DesktopMixerState> for Desktopk6Protocol {
+    const NAME: &'static str = "mixer-state";
+    const OFFSET: usize = 0x00b8;
     const SIZE: usize = 688;
+
+    fn serialize(params: &DesktopMixerState, raw: &mut [u8]) -> Result<(), String> {
+        params.mic_inst_level[0].build_quadlet(&mut raw[12..16]);
+        params.mic_inst_pan[0].build_quadlet(&mut raw[16..20]);
+        params.mic_inst_send[0].build_quadlet(&mut raw[20..24]);
+        params.mic_inst_level[1].build_quadlet(&mut raw[28..32]);
+        params.mic_inst_pan[1].build_quadlet(&mut raw[32..36]);
+        params.mic_inst_send[1].build_quadlet(&mut raw[40..44]);
+
+        params.dual_inst_level[0].build_quadlet(&mut raw[228..232]);
+        params.dual_inst_pan[0].build_quadlet(&mut raw[232..236]);
+        params.dual_inst_send[0].build_quadlet(&mut raw[240..244]);
+        params.dual_inst_level[1].build_quadlet(&mut raw[248..252]);
+        params.dual_inst_pan[1].build_quadlet(&mut raw[252..256]);
+        params.dual_inst_send[1].build_quadlet(&mut raw[260..264]);
+
+        params.stereo_in_level.build_quadlet(&mut raw[444..448]);
+        params.stereo_in_pan.build_quadlet(&mut raw[448..452]);
+        params.stereo_in_send.build_quadlet(&mut raw[452..456]);
+
+        params.hp_src.build_quadlet(&mut raw[648..652]);
+        Ok(())
+    }
+
+    fn deserialize(params: &mut DesktopMixerState, raw: &[u8]) -> Result<(), String> {
+        params.mic_inst_level[0].parse_quadlet(&raw[12..16]);
+        params.mic_inst_pan[0].parse_quadlet(&raw[16..20]);
+        params.mic_inst_send[0].parse_quadlet(&raw[20..24]);
+        params.mic_inst_level[1].parse_quadlet(&raw[28..32]);
+        params.mic_inst_pan[1].parse_quadlet(&raw[32..36]);
+        params.mic_inst_send[1].parse_quadlet(&raw[40..44]);
+
+        params.dual_inst_level[0].parse_quadlet(&raw[228..232]);
+        params.dual_inst_pan[0].parse_quadlet(&raw[232..236]);
+        params.dual_inst_send[0].parse_quadlet(&raw[240..244]);
+        params.dual_inst_level[1].parse_quadlet(&raw[248..252]);
+        params.dual_inst_pan[1].parse_quadlet(&raw[252..256]);
+        params.dual_inst_send[1].parse_quadlet(&raw[260..264]);
+
+        params.stereo_in_level.parse_quadlet(&raw[444..448]);
+        params.stereo_in_pan.parse_quadlet(&raw[448..452]);
+        params.stereo_in_send.parse_quadlet(&raw[452..456]);
+
+        params.hp_src.parse_quadlet(&raw[648..652]);
+        Ok(())
+    }
+}
+
+impl TcKonnektMutableSegmentOperation<DesktopMixerState> for Desktopk6Protocol {}
+
+impl TcKonnektNotifiedSegmentOperation<DesktopMixerState> for Desktopk6Protocol {
+    const NOTIFY_FLAG: u32 = DESKTOP_MIXER_STATE_NOTIFY_FLAG;
 }
 
 impl TcKonnektSegmentData for DesktopMixerState {
     fn build(&self, raw: &mut [u8]) {
-        self.mic_inst_level[0].build_quadlet(&mut raw[12..16]);
-        self.mic_inst_pan[0].build_quadlet(&mut raw[16..20]);
-        self.mic_inst_send[0].build_quadlet(&mut raw[20..24]);
-        self.mic_inst_level[1].build_quadlet(&mut raw[28..32]);
-        self.mic_inst_pan[1].build_quadlet(&mut raw[32..36]);
-        self.mic_inst_send[1].build_quadlet(&mut raw[40..44]);
-
-        self.dual_inst_level[0].build_quadlet(&mut raw[228..232]);
-        self.dual_inst_pan[0].build_quadlet(&mut raw[232..236]);
-        self.dual_inst_send[0].build_quadlet(&mut raw[240..244]);
-        self.dual_inst_level[1].build_quadlet(&mut raw[248..252]);
-        self.dual_inst_pan[1].build_quadlet(&mut raw[252..256]);
-        self.dual_inst_send[1].build_quadlet(&mut raw[260..264]);
-
-        self.stereo_in_level.build_quadlet(&mut raw[444..448]);
-        self.stereo_in_pan.build_quadlet(&mut raw[448..452]);
-        self.stereo_in_send.build_quadlet(&mut raw[452..456]);
-
-        self.hp_src.build_quadlet(&mut raw[648..652]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::serialize(self, raw);
     }
 
     fn parse(&mut self, raw: &[u8]) {
-        self.mic_inst_level[0].parse_quadlet(&raw[12..16]);
-        self.mic_inst_pan[0].parse_quadlet(&raw[16..20]);
-        self.mic_inst_send[0].parse_quadlet(&raw[20..24]);
-        self.mic_inst_level[1].parse_quadlet(&raw[28..32]);
-        self.mic_inst_pan[1].parse_quadlet(&raw[32..36]);
-        self.mic_inst_send[1].parse_quadlet(&raw[40..44]);
-
-        self.dual_inst_level[0].parse_quadlet(&raw[228..232]);
-        self.dual_inst_pan[0].parse_quadlet(&raw[232..236]);
-        self.dual_inst_send[0].parse_quadlet(&raw[240..244]);
-        self.dual_inst_level[1].parse_quadlet(&raw[248..252]);
-        self.dual_inst_pan[1].parse_quadlet(&raw[252..256]);
-        self.dual_inst_send[1].parse_quadlet(&raw[260..264]);
-
-        self.stereo_in_level.parse_quadlet(&raw[444..448]);
-        self.stereo_in_pan.parse_quadlet(&raw[448..452]);
-        self.stereo_in_send.parse_quadlet(&raw[452..456]);
-
-        self.hp_src.parse_quadlet(&raw[648..652]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::deserialize(self, raw);
     }
 }
 
 impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopMixerState> {
-    const OFFSET: usize = 0x00b8;
-    const SIZE: usize = DesktopMixerState::SIZE;
+    const OFFSET: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopMixerState>>::OFFSET;
+    const SIZE: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopMixerState>>::SIZE;
 }
 
 impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<DesktopMixerState> {
-    const NOTIFY_FLAG: u32 = DESKTOP_MIXER_STATE_NOTIFY_FLAG;
+    const NOTIFY_FLAG: u32 =
+        <Desktopk6Protocol as TcKonnektNotifiedSegmentOperation<DesktopMixerState>>::NOTIFY_FLAG;
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct DesktopPanel {
     /// The count of panel button to push.
     pub panel_button_count: u32,
@@ -352,67 +416,97 @@ pub struct DesktopPanel {
     pub firewire_led: FireWireLedState,
 }
 
-impl DesktopPanel {
+impl TcKonnektSegmentSerdes<DesktopPanel> for Desktopk6Protocol {
+    const NAME: &'static str = "hardware-panel";
+    const OFFSET: usize = 0x2008;
     const SIZE: usize = 64;
+
+    fn serialize(params: &DesktopPanel, raw: &mut [u8]) -> Result<(), String> {
+        params.panel_button_count.build_quadlet(&mut raw[..4]);
+        params.main_knob_value.build_quadlet(&mut raw[4..8]);
+        params.phone_knob_value.build_quadlet(&mut raw[8..12]);
+        params.mix_knob_value.build_quadlet(&mut raw[12..16]);
+        params.reverb_led_on.build_quadlet(&mut raw[16..20]);
+        params.reverb_knob_value.build_quadlet(&mut raw[24..28]);
+        params.firewire_led.build_quadlet(&mut raw[36..40]);
+        Ok(())
+    }
+
+    fn deserialize(params: &mut DesktopPanel, raw: &[u8]) -> Result<(), String> {
+        params.panel_button_count.parse_quadlet(&raw[..4]);
+        params.main_knob_value.parse_quadlet(&raw[4..8]);
+        params.phone_knob_value.parse_quadlet(&raw[8..12]);
+        params.mix_knob_value.parse_quadlet(&raw[12..16]);
+        params.reverb_led_on.parse_quadlet(&raw[16..20]);
+        params.reverb_knob_value.parse_quadlet(&raw[24..28]);
+        params.firewire_led.parse_quadlet(&raw[36..40]);
+        Ok(())
+    }
 }
 
+impl TcKonnektMutableSegmentOperation<DesktopPanel> for Desktopk6Protocol {}
+
+impl TcKonnektNotifiedSegmentOperation<DesktopPanel> for Desktopk6Protocol {
+    const NOTIFY_FLAG: u32 = DESKTOP_PANEL_NOTIFY_FLAG;
+}
 impl TcKonnektSegmentData for DesktopPanel {
     fn build(&self, raw: &mut [u8]) {
-        self.panel_button_count.build_quadlet(&mut raw[..4]);
-        self.main_knob_value.build_quadlet(&mut raw[4..8]);
-        self.phone_knob_value.build_quadlet(&mut raw[8..12]);
-        self.mix_knob_value.build_quadlet(&mut raw[12..16]);
-        self.reverb_led_on.build_quadlet(&mut raw[16..20]);
-        self.reverb_knob_value.build_quadlet(&mut raw[24..28]);
-        self.firewire_led.build_quadlet(&mut raw[36..40]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::serialize(self, raw);
     }
 
     fn parse(&mut self, raw: &[u8]) {
-        self.panel_button_count.parse_quadlet(&raw[..4]);
-        self.main_knob_value.parse_quadlet(&raw[4..8]);
-        self.phone_knob_value.parse_quadlet(&raw[8..12]);
-        self.mix_knob_value.parse_quadlet(&raw[12..16]);
-        self.reverb_led_on.parse_quadlet(&raw[16..20]);
-        self.reverb_knob_value.parse_quadlet(&raw[24..28]);
-        self.firewire_led.parse_quadlet(&raw[36..40]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::deserialize(self, raw);
     }
 }
 
 impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopPanel> {
-    const OFFSET: usize = 0x2008;
-    const SIZE: usize = DesktopPanel::SIZE;
+    const OFFSET: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopPanel>>::OFFSET;
+    const SIZE: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopPanel>>::SIZE;
 }
 
 impl TcKonnektNotifiedSegmentSpec for TcKonnektSegment<DesktopPanel> {
-    const NOTIFY_FLAG: u32 = DESKTOP_PANEL_NOTIFY_FLAG;
+    const NOTIFY_FLAG: u32 =
+        <Desktopk6Protocol as TcKonnektNotifiedSegmentOperation<DesktopPanel>>::NOTIFY_FLAG;
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct DesktopMeter {
     pub analog_inputs: [i32; 2],
     pub mixer_outputs: [i32; 2],
     pub stream_inputs: [i32; 2],
 }
 
-impl DesktopMeter {
+impl TcKonnektSegmentSerdes<DesktopMeter> for Desktopk6Protocol {
+    const NAME: &'static str = "hardware-meter";
+    const OFFSET: usize = 0x20e4;
     const SIZE: usize = 92;
+
+    fn serialize(params: &DesktopMeter, raw: &mut [u8]) -> Result<(), String> {
+        params.analog_inputs.build_quadlet_block(&mut raw[..8]);
+        params.mixer_outputs.build_quadlet_block(&mut raw[40..48]);
+        params.stream_inputs.build_quadlet_block(&mut raw[48..56]);
+        Ok(())
+    }
+
+    fn deserialize(params: &mut DesktopMeter, raw: &[u8]) -> Result<(), String> {
+        params.analog_inputs.parse_quadlet_block(&raw[..8]);
+        params.mixer_outputs.parse_quadlet_block(&raw[40..48]);
+        params.stream_inputs.parse_quadlet_block(&raw[48..56]);
+        Ok(())
+    }
 }
 
 impl TcKonnektSegmentData for DesktopMeter {
     fn build(&self, raw: &mut [u8]) {
-        self.analog_inputs.build_quadlet_block(&mut raw[..8]);
-        self.mixer_outputs.build_quadlet_block(&mut raw[40..48]);
-        self.stream_inputs.build_quadlet_block(&mut raw[48..56]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::serialize(self, raw);
     }
 
     fn parse(&mut self, raw: &[u8]) {
-        self.analog_inputs.parse_quadlet_block(&raw[..8]);
-        self.mixer_outputs.parse_quadlet_block(&raw[40..48]);
-        self.stream_inputs.parse_quadlet_block(&raw[48..56]);
+        let _ = <Desktopk6Protocol as TcKonnektSegmentSerdes<Self>>::deserialize(self, raw);
     }
 }
 
 impl TcKonnektSegmentSpec for TcKonnektSegment<DesktopMeter> {
-    const OFFSET: usize = 0x20e4;
-    const SIZE: usize = DesktopMeter::SIZE;
+    const OFFSET: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopMeter>>::OFFSET;
+    const SIZE: usize = <Desktopk6Protocol as TcKonnektSegmentSerdes<DesktopMeter>>::SIZE;
 }
