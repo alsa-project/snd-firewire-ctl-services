dead30201e8a12cf7d25274fb62009a16ae2ca51
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-10-20 18:45:44 +0900

    protocols/dice: focusrite: remove unused methods of I/O params operation
    This commit removes unused methods from trait of protocol.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/dice/src/focusrite/spro40.rs
+++ protocols/dice/src/focusrite/spro40.rs
@@ -8,9 +8,6 @@
 
 use super::{tcat::tcd22xx_spec::*, *};
 
-const ANALOG_OUT_0_1_PAD_OFFSET: usize = 0x0040;
-const IO_FLAGS_OFFSET: usize = 0x005c;
-
 /// Protocol implementation specific to Saffire Pro 40.
 #[derive(Default, Debug)]
 pub struct SPro40Protocol;
@@ -123,8 +120,6 @@ impl SaffireproSwNoticeOperation for SPro40Protocol {
 
 const SRC_SW_NOTICE: u32 = 0x00000001;
 const DIM_MUTE_SW_NOTICE: u32 = 0x00000002;
-const OUT_PAD_SW_NOTICE: u32 = 0x00000003;
-const IO_FLAG_SW_NOTICE: u32 = 0x00000004;
 
 impl SaffireproOutGroupOperation for SPro40Protocol {
     const OUT_GROUP_STATE_OFFSET: usize = 0x000c;
@@ -139,118 +134,4 @@ impl SaffireproOutGroupOperation for SPro40Protocol {
 impl SaffireproIoParamsOperation for SPro40Protocol {
     const AESEBU_IS_SUPPORTED: bool = false;
     const MIC_PREAMP_TRANSFORMER_IS_SUPPORTED: bool = false;
-}
-
-/// Type of signal for optical output interface.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum OptOutIfaceMode {
-    Adat,
-    Spdif,
-}
-
-impl Default for OptOutIfaceMode {
-    fn default() -> Self {
-        Self::Adat
-    }
-}
-
-/// Protocol specific to Saffire Pro 26.
-impl SPro40Protocol {
-    pub fn read_analog_out_0_1_pad(
-        req: &mut FwReq,
-        node: &mut FwNode,
-        sections: &ExtensionSections,
-        timeout_ms: u32,
-    ) -> Result<bool, Error> {
-        let mut raw = [0; 4];
-        ApplSectionProtocol::read_appl_data(
-            req,
-            node,
-            sections,
-            ANALOG_OUT_0_1_PAD_OFFSET,
-            &mut raw,
-            timeout_ms,
-        )
-        .map(|_| u32::from_be_bytes(raw) > 0)
-    }
-
-    pub fn write_analog_out_0_1_pad(
-        req: &mut FwReq,
-        node: &mut FwNode,
-        sections: &ExtensionSections,
-        enable: bool,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
-        let mut raw = [0; 4];
-        enable.build_quadlet(&mut raw);
-        ApplSectionProtocol::write_appl_data(
-            req,
-            node,
-            sections,
-            ANALOG_OUT_0_1_PAD_OFFSET,
-            &mut raw,
-            timeout_ms,
-        )?;
-        Self::write_sw_notice(req, node, sections, OUT_PAD_SW_NOTICE, timeout_ms)
-    }
-
-    pub fn read_opt_out_iface_mode(
-        req: &mut FwReq,
-        node: &mut FwNode,
-        sections: &ExtensionSections,
-        timeout_ms: u32,
-    ) -> Result<OptOutIfaceMode, Error> {
-        let mut raw = [0; 4];
-        ApplSectionProtocol::read_appl_data(
-            req,
-            node,
-            sections,
-            IO_FLAGS_OFFSET,
-            &mut raw,
-            timeout_ms,
-        )
-        .map(|_| {
-            let val = u32::from_be_bytes(raw);
-            if val & 0x00000001 > 0 {
-                OptOutIfaceMode::Spdif
-            } else {
-                OptOutIfaceMode::Adat
-            }
-        })
-    }
-
-    pub fn write_opt_out_iface_mode(
-        req: &mut FwReq,
-        node: &mut FwNode,
-        sections: &ExtensionSections,
-        mode: OptOutIfaceMode,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
-        let mut raw = [0; 4];
-        ApplSectionProtocol::read_appl_data(
-            req,
-            node,
-            sections,
-            IO_FLAGS_OFFSET,
-            &mut raw,
-            timeout_ms,
-        )?;
-
-        let mut val = u32::from_be_bytes(raw);
-        val &= !0x00000003;
-
-        if mode == OptOutIfaceMode::Spdif {
-            val |= 0x00000001;
-        }
-        val.build_quadlet(&mut raw);
-        ApplSectionProtocol::write_appl_data(
-            req,
-            node,
-            sections,
-            IO_FLAGS_OFFSET,
-            &mut raw,
-            timeout_ms,
-        )?;
-        Self::write_sw_notice(req, node, sections, IO_FLAG_SW_NOTICE, timeout_ms)
-    }
 }
