b6bb6728b036f6cd1ff446376e960fe64211fff0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-10-23 00:43:12 +0900

    protocols/dice: tcat: adds new functions to read and write router section
    This commit adds alternative functions to read and write parameters in
    router section of TCAT protocol extension with helper function to
    serialize and deserialize the parameters.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/dice/src/tcat/extension/router_section.rs
+++ protocols/dice/src/tcat/extension/router_section.rs
@@ -13,6 +13,58 @@ use super::{caps_section::*, router_entry::*, *};
 pub struct RouterSectionProtocol;
 
 impl RouterSectionProtocol {
+    /// Read from router section and deserialize entries.
+    pub fn read_router_whole_entries(
+        req: &mut FwReq,
+        node: &mut FwNode,
+        sections: &ExtensionSections,
+        caps: &ExtensionCaps,
+        entries: &mut Vec<RouterEntry>,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let size = 4 + calculate_router_entries_size(caps.router.maximum_entry_count as usize);
+        let size = std::cmp::min(sections.router.size, size);
+        let mut raw = vec![0u8; size];
+
+        extension_read(req, node, sections.router.offset, &mut raw, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Router, &e.to_string()))?;
+
+        let mut val = 0u32;
+        val.parse_quadlet(&raw[..4]);
+        entries.resize_with(val as usize, Default::default);
+
+        deserialize_router_entries(entries, &mut raw[4..])
+            .map_err(|cause| Error::new(ProtocolExtensionError::Router, &cause))
+    }
+
+    /// Serialize entries and write to router section.
+    pub fn write_router_whole_entries(
+        req: &mut FwReq,
+        node: &mut FwNode,
+        sections: &ExtensionSections,
+        caps: &ExtensionCaps,
+        entries: &[RouterEntry],
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        if entries.len() >= caps.router.maximum_entry_count as usize {
+            let msg = format!(
+                "The number of router entries should be less than{}, but {} given",
+                caps.router.maximum_entry_count,
+                entries.len(),
+            );
+            Err(Error::new(ProtocolExtensionError::Router, &msg))?;
+        }
+
+        let size = 4 + calculate_router_entries_size(entries.len() as usize);
+        let mut raw = vec![0u8; size];
+
+        (entries.len() as u32).build_quadlet(&mut raw[..4]);
+        serialize_router_entries(entries, &mut raw[4..])
+            .map_err(|cause| Error::new(ProtocolExtensionError::Router, &cause))?;
+
+        extension_write(req, node, sections.router.offset, &mut raw, timeout_ms)
+    }
+
     pub fn read_router_entries(
         req: &mut FwReq,
         node: &mut FwNode,
