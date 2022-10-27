alsa-project/snd-firewire-ctl-services

commit 40cf0b3b326627d9af05db434fb6386b46465d7b

protocols/dice: tcat: code cleanup for router entries
This commit rewrites current implementation of router section of TCAT
protocol extension by helper functions to serialize and deserialize
parameters in router section.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/tcat/extension.rs
@@ -295,7 +295,7 @@ fn extension_write(
 }
 
 /// Identifier of destination block.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum DstBlkId {
     Aes,
     Adat,
@@ -316,23 +316,34 @@ impl Default for DstBlkId {
 }
 
 /// Destination block in router function.
-#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct DstBlk {
+    /// The identifier of destination block.
     pub id: DstBlkId,
+    /// The channel number.
     pub ch: u8,
 }
 
 /// Identifier of source block.
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum SrcBlkId {
+    /// AES, S/PDIF, and TOS outputs.
     Aes,
+    /// ADAT outputs.
     Adat,
+    /// mixer outputs.
     Mixer,
+    /// Analog outputs A (Inter IC sound interface).
     Ins0,
+    /// Analog outputs B (Inter IC sound interface).
     Ins1,
+    /// Standard AMBA 2.0 compliant APB interface.
     ArmAprAudio,
+    /// 1394 Audio video system A.
     Avs0,
+    /// 1394 Audio video system B.
     Avs1,
+    /// Discard audio signal.
     Mute,
     Reserved(u8),
 }
@@ -344,17 +355,22 @@ impl Default for SrcBlkId {
 }
 
 /// Source block in router function.
-#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct SrcBlk {
+    /// The identifier of source block.
     pub id: SrcBlkId,
+    /// The channel number.
     pub ch: u8,
 }
 
 /// Entry of route in router function.
-#[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
 pub struct RouterEntry {
+    /// Destination block.
     pub dst: DstBlk,
+    /// Source block.
     pub src: SrcBlk,
+    /// Detected level of audio signal.
     pub peak: u16,
 }
 
@@ -369,6 +385,26 @@ pub struct FormatEntry {
 
 /// The number of channels in stream format for AC3 channels.
 pub const AC3_CHANNELS: usize = 32;
+
+/// Serialize and deserialize parameters for variable length of data in extension section.
+pub trait TcatExtensionVariableLengthSectionParamsSerdes<T> {
+    /// Offset to operate.
+    const OFFSET: usize;
+
+    /// Detect size to operate.
+    fn detect_size(
+        req: &FwReq,
+        node: &FwNode,
+        sections: &ExtensionSections,
+        timeout_ms: u32,
+    ) -> Result<usize, Error>;
+
+    /// Serialize parameters.
+    fn serialize_extension_variable_params(params: &T, raw: &mut [u8]) -> Result<(), String>;
+
+    /// Deserialize parameters.
+    fn deserialize_extension_variable_params(params: &mut T, raw: &[u8]) -> Result<(), String>;
+}
 
 #[cfg(test)]
 mod test {

protocols/dice/src/tcat/extension/current_config_section.rs
@@ -48,8 +48,10 @@ impl CurrentConfigSectionProtocol {
             caps.router.maximum_entry_count as usize,
         );
 
-        read_router_entries(req, node, caps, offset + 4, entry_count, timeout_ms)
+        let mut entries = vec![RouterEntry::default(); entry_count];
+        read_router_entries(req, node, caps, offset + 4, &mut entries, timeout_ms)
             .map_err(|e| Error::new(ProtocolExtensionError::CurrentConfig, &e.to_string()))
+            .map(|_| entries)
     }
 
     pub fn read_current_stream_format_entries(

protocols/dice/src/tcat/extension/peak_section.rs
@@ -26,8 +26,17 @@ impl PeakSectionProtocol {
             ))?
         }
 
-        let entries = caps.router.maximum_entry_count as usize;
-        read_router_entries(req, node, caps, sections.peak.offset, entries, timeout_ms)
-            .map_err(|e| Error::new(ProtocolExtensionError::Peak, &e.to_string()))
+        let entry_count = caps.router.maximum_entry_count as usize;
+        let mut entries = vec![RouterEntry::default(); entry_count];
+        read_router_entries(
+            req,
+            node,
+            caps,
+            sections.peak.offset,
+            &mut entries,
+            timeout_ms,
+        )
+        .map_err(|e| Error::new(ProtocolExtensionError::Peak, &e.to_string()))
+        .map(|_| entries)
     }
 }

protocols/dice/src/tcat/extension/router_entry.rs
@@ -2,105 +2,96 @@
 // Copyright (c) 2020 Takashi Sakamoto
 use super::{caps_section::*, *};
 
-impl From<u8> for DstBlkId {
-    fn from(val: u8) -> Self {
-        match val {
-            0 => Self::Aes,
-            1 => Self::Adat,
-            2 => Self::MixerTx0,
-            3 => Self::MixerTx1,
-            4 => Self::Ins0,
-            5 => Self::Ins1,
-            10 => Self::ArmApbAudio,
-            11 => Self::Avs0,
-            12 => Self::Avs1,
-            _ => Self::Reserved(val),
-        }
-    }
-}
-
-impl From<DstBlkId> for u8 {
-    fn from(id: DstBlkId) -> Self {
-        match id {
-            DstBlkId::Aes => 0,
-            DstBlkId::Adat => 1,
-            DstBlkId::MixerTx0 => 2,
-            DstBlkId::MixerTx1 => 3,
-            DstBlkId::Ins0 => 4,
-            DstBlkId::Ins1 => 5,
-            DstBlkId::ArmApbAudio => 10,
-            DstBlkId::Avs0 => 11,
-            DstBlkId::Avs1 => 12,
-            DstBlkId::Reserved(val) => val,
-        }
-    }
-}
-
 impl DstBlk {
     const ID_MASK: u8 = 0xf0;
     const ID_SHIFT: usize = 4;
     const CH_MASK: u8 = 0x0f;
     const CH_SHIFT: usize = 0;
+
+    const AES_VALUE: u8 = 0;
+    const ADAT_VALUE: u8 = 1;
+    const MIXER_TX0_VALUE: u8 = 2;
+    const MIXER_TX1_VALUE: u8 = 3;
+    const INS0_VALUE: u8 = 4;
+    const INS1_VALUE: u8 = 5;
+    const ARM_APB_AUDIO_VALUE: u8 = 10;
+    const AVS0_VALUE: u8 = 11;
+    const AVS1_VALUE: u8 = 12;
+}
+
+fn serialize_dst_blk(dst: &DstBlk, val: &mut u8) -> Result<(), String> {
+    let id = match dst.id {
+        DstBlkId::Aes => DstBlk::AES_VALUE,
+        DstBlkId::Adat => DstBlk::ADAT_VALUE,
+        DstBlkId::MixerTx0 => DstBlk::MIXER_TX0_VALUE,
+        DstBlkId::MixerTx1 => DstBlk::MIXER_TX1_VALUE,
+        DstBlkId::Ins0 => DstBlk::INS0_VALUE,
+        DstBlkId::Ins1 => DstBlk::INS1_VALUE,
+        DstBlkId::ArmApbAudio => DstBlk::ARM_APB_AUDIO_VALUE,
+        DstBlkId::Avs0 => DstBlk::AVS0_VALUE,
+        DstBlkId::Avs1 => DstBlk::AVS1_VALUE,
+        DstBlkId::Reserved(id) => id,
+    };
+
+    let ch = dst.ch;
+
+    *val =
+        ((id << DstBlk::ID_SHIFT) & DstBlk::ID_MASK) | ((ch << DstBlk::CH_SHIFT) & DstBlk::CH_MASK);
+
+    Ok(())
+}
+
+fn deserialize_dst_blk(dst: &mut DstBlk, val: &u8) -> Result<(), String> {
+    let id = (*val & DstBlk::ID_MASK) >> DstBlk::ID_SHIFT;
+    dst.id = match id {
+        DstBlk::AES_VALUE => DstBlkId::Aes,
+        DstBlk::ADAT_VALUE => DstBlkId::Adat,
+        DstBlk::MIXER_TX0_VALUE => DstBlkId::MixerTx0,
+        DstBlk::MIXER_TX1_VALUE => DstBlkId::MixerTx1,
+        DstBlk::INS0_VALUE => DstBlkId::Ins0,
+        DstBlk::INS1_VALUE => DstBlkId::Ins1,
+        DstBlk::ARM_APB_AUDIO_VALUE => DstBlkId::ArmApbAudio,
+        DstBlk::AVS0_VALUE => DstBlkId::Avs0,
+        DstBlk::AVS1_VALUE => DstBlkId::Avs1,
+        _ => DstBlkId::Reserved(id),
+    };
+
+    dst.ch = (*val & DstBlk::CH_MASK) >> DstBlk::CH_SHIFT;
+
+    Ok(())
 }
 
 impl From<u8> for DstBlk {
     fn from(val: u8) -> Self {
-        DstBlk {
-            id: DstBlkId::from((val & DstBlk::ID_MASK) >> DstBlk::ID_SHIFT),
-            ch: (val & DstBlk::CH_MASK) >> DstBlk::CH_SHIFT,
-        }
+        let mut dst = DstBlk::default();
+        deserialize_dst_blk(&mut dst, &val).unwrap();
+        dst
     }
 }
 
 impl From<DstBlk> for u8 {
     fn from(blk: DstBlk) -> Self {
-        (u8::from(blk.id) << DstBlk::ID_SHIFT) | blk.ch
+        let mut val = 0u8;
+        serialize_dst_blk(&blk, &mut val).unwrap();
+        val
     }
 }
 
 impl Ord for DstBlk {
     fn cmp(&self, other: &Self) -> Ordering {
-        u8::from(*self).cmp(&u8::from(*other))
+        let mut lval = 0u8;
+        serialize_dst_blk(self, &mut lval).unwrap();
+
+        let mut rval = 0u8;
+        serialize_dst_blk(other, &mut rval).unwrap();
+
+        lval.cmp(&rval)
     }
 }
 
 impl PartialOrd for DstBlk {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(u8::from(*self).cmp(&u8::from(*other)))
-    }
-}
-
-impl From<u8> for SrcBlkId {
-    fn from(val: u8) -> Self {
-        match val {
-            0 => Self::Aes,
-            1 => Self::Adat,
-            2 => Self::Mixer,
-            4 => Self::Ins0,
-            5 => Self::Ins1,
-            10 => Self::ArmAprAudio,
-            11 => Self::Avs0,
-            12 => Self::Avs1,
-            15 => Self::Mute,
-            _ => Self::Reserved(val),
-        }
-    }
-}
-
-impl From<SrcBlkId> for u8 {
-    fn from(id: SrcBlkId) -> Self {
-        match id {
-            SrcBlkId::Aes => 0,
-            SrcBlkId::Adat => 1,
-            SrcBlkId::Mixer => 2,
-            SrcBlkId::Ins0 => 4,
-            SrcBlkId::Ins1 => 5,
-            SrcBlkId::ArmAprAudio => 10,
-            SrcBlkId::Avs0 => 11,
-            SrcBlkId::Avs1 => 12,
-            SrcBlkId::Mute => 15,
-            SrcBlkId::Reserved(val) => val,
-        }
+        Some(self.cmp(other))
     }
 }
 
@@ -109,84 +100,196 @@ impl SrcBlk {
     const ID_SHIFT: usize = 4;
     const CH_MASK: u8 = 0x0f;
     const CH_SHIFT: usize = 0;
+
+    const AES_VALUE: u8 = 0;
+    const ADAT_VALUE: u8 = 1;
+    const MIXER_VALUE: u8 = 2;
+    const INS0_VALUE: u8 = 4;
+    const INS1_VALUE: u8 = 5;
+    const ARMAPRAUDIO_VALUE: u8 = 10;
+    const AVS0_VALUE: u8 = 11;
+    const AVS1_VALUE: u8 = 12;
+    const MUTE_VALUE: u8 = 15;
+}
+
+fn serialize_src_blk(src: &SrcBlk, val: &mut u8) -> Result<(), String> {
+    let id = match src.id {
+        SrcBlkId::Aes => SrcBlk::AES_VALUE,
+        SrcBlkId::Adat => SrcBlk::ADAT_VALUE,
+        SrcBlkId::Mixer => SrcBlk::MIXER_VALUE,
+        SrcBlkId::Ins0 => SrcBlk::INS0_VALUE,
+        SrcBlkId::Ins1 => SrcBlk::INS1_VALUE,
+        SrcBlkId::ArmAprAudio => SrcBlk::ARMAPRAUDIO_VALUE,
+        SrcBlkId::Avs0 => SrcBlk::AVS0_VALUE,
+        SrcBlkId::Avs1 => SrcBlk::AVS1_VALUE,
+        SrcBlkId::Mute => SrcBlk::MUTE_VALUE,
+        SrcBlkId::Reserved(id) => id,
+    };
+
+    let ch = src.ch;
+
+    *val =
+        ((id << SrcBlk::ID_SHIFT) & SrcBlk::ID_MASK) | ((ch << SrcBlk::CH_SHIFT) & SrcBlk::CH_MASK);
+
+    Ok(())
+}
+
+fn deserialize_src_blk(src: &mut SrcBlk, val: &u8) -> Result<(), String> {
+    let id = (*val & SrcBlk::ID_MASK) >> SrcBlk::ID_SHIFT;
+    src.id = match id {
+        SrcBlk::AES_VALUE => SrcBlkId::Aes,
+        SrcBlk::ADAT_VALUE => SrcBlkId::Adat,
+        SrcBlk::MIXER_VALUE => SrcBlkId::Mixer,
+        SrcBlk::INS0_VALUE => SrcBlkId::Ins0,
+        SrcBlk::INS1_VALUE => SrcBlkId::Ins1,
+        SrcBlk::ARMAPRAUDIO_VALUE => SrcBlkId::ArmAprAudio,
+        SrcBlk::AVS0_VALUE => SrcBlkId::Avs0,
+        SrcBlk::AVS1_VALUE => SrcBlkId::Avs1,
+        SrcBlk::MUTE_VALUE => SrcBlkId::Mute,
+        _ => SrcBlkId::Reserved(id),
+    };
+
+    src.ch = (*val & SrcBlk::CH_MASK) >> SrcBlk::CH_SHIFT;
+
+    Ok(())
 }
 
 impl From<u8> for SrcBlk {
     fn from(val: u8) -> Self {
-        SrcBlk {
-            id: SrcBlkId::from((val & SrcBlk::ID_MASK) >> SrcBlk::ID_SHIFT),
-            ch: (val & SrcBlk::CH_MASK) >> SrcBlk::CH_SHIFT,
-        }
+        let mut src = SrcBlk::default();
+        deserialize_src_blk(&mut src, &val).unwrap();
+        src
     }
 }
 
 impl From<SrcBlk> for u8 {
-    fn from(blk: SrcBlk) -> Self {
-        (u8::from(blk.id) << SrcBlk::ID_SHIFT) | blk.ch
+    fn from(src: SrcBlk) -> Self {
+        let mut val = 0u8;
+        serialize_src_blk(&src, &mut val).unwrap();
+        val
     }
 }
 
 impl Ord for SrcBlk {
     fn cmp(&self, other: &Self) -> Ordering {
-        u8::from(*self).cmp(&u8::from(*other))
+        let mut lval = 0u8;
+        serialize_src_blk(self, &mut lval).unwrap();
+
+        let mut rval = 0u8;
+        serialize_src_blk(other, &mut rval).unwrap();
+
+        lval.cmp(&rval)
     }
 }
 
 impl PartialOrd for SrcBlk {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(u8::from(*self).cmp(&u8::from(*other)))
+        Some(self.cmp(other))
     }
 }
 
 impl RouterEntry {
     const SIZE: usize = 4;
 
-    const SRC_OFFSET: usize = 2;
-    const DST_OFFSET: usize = 3;
+    const DST_MASK: u32 = 0x000000ff;
+    const DST_SHIFT: usize = 0;
 
-    fn build(&self, raw: &mut [u8]) {
-        raw[Self::DST_OFFSET] = u8::from(self.dst);
-        raw[Self::SRC_OFFSET] = u8::from(self.src);
-        raw[..Self::SRC_OFFSET].copy_from_slice(&self.peak.to_be_bytes());
-    }
+    const SRC_MASK: u32 = 0x0000ff00;
+    const SRC_SHIFT: usize = 8;
 
-    fn parse(&mut self, raw: &[u8]) {
-        self.dst = raw[Self::DST_OFFSET].into();
-        self.src = raw[Self::SRC_OFFSET].into();
-        let mut doublet = [0; 2];
-        doublet.copy_from_slice(&raw[..Self::SRC_OFFSET]);
-        self.peak = u16::from_be_bytes(doublet);
-    }
+    const PEAK_MASK: u32 = 0xffff0000;
+    const PEAK_SHIFT: usize = 16;
 }
 
-pub fn read_router_entries(
+fn serialize_router_entry(entry: &RouterEntry, raw: &mut [u8]) -> Result<(), String> {
+    assert!(raw.len() >= RouterEntry::SIZE);
+
+    let mut dst_val = 0u8;
+    serialize_dst_blk(&entry.dst, &mut dst_val)?;
+
+    let mut src_val = 0u8;
+    serialize_src_blk(&entry.src, &mut src_val)?;
+
+    let val = (((entry.peak as u32) << RouterEntry::PEAK_SHIFT) & RouterEntry::PEAK_MASK)
+        | (((src_val as u32) << RouterEntry::SRC_SHIFT) & RouterEntry::SRC_MASK)
+        | (((dst_val as u32) << RouterEntry::DST_SHIFT) & RouterEntry::DST_MASK);
+    val.build_quadlet(raw);
+
+    Ok(())
+}
+
+fn deserialize_router_entry(entry: &mut RouterEntry, raw: &[u8]) -> Result<(), String> {
+    assert!(raw.len() >= RouterEntry::SIZE);
+
+    let mut val = 0u32;
+    val.parse_quadlet(raw);
+
+    let dst_val = ((val & RouterEntry::DST_MASK) >> RouterEntry::DST_SHIFT) as u8;
+    deserialize_dst_blk(&mut entry.dst, &dst_val)?;
+
+    let src_val = ((val & RouterEntry::SRC_MASK) >> RouterEntry::SRC_SHIFT) as u8;
+    deserialize_src_blk(&mut entry.src, &src_val)?;
+
+    entry.peak = ((val & RouterEntry::PEAK_MASK) >> RouterEntry::PEAK_SHIFT) as u16;
+
+    Ok(())
+}
+
+pub(crate) fn calculate_router_entries_size(entry_count: usize) -> usize {
+    entry_count * RouterEntry::SIZE
+}
+
+pub(crate) fn serialize_router_entries(
+    entries: &[RouterEntry],
+    raw: &mut [u8],
+) -> Result<(), String> {
+    assert!(raw.len() >= calculate_router_entries_size(entries.len()));
+
+    entries.iter().enumerate().try_for_each(|(i, entry)| {
+        let pos = i * RouterEntry::SIZE;
+        serialize_router_entry(entry, &mut raw[pos..(pos + RouterEntry::SIZE)])
+    })
+}
+
+pub(crate) fn deserialize_router_entries(
+    entries: &mut [RouterEntry],
+    raw: &[u8],
+) -> Result<(), String> {
+    assert!(raw.len() >= calculate_router_entries_size(entries.len()));
+
+    entries.iter_mut().enumerate().try_for_each(|(i, entry)| {
+        let pos = i * RouterEntry::SIZE;
+        deserialize_router_entry(entry, &raw[pos..(pos + RouterEntry::SIZE)])
+    })
+}
+
+// NOTE: In peak section, the number of router entries is not available.
+pub(crate) fn read_router_entries(
     req: &mut FwReq,
     node: &mut FwNode,
     caps: &ExtensionCaps,
     offset: usize,
-    entry_count: usize,
+    entries: &mut [RouterEntry],
     timeout_ms: u32,
-) -> Result<Vec<RouterEntry>, Error> {
-    if entry_count > caps.router.maximum_entry_count as usize {
+) -> Result<(), Error> {
+    if entries.len() > caps.router.maximum_entry_count as usize {
         let msg = format!(
             "Invalid entries to read: {} but greater than {}",
-            entry_count, caps.router.maximum_entry_count
+            entries.len(),
+            caps.router.maximum_entry_count
         );
         Err(Error::new(ProtocolExtensionError::RouterEntry, &msg))?
     }
 
-    let mut raw = vec![0; entry_count * RouterEntry::SIZE];
-    extension_read(req, node, offset, &mut raw, timeout_ms).map(|_| {
-        let mut entries = vec![RouterEntry::default(); entry_count];
-        entries.iter_mut().enumerate().for_each(|(i, entry)| {
-            let pos = i * 4;
-            entry.parse(&raw[pos..(pos + 4)]);
-        });
-        entries
-    })
+    let size = calculate_router_entries_size(entries.len());
+    let mut raw = vec![0; size];
+    extension_read(req, node, offset, &mut raw, timeout_ms)?;
+
+    deserialize_router_entries(entries, &raw)
+        .map_err(|cause| Error::new(ProtocolExtensionError::RouterEntry, &cause))
 }
 
-pub fn write_router_entries(
+pub(crate) fn write_router_entries(
     req: &mut FwReq,
     node: &mut FwNode,
     caps: &ExtensionCaps,
@@ -203,16 +306,12 @@ pub fn write_router_entries(
         Err(Error::new(ProtocolExtensionError::RouterEntry, &msg))?
     }
 
-    let mut data = [0; 4];
-    data.copy_from_slice(&(entries.len() as u32).to_be_bytes());
-    extension_write(req, node, offset, &mut data, timeout_ms)?;
-
-    let mut raw = vec![0; entries.len() * RouterEntry::SIZE];
-    entries.iter().enumerate().for_each(|(i, entry)| {
-        let pos = i * 4;
-        entry.build(&mut raw[pos..(pos + 4)]);
-    });
-    extension_write(req, node, offset + 4, &mut raw, timeout_ms)
+    let size = 4 + calculate_router_entries_size(entries.len());
+    let mut raw = vec![0; size];
+    (entries.len() as u32).build_quadlet(&mut raw[..4]);
+    serialize_router_entries(entries, &mut raw[4..])
+        .map_err(|cause| Error::new(ProtocolExtensionError::RouterEntry, &cause))?;
+    extension_write(req, node, offset, &mut raw, timeout_ms)
 }
 
 #[cfg(test)]

protocols/dice/src/tcat/extension/router_section.rs
@@ -28,15 +28,17 @@ impl RouterSectionProtocol {
             u32::from_be_bytes(data) as usize,
             caps.router.maximum_entry_count as usize,
         );
+        let mut entries = vec![RouterEntry::default(); entry_count];
         read_router_entries(
             req,
             node,
             caps,
             sections.router.offset + 4,
-            entry_count,
+            &mut entries,
             timeout_ms,
         )
         .map_err(|e| Error::new(ProtocolExtensionError::Router, &e.to_string()))
+        .map(|_| entries)
     }
 
     pub fn write_router_entries(

commit a135bd6286ea68f40ec982e716f0759502de203a

protocols/dice: tcat: remove trait added carelessfully
Fixes: 40cf0b3b3266 ("protocols/dice: tcat: code cleanup for router entries")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/tcat/extension.rs
@@ -387,26 +387,6 @@ pub struct FormatEntry {
 /// The number of channels in stream format for AC3 channels.
 pub const AC3_CHANNELS: usize = 32;
 
-/// Serialize and deserialize parameters for variable length of data in extension section.
-pub trait TcatExtensionVariableLengthSectionParamsSerdes<T> {
-    /// Offset to operate.
-    const OFFSET: usize;
-
-    /// Detect size to operate.
-    fn detect_size(
-        req: &FwReq,
-        node: &FwNode,
-        sections: &ExtensionSections,
-        timeout_ms: u32,
-    ) -> Result<usize, Error>;
-
-    /// Serialize parameters.
-    fn serialize_extension_variable_params(params: &T, raw: &mut [u8]) -> Result<(), String>;
-
-    /// Deserialize parameters.
-    fn deserialize_extension_variable_params(params: &mut T, raw: &[u8]) -> Result<(), String>;
-}
-
 #[cfg(test)]
 mod test {
     use super::*;