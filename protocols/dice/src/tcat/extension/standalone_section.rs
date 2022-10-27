alsa-project/snd-firewire-ctl-services

commit 0cd47dc8b0f18e92f56a394c281b8a9a7bddcad4

protocols/dice: tcat: add intermediate structure expression for standalone configuration parameters
It's helpful for runtime crate to have intermediate structure expression
for parameters of standalone configuration.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/tcat/extension/standalone_section.rs
@@ -137,9 +137,98 @@ impl From<WordClockParam> for [u8; 4] {
     }
 }
 
+/// Parameters in standalone configuration section.
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
+pub struct StandaloneParameters {
+    /// Source of sampling clock.
+    pub clock_source: ClockSource,
+    /// Mode of AES input at high rate.
+    pub aes_high_rate: bool,
+    /// Mode of ADAT input for supported rates.
+    pub adat_mode: AdatParam,
+    /// Mode of word clock input.
+    pub word_clock_param: WordClockParam,
+    /// Internally generated sampling clock.
+    pub internal_rate: ClockRate,
+}
+
 /// Protocol implementation of standalone section.
 #[derive(Default)]
 pub struct StandaloneSectionProtocol;
+
+const MIN_SIZE: usize = 20;
+
+fn serialize(params: &StandaloneParameters, raw: &mut [u8]) -> Result<(), String> {
+    assert!(raw.len() >= MIN_SIZE);
+
+    (u8::from(params.clock_source) as u32).build_quadlet(&mut raw[..4]);
+
+    (params.aes_high_rate as u32).build_quadlet(&mut raw[4..8]);
+
+    (match params.adat_mode {
+        AdatParam::Normal => 0x00u32,
+        AdatParam::SMUX2 => 0x01,
+        AdatParam::SMUX4 => 0x02,
+        AdatParam::Auto => 0x03,
+    })
+    .build_quadlet(&mut raw[8..12]);
+
+    let mut val = match params.word_clock_param.mode {
+        WordClockMode::Normal => 0x00u32,
+        WordClockMode::Low => 0x01,
+        WordClockMode::Middle => 0x02,
+        WordClockMode::High => 0x03,
+    };
+    if params.word_clock_param.rate.numerator < 1 || params.word_clock_param.rate.denominator < 1 {
+        let msg = format!(
+            "Invalid parameters for rate of word clock: {} / {}",
+            params.word_clock_param.rate.numerator, params.word_clock_param.rate.denominator
+        );
+        Err(msg)?;
+    }
+    val |= ((params.word_clock_param.rate.numerator as u32) - 1) << 4;
+    val |= ((params.word_clock_param.rate.denominator as u32) - 1) << 16;
+    val.build_quadlet(&mut raw[12..16]);
+
+    (u8::from(params.internal_rate) as u32).build_quadlet(&mut raw[16..20]);
+
+    Ok(())
+}
+
+fn deserialize(params: &mut StandaloneParameters, raw: &[u8]) -> Result<(), String> {
+    assert!(raw.len() >= MIN_SIZE);
+
+    let mut val = 0u32;
+
+    val.parse_quadlet(&raw[..4]);
+    params.clock_source = ClockSource::from(val as u8);
+
+    val.parse_quadlet(&raw[4..8]);
+    params.aes_high_rate = val > 0;
+
+    val.parse_quadlet(&raw[8..12]);
+    params.adat_mode = match val {
+        0x01 => AdatParam::SMUX2,
+        0x02 => AdatParam::SMUX4,
+        0x03 => AdatParam::Auto,
+        _ => AdatParam::Normal,
+    };
+
+    val.parse_quadlet(&raw[12..16]);
+    params.word_clock_param.mode = match val & 0x03 {
+        0x01 => WordClockMode::Low,
+        0x02 => WordClockMode::Middle,
+        0x03 => WordClockMode::High,
+        _ => WordClockMode::Normal,
+    };
+    params.word_clock_param.rate.numerator = 1 + ((val >> 4) & 0x0fff) as u16;
+    params.word_clock_param.rate.denominator = 1 + ((val >> 16) & 0xffff) as u16;
+
+    val.parse_quadlet(&raw[16..20]);
+    params.internal_rate = ClockRate::from(val as u8);
+
+    Ok(())
+}
 
 impl StandaloneSectionProtocol {
     const CLK_SRC_OFFSET: usize = 0x00;
@@ -147,6 +236,57 @@ impl StandaloneSectionProtocol {
     const ADAT_CFG_OFFSET: usize = 0x08;
     const WC_CFG_OFFSET: usize = 0x0c;
     const INTERNAL_CFG_OFFSET: usize = 0x10;
+
+    pub fn cache_standalone_params(
+        req: &mut FwReq,
+        node: &mut FwNode,
+        sections: &ExtensionSections,
+        params: &mut StandaloneParameters,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let mut raw = vec![0; sections.standalone.size];
+        extension_read(req, node, sections.standalone.offset, &mut raw, timeout_ms)
+            .map_err(|e| Error::new(ProtocolExtensionError::Standalone, &e.to_string()))?;
+
+        deserialize(params, &raw)
+            .map_err(|msg| Error::new(ProtocolExtensionError::Standalone, &msg))
+    }
+
+    pub fn update_standalone_params(
+        req: &mut FwReq,
+        node: &mut FwNode,
+        sections: &ExtensionSections,
+        params: &StandaloneParameters,
+        prev: &mut StandaloneParameters,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let mut new = vec![0; sections.standalone.size];
+        serialize(params, &mut new)
+            .map_err(|e| Error::new(ProtocolExtensionError::Standalone, &e.to_string()))?;
+
+        let mut old = vec![0; sections.standalone.size];
+        serialize(params, &mut old)
+            .map_err(|e| Error::new(ProtocolExtensionError::Standalone, &e.to_string()))?;
+
+        (0..sections.standalone.size)
+            .step_by(4)
+            .try_for_each(|pos| {
+                if new[pos] != old[pos] {
+                    extension_read(
+                        req,
+                        node,
+                        sections.standalone.offset + pos,
+                        &mut new[pos..(pos + 4)],
+                        timeout_ms,
+                    )
+                } else {
+                    Ok(())
+                }
+            })?;
+
+        deserialize(prev, &new)
+            .map_err(|e| Error::new(ProtocolExtensionError::Standalone, &e.to_string()))
+    }
 
     pub fn read_standalone_clock_source(
         req: &mut FwReq,
@@ -329,5 +469,35 @@ impl StandaloneSectionProtocol {
             timeout_ms,
         )
         .map_err(|e| Error::new(ProtocolExtensionError::Standalone, &e.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn standalone_params_serdes() {
+        let params = StandaloneParameters {
+            clock_source: ClockSource::Tdif,
+            aes_high_rate: true,
+            adat_mode: AdatParam::SMUX4,
+            word_clock_param: WordClockParam {
+                mode: WordClockMode::Middle,
+                rate: WordClockRate {
+                    numerator: 12,
+                    denominator: 7,
+                },
+            },
+            internal_rate: ClockRate::R88200,
+        };
+
+        let mut raw = [0u8; MIN_SIZE];
+        assert!(serialize(&params, &mut raw).is_ok());
+
+        let mut p = StandaloneParameters::default();
+        assert!(deserialize(&mut p, &raw).is_ok());
+
+        assert_eq!(params, p);
     }
 }

protocols/dice/src/bin/tcat-extension-parser.rs
@@ -241,25 +241,30 @@ fn print_standalone_config(
     sections: &ExtensionSections,
 ) -> Result<(), Error> {
     println!("Standalone configurations:");
-    let src =
-        StandaloneSectionProtocol::read_standalone_clock_source(req, node, sections, TIMEOUT_MS)?;
-    println!("  clock source: {}", clock_source_to_string(&src));
-    let mode =
-        StandaloneSectionProtocol::read_standalone_aes_high_rate(req, node, sections, TIMEOUT_MS)?;
-    println!("  AES high rate: {}", mode);
-    let mode =
-        StandaloneSectionProtocol::read_standalone_adat_mode(req, node, sections, TIMEOUT_MS)?;
-    println!("  ADAT mode: {}", mode);
-    let params = StandaloneSectionProtocol::read_standalone_word_clock_param(
-        req, node, sections, TIMEOUT_MS,
+    let mut params = StandaloneParameters::default();
+    StandaloneSectionProtocol::cache_standalone_params(
+        req,
+        node,
+        sections,
+        &mut params,
+        TIMEOUT_MS,
     )?;
     println!(
-        "  Word clock params: {}, {} / {}",
-        params.mode, params.rate.numerator, params.rate.denominator
+        "  clock source: {}",
+        clock_source_to_string(&params.clock_source)
     );
-    let rate =
-        StandaloneSectionProtocol::read_standalone_internal_rate(req, node, sections, TIMEOUT_MS)?;
-    println!("  Internal rate: {}", clock_rate_to_string(&rate));
+    println!("  AES high rate: {}", params.aes_high_rate);
+    println!("  ADAT mode: {}", params.adat_mode);
+    println!(
+        "  Word clock params: {}, {} / {}",
+        params.word_clock_param.mode,
+        params.word_clock_param.rate.numerator,
+        params.word_clock_param.rate.denominator
+    );
+    println!(
+        "  Internal rate: {}",
+        clock_rate_to_string(&params.internal_rate)
+    );
     Ok(())
 }
 

commit 34a8d85441b6fd7ed7e46dad6997ce6baf63ef0b

protocols/dice: tcat: fix wrong operation to update for standalone configuration in extension section
Fixes: 0cd47dc8b0f ("protocols/dice: tcat: add intermediate structure expression for standalone configuration parameters")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/tcat/extension/standalone_section.rs
@@ -186,7 +186,7 @@ impl StandaloneSectionProtocol {
             .step_by(4)
             .try_for_each(|pos| {
                 if new[pos] != old[pos] {
-                    extension_read(
+                    extension_write(
                         req,
                         node,
                         sections.standalone.offset + pos,