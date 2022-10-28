3ff0abbeb76536b5de9946406f92a6ce650543c5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-10-28 22:39:39 +0900

    protocols/dice: tcat: implement trait to cache peak parameters in extension section
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/dice/src/tcat/extension/peak_section.rs
+++ protocols/dice/src/tcat/extension/peak_section.rs
@@ -7,6 +7,40 @@
 //! in protocol extension defined by TCAT for ASICs of DICE.
 use super::{caps_section::*, router_entry::*, *};
 
+/// Parameters of meter detections in peak section.
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+pub struct PeakParams(pub RouterParams);
+
+impl<O: TcatExtensionOperation> TcatExtensionSectionParamsOperation<PeakParams> for O {
+    fn cache_extension_whole_params(
+        req: &FwReq,
+        node: &FwNode,
+        sections: &ExtensionSections,
+        caps: &ExtensionCaps,
+        params: &mut PeakParams,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        if !caps.general.peak_avail {
+            Err(Error::new(
+                ProtocolExtensionError::Peak,
+                "Peak is not available",
+            ))?
+        }
+
+        params
+            .0
+             .0
+            .resize_with(caps.router.maximum_entry_count as usize, Default::default);
+        let size = calculate_router_entries_size(params.0 .0.len());
+        let mut raw = vec![0u8; size];
+
+        Self::read_extension(req, node, &sections.peak, 0, &mut raw, timeout_ms)?;
+
+        deserialize_router_entries(&mut params.0 .0, &raw)
+            .map_err(|cause| Error::new(ProtocolExtensionError::Peak, &cause))
+    }
+}
+
 /// Protocol implementation of peak section.
 #[derive(Default)]
 pub struct PeakSectionProtocol;
