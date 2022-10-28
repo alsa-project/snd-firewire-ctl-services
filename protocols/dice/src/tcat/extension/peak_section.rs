11cefaa8e363a2b87f53e526fc772fe6bb0f33e5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-10-28 22:39:39 +0900

    protocols/dice: tcat: remove unused method to cache peak parameters in extension section
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/dice/src/tcat/extension/peak_section.rs
+++ protocols/dice/src/tcat/extension/peak_section.rs
@@ -40,35 +40,3 @@ impl<O: TcatExtensionOperation> TcatExtensionSectionParamsOperation<PeakParams> 
             .map_err(|cause| Error::new(ProtocolExtensionError::Peak, &cause))
     }
 }
-
-/// Protocol implementation of peak section.
-#[derive(Default)]
-pub struct PeakSectionProtocol;
-
-impl PeakSectionProtocol {
-    /// Cache state of hardware for peak entries.
-    pub fn cache_peak_whole_entries(
-        req: &mut FwReq,
-        node: &mut FwNode,
-        sections: &ExtensionSections,
-        caps: &ExtensionCaps,
-        entries: &mut Vec<RouterEntry>,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
-        if !caps.general.peak_avail {
-            Err(Error::new(
-                ProtocolExtensionError::Peak,
-                "Peak is not available",
-            ))?
-        }
-
-        entries.resize_with(caps.router.maximum_entry_count as usize, Default::default);
-        let size = calculate_router_entries_size(entries.len());
-        let mut raw = vec![0u8; size];
-
-        extension_read(req, node, sections.peak.offset, &mut raw, timeout_ms)?;
-
-        deserialize_router_entries(entries, &raw)
-            .map_err(|cause| Error::new(ProtocolExtensionError::Peak, &cause))
-    }
-}
