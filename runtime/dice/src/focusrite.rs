alsa-project/snd-firewire-ctl-services

commit 4a202c93d555c3fda9074e0c1c3e00c70e3bd5fa

runtime/dice: focusrite: use trait implementation to notified specific parameters in extension section
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/dice/src/focusrite.rs
@@ -10,10 +10,7 @@ pub mod spro40_model;
 
 use {
     super::{tcd22xx_ctl::*, *},
-    protocols::{
-        focusrite::*,
-        tcat::extension::{appl_section::*, *},
-    },
+    protocols::{focusrite::*, tcat::extension::*},
     std::marker::PhantomData,
 };
 
@@ -31,14 +28,14 @@ where
     T: SaffireproOutGroupSpecification
         + TcatExtensionSectionParamsOperation<OutGroupState>
         + TcatExtensionSectionPartialMutableParamsOperation<OutGroupState>
-        + TcatApplSectionNotifiedParamsOperation<OutGroupState>;
+        + TcatExtensionSectionNotifiedParamsOperation<OutGroupState>;
 
 impl<T> Default for OutGroupCtl<T>
 where
     T: SaffireproOutGroupSpecification
         + TcatExtensionSectionParamsOperation<OutGroupState>
         + TcatExtensionSectionPartialMutableParamsOperation<OutGroupState>
-        + TcatApplSectionNotifiedParamsOperation<OutGroupState>,
+        + TcatExtensionSectionNotifiedParamsOperation<OutGroupState>,
 {
     fn default() -> Self {
         Self(
@@ -54,7 +51,7 @@ where
     T: SaffireproOutGroupSpecification
         + TcatExtensionSectionParamsOperation<OutGroupState>
         + TcatExtensionSectionPartialMutableParamsOperation<OutGroupState>
-        + TcatApplSectionNotifiedParamsOperation<OutGroupState>,
+        + TcatExtensionSectionNotifiedParamsOperation<OutGroupState>,
 {
     const LEVEL_MIN: i32 = 0x00;
     const LEVEL_MAX: i32 = 0x7f;
@@ -307,10 +304,11 @@ where
         req: &mut FwReq,
         node: &mut FwNode,
         sections: &ExtensionSections,
+        caps: &ExtensionCaps,
         msg: u32,
         timeout_ms: u32,
     ) -> Result<(), Error> {
-        T::cache_appl_notified_params(req, node, sections, &mut self.0, msg, timeout_ms)
+        T::cache_extension_notified_params(req, node, sections, caps, &mut self.0, msg, timeout_ms)
     }
 }
 

runtime/dice/src/focusrite/liquids56_model.rs
@@ -204,6 +204,7 @@ impl NotifyModel<(SndDice, FwNode), u32> for LiquidS56Model {
             &mut self.req,
             &mut unit.1,
             &self.extension_sections,
+            &self.tcd22xx_ctls.caps,
             *msg,
             TIMEOUT_MS,
         )?;

runtime/dice/src/focusrite/spro24_model.rs
@@ -177,6 +177,7 @@ impl NotifyModel<(SndDice, FwNode), u32> for SPro24Model {
             &mut self.req,
             &mut unit.1,
             &self.extension_sections,
+            &self.tcd22xx_ctls.caps,
             *msg,
             TIMEOUT_MS,
         )?;

runtime/dice/src/focusrite/spro24dsp_model.rs
@@ -270,6 +270,7 @@ impl NotifyModel<(SndDice, FwNode), u32> for SPro24DspModel {
             &mut self.req,
             &mut unit.1,
             &self.extension_sections,
+            &self.tcd22xx_ctls.caps,
             *msg,
             TIMEOUT_MS,
         )?;

runtime/dice/src/focusrite/spro26_model.rs
@@ -155,6 +155,7 @@ impl NotifyModel<(SndDice, FwNode), u32> for SPro26Model {
             &mut self.req,
             &mut unit.1,
             &self.extension_sections,
+            &self.tcd22xx_ctls.caps,
             *msg,
             TIMEOUT_MS,
         )?;

runtime/dice/src/focusrite/spro40_model.rs
@@ -177,6 +177,7 @@ impl NotifyModel<(SndDice, FwNode), u32> for SPro40Model {
             &mut self.req,
             &mut unit.1,
             &self.extension_sections,
+            &self.tcd22xx_ctls.caps,
             *msg,
             TIMEOUT_MS,
         )?;

commit cb9a7b7c644d269ee939b947830d7964fa0b8268

runtime/dice: focusrite: add missing logging for output group control at notified
I missed it.

Fixes: 4a202c93d55 ("runtime/dice: focusrite: use trait implementation to notified specific parameters in extension section")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/dice/src/focusrite.rs
@@ -308,7 +308,9 @@ where
         msg: u32,
         timeout_ms: u32,
     ) -> Result<(), Error> {
-        T::cache_extension_notified_params(req, node, sections, caps, &mut self.0, msg, timeout_ms)
+        let res = T::cache_extension_notified_params(req, node, sections, caps, &mut self.0, msg, timeout_ms);
+        debug!(params = ?self.0, ?res);
+        res
     }
 }
 