6ede9ace6a8d9b9f3338b54622edefca8b6463cf
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-11-02 12:38:06 +0900

    protocols/oxfw: loud: remove unused methods from protocol implementation
    This commit removes unused implementations.

--- protocols/oxfw/src/loud.rs
+++ protocols/oxfw/src/loud.rs
@@ -9,9 +9,11 @@
 use super::*;
 
 /// Source of capture.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum LinkFwInputSource {
+    /// Analog inputs.
     Analog,
+    /// Digital inputs.
     Digital,
 }
 
@@ -24,6 +26,23 @@ impl Default for LinkFwInputSource {
 /// The protocol implementation for Tapco Link.FireWire 4x6.
 #[derive(Default, Debug)]
 pub struct LinkFwProtocol;
+
+impl LinkFwProtocol {
+    const SIG_DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
+        subunit: AvcAddrSubunit {
+            subunit_type: AvcSubunitType::Audio,
+            subunit_id: 0,
+        },
+        plug_id: 1,
+    });
+
+    const SIG_SRCS: [SignalAddr; 2] = [
+        // Analog inputs.
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x00)),
+        // Digital inputs.
+        SignalAddr::Unit(SignalUnitAddr::Ext(0x01)),
+    ];
+}
 
 impl OxfwFcpParamsOperation<OxfwAvc, LinkFwInputSource> for LinkFwProtocol {
     fn cache(
@@ -67,56 +86,5 @@ impl OxfwFcpMutableParamsOperation<OxfwAvc, LinkFwInputSource> for LinkFwProtoco
         }
         *prev = *params;
         Ok(())
-    }
-}
-
-impl LinkFwProtocol {
-    const SIG_DST: SignalAddr = SignalAddr::Subunit(SignalSubunitAddr {
-        subunit: AvcAddrSubunit {
-            subunit_type: AvcSubunitType::Audio,
-            subunit_id: 0,
-        },
-        plug_id: 1,
-    });
-
-    const SIG_SRCS: [SignalAddr; 2] = [
-        SignalAddr::Unit(SignalUnitAddr::Ext(0x00)),
-        SignalAddr::Unit(SignalUnitAddr::Ext(0x01)),
-    ];
-
-    pub fn read_input_source(
-        avc: &mut OxfwAvc,
-        src: &mut LinkFwInputSource,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
-        let mut op = SignalSource::new(&Self::SIG_DST);
-        avc.status(&AvcAddr::Unit, &mut op, timeout_ms).map(|_| {
-            let pos = Self::SIG_SRCS
-                .iter()
-                .position(|src| src.eq(&op.src))
-                .unwrap();
-            *src = if pos > 0 {
-                LinkFwInputSource::Digital
-            } else {
-                LinkFwInputSource::Analog
-            };
-        })
-    }
-
-    pub fn write_input_source(
-        avc: &mut OxfwAvc,
-        src: LinkFwInputSource,
-        timeout_ms: u32,
-    ) -> Result<(), Error> {
-        let pos = if src == LinkFwInputSource::Digital {
-            1
-        } else {
-            0
-        };
-        let mut op = SignalSource {
-            src: Self::SIG_SRCS[pos],
-            dst: Self::SIG_DST,
-        };
-        avc.control(&AvcAddr::Unit, &mut op, timeout_ms)
     }
 }
