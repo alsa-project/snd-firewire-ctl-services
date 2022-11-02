4b7ccc130898eab878f2cc487287d59f954024b5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-11-02 11:54:33 +0900

    runtime/oxfw: loud: use new trait implementation for soruce of capture stream
    This commit uses new implementation for source of capture stream.

--- runtime/oxfw/src/loud_model.rs
+++ runtime/oxfw/src/loud_model.rs
@@ -20,6 +20,8 @@ impl CtlModel<(SndUnit, FwNode)> for LinkFwModel {
     ) -> Result<(), Error> {
         self.avc.bind(&unit.1)?;
 
+        self.specific_ctl.cache(&mut self.avc, FCP_TIMEOUT_MS)?;
+
         self.common_ctl.load(&self.avc, card_cntr, FCP_TIMEOUT_MS)?;
         self.specific_ctl.load(card_cntr)?;
 
@@ -37,10 +39,7 @@ impl CtlModel<(SndUnit, FwNode)> for LinkFwModel {
             .read(&self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)?
         {
             Ok(true)
-        } else if self
-            .specific_ctl
-            .read(&mut self.avc, elem_id, elem_value, FCP_TIMEOUT_MS)?
-        {
+        } else if self.specific_ctl.read(elem_id, elem_value)? {
             Ok(true)
         } else {
             Ok(false)
@@ -91,7 +90,7 @@ impl NotifyModel<(SndUnit, FwNode), bool> for LinkFwModel {
 }
 
 #[derive(Default, Debug)]
-struct SpecificCtl;
+struct SpecificCtl(LinkFwInputSource);
 
 fn input_source_to_str(src: &LinkFwInputSource) -> &str {
     match src {
@@ -105,6 +104,10 @@ const CAPTURE_SOURCE_NAME: &str = "capture-source";
 impl SpecificCtl {
     const SRCS: [LinkFwInputSource; 2] = [LinkFwInputSource::Analog, LinkFwInputSource::Digital];
 
+    fn cache(&mut self, avc: &mut OxfwAvc, timeout_ms: u32) -> Result<(), Error> {
+        LinkFwProtocol::cache(avc, &mut self.0, timeout_ms)
+    }
+
     fn load(&self, card_cntr: &mut CardCntr) -> Result<(), Error> {
         let labels: Vec<&str> = Self::SRCS.iter().map(|s| input_source_to_str(s)).collect();
         let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, CAPTURE_SOURCE_NAME, 0);
@@ -113,19 +116,11 @@ impl SpecificCtl {
             .map(|_| ())
     }
 
-    fn read(
-        &self,
-        avc: &mut OxfwAvc,
-        elem_id: &ElemId,
-        elem_value: &mut ElemValue,
-        timeout_ms: u32,
-    ) -> Result<bool, Error> {
+    fn read(&self, elem_id: &ElemId, elem_value: &mut ElemValue) -> Result<bool, Error> {
         match elem_id.name().as_str() {
             CAPTURE_SOURCE_NAME => {
-                let mut src = LinkFwInputSource::default();
-                LinkFwProtocol::read_input_source(avc, &mut src, timeout_ms)?;
-                let idx = Self::SRCS.iter().position(|src| src.eq(&src)).unwrap();
-                elem_value.set_enum(&[idx as u32]);
+                let pos = Self::SRCS.iter().position(|s| self.0.eq(s)).unwrap();
+                elem_value.set_enum(&[pos as u32]);
                 Ok(true)
             }
             _ => Ok(false),
@@ -133,7 +128,7 @@ impl SpecificCtl {
     }
 
     fn write(
-        &self,
+        &mut self,
         avc: &mut OxfwAvc,
         elem_id: &ElemId,
         elem_value: &ElemValue,
@@ -141,12 +136,16 @@ impl SpecificCtl {
     ) -> Result<bool, Error> {
         match elem_id.name().as_str() {
             CAPTURE_SOURCE_NAME => {
-                let val = elem_value.enumerated()[0];
-                let &src = Self::SRCS.iter().nth(val as usize).ok_or_else(|| {
-                    let msg = format!("Invalid value for index of signal source: {}", val);
-                    Error::new(FileError::Inval, &msg)
-                })?;
-                LinkFwProtocol::write_input_source(avc, src, timeout_ms).map(|_| true)
+                let pos = elem_value.enumerated()[0] as usize;
+                let src = Self::SRCS
+                    .iter()
+                    .nth(pos)
+                    .ok_or_else(|| {
+                        let msg = format!("Invalid value for index of signal source: {}", pos);
+                        Error::new(FileError::Inval, &msg)
+                    })
+                    .copied()?;
+                LinkFwProtocol::update(avc, &src, &mut self.0, timeout_ms).map(|_| true)
             }
             _ => Ok(false),
         }
