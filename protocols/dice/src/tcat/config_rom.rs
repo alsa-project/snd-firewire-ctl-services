alsa-project/snd-firewire-ctl-services

commit 3cd49cd8619900dd951536ee18a56351f1ab582d

protocols/dice: tcat fulfill doc comment for configuration ROM parser
For better documentation.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/tcat/config_rom.rs
@@ -11,31 +11,44 @@ use ieee1212_config_rom::*;
 /// Identifier in bus information block of Configuration ROM.
 #[derive(Default, Debug, Clone, Copy)]
 pub struct Identifier {
+    /// The numeric identifier of vendor, usually Organizationally Unique Identifier (OUI)
+    /// registered in IEEE.
     pub vendor_id: u32,
+    /// The numeric value of category.
     pub category: u8,
+    /// The numeric identifier of product.
     pub product_id: u16,
+    /// The serial number.
     pub serial: u32,
 }
 
 /// Data in root directory block of Configuration ROM.
 #[derive(Default, Debug, Clone, Copy)]
 pub struct RootData<'a> {
+    /// The numeric identifier of vendor, usually Organizationally Unique Identifier (OUI)
+    /// registered in IEEE.
     pub vendor_id: u32,
+    /// The name of vendor.
     pub vendor_name: &'a str,
+    /// The numeric identifier of product.
     pub product_id: u32,
+    /// The name of product.
     pub product_name: &'a str,
 }
 
 /// Data in unit directory block of Configuration ROM.
 #[derive(Default, Debug, Clone, Copy)]
 pub struct UnitData<'a> {
+    /// The numeric identifier of model.
     pub model_id: u32,
+    /// The name of model.
     pub model_name: &'a str,
+    /// The specifier identifier.
     pub specifier_id: u32,
+    /// Version.
     pub version: u32,
 }
 
-pub trait DiceConfigRom<'a> {
     const VENDOR_ID_MASK: u32 = 0xffffff00;
     const VENDOR_ID_SHIFT: usize = 8;
     const CATEGORY_MASK: u32 = 0x000000ff;
@@ -45,8 +58,13 @@ pub trait DiceConfigRom<'a> {
     const SERIAL_MASK: u32 = 0x003fffff;
     const SERIAL_SHIFT: usize = 0;
 
+/// Parser of configuration ROM which has layout specific to DICE.
+pub trait DiceConfigRom<'a> {
+    /// Get identifier.
     fn get_identifier(&self) -> Option<Identifier>;
+    /// Get data in root directory.
     fn get_root_data(&'a self) -> Option<RootData<'a>>;
+    /// Get data in unit directory.
     fn get_unit_data(&'a self) -> Option<UnitData<'a>>;
 }
 
@@ -59,13 +77,13 @@ impl<'a> DiceConfigRom<'a> for ConfigRom<'a> {
 
             quadlet.copy_from_slice(&self.bus_info[8..12]);
             let val = u32::from_be_bytes(quadlet);
-            let vendor_id = (val & Self::VENDOR_ID_MASK) >> Self::VENDOR_ID_SHIFT;
-            let category = ((val & Self::CATEGORY_MASK) >> Self::CATEGORY_SHIFT) as u8;
+            let vendor_id = (val & VENDOR_ID_MASK) >> VENDOR_ID_SHIFT;
+            let category = ((val & CATEGORY_MASK) >> CATEGORY_SHIFT) as u8;
 
             quadlet.copy_from_slice(&self.bus_info[12..16]);
             let val = u32::from_be_bytes(quadlet);
-            let product_id = ((val & Self::PRODUCT_ID_MASK) >> Self::PRODUCT_ID_SHIFT) as u16;
-            let serial = (val & Self::SERIAL_MASK) >> Self::SERIAL_SHIFT;
+            let product_id = ((val & PRODUCT_ID_MASK) >> PRODUCT_ID_SHIFT) as u16;
+            let serial = (val & SERIAL_MASK) >> SERIAL_SHIFT;
 
             Some(Identifier {
                 vendor_id,

commit 3dbed8ab071ddd9b3fb7bd90f1ba939c20ea76c9

protocols/dice: config_rom: fix indentation
Fixes: 3cd49cd8619 ("protocols/dice: tcat fulfill doc comment for configuration ROM parser")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/tcat/config_rom.rs
@@ -49,14 +49,14 @@ pub struct UnitData<'a> {
     pub version: u32,
 }
 
-    const VENDOR_ID_MASK: u32 = 0xffffff00;
-    const VENDOR_ID_SHIFT: usize = 8;
-    const CATEGORY_MASK: u32 = 0x000000ff;
-    const CATEGORY_SHIFT: usize = 0;
-    const PRODUCT_ID_MASK: u32 = 0xffc00000;
-    const PRODUCT_ID_SHIFT: usize = 22;
-    const SERIAL_MASK: u32 = 0x003fffff;
-    const SERIAL_SHIFT: usize = 0;
+const VENDOR_ID_MASK: u32 = 0xffffff00;
+const VENDOR_ID_SHIFT: usize = 8;
+const CATEGORY_MASK: u32 = 0x000000ff;
+const CATEGORY_SHIFT: usize = 0;
+const PRODUCT_ID_MASK: u32 = 0xffc00000;
+const PRODUCT_ID_SHIFT: usize = 22;
+const SERIAL_MASK: u32 = 0x003fffff;
+const SERIAL_SHIFT: usize = 0;
 
 /// Parser of configuration ROM which has layout specific to DICE.
 pub trait DiceConfigRom<'a> {