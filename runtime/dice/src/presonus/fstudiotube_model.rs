78fc18d808278046d505bd3edbc587444c08b63a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-01-06 12:40:50 +0900

    runtime/dice: add support for FireStudio Tube
    This commit adds support for FireStudio Tube.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ runtime/dice/src/presonus/fstudiotube_model.rs
@@ -0,0 +1,189 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Takashi Sakamoto
+
+use {
+    super::{tcd22xx_ctl::*, *},
+    protocols::{presonus::fstudiotube::*, tcat::extension::*},
+};
+
+#[derive(Default)]
+pub struct FStudioTubeModel {
+    req: FwReq,
+    sections: GeneralSections,
+    extension_sections: ExtensionSections,
+    common_ctl: CommonCtl<FStudioTubeProtocol>,
+    tcd22xx_ctls: Tcd22xxCtls<FStudioTubeProtocol>,
+}
+
+const TIMEOUT_MS: u32 = 20;
+
+impl FStudioTubeModel {
+    pub fn cache(&mut self, unit: &mut (SndDice, FwNode)) -> Result<(), Error> {
+        FStudioTubeProtocol::read_general_sections(
+            &self.req,
+            &unit.1,
+            &mut self.sections,
+            TIMEOUT_MS,
+        )?;
+
+        self.common_ctl
+            .cache_whole_params(&self.req, &unit.1, &mut self.sections, TIMEOUT_MS)?;
+
+        FStudioTubeProtocol::read_extension_sections(
+            &self.req,
+            &unit.1,
+            &mut self.extension_sections,
+            TIMEOUT_MS,
+        )?;
+
+        self.tcd22xx_ctls.cache_whole_params(
+            &mut self.req,
+            &mut unit.1,
+            &self.extension_sections,
+            &self.common_ctl.global_params,
+            TIMEOUT_MS,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn store_configuration(&mut self, node: &FwNode) -> Result<(), Error> {
+        self.tcd22xx_ctls.store_configuration(
+            &mut self.req,
+            node,
+            &self.extension_sections,
+            TIMEOUT_MS,
+        )
+    }
+}
+
+impl CtlModel<(SndDice, FwNode)> for FStudioTubeModel {
+    fn load(&mut self, _: &mut (SndDice, FwNode), card_cntr: &mut CardCntr) -> Result<(), Error> {
+        self.common_ctl.load(card_cntr)?;
+
+        self.tcd22xx_ctls.load(card_cntr)?;
+
+        Ok(())
+    }
+
+    fn read(
+        &mut self,
+        _: &mut (SndDice, FwNode),
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        if self.common_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.tcd22xx_ctls.read(elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    fn write(
+        &mut self,
+        unit: &mut (SndDice, FwNode),
+        elem_id: &ElemId,
+        _: &ElemValue,
+        new: &ElemValue,
+    ) -> Result<bool, Error> {
+        if self.common_ctl.write(
+            &unit.0,
+            &self.req,
+            &unit.1,
+            &mut self.sections,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else if self.tcd22xx_ctls.write(
+            &mut self.req,
+            &mut unit.1,
+            &self.extension_sections,
+            elem_id,
+            new,
+            TIMEOUT_MS,
+        )? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl NotifyModel<(SndDice, FwNode), u32> for FStudioTubeModel {
+    fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
+        elem_id_list.extend_from_slice(&self.common_ctl.notified_elem_id_list);
+        elem_id_list.extend_from_slice(&self.tcd22xx_ctls.notified_elem_id_list);
+    }
+
+    fn parse_notification(&mut self, unit: &mut (SndDice, FwNode), msg: &u32) -> Result<(), Error> {
+        self.common_ctl.parse_notification(
+            &self.req,
+            &unit.1,
+            &mut self.sections,
+            *msg,
+            TIMEOUT_MS,
+        )?;
+        self.tcd22xx_ctls.parse_notification(
+            &mut self.req,
+            &mut unit.1,
+            &self.extension_sections,
+            &self.common_ctl.global_params,
+            TIMEOUT_MS,
+            *msg,
+        )?;
+        Ok(())
+    }
+
+    fn read_notified_elem(
+        &mut self,
+        _: &(SndDice, FwNode),
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        if self.common_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.tcd22xx_ctls.read(elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl MeasureModel<(SndDice, FwNode)> for FStudioTubeModel {
+    fn get_measure_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
+        elem_id_list.extend_from_slice(&self.common_ctl.measured_elem_id_list);
+        elem_id_list.extend_from_slice(&self.tcd22xx_ctls.measured_elem_id_list);
+    }
+
+    fn measure_states(&mut self, unit: &mut (SndDice, FwNode)) -> Result<(), Error> {
+        self.common_ctl
+            .cache_partial_params(&self.req, &unit.1, &mut self.sections, TIMEOUT_MS)?;
+        self.tcd22xx_ctls.cache_partial_params(
+            &mut self.req,
+            &mut unit.1,
+            &self.extension_sections,
+            TIMEOUT_MS,
+        )?;
+        Ok(())
+    }
+
+    fn measure_elem(
+        &mut self,
+        _: &(SndDice, FwNode),
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        if self.common_ctl.read(elem_id, elem_value)? {
+            Ok(true)
+        } else if self.tcd22xx_ctls.read(elem_id, elem_value)? {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+}
