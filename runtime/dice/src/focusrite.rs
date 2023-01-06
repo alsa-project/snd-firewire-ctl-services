alsa-project/snd-firewire-ctl-services

commit cb9a7b7c644d269ee939b947830d7964fa0b8268

runtime/dice: focusrite: add missing logging for output group control at notified
I missed it.

Fixes: 4a202c93d55 ("runtime/dice: focusrite: use trait implementation to notified specific parameters in extension section")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/dice/src/focusrite.rs
@@ -308,7 +308,9 @@ where
         msg: u32,
         timeout_ms: u32,
     ) -> Result<(), Error> {
-        T::cache_extension_notified_params(req, node, sections, caps, &mut self.0, msg, timeout_ms)
+        let res = T::cache_extension_notified_params(req, node, sections, caps, &mut self.0, msg, timeout_ms);
+        debug!(params = ?self.0, ?res);
+        res
     }
 }
 

commit 84f701b6763b159352b67e9507da3a350e904451

runtime/dice: fix code style according to standard format
Fixes: cb9a7b7c644d ("runtime/dice: focusrite: add missing logging for output group control at notified")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/dice/src/focusrite.rs
@@ -304,7 +304,15 @@ where
         msg: u32,
         timeout_ms: u32,
     ) -> Result<(), Error> {
-        let res = T::cache_extension_notified_params(req, node, sections, caps, &mut self.0, msg, timeout_ms);
+        let res = T::cache_extension_notified_params(
+            req,
+            node,
+            sections,
+            caps,
+            &mut self.0,
+            msg,
+            timeout_ms,
+        );
         debug!(params = ?self.0, ?res);
         res
     }