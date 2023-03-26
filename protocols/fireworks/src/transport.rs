ef3c6c7d680b7a00c9fb1714af7fb4d053fbb91c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-03-27 07:14:29 +0900

    protocols/fireworks: implement update traits for tx packet format
    This commit implements the traits to update the format of tx packet.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- protocols/fireworks/src/transport.rs
+++ protocols/fireworks/src/transport.rs
@@ -13,6 +13,7 @@ const CATEGORY_TRANSPORT: u32 = 2;
 const CMD_SET_TRANSMIT_MODE: u32 = 0;
 
 /// The format of packet in transmission.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum TxPacketFormat {
     /// Unique.
     Unique,
@@ -20,19 +21,54 @@ pub enum TxPacketFormat {
     Iec61883,
 }
 
-impl From<TxPacketFormat> for u32 {
-    fn from(fmt: TxPacketFormat) -> Self {
-        match fmt {
-            TxPacketFormat::Unique => 0,
-            TxPacketFormat::Iec61883 => 1,
-        }
+impl Default for TxPacketFormat {
+    fn default() -> Self {
+        Self::Iec61883
+    }
+}
+
+fn serialize_tx_packet_format(fmt: &TxPacketFormat) -> u32 {
+    match fmt {
+        TxPacketFormat::Unique => 0,
+        TxPacketFormat::Iec61883 => 1,
+    }
+}
+
+#[cfg(test)]
+fn deserialize_tx_packet_format(fmt: &mut TxPacketFormat, val: u32) {
+    *fmt = match val {
+        1 => TxPacketFormat::Iec61883,
+        _ => TxPacketFormat::Unique,
+    };
+}
+
+impl<O, P> EfwWhollyUpdatableParamsOperation<P, TxPacketFormat> for O
+where
+    O: EfwHardwareSpecification,
+    P: EfwProtocolExtManual,
+{
+    fn update_wholly(proto: &mut P, states: &TxPacketFormat, timeout_ms: u32) -> Result<(), Error> {
+        assert!(Self::CAPABILITIES
+            .iter()
+            .find(|cap| HwCap::ChangeableRespAddr.eq(cap))
+            .is_some());
+
+        let args = [serialize_tx_packet_format(states)];
+        let mut params = Vec::new();
+        proto.transaction(
+            CATEGORY_TRANSPORT,
+            CMD_SET_TRANSMIT_MODE,
+            &args,
+            &mut params,
+            timeout_ms,
+        )
     }
 }
 
 /// Protocol about transmission for Fireworks board module.
 pub trait TransportProtocol: EfwProtocolExtManual {
     fn get_packet_format(&mut self, fmt: TxPacketFormat, timeout_ms: u32) -> Result<(), Error> {
-        let args = [u32::from(fmt)];
+        let args = [serialize_tx_packet_format(&fmt)];
         self.transaction(
             CATEGORY_TRANSPORT,
             CMD_SET_TRANSMIT_MODE,
@@ -44,3 +80,20 @@ pub trait TransportProtocol: EfwProtocolExtManual {
 }
 
 impl<O: EfwProtocolExtManual> TransportProtocol for O {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tx_packet_format_serdes() {
+        [TxPacketFormat::Unique, TxPacketFormat::Iec61883]
+            .iter()
+            .for_each(|fmt| {
+                let val = serialize_tx_packet_format(fmt);
+                let mut f = TxPacketFormat::default();
+                deserialize_tx_packet_format(&mut f, val);
+                assert_eq!(*fmt, f);
+            });
+    }
+}
