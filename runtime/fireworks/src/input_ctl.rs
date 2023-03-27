71f75bb82fdd02b7d64db6deaaf621f54b278f8d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-03-27 18:27:48 +0900

    runtime/fireworks: implement generic structure for physical input controls
    This commit implements the new control for physical input.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- runtime/fireworks/src/input_ctl.rs
+++ runtime/fireworks/src/input_ctl.rs
@@ -3,6 +3,123 @@
 
 use {super::*, protocols::phys_input::*};
 
+#[derive(Debug)]
+pub(crate) struct PhysInputCtl<T>
+where
+    T: EfwPhysInputSpecification
+        + EfwWhollyCachableParamsOperation<SndEfw, EfwPhysInputParameters>
+        + EfwPartiallyUpdatableParamsOperation<SndEfw, EfwPhysInputParameters>,
+{
+    pub elem_id_list: Vec<ElemId>,
+    params: EfwPhysInputParameters,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> Default for PhysInputCtl<T>
+where
+    T: EfwPhysInputSpecification
+        + EfwWhollyCachableParamsOperation<SndEfw, EfwPhysInputParameters>
+        + EfwPartiallyUpdatableParamsOperation<SndEfw, EfwPhysInputParameters>,
+{
+    fn default() -> Self {
+        Self {
+            elem_id_list: Default::default(),
+            params: T::create_phys_input_parameters(),
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<T> PhysInputCtl<T>
+where
+    T: EfwPhysInputSpecification
+        + EfwWhollyCachableParamsOperation<SndEfw, EfwPhysInputParameters>
+        + EfwPartiallyUpdatableParamsOperation<SndEfw, EfwPhysInputParameters>,
+{
+    const IN_NOMINAL_LABELS: [&'static str; 2] = ["+4dBu", "-10dBV"];
+    const IN_NOMINAL_LEVELS: [NominalSignalLevel; 2] = [
+        NominalSignalLevel::Professional,
+        NominalSignalLevel::Consumer,
+    ];
+
+    pub(crate) fn cache(&mut self, unit: &mut SndEfw, timeout_ms: u32) -> Result<(), Error> {
+        T::cache_wholly(unit, &mut self.params, timeout_ms)
+    }
+
+    pub(crate) fn load(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
+        let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, IN_NOMINAL_NAME, 0);
+        card_cntr
+            .add_enum_elems(
+                &elem_id,
+                1,
+                self.params.nominals.len(),
+                &Self::IN_NOMINAL_LABELS,
+                None,
+                true,
+            )
+            .map(|mut elem_id_list| self.elem_id_list.append(&mut elem_id_list))
+    }
+
+    pub(crate) fn read(
+        &mut self,
+        elem_id: &ElemId,
+        elem_value: &mut ElemValue,
+    ) -> Result<bool, Error> {
+        match elem_id.name().as_str() {
+            IN_NOMINAL_NAME => {
+                let vals: Vec<u32> = self
+                    .params
+                    .nominals
+                    .iter()
+                    .map(|level| {
+                        Self::IN_NOMINAL_LEVELS
+                            .iter()
+                            .position(|l| level.eq(l))
+                            .map(|pos| pos as u32)
+                            .unwrap()
+                    })
+                    .collect();
+                elem_value.set_enum(&vals);
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+
+    pub(crate) fn write(
+        &mut self,
+        unit: &mut SndEfw,
+        elem_id: &ElemId,
+        elem_value: &ElemValue,
+        timeout_ms: u32,
+    ) -> Result<bool, Error> {
+        match elem_id.name().as_str() {
+            IN_NOMINAL_NAME => {
+                let mut params = self.params.clone();
+                params
+                    .nominals
+                    .iter_mut()
+                    .zip(elem_value.enumerated())
+                    .try_for_each(|(level, &val)| {
+                        let pos = val as usize;
+                        Self::IN_NOMINAL_LEVELS
+                            .iter()
+                            .nth(pos)
+                            .ok_or_else(|| {
+                                let label =
+                                    format!("Invalid value for nominal level of input: {}", pos);
+                                Error::new(FileError::Inval, &label)
+                            })
+                            .map(|&l| *level = l)
+                    })?;
+                T::update_partially(unit, &mut self.params, params, timeout_ms)?;
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct InputCtl {
     params: EfwPhysInputParameters,
