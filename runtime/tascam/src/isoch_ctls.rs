alsa-project/snd-firewire-ctl-services

commit b764550b63a92f23b6beb54a099c526545bfec4f

runtime/tascam: add generic structure of controls for optical interfaces
This commit uses generic structure of controls for optical interfaces
parameters. Additionally, an intermediate expression for the parameter
is added to protocol crate.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/tascam/src/isoch_ctls.rs
@@ -644,6 +644,16 @@ where
     }
 }
 
+#[derive(Default, Debug)]
+pub(crate) struct OpticalIfaceCtl<T>
+where
+    T: IsochOpticalOperation,
+{
+    elem_id_list: Vec<ElemId>,
+    params: TascamOpticalIfaceParameters,
+    _phantom: PhantomData<T>,
+}
+
 const OPT_OUT_SRC_NAME: &str = "opt-output-source";
 const SPDIF_IN_SRC_NAME: &str = "spdif-input-source";
 
@@ -663,53 +673,66 @@ fn optical_output_source_to_str(src: &OpticalOutputSource) -> &'static str {
     }
 }
 
-pub trait IsochOpticalCtlOperation<T: IsochOpticalOperation> {
-    const SPDIF_INPUT_SOURCES: [SpdifCaptureSource; 2] =
-        [SpdifCaptureSource::Coaxial, SpdifCaptureSource::Optical];
+impl<T> OpticalIfaceCtl<T>
+where
+    T: IsochOpticalOperation,
+{
+    const SPDIF_INPUT_SOURCES: &'static [SpdifCaptureSource] =
+        &[SpdifCaptureSource::Coaxial, SpdifCaptureSource::Optical];
 
-    const OPTICAL_OUTPUT_SOURCES: &'static [OpticalOutputSource];
+    pub(crate) fn cache(
+        &mut self,
+        req: &mut FwReq,
+        node: &mut FwNode,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        T::get_opt_output_source(req, node, timeout_ms)
+            .map(|src| self.params.output_source = src)?;
+        T::get_spdif_capture_source(req, node, timeout_ms)
+            .map(|src| self.params.capture_source = src)?;
+        Ok(())
+    }
 
-    fn load_params(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
+    pub(crate) fn load(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
         let labels: Vec<&str> = Self::SPDIF_INPUT_SOURCES
             .iter()
             .map(|s| spdif_capture_source_to_str(s))
             .collect();
         let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OPT_OUT_SRC_NAME, 0);
-        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+        card_cntr
+            .add_enum_elems(&elem_id, 1, 1, &labels, None, true)
+            .map(|mut elem_id_list| self.elem_id_list.append(&mut elem_id_list))?;
 
-        let labels: Vec<&str> = Self::OPTICAL_OUTPUT_SOURCES
+        let labels: Vec<&str> = T::OPTICAL_OUTPUT_SOURCES
             .iter()
-            .map(|s| optical_output_source_to_str(s))
+            .map(|(s, _, _)| optical_output_source_to_str(s))
             .collect();
         let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, SPDIF_IN_SRC_NAME, 0);
-        let _ = card_cntr.add_enum_elems(&elem_id, 1, 1, &labels, None, true)?;
+        card_cntr
+            .add_enum_elems(&elem_id, 1, 1, &labels, None, true)
+            .map(|mut elem_id_list| self.elem_id_list.append(&mut elem_id_list))?;
 
         Ok(())
     }
 
-    fn read_params(
+    pub(crate) fn read(
         &mut self,
-        node: &mut FwNode,
-        req: &mut FwReq,
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
-        timeout_ms: u32,
     ) -> Result<bool, Error> {
         match elem_id.name().as_str() {
             OPT_OUT_SRC_NAME => {
-                let src = T::get_opt_output_source(req, node, timeout_ms)?;
-                let pos = Self::OPTICAL_OUTPUT_SOURCES
+                let pos = T::OPTICAL_OUTPUT_SOURCES
                     .iter()
-                    .position(|s| s.eq(&src))
+                    .position(|(s, _, _)| self.params.output_source.eq(s))
                     .unwrap();
                 elem_value.set_enum(&[pos as u32]);
                 Ok(true)
             }
             SPDIF_IN_SRC_NAME => {
-                let src = T::get_spdif_capture_source(req, node, timeout_ms)?;
                 let pos = Self::SPDIF_INPUT_SOURCES
                     .iter()
-                    .position(|s| s.eq(&src))
+                    .position(|s| self.params.capture_source.eq(s))
                     .unwrap();
                 elem_value.set_enum(&[pos as u32]);
                 Ok(true)
@@ -718,36 +741,44 @@ pub trait IsochOpticalCtlOperation<T: IsochOpticalOperation> {
         }
     }
 
-    fn write_params(
+    pub(crate) fn write(
         &mut self,
-        node: &mut FwNode,
         req: &mut FwReq,
+        node: &mut FwNode,
         elem_id: &ElemId,
         elem_value: &ElemValue,
         timeout_ms: u32,
     ) -> Result<bool, Error> {
         match elem_id.name().as_str() {
             OPT_OUT_SRC_NAME => {
-                let val = elem_value.enumerated()[0];
-                let &src = Self::OPTICAL_OUTPUT_SOURCES
+                let pos = elem_value.enumerated()[0] as usize;
+                let mut params = self.params.clone();
+                T::OPTICAL_OUTPUT_SOURCES
                     .iter()
-                    .nth(val as usize)
+                    .nth(pos)
                     .ok_or_else(|| {
-                        let msg = format!("Invalid index for optical output sources: {}", val);
+                        let msg = format!("Invalid index for optical output sources: {}", pos);
                         Error::new(FileError::Inval, &msg)
-                    })?;
-                T::set_opt_output_source(req, node, src, timeout_ms).map(|_| true)
+                    })
+                    .map(|(src, _, _)| params.output_source = *src)?;
+                T::set_opt_output_source(req, node, params.output_source, timeout_ms)
+                    .map(|_| self.params = params)?;
+                Ok(true)
             }
             SPDIF_IN_SRC_NAME => {
-                let val = elem_value.enumerated()[0];
-                let &src = Self::SPDIF_INPUT_SOURCES
+                let pos = elem_value.enumerated()[0] as usize;
+                let mut params = self.params.clone();
+                Self::SPDIF_INPUT_SOURCES
                     .iter()
-                    .nth(val as usize)
+                    .nth(pos)
                     .ok_or_else(|| {
-                        let msg = format!("Invalid index for spdif input sources: {}", val);
+                        let msg = format!("Invalid index for spdif input sources: {}", pos);
                         Error::new(FileError::Inval, &msg)
-                    })?;
-                T::set_spdif_capture_source(req, node, src, timeout_ms).map(|_| true)
+                    })
+                    .map(|&src| params.capture_source = src)?;
+                T::set_spdif_capture_source(req, node, params.capture_source, timeout_ms)
+                    .map(|_| self.params = params)?;
+                Ok(true)
             }
             _ => Ok(false),
         }

protocols/tascam/src/isoch.rs
@@ -503,9 +503,11 @@ pub trait IsochCommonOperation {
 }
 
 /// Source of S/PDIF input.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum SpdifCaptureSource {
+    /// To coaxial interface.
     Coaxial,
+    /// To optical interface.
     Optical,
 }
 
@@ -516,7 +518,7 @@ impl Default for SpdifCaptureSource {
 }
 
 /// Source of optical output.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum OpticalOutputSource {
     /// 4 pairs in stream inputs.
     StreamInputPairs,
@@ -534,7 +536,16 @@ impl Default for OpticalOutputSource {
     }
 }
 
-const SPDIF_CAPTURE_SOURCES: [(SpdifCaptureSource, u32, u32); 2] = [
+/// The parameters of digital input and output interfaces.
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
+pub struct TascamOpticalIfaceParameters {
+    /// The input interface from which the S/PDIF signal is captured.
+    pub capture_source: SpdifCaptureSource,
+    /// The source signal of optical output interface.
+    pub output_source: OpticalOutputSource,
+}
+
+const SPDIF_CAPTURE_SOURCES: &[(SpdifCaptureSource, u32, u32)] = &[
     (SpdifCaptureSource::Coaxial, 0x00000000, 0x00010000),
     (SpdifCaptureSource::Optical, 0x00000001, 0x00000100),
 ];

runtime/tascam/src/fw1804_model.rs
@@ -13,8 +13,8 @@ pub struct Fw1804Model {
     clock_ctl: ClockCtl<Fw1804Protocol>,
     input_threshold_ctl: InputDetectionThreshold<Fw1804Protocol>,
     coax_output_ctl: CoaxOutputCtl<Fw1804Protocol>,
+    opt_iface_ctl: OpticalIfaceCtl<Fw1804Protocol>,
     meter_ctl: MeterCtl,
-    optical_ctl: OpticalCtl,
     rack_ctl: RackCtl,
 }
 
@@ -27,7 +27,7 @@ impl Default for Fw1804Model {
             coax_output_ctl: Default::default(),
             input_threshold_ctl: Default::default(),
             meter_ctl: Default::default(),
-            optical_ctl: Default::default(),
+            opt_iface_ctl: Default::default(),
             rack_ctl: Default::default(),
         }
     }
@@ -90,17 +90,6 @@ impl IsochMeterCtlOperation<Fw1804Protocol> for MeterCtl {
 }
 
 #[derive(Default)]
-struct OpticalCtl;
-
-impl IsochOpticalCtlOperation<Fw1804Protocol> for OpticalCtl {
-    const OPTICAL_OUTPUT_SOURCES: &'static [OpticalOutputSource] = &[
-        OpticalOutputSource::StreamInputPairs,
-        OpticalOutputSource::CoaxialOutputPair0,
-        OpticalOutputSource::AnalogInputPair0,
-    ];
-}
-
-#[derive(Default)]
 struct RackCtl(IsochRackState);
 
 impl IsochRackCtlOperation<Fw1804Protocol> for RackCtl {
@@ -151,13 +140,16 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1804Model {
             .cache(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
         self.coax_output_ctl
             .cache(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
+        self.opt_iface_ctl
+            .cache(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
 
         self.clock_ctl.load(card_cntr)?;
         self.input_threshold_ctl.load(card_cntr)?;
         self.coax_output_ctl.load(card_cntr)?;
+        self.opt_iface_ctl.load(card_cntr)?;
 
         self.meter_ctl.load_state(card_cntr, &self.image)?;
-        self.optical_ctl.load_params(card_cntr)?;
+
         self.rack_ctl
             .load_params(card_cntr, &mut unit.1, &mut self.req, TIMEOUT_MS)?;
         Ok(())
@@ -165,7 +157,7 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1804Model {
 
     fn read(
         &mut self,
-        unit: &mut (SndTascam, FwNode),
+        _: &mut (SndTascam, FwNode),
         elem_id: &ElemId,
         elem_value: &mut ElemValue,
     ) -> Result<bool, Error> {
@@ -177,13 +169,7 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1804Model {
             Ok(true)
         } else if self.meter_ctl.read_state(elem_id, elem_value)? {
             Ok(true)
-        } else if self.optical_ctl.read_params(
-            &mut unit.1,
-            &mut self.req,
-            elem_id,
-            elem_value,
-            TIMEOUT_MS,
-        )? {
+        } else if self.opt_iface_ctl.read(elem_id, elem_value)? {
             Ok(true)
         } else if self.rack_ctl.read_params(elem_id, elem_value)? {
             Ok(true)
@@ -224,9 +210,9 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1804Model {
             TIMEOUT_MS,
         )? {
             Ok(true)
-        } else if self.optical_ctl.write_params(
-            &mut unit.1,
+        } else if self.opt_iface_ctl.write(
             &mut self.req,
+            &mut unit.1,
             elem_id,
             new,
             TIMEOUT_MS,

runtime/tascam/src/fw1884_model.rs
@@ -10,11 +10,11 @@ use {
 pub struct Fw1884Model {
     req: FwReq,
     image: Vec<u32>,
-    meter_ctl: MeterCtl,
     clock_ctl: ClockCtl<Fw1884Protocol>,
     input_threshold_ctl: InputDetectionThreshold<Fw1884Protocol>,
     coax_output_ctl: CoaxOutputCtl<Fw1884Protocol>,
-    optical_ctl: OpticalCtl,
+    opt_iface_ctl: OpticalIfaceCtl<Fw1884Protocol>,
+    meter_ctl: MeterCtl,
     console_ctl: ConsoleCtl,
     specific_ctl: SpecificCtl,
     seq_state: SequencerState<Fw1884SurfaceState>,
@@ -28,8 +28,8 @@ impl Default for Fw1884Model {
             clock_ctl: Default::default(),
             input_threshold_ctl: Default::default(),
             coax_output_ctl: Default::default(),
+            opt_iface_ctl: Default::default(),
             meter_ctl: Default::default(),
-            optical_ctl: Default::default(),
             console_ctl: Default::default(),
             specific_ctl: Default::default(),
             seq_state: Default::default(),
@@ -90,18 +90,6 @@ impl IsochMeterCtlOperation<Fw1884Protocol> for MeterCtl {
         "adat-output-8",
         "spdif-input-1",
         "spdif-input-2",
-    ];
-}
-
-#[derive(Default)]
-struct OpticalCtl;
-
-impl IsochOpticalCtlOperation<Fw1884Protocol> for OpticalCtl {
-    const OPTICAL_OUTPUT_SOURCES: &'static [OpticalOutputSource] = &[
-        OpticalOutputSource::StreamInputPairs,
-        OpticalOutputSource::AnalogOutputPairs,
-        OpticalOutputSource::CoaxialOutputPair0,
-        OpticalOutputSource::AnalogInputPair0,
     ];
 }
 
@@ -222,16 +210,17 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1884Model {
             .cache(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
         self.coax_output_ctl
             .cache(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
+        self.opt_iface_ctl
+            .cache(&mut self.req, &mut unit.1, TIMEOUT_MS)?;
 
         self.clock_ctl.load(card_cntr)?;
         self.input_threshold_ctl.load(card_cntr)?;
         self.coax_output_ctl.load(card_cntr)?;
+        self.opt_iface_ctl.load(card_cntr)?;
 
         self.meter_ctl
             .load_state(card_cntr, &self.image)
             .map(|mut elem_id_list| self.meter_ctl.1.append(&mut elem_id_list))?;
-
-        self.optical_ctl.load_params(card_cntr)?;
 
         self.console_ctl
             .load_params(card_cntr, &self.image)
@@ -256,13 +245,7 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1884Model {
             Ok(true)
         } else if self.meter_ctl.read_state(elem_id, elem_value)? {
             Ok(true)
-        } else if self.optical_ctl.read_params(
-            &mut unit.1,
-            &mut self.req,
-            elem_id,
-            elem_value,
-            TIMEOUT_MS,
-        )? {
+        } else if self.opt_iface_ctl.read(elem_id, elem_value)? {
             Ok(true)
         } else if self.console_ctl.read_params(
             &mut unit.1,
@@ -317,9 +300,9 @@ impl CtlModel<(SndTascam, FwNode)> for Fw1884Model {
             TIMEOUT_MS,
         )? {
             Ok(true)
-        } else if self.optical_ctl.write_params(
-            &mut unit.1,
+        } else if self.opt_iface_ctl.write(
             &mut self.req,
+            &mut unit.1,
             elem_id,
             new,
             TIMEOUT_MS,

commit f39574ebae7c74cf06d56661e941dedb5498c772

runtime/tascam: fix invalid count of values for controls optical interfaces
Fixes: b764550b63a9 ("runtime/tascam: add generic structure of controls for optical interfaces")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

runtime/tascam/src/isoch_ctls.rs
@@ -693,18 +693,18 @@ where
     }
 
     pub(crate) fn load(&mut self, card_cntr: &mut CardCntr) -> Result<(), Error> {
-        let labels: Vec<&str> = Self::SPDIF_INPUT_SOURCES
+        let labels: Vec<&str> = T::OPTICAL_OUTPUT_SOURCES
             .iter()
-            .map(|s| spdif_capture_source_to_str(s))
+            .map(|(s, _, _)| optical_output_source_to_str(s))
             .collect();
         let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, OPT_OUT_SRC_NAME, 0);
         card_cntr
             .add_enum_elems(&elem_id, 1, 1, &labels, None, true)
             .map(|mut elem_id_list| self.elem_id_list.append(&mut elem_id_list))?;
 
-        let labels: Vec<&str> = T::OPTICAL_OUTPUT_SOURCES
+        let labels: Vec<&str> = Self::SPDIF_INPUT_SOURCES
             .iter()
-            .map(|(s, _, _)| optical_output_source_to_str(s))
+            .map(|s| spdif_capture_source_to_str(s))
             .collect();
         let elem_id = ElemId::new_by_name(ElemIfaceType::Mixer, 0, 0, SPDIF_IN_SRC_NAME, 0);
         card_cntr