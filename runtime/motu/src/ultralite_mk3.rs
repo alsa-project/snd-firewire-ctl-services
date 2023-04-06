8cb10a2dc30dd806bad614131f9d7e2050e06998
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-04-06 12:32:56 +0900

    runtime/motu: use generic structure for mixer controls in Command DSP models
    This commit uses generic structure for mixer controls in Command DSP
    models.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- runtime/motu/src/ultralite_mk3.rs
+++ runtime/motu/src/ultralite_mk3.rs
@@ -15,33 +15,12 @@ pub struct UltraLiteMk3 {
     sequence_number: u8,
     reverb_ctl: CommandDspReverbCtl<UltraliteMk3Protocol>,
     monitor_ctl: CommandDspMonitorCtl<UltraliteMk3Protocol>,
-    mixer_ctl: MixerCtl,
+    mixer_ctl: CommandDspMixerCtl<UltraliteMk3Protocol>,
     input_ctl: InputCtl,
     output_ctl: OutputCtl,
     resource_ctl: ResourceCtl,
     meter: CommandDspMeterImage,
     meter_ctl: MeterCtl,
-}
-
-struct MixerCtl(CommandDspMixerState, Vec<ElemId>);
-
-impl Default for MixerCtl {
-    fn default() -> Self {
-        Self(
-            UltraliteMk3Protocol::create_mixer_state(),
-            Default::default(),
-        )
-    }
-}
-
-impl CommandDspMixerCtlOperation<UltraliteMk3Protocol> for MixerCtl {
-    fn state(&self) -> &CommandDspMixerState {
-        &self.0
-    }
-
-    fn state_mut(&mut self) -> &mut CommandDspMixerState {
-        &mut self.0
-    }
 }
 
 struct InputCtl(CommandDspInputState, Vec<ElemId>);
@@ -138,9 +117,7 @@ impl CtlModel<(SndMotu, FwNode)> for UltraLiteMk3 {
         self.phone_assign_ctl.load(card_cntr)?;
         self.reverb_ctl.load(card_cntr)?;
         self.monitor_ctl.load(card_cntr)?;
-        self.mixer_ctl
-            .load(card_cntr)
-            .map(|mut elem_id_list| self.mixer_ctl.1.append(&mut elem_id_list))?;
+        self.mixer_ctl.load(card_cntr)?;
         self.input_ctl
             .load(card_cntr)
             .map(|mut elem_id_list| self.input_ctl.1.append(&mut elem_id_list))?;
@@ -259,8 +236,8 @@ impl CtlModel<(SndMotu, FwNode)> for UltraLiteMk3 {
             Ok(true)
         } else if self.mixer_ctl.write(
             &mut self.sequence_number,
-            unit,
             &mut self.req,
+            &mut unit.1,
             elem_id,
             new,
             TIMEOUT_MS,
@@ -366,7 +343,7 @@ impl NotifyModel<(SndMotu, FwNode), Vec<DspCmd>> for UltraLiteMk3 {
     fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.reverb_ctl.elem_id_list);
         elem_id_list.extend_from_slice(&self.monitor_ctl.elem_id_list);
-        elem_id_list.extend_from_slice(&self.mixer_ctl.1);
+        elem_id_list.extend_from_slice(&self.mixer_ctl.elem_id_list);
         elem_id_list.extend_from_slice(&self.input_ctl.1);
         elem_id_list.extend_from_slice(&self.output_ctl.1);
         elem_id_list.extend_from_slice(&self.resource_ctl.1);
--- runtime/motu/src/ultralite_mk3_hybrid.rs
+++ runtime/motu/src/ultralite_mk3_hybrid.rs
@@ -15,33 +15,12 @@ pub struct UltraliteMk3Hybrid {
     sequence_number: u8,
     reverb_ctl: CommandDspReverbCtl<UltraliteMk3HybridProtocol>,
     monitor_ctl: CommandDspReverbCtl<UltraliteMk3HybridProtocol>,
-    mixer_ctl: MixerCtl,
+    mixer_ctl: CommandDspMixerCtl<UltraliteMk3HybridProtocol>,
     input_ctl: InputCtl,
     output_ctl: OutputCtl,
     resource_ctl: ResourceCtl,
     meter: CommandDspMeterImage,
     meter_ctl: MeterCtl,
-}
-
-struct MixerCtl(CommandDspMixerState, Vec<ElemId>);
-
-impl Default for MixerCtl {
-    fn default() -> Self {
-        Self(
-            UltraliteMk3HybridProtocol::create_mixer_state(),
-            Default::default(),
-        )
-    }
-}
-
-impl CommandDspMixerCtlOperation<UltraliteMk3HybridProtocol> for MixerCtl {
-    fn state(&self) -> &CommandDspMixerState {
-        &self.0
-    }
-
-    fn state_mut(&mut self) -> &mut CommandDspMixerState {
-        &mut self.0
-    }
 }
 
 struct InputCtl(CommandDspInputState, Vec<ElemId>);
@@ -138,9 +117,7 @@ impl CtlModel<(SndMotu, FwNode)> for UltraliteMk3Hybrid {
         self.phone_assign_ctl.load(card_cntr)?;
         self.reverb_ctl.load(card_cntr)?;
         self.monitor_ctl.load(card_cntr)?;
-        self.mixer_ctl
-            .load(card_cntr)
-            .map(|mut elem_id_list| self.mixer_ctl.1.append(&mut elem_id_list))?;
+        self.mixer_ctl.load(card_cntr)?;
         self.input_ctl
             .load(card_cntr)
             .map(|mut elem_id_list| self.input_ctl.1.append(&mut elem_id_list))?;
@@ -259,8 +236,8 @@ impl CtlModel<(SndMotu, FwNode)> for UltraliteMk3Hybrid {
             Ok(true)
         } else if self.mixer_ctl.write(
             &mut self.sequence_number,
-            unit,
             &mut self.req,
+            &mut unit.1,
             elem_id,
             new,
             TIMEOUT_MS,
@@ -366,7 +343,7 @@ impl NotifyModel<(SndMotu, FwNode), Vec<DspCmd>> for UltraliteMk3Hybrid {
     fn get_notified_elem_list(&mut self, elem_id_list: &mut Vec<ElemId>) {
         elem_id_list.extend_from_slice(&self.reverb_ctl.elem_id_list);
         elem_id_list.extend_from_slice(&self.monitor_ctl.elem_id_list);
-        elem_id_list.extend_from_slice(&self.mixer_ctl.1);
+        elem_id_list.extend_from_slice(&self.mixer_ctl.elem_id_list);
         elem_id_list.extend_from_slice(&self.input_ctl.1);
         elem_id_list.extend_from_slice(&self.output_ctl.1);
         elem_id_list.extend_from_slice(&self.resource_ctl.1);
