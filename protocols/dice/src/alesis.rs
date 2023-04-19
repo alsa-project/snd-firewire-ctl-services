alsa-project/snd-firewire-ctl-services

commit a83b104332d489f93b380bc7dd1f6e33e7d85010

protocols/dice: alesis: add renewed traits and implementations for protocol of iO FireWire series
This commit adds new trait and its generic implementation to renew
current protocol implementation.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/alesis.rs
@@ -11,8 +11,11 @@ pub mod mixer;
 pub mod output;
 
 use {
-    self::{meter::*, mixer::*, output::*},
     super::{tcat::*, *},
+    meter::*,
+    mixer::*,
+    output::*,
+    std::ops::Range,
 };
 
 /// Protocol implementation specific to iO 14 FireWire.
@@ -22,6 +25,8 @@ pub struct Io14fwProtocol;
 impl TcatOperation for Io14fwProtocol {}
 
 impl TcatGlobalSectionSpecification for Io14fwProtocol {}
+
+impl AlesisOperation for Io14fwProtocol {}
 
 impl IofwMeterOperation for Io14fwProtocol {
     const ANALOG_INPUT_COUNT: usize = 4;
@@ -46,6 +51,8 @@ impl TcatOperation for Io26fwProtocol {}
 
 impl TcatGlobalSectionSpecification for Io26fwProtocol {}
 
+impl AlesisOperation for Io26fwProtocol {}
+
 impl IofwMeterOperation for Io26fwProtocol {
     const ANALOG_INPUT_COUNT: usize = 8;
     const DIGITAL_B_INPUT_COUNT: usize = 8;
@@ -62,6 +69,186 @@ impl IofwOutputOperation for Io26fwProtocol {
 }
 
 const BASE_OFFSET: usize = 0x00200000;
+
+/// Serialize and deserialize for parameters of iO FireWire series.
+pub trait AlesisParametersSerdes<T> {
+    /// The name of parameters
+    const NAME: &'static str;
+
+    /// The range of offset for parameters.
+    const OFFSET_RANGES: &'static [Range<usize>];
+
+    /// Serialize parameters to raw layout of data.
+    fn serialize_params(params: &T, raw: &mut [u8]) -> Result<(), String>;
+
+    /// Deserialize parameters from raw layout of data.
+    fn deserialize_params(params: &mut T, raw: &[u8]) -> Result<(), String>;
+}
+
+fn compute_params_size(ranges: &[Range<usize>]) -> usize {
+    ranges
+        .iter()
+        .fold(0usize, |size, range| size + range.end - range.start)
+}
+
+fn generate_err(name: &str, cause: &str, raw: &[u8]) -> Error {
+    let msg = format!("parms: {}, cause: {}, raw: {:02x?}", name, cause, raw);
+    Error::new(GeneralProtocolError::VendorDependent, &msg)
+}
+
+/// Operation for Alesis iO FireWire series.
+pub trait AlesisOperation: TcatOperation {
+    /// Read from specific range of address.
+    fn read_params(
+        req: &FwReq,
+        node: &FwNode,
+        offset: usize,
+        raw: &mut [u8],
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        Self::read(req, node, BASE_OFFSET + offset, raw, timeout_ms)
+    }
+
+    /// Write to specific range of address.
+    fn write_params(
+        req: &FwReq,
+        node: &FwNode,
+        offset: usize,
+        raw: &mut [u8],
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        Self::write(req, node, BASE_OFFSET + offset, raw, timeout_ms)
+    }
+}
+
+/// Operation to cache content of segment in TC Electronic Konnekt series.
+pub trait AlesisParametersOperation<T>: AlesisOperation + AlesisParametersSerdes<T> {
+    /// Cache whole segment and deserialize for parameters.
+    fn cache_whole_params(
+        req: &FwReq,
+        node: &FwNode,
+        params: &mut T,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let size = compute_params_size(Self::OFFSET_RANGES);
+        let mut raw = vec![0u8; size];
+
+        let mut pos = 0;
+
+        Self::OFFSET_RANGES.iter().try_for_each(|range| {
+            let size = range.end - range.start;
+            Self::read_params(
+                req,
+                node,
+                range.start,
+                &mut raw[pos..(pos + size)],
+                timeout_ms,
+            )
+            .map(|_| pos += size)
+        })?;
+
+        Self::deserialize_params(params, &raw)
+            .map_err(|cause| generate_err(Self::NAME, &cause, &raw))
+    }
+}
+
+impl<O: AlesisOperation + AlesisParametersSerdes<T>, T> AlesisParametersOperation<T> for O {}
+
+/// Operation for parameters to update state of hardware.
+pub trait IofwMutableParametersOperation<T>: AlesisOperation + AlesisParametersSerdes<T> {
+    /// Update the hardware partially for any change of parameter.
+    fn update_partial_params(
+        req: &FwReq,
+        node: &FwNode,
+        params: &T,
+        prev: &mut T,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let size = compute_params_size(Self::OFFSET_RANGES);
+
+        let mut new = vec![0u8; size];
+        let mut old = vec![0u8; size];
+        Self::serialize_params(params, &mut new)
+            .map_err(|cause| generate_err(Self::NAME, &cause, &new))?;
+        Self::serialize_params(prev, &mut old)
+            .map_err(|cause| generate_err(Self::NAME, &cause, &old))?;
+
+        let mut pos = 0;
+
+        Self::OFFSET_RANGES.iter().try_for_each(|range| {
+            let size = range.end - range.start;
+
+            if new[pos..(pos + size)] != old[pos..(pos + size)] {
+                (0..size).step_by(4).try_for_each(|offset| {
+                    let p = pos + offset;
+                    if new[p..(p + 4)] != old[p..(p + 4)] {
+                        Self::write_params(
+                            req,
+                            node,
+                            range.start + offset,
+                            &mut new[p..(p + 4)],
+                            timeout_ms,
+                        )
+                    } else {
+                        Ok(())
+                    }
+                })
+            } else {
+                Ok(())
+            }
+            .map(|_| pos += size)
+        })?;
+
+        Self::deserialize_params(prev, &new).map_err(|cause| generate_err(Self::NAME, &cause, &new))
+    }
+}
+
+/// Operation for parameters to include fluctuated values.
+pub trait AlesisFluctuatedParametersOperation<T>:
+    AlesisOperation + AlesisParametersSerdes<T>
+{
+    /// The set of address offsets in which any value is changed apart from software operation.
+    const FLUCTUATED_OFFSET_RANGES: &'static [Range<usize>];
+
+    /// Cache part of offset ranges for fluctuated values, then deserialize for parameters.
+    fn cache_partial_params(
+        req: &FwReq,
+        node: &FwNode,
+        params: &mut T,
+        timeout_ms: u32,
+    ) -> Result<(), Error> {
+        let size = compute_params_size(Self::OFFSET_RANGES);
+
+        let mut raw = vec![0u8; size];
+        Self::serialize_params(params, &mut raw)
+            .map_err(|cause| generate_err(Self::NAME, &cause, &raw))?;
+
+        Self::FLUCTUATED_OFFSET_RANGES
+            .iter()
+            .try_for_each(|range| {
+                let mut pos = 0;
+                for r in Self::OFFSET_RANGES {
+                    if !r.contains(&range.start) {
+                        pos += r.end - r.start;
+                    } else {
+                        pos += range.start - r.start;
+                        break;
+                    }
+                }
+                assert!(
+                    pos < size,
+                    "Programming error. The offset range should be found."
+                );
+
+                let end = pos + range.end - range.start;
+                Self::read_params(req, node, range.start, &mut raw[pos..end], timeout_ms)
+            })
+            .and_then(|_| {
+                Self::deserialize_params(params, &raw)
+                    .map_err(|cause| generate_err(Self::NAME, &cause, &raw))
+            })
+    }
+}
 
 fn alesis_read_block(
     req: &mut FwReq,

commit 7c0f3a95436acf249953f2146233c221531371ac

protocols/dice: alesis: fix copy-and-paste mistake
Fixes: a83b104332d4 ("protocols/dice: alesis: add renewed traits and implementations for protocol of iO FireWire series")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

protocols/dice/src/alesis.rs
@@ -437,7 +437,7 @@ pub trait AlesisOperation: TcatOperation {
     }
 }
 
-/// Operation to cache content of segment in TC Electronic Konnekt series.
+/// Operation for parameters to cache state of hardware.
 pub trait AlesisParametersOperation<T>: AlesisOperation + AlesisParametersSerdes<T> {
     /// Cache whole segment and deserialize for parameters.
     fn cache_whole_params(